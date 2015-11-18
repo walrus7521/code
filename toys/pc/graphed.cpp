0916a56c326d8a9b28db563476af9356cb08cea6
Bart Bartel <bartb7@gmail.com>
2015-11-17 18:44:34 -0800

    graphed

---
+++ toys/pc/graphed.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+ 
+/*
+ * create a singleton class to implement these methods
+ *
+  I M N   Create a new M x N image with all pixels initially colored white (O).
+  C   Clear the table by setting all pixels white (O). The size remains unchanged.
+  L X Y C Colors the pixel (X, Y) in color (C).
+  V X Y1 Y2 C Draw a vertical segment of color (C) in column X, between the rows Y1 and Y2 inclusive.
+  H X1 X2 Y C Draw a horizontal segment of color (C) in the row Y, between the columns X1 and X2 inclusive.
+  K X1 Y1 X2 Y2 C Draw a filled rectangle of color C, where (X1, Y1) is the upper-left and (X2, Y2) the lower right corner.
+  F X Y C Fill the region R with the color C, where R is defined as follows. Pixel (X, Y) belongs to R. Any other pixel which is the same color as pixel (X, Y) and shares a common side with any pixel in R also belongs to this region.
+  S Name  Write the file name in MSDOS 8.3 format followed by the contents of the current image.
+  X   Terminate the session.
+*/
+
+
+using namespace std;
+
+
+class Singleton
+{
+    public:
+        static Singleton& getInstance() {
+            static Singleton instance; // Guaranteed to be destroyed.
+            // Instantiated on first use.
+            return instance;
+        }
+    private:
+        Singleton() {}; // Constructor? (the {} brackets) are needed here.
+        // C++ 03
+        // ========
+        // Dont forget to declare these two. You want to make sure they
+        // are unacceptable otherwise you may accidentally get copies of
+        // your singleton appearing.
+        Singleton(Singleton const&);      // Don't Implement
+        void operator=(Singleton const&); // Don't implement
+        // C++ 11
+        // =======
+        // We can use the better technique of deleting the methods
+        // we don't want.
+        //S(S const&)               = delete;
+        //void operator=(S const&)  = delete;
+};
+
+class GraphEd {
+public:
+    static GraphEd& getInstance() {
+        static GraphEd instance;
+        return instance;
+    }
+    void create(int m, int n) { 
+        M = m; N = n; 
+        grid = (char**) calloc(n, sizeof(char*));  
+        for (int i = 0; i < n; i++) {
+            grid[i] = (char*) calloc(m, sizeof(char));
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                grid[i][j] = 'o';
+            }
+        }
+    }
+    void clear() { 
+    }
+    void fill() {
+    }
+    void draw() { 
+    }
+    void draw(int x, int y1, int y2, char color)  { 
+    }
+    void color(int x, int y, char color) { 
+        grid[x-1][y-1] = color;
+    }
+    void write(string name) { 
+        Name = name; cout << "write: " << Name << endl; 
+    }
+    void show(string name) {
+        cout << name << endl;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cout << grid[i][j];
+            }
+            cout << endl;
+        }
+    }
+    void terminate() { 
+    }
+private:
+    GraphEd() {};
+    void operator=(GraphEd const&);
+    string Name;
+    int M, N;
+    char **grid;
+};
+
+int main() {
+    string line, Name;
+    int M, N, X, X1, X2, Y, Y1, Y2;
+    int m, n, x, x1, x2, y, y1, y2;
+    char c, cmd, name[32];
+
+    GraphEd ged = GraphEd::getInstance();
+
+    while (getline(cin, line)) {
+        stringstream in(line);
+        //cout << "got line: " << line << endl;
+        switch (line[0]) {
+            case 'C':
+                // C   Clear the table by setting all pixels white (O). The size remains unchanged.
+                ged.clear();
+                break;
+            case 'F':
+                // F x y C Fill the region R with the color C, where R is defined as follows. 
+                // Pixel (x, y) belongs to R. Any other pixel which is the same color as 
+                // pixel (x, y) and shares a common side with any pixel in R also belongs to this region.
+                sscanf(line.c_str(), "%c %d %d %c\n", &cmd, &x, &y, &c);
+                ged.fill();
+                break;
+            case 'H':
+                // H x1 x2 y C Draw a horizontal segment of color (C) in the row y, between 
+                // the columns x1 and x2 inclusive.
+                sscanf(line.c_str(), "%c %d %d %d %c\n", &cmd, &x1, &x2, &y, &c);
+                ged.draw();
+                break;
+            case 'I': 
+                // I M N   Create a new M x N image with all pixels initially colored white (O).
+                sscanf(line.c_str(), "%c %d %d\n", &cmd, &M, &N);
+                ged.create(M, N);
+                break;
+            case 'K':
+                // K x1 y1 x2 y2 c Draw a filled rectangle of color c, where (x1, y1) is the 
+                // upper-left and (x2, y2) the lower right corner.
+                sscanf(line.c_str(), "%c %d %d %d %d %c\n", &cmd, &x1, &y1, &x2, &y2, &c);
+                ged.draw();
+                break;
+            case 'L':
+                // L x y c Colors the pixel (x, y) in color (c).
+                sscanf(line.c_str(), "%c %d %d %c\n", &cmd, &x, &y, &c);
+                ged.color(x, y, c);
+                break;
+            case 'S':
+                // S Name  Write the file name in MSDOS 8.3 format followed by the contents of the current image.
+                sscanf(line.c_str(), "%c %s\n", &cmd, name);
+                Name = name;
+                ged.show(Name);
+                break;
+            case 'V':
+                // V x y1 y2 c Draw a vertical segment of color (c) in column x, between the rows y1 and y2 inclusive.
+                sscanf(line.c_str(), "%c %d %d %d %c\n", &cmd, &x, &y1, &y2, &c);
+                ged.draw(x, y1, y2, c);
+                break;
+            case 'X':
+                // X   Terminate the session.
+                ged.terminate();
+                goto end;
+                break;
+        }
+    }
+end:
+    return 0;
+}
+
