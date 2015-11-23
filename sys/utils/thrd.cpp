1a757271409d6f21e851c88c19b388e84d104edb
Bart Bartel <bartb7@gmail.com>
2015-11-22 21:39:31 -0800

    threads

--- sys/utils/thrd.cpp
+++ sys/utils/thrd.cpp
@@ -8,12 +8,15 @@
 using namespace std;
 
 #define NUM_THREADS     5
+pthread_mutex_t myMutex;
 
 void *PrintHello(void *threadid)
 {
+   pthread_mutex_lock(&myMutex);   
    long tid;
    tid = (long)threadid;
    cout << "Hello World! Thread ID, " << tid << endl;
+   pthread_mutex_unlock(&myMutex);   
    pthread_exit(NULL);
 }
 
@@ -22,14 +25,22 @@ int main ()
    pthread_t threads[NUM_THREADS];
    int rc;
    int i;
+   pthread_mutex_init(&myMutex,0);
+
    for( i=0; i < NUM_THREADS; i++ ){
+      pthread_mutex_lock(&myMutex);   
       cout << "main() : creating thread, " << i << endl;
-      rc = pthread_create(&threads[i], NULL, 
-                          PrintHello, (void *)i);
+      rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
+      pthread_mutex_unlock(&myMutex);   
       if (rc){
          cout << "Error:unable to create thread," << rc << endl;
          exit(-1);
       }
    }
+   for( i=0; i < NUM_THREADS; i++ ){
+       //pthread_join(threads[i], NULL);
+   }
+
+   pthread_mutex_destroy(&myMutex);   
    pthread_exit(NULL);
 }
