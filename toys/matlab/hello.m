cacd95df5c00cb129b3fda1a6c289f8be7470340
Bart Bartel <bart.bartel@osnexus.com>
2015-12-07 12:13:52 -0800

    hello

--- toys/matlab/hello.m
+++ toys/matlab/hello.m
@@ -1,8 +1,23 @@
-#!/usr/local/bin/octave -qf
+#!/usr/bin/octave -qf
 # a sample Octave program
-printf ("Hello, world!\n");
+#printf ("Hello, world!\n");
 
 
+# list arguments
+# printf ("%s", program_name ());
+# arg_list = argv ();
+# for i = 1:nargin
+#   printf (" %s", arg_list{i});
+# endfor
+# printf ("\n");
 
-#! octave-interpreter-name -qf
+  fprintf ( 1, '\n' );
+  fprintf ( 1, 'HELLO:\n' );
+  fprintf ( 1, '  MATLAB version\n' );
+  fprintf ( 1, '  This is how to say:\n' );
+  fprintf ( 1, '\n' );
+  fprintf ( 1, '  Hello, world!\n' );
+
+  quit
+
 
