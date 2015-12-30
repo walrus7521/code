f200f0e8c8769010b3ce6a8de5e031a0d2f48245
U-HELITRAK\bartb <bartb@BartB-LT.helitrak.com>
2015-12-30 12:03:28 -0800

    cpp

--- toys/epi/06.01.cpp
+++ toys/epi/06.01.cpp
@@ -2,13 +2,48 @@
 #include <iomanip>
 #include <vector>
 #include <algorithm>
+#include <cstdio>
 
 using std::cout;
 using std::endl;
 using std::vector;
 using std::swap;
 
-void DutchFlagPartition(int pivot_index, vector<int>* A) {
+void myswap(int *a, int i, int j)
+{
+    int t = a[i];
+    a[i] = a[j];
+    a[j] = t;
+}
+
+void DutchFlagPartition2(int pivot_index, int *A, int len) {
+    int pivot = A[pivot_index];
+
+    printf("pivot = %d\n", pivot);
+    /**
+      * keep the following invariants during partitioning:
+      * bottom group: A_ref[0 : smaller - 1].
+      * middle group: A_ref[smaller : equal - 1].
+      * unclassified group: A_ref[equal : larger].
+      * top group: A_ref[larger + 1 : A-ref.size() - 1].
+      */
+
+    int smaller = 0, equal = 0, larger = len - 1;
+    // when there is any unclassified element.
+    while (equal <= larger) {
+        // A_ref[equal] is the incoming unclassified element.
+        if (A[equal] < pivot) {
+            myswap(A, smaller++, equal++);
+        } else
+        if (A[equal] == pivot) {
+            ++equal;
+        } else { // A_ref[equal] > pivot.
+            myswap(A, equal, larger--);
+        }
+    }
+}
+
+void DutchFlagPartition1(int pivot_index, vector<int>* A) {
     auto& A_ref = *A;
     int pivot = A_ref[pivot_index];
 
@@ -38,8 +73,16 @@ void DutchFlagPartition(int pivot_index, vector<int>* A) {
 
 /* still need to implement variants
  */
+void dump2(int *vi, int len)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        printf("%d\n", vi[i]);
+    }
+    printf("\n");
+}
 
-void dump(vector<int>& vi)
+void dump1(vector<int>& vi)
 {
     for (vector<int>::const_iterator vit = vi.begin(); vit != vi.end(); ++vit) {
         cout << *vit << endl;
@@ -47,11 +90,25 @@ void dump(vector<int>& vi)
     cout << endl << endl;
 }
 
-int main()
+void test1()
 {
     vector<int> vi = {3, 1, 0, 8, 6, 7, 5, 3, 0, 9};
-    dump(vi);
-    DutchFlagPartition(7, &vi);
-    dump(vi);
+    dump1(vi);
+    DutchFlagPartition1(7, &vi);
+    dump1(vi);
+}
+
+void test2()
+{
+    int vi[] = {3, 1, 0, 8, 6, 7, 5, 3, 0, 9};
+    dump2(vi, 10);
+    DutchFlagPartition2(7, vi, 10);
+    dump2(vi, 10);
+}
+
+int main()
+{
+    test1();
+    test2();
     return 0;
 }
