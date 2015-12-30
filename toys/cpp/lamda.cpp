348112cb9109f6be69f2bee4e5594c81f4658ff6
U-HELITRAK\bartb <bartb@BartB-LT.helitrak.com>
2015-12-30 12:31:23 -0800

    lambda

--- toys/cpp/lamda.cpp
+++ toys/cpp/lamda.cpp
@@ -6,7 +6,9 @@ int main()
 {
     int x, y;
     auto add = [](int x, int y) { return x+y; };
+    auto lambda = [](auto x){ return x; };
     cout << add(40,2) << endl;
+    cout << lambda("Hello generic lambda!\n");
     return 0;
 }
 
