a9db3133b996cd0b00da67e32bbfc92cd29924eb
System Administrator <bartb7@gmail.com>
2015-12-31 11:49:37 -0800

    merge_arrays

--- toys/box/heap.h
+++ toys/box/heap.h
@@ -170,6 +170,33 @@ void heapify(heap *h, int *a, int size)
     printf("\n");
 }
 
+
+int *merge_arrays(int *a, int a_sz, int *b, int b_sz, int *c, int c_sz)
+{
+    heap *h = create(a_sz + b_sz + c_sz, MIN_HEAP);
+    int *r = malloc(sizeof(int) * (a_sz + b_sz + c_sz));
+    int i;
+    for (i = 0; i < a_sz; i++) insert_min(h, a[i]);
+    for (i = 0; i < b_sz; i++) insert_min(h, b[i]);
+    for (i = 0; i < c_sz; i++) insert_min(h, c[i]);
+    i = 0;
+    while (!is_empty(h)) r[i++] = delete_min(h);
+    return r;
+}
+
+void merge_arrays_test()
+{
+    int a[] = { 34, 5, 23, 12, 33, 98, 4, 13, 44, 37, 1, 86, 8};
+    int b[] = { 34, 5, 23, 12, 33, 98, 4, 13, 44, 37, 1, 86, 8};
+    int c[] = { 34, 5, 23, 12, 33, 98, 4, 13, 44, 37, 1, 86, 8};
+    int *r = merge_arrays(a, 13, b, 13, c, 13);
+    if (r) {
+        int i;
+        for (i = 0; i < 13 * 3; i++) 
+            printf("r[%d] = %d\n", i, r[i]);
+    }
+}
+
 int heap_test()
 {
     //heap *h = create(13, MIN_HEAP);
@@ -181,6 +208,9 @@ int heap_test()
         //printf("%d ", delete_min(h));
         printf("%d ", delete_max(h));
     }
+    printf("\n");
+    printf("now do a merge arrays test\n");
+    merge_arrays_test();
     printf("\n");
     return 0;
 }
