1eaa315be66edbf58d5145df1ef7494e127526eb
Bart Bartel <bartb7@gmail.com>
2016-01-13 21:39:21 -0800

    random

---
+++ toys/box/rand.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+unsigned long random()
+{
+#define MULT 1664525L
+#define ADD  1013904223L
+    static unsigned long r = 0;
+    r = r * MULT + ADD;
+    return r;
+}
+
+int main()
+{
+    int i;
+    for (i = 0; i < 16; ++i) {
+        printf("[%02d] = %08lX\n", i, random() & 0xFFFFFFFF);
+    }
+}
+
