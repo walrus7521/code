4bc26995e438273a22b857e6ede55eb428485f13
Bart Bartel <bartb7@gmail.com>
2016-01-15 18:21:16 -0800

    go

--- toys/go/basic.go
+++ toys/go/basic.go
@@ -1,5 +1,7 @@
 // http://thenewstack.io/make-a-restful-json-api-go/
 
+// http://localhost:8080/dude
+
 package main
 
 import (
@@ -11,7 +13,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+        fmt.Fprintf(w, "Sup Hello, %q", html.EscapeString(r.URL.Path))
         fmt.Printf("Hello, %q\n", html.EscapeString(r.URL.Path))
     })
 
