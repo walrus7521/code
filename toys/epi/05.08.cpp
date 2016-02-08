1be2428de3e43a63b96ef78eafc648ee5fa1a7b6
Bart Bartel <bartb7@gmail.com>
2016-02-07 19:49:26 -0800

    base

--- toys/epi/05.08.cpp
+++ toys/epi/05.08.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+string ConvertBase(const string& s, int b1, int b2);
 /* Convert Base: Write a function that performs base conversion.  Specifically, the input
  * is an integer base b1, a string s, representing an integer in base b1, and another integer
  * base b2; the output is the string representing the integer x in base b2.  Assume 2 <= b1,
@@ -13,7 +14,7 @@ using namespace std;
  */
 int power(int x, int y);
 int ishexdigit(char c);
-string *convert_base(const string& s, int b1, int b2)
+string convert_base(const string& s, int b1, int b2)
 {
     int p, i, len = s.length(), n=0, count=0;
     bool is_negative = s.front() == '-';
@@ -27,18 +28,18 @@ string *convert_base(const string& s, int b1, int b2)
     /* now convert from decimal to an arbitrary base b2 */
     int v, save_n = n;
     char c;
-    string *out = new string();
+    string result;
     while (n) {
         v = (n%b2);
         if (v >= 0 && v <= 9) c = '0'+v;
         else if (v >= 10 && v <= 15) c = 'a' + (v - 10);
-        out->push_back(c);
+        result.push_back(c);
         n/=b2;
     }
-    if (is_negative) out->push_back('-');
-    reverse(out->begin(), out->end());
-    printf("%d base 10 is equal to %s base %d\n", save_n, out->c_str(), b2);
-    return out; 
+    if (is_negative) result.push_back('-');
+    reverse(result.begin(), result.end());
+    printf("%d base 10 is equal to %s base %d\n", save_n, result.c_str(), b2);
+    return result;
 }
 
 int ishexdigit(char c) {
@@ -61,7 +62,33 @@ int main()
     int b1, b2;
     std::string s1 = "615";
     b1 = 7; b2 = 13;
-    string *s2 = convert_base(s1, b1, b2);
-    cout << s1 << " base " << b1 << " equals " << *s2 << " base " << b2 << endl;
+    string s2 = convert_base(s1, b1, b2);
+    //string s2 = ConvertBase(s1, b1, b2);
+    cout << s1 << " base " << b1 << " equals " << s2 << " base " << b2 << endl;
     return 0;
 }
+
+// solution in the book
+string ConvertBase(const string& s, int b1, int b2)
+{
+    bool is_negative = s.front() == '-';
+    int x = 0;
+    for (size_t i = (is_negative == true ? 1 : 0); i < s.size(); ++i) {
+        x *= b1;
+        x+= isdigit(s[i]) ? s[i] - '0' : s[i] - 'A' + 10;
+    }
+
+    string result;
+    do {
+        int remainder = x % b2;
+        result.push_back(remainder >= 10 ? 'A' + remainder - 10 : '0' + remainder);
+        x /= b2;
+    } while (x);
+
+    if (is_negative) {
+        result.push_back('-');
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
