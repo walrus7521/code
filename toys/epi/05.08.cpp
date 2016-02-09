6664119061f60f2f64399040009ee3e87b394644
Bart Bartel <bartb7@gmail.com>
2016-02-08 18:49:26 -0800

    base

--- toys/epi/05.08.cpp
+++ toys/epi/05.08.cpp
@@ -16,20 +16,17 @@ int power(int x, int y);
 int ishexdigit(char c);
 string convert_base(const string& s, int b1, int b2)
 {
-    int p, i, len = s.length(), n=0, count=0;
+    int v, save_n, i, len = s.length(), n=0, x=1;
     bool is_negative = s.front() == '-';
     /* first convert from arbitrary base b1 to base 10 */
     for (i=len-1; ishexdigit(s[i]); --i) {
-        p = power(b1, count);
-        n += (s[i] - '0') * p;
-        count++;
+        n += (s[i] - '0') * x;
+        x *= b1;
     }
-    printf("%s base %d is equal to %d decimal\n", s.c_str(), b1, n);
     /* now convert from decimal to an arbitrary base b2 */
-    int v, save_n = n;
-    char c;
     string result;
     while (n) {
+        char c;
         v = (n%b2);
         if (v >= 0 && v <= 9) c = '0'+v;
         else if (v >= 10 && v <= 15) c = 'a' + (v - 10);
@@ -38,23 +35,12 @@ string convert_base(const string& s, int b1, int b2)
     }
     if (is_negative) result.push_back('-');
     reverse(result.begin(), result.end());
-    printf("%d base 10 is equal to %s base %d\n", save_n, result.c_str(), b2);
     return result;
 }
 
 int ishexdigit(char c) {
     if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f')) return 1;
     return 0;
-}
-
-int power(int x, int y)
-{
-    int pow, i;
-    if (x == 0) return 0.0;
-    if (y == 0) return 1.0;
-    if (y < 0) y = -y, x = 1.0 / x;
-    for (i = 0, pow = x; i < y-1; i++) pow *= x;
-    return pow;
 }
 
 int main()
@@ -77,14 +63,12 @@ string ConvertBase(const string& s, int b1, int b2)
         x *= b1;
         x+= isdigit(s[i]) ? s[i] - '0' : s[i] - 'A' + 10;
     }
-
     string result;
     do {
         int remainder = x % b2;
         result.push_back(remainder >= 10 ? 'A' + remainder - 10 : '0' + remainder);
         x /= b2;
     } while (x);
-
     if (is_negative) {
         result.push_back('-');
     }
