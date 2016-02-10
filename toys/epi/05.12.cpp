30a1157a5d8b4879c2f1f1a5f8bc1b037d2b07ad
Bart Bartel <bartb7@gmail.com>
2016-02-09 20:17:18 -0800

    randnum

---
+++ toys/epi/05.12.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+/* Generate uniform random numbers
+ *
+ * This problem is motivated by the following.  Five friends have to select a
+ * designated driver using a single unbiased coin.  The process should be fair
+ * to everyone.
+ *
+ * How would you implement a random number generator that generates a random
+ * integer i between a and b, inclusive, given a random number generator that
+ * produces either zero or one with equal probability?  All generated values
+ * should be equally likely.
+ */
+
+int random_numbers()
+{
+    return 0;
+}
+
+int main()
+{
+}
