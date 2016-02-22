3b6e1b8c721f785526c44523176bbf43c39192a3
Bart Bartel <bartb7@gmail.com>
2016-02-21 17:41:30 -0800

    500 doors

--- toys/epi/05.14.cpp
+++ toys/epi/05.14.cpp
@@ -9,7 +9,44 @@
 
 #include <iostream>
 
+using namespace std;
+
+#define N_DOORS 50
+#define OPEN   0
+#define CLOSED 1
+int doors[500] = {0};
+
+void show()
+{
+    for (int i = 1; i < N_DOORS; ++i) {
+        printf("doors[%02d] = %d\n", i, doors[i]);
+    }
+}
+
+void predict_open(int k, int n)
+{
+    printf("door[1]\n");
+    for (int d = 2; d*d < n; ++d) {
+        printf("door[%d]\n", d*d);
+    }
+}
 
 int main()
 {
+    int outer;
+    //predict_open(5, 500);
+    //return 0;
+    for (int i = 1; i < N_DOORS; ++i) {
+        doors[i] = OPEN;
+    }
+    for (int p = 2; p < N_DOORS; ++p) {
+        cout << "p: " << p << endl;
+        for (int i = p; i < N_DOORS; ++i) {
+            if ((i % p) == 0) {
+                doors[i] ^= 1;
+            }
+        }
+        show();
+    }
+   
 }
