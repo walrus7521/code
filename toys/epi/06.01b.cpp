e3c2f6d22d9f4a70d0c351ad64d0b76492ef26fe
Bart Bartel <bartb7@gmail.com>
2016-02-26 11:46:55 -0800

    epi

---
+++ toys/epi/06.01b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*  The Dutch national flag problem
+ *
+ *  Write a function that takes an array A of length n and an index i into 
+ *  A, and rearranges the elements such that all elements less than A[i] appear
+ *  first, followed by elements equal to A[i], followed by elements greater
+ *  than A[i].
+ *
+ */
+
+using namespace std;
+
+int main()
+{
+}
