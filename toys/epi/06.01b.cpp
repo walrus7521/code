489b5551cb8d7af9cc0d48260c94b80a1b50e5a7
Bart Bartel <bartb7@gmail.com>
2016-02-27 19:00:53 -0800

    dutch

--- toys/epi/06.01b.cpp
+++ toys/epi/06.01b.cpp
@@ -4,50 +4,55 @@
 
 using namespace std;
 
-
-/*  The Dutch national flag problem
+/*  The Dutch flag problem
  *
  *  Write a function that takes an array A of length n and an index i into 
  *  A, and rearranges the elements such that all elements less than A[i] appear
  *  first, followed by elements equal to A[i], followed by elements greater
  *  than A[i].
  *
+ *  Idea: red, white, blue - just pinch and swap
+ *  [?] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [b] [r] [b] [r] [w] [w] [b]
+ *   ^   ^                                                       ^           ^
+ *   |-->|                                                       |<--- scan -|
+ *   | yes                                                       |
+ *   red? no ------------------- swap ----------------------------
  */
 
 using namespace std;
 
-void myswap(vector<int> a, int i, int j)
-{
-    cout << "swap: " << i << ", " << j << endl;
-    int t = a[i];
-    a[i] = a[j];
-    a[j] = t;
-}
+class Puzzle {
+public:
+    virtual void Show() = 0;
+    virtual int Process() = 0;
+};
 
-/* i is the pivot
- */
-void dutch(vector<int>& a, int i)
-{
-    int m = a[i], n = a.size();
-    if (i == 0 || i == n-1) return;
-    a[0] = m;
-    /* perk it down */
-    for (int j= 2; j < n; ++j) {
-        cout << "i: " << a[j] << "j: " << a[j-1] << endl;
-        if (a[j] < a[j-1]) myswap(a, j, j-1);
+class Dutch : public Puzzle {
+public:
+    Dutch(vector<int> v, int m) : data{v}, pivot{m}
+        { cout << "create Dutch " << endl; }
+    ~Dutch(){ cout << "destroy Dutch " << endl; }
+    void Show() {
+        cout << "pivot: " << pivot << endl;
+        cout << "data: ";
+        for(const auto& s: data)
+            cout << s << ' ';
+        cout << endl;
     }
-}
-
-void show(const vector<int>& a)
-{
-    for(const auto& s: a)
-        cout << s << ' ';
-    cout << endl;
-}
+    int Process() {
+        return 0;
+    }
+private:
+    vector<int> data;
+    int pivot;
+};
 
 int main()
 {
-    vector<int> a{ 0, 8, 2, 3, 5, 9, 1};
-    dutch(a, 1);
-    show(a);
+    vector<int> a{ 8, 2, 3, 5, 9, 1};
+    int pivot = 3;
+    Dutch d(a, pivot);
+    d.Process();
+    d.Show();
 }
+
