2145bb4dd283fd75e79de7363496a4fba2431e23
Bart Bartel <bartb7@gmail.com>
2016-02-28 19:35:37 -0800

    dutch

--- toys/epi/06.01b.cpp
+++ toys/epi/06.01b.cpp
@@ -42,28 +42,7 @@ public:
         cout << endl;
     }
     int Process() {
-        return Process_nums();
-    }
-    int Process_nums() {
-        // this is broken!!! my brain is broken
-        int head, tail;
-        head = 0;
-        tail = data.size();
-        while (head != tail) {
-            if (data[head] < pivot) {
-                ++head;
-            } else 
-            if (data[tail] > pivot) {
-                --tail;
-            } else 
-            if (data[head] > data[tail]) {
-                // swapo
-                int tmp = data[head];
-                data[head] = data[tail];
-                data[tail] = tmp;
-            }
-        }
-        return 0;
+        return Process_dutch();
     }
 
     int Process_dutch() {
@@ -97,9 +76,9 @@ private:
 
 int main()
 {
-    //vector<int> a{ RED, BLUE, WHITE, WHITE, BLUE, BLUE, WHITE, BLUE, RED, RED, WHITE };
+    vector<int> a{ RED, BLUE, WHITE, WHITE, BLUE, BLUE, WHITE, BLUE, RED, RED, WHITE };
     //vector<int> a{ 1,2,4,3,6,3,4,2,7,6,2,3,2,1,3 };
-    vector<int> a{ 1,2,2,3,3,3,4,5,5,6,7 };
+    //vector<int> a{ 1,2,2,3,3,3,4,5,5,6,7 };
     int pivot = 3;
     Dutch d(a, pivot);
     d.Show();
