9e547adbe99edb3c29ec98f94b50c3e56119952b
Bart Bartel <bartb7@gmail.com>
2016-03-06 15:26:27 -0800

    bigint

---
+++ toys/epi/06.02.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+ * Increment a big integer
+ * 
+ * Write a function which takes as input an array A of digits encoding
+ * a decimal number D and updates A to represent the number D + 1.  For
+ * example, if A = <1,2,9> then you should update A to <1,3,0>.
+ *
+ * returns new string.
+ */
+
+class Puzzle {
+public:
+    virtual void Show() = 0;
+    virtual int Process() = 0;
+};
+
+class BigInt : Puzzle {
+public:
+    BigInt(string s) {
+        num = s; //new string(s);
+    }
+    void Show() {
+        cout << num << endl;
+    }
+    BigInt& operator++(){
+        int n = num.size(), i, msd = 0;
+        string *B = new string();
+        for (i = n-1; i >= 0; --i) {
+            if (num[i] < '9') {
+                num[i]++;
+                break;
+            }
+            num[i] = '0';
+            if (i == 0) msd = 1;
+        }
+        if (msd == 1) {
+            *B += '1';
+        }
+        for (i = 0; i < n; i++) {
+            *B += num[i];
+        }
+        num = *B;
+        delete B;
+        return *this;
+    }
+    int Process() { return 0; }
+private:
+    string num;
+};
+
+int main()
+{
+    string num = "999";
+    BigInt i(num);
+    i.Show();
+    ++i;
+    i.Show();
+}
+
