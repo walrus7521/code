4fe4016b151fe1641009c3a0c6b3dc6e53a25c61
Bart Bartel <BartB@helitrak.com>
2016-04-13 12:57:13 -0700

    csharp

--- toys/c#/utils/log.cs
+++ toys/c#/utils/log.cs
@@ -1,29 +1,97 @@
 using System;
+using System.IO;
  
 public class Logger
 {
-    // Field
     private string name;
 
     // Constructor that takes no arguments.
     public Logger()
     {
         name = "unknown";
-        Console.WriteLine("Logger() name: " + name);
     }
 
     // Constructor that takes one argument.
     public Logger(string nm)
     {
         name = nm;
-        Console.WriteLine("Logger(name): " + name);
-     }
-
-    // Method
-    public void SetName(string newName)
+    }
+    
+    public string LogRead(string filePath)
     {
-        name = newName;
-        Console.WriteLine ("SetName: " + name);
+        StreamReader sr = null;
+        string fileText;
+        try
+        {
+            sr = File.OpenText(filePath);
+            fileText = sr.ReadToEnd();
+            return fileText;
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            return ex.Message;
+        }
+        catch (FileNotFoundException ex)
+        {
+            return ex.Message;
+        }
+        catch (IOException ex)
+        {
+            return ex.Message;
+        }
+        catch
+        {
+            return "Logging failed";
+        }
+        finally
+        {
+            if (sr != null)
+            {
+                sr.Close();
+                sr.Dispose();
+            }
+        }
+    }
+
+    public string LogWrite(string logPath, string logInfo)
+    {
+        StreamWriter sw = null;
+        try
+        {
+            FileStream oFileStream = new FileStream(logPath, FileMode.Open,
+                    FileAccess.Write);
+            sw = new StreamWriter(oFileStream);
+            oFileStream.Seek(0, SeekOrigin.End);
+            sw.WriteLine(DateTime.Now);
+            sw.WriteLine(logInfo);
+            sw.WriteLine();
+            sw.Close();
+            return "Logging succeeded";
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            return ex.Message;
+        }
+        catch (FileNotFoundException ex)
+        {
+            return ex.Message;
+        }
+        catch (IOException ex)
+        {
+            return ex.Message;
+        }
+        catch
+        {
+            return "Logging failed";
+        }
+        finally
+        {
+            if (sw != null)
+            {
+                sw.Close();
+                sw.Dispose();
+            }
+        }
     }
 }
 
@@ -31,6 +99,11 @@ class TestLogger
 {
     static void Main() {
         var log = new Logger("dude");
-        log.SetName("sup");
+        string path = "log.txt";
+        string res;
+        string info = "hey dude wusup";
+        log.LogWrite(path, info);
+        res = log.LogRead(path);
+        Console.WriteLine(res);
     }
 }
