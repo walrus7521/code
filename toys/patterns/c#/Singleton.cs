318b452e4ccc3659dbce536ce0bd13e947d1df14
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-05-07 06:48:28 -0700

    patts

--- toys/patterns/c#/Singleton.cs
+++ toys/patterns/c#/Singleton.cs
@@ -1,56 +1,60 @@
-/*
- *  C# Design Pattern Essentials
- *  Copyright 2012, Ability First Limited
- *
- *  This source code is provided to accompany the book and is provided AS-IS without warranty of any kind.
- *  It is intended for educational and illustrative purposes only, and may not be re-published
- *  without the express written permission of the publisher.
- */
 using System;
 
-namespace Chapter06
-{
-    public class SerialNumberGenerator
-    {
-        // static members
-        private static volatile SerialNumberGenerator instance;
-        private static object synchronizationRoot = new Object();
+// mcs Singleton.cs
 
-        public static SerialNumberGenerator Instance
+public class SerialNumberGenerator
+{
+    // static members
+    private static volatile SerialNumberGenerator instance;
+    private static object synchronizationRoot = new Object();
+
+    public static SerialNumberGenerator Instance
+    {
+        get
         {
-            get
+            if (instance == null)
             {
-                if (instance == null)
+                lock (synchronizationRoot)
                 {
-                    lock (synchronizationRoot)
+                    if (instance == null)
                     {
-                        if (instance == null)
-                        {
-                            instance = new SerialNumberGenerator();
-                        }
+                        instance = new SerialNumberGenerator();
                     }
                 }
-                return instance;
             }
+            return instance;
         }
+    }
 
+    // instance variables
+    private int count;
 
-        // instance variables
-        private int count;
+    // private constructor
+    private SerialNumberGenerator()
+    {
+    }
 
-        // private constructor
-        private SerialNumberGenerator()
+    // instance methods
+    public virtual int NextSerial
+    {
+        get
         {
+            return ++count;
         }
+    }
 
-        // instance methods
-        public virtual int NextSerial
-        {
-            get
-            {
-                return ++count;
-            }
+}
+
+public class Tester
+{
+    public static void Main()
+    {
+        var sngen = SerialNumberGenerator.Instance;
+        for (int i = 0; i < 4; ++i) {
+            int sn = sngen.NextSerial;
+            Console.WriteLine("serial number: " + sn);
         }
-
     }
 }
+
+
