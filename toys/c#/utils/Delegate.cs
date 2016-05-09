2a883d4498f10e3399b15d6b73e11450654af307
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-05-09 07:59:38 -0700

    stuff

--- toys/c#/utils/Delegate.cs
+++ toys/c#/utils/Delegate.cs
@@ -2,40 +2,64 @@ using System;
 
 delegate int NumberChanger(int n);
 
+delegate bool CompareInt(int I1, int I2);
+
 namespace DelegateAppl
 {
-   class TestDelegate
-   {
-      static int num = 10;
-      public static int AddNum(int p)
-      {
-         num += p;
-         return num;
-      }
+    class TestDelegate
+    {
+        static int num = 10;
 
-      public static int MultNum(int q)
-      {
-         num *= q;
-         return num;
-      }
-      public static int getNum()
-      {
-         return num;
-      }
+        // create methods that will be delegated to
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
 
-      static void Main(string[] args)
-      {
-         //create delegate instances
-         NumberChanger nc1 = new NumberChanger(AddNum);
-         NumberChanger nc2 = new NumberChanger(MultNum);
+        public static int getNum()
+        {
+            return num;
+        }
+
+        public static TestNumberChanger()
+        {
+
+            //create delegate instances
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
          
-         //calling the methods using the delegate objects
-         nc1(25);
-         Console.WriteLine("Value of Num: {0}", getNum());
-         nc2(5);
-         Console.WriteLine("Value of Num: {0}", getNum());
-         Console.ReadKey();
-      }
-   }
+            //calling the methods using the delegate objects
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
+        }     
+
+        // create methods that will be delegated to
+        public bool AscendOrder(int I1, int I2)
+        {
+
+        }
+
+        public bool DescendOrder(int I1, int I2)
+        {
+        }
+
+        public static TestCompareInt()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            TestNumberChanger();
+        }
+    }
 }
 
