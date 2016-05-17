3b093cfcddf442d4785db11e16a62e150d14bd65
Bart Bartel <BartB@helitrak.com>
2016-05-17 09:21:12 -0700

    utils

--- toys/cs/utils/Log.cs
+++ toys/cs/utils/Log.cs
@@ -4,7 +4,39 @@ using System.Collections;
 using System.Collections.Generic;
  
 public class Log {
+    private static readonly int SIZE = 256;
     private static int instanceCount = 0;
+    private int count = 0;
+    private string[] log = new string[SIZE];
+    public Log() { instanceCount++; }
+    public static int InstanceCount { get { return instanceCount; }}
+    public void Add(string msg) { log[count++ % SIZE] = msg; }
+    public int Count { get { return count; }}
+    public string Last {
+        get
+        {
+            return count == 0 ? null : log[(count-1)%SIZE];
+        }
+        set
+        {
+            if (count == 0)
+            {
+                log[count++] = value;
+            }
+            else
+            {
+                log[(count-1)%SIZE] = value;
+            }
+        }
+    }
+    public string this[int i]
+    {
+        get
+        {
+            return log[(i)%SIZE];
+        }
+    }
+    //public string[] All { ... }
 }
 
 
@@ -117,7 +149,24 @@ class TestLogger
 
     }
 
+    static void Test3()
+    {
+        Log log = new Log();
+        log.Add("hey");
+        log.Add("sup");
+        log.Add("homi");
+        log.Add("dude");
+        Console.WriteLine("last: " + log.Last);
+        for (int i = 0; i < log.Count; i++) {
+            Console.WriteLine("get: " + log[i]);
+        }
+        //foreach (var s in log)
+        //{
+        //    Console.WriteLine("get: " + s);
+        //}
+    }
+
     static void Main() {
-        Test2();
+        Test3();
     }
 }
