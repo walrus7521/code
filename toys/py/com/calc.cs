bd393c4a34d350ec01fd69c92c63f180c0159045
Bart Bartel <BartB@helitrak.com>
2016-05-25 17:38:38 -0700

    compy

---
+++ toys/py/com/calc.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Dynamic;
+
+// https://www.simple-talk.com/dotnet/.net-framework/dynamic-language-integration-in-a-c-world/
+// csc /target:library calc.cs
+
+public class Calculator 
+{
+    public double add(double argA, double argB)
+    {
+        return argA + argB;
+    }
+    public double sub(double argA, double argB)
+    {
+        return argA - argB;
+    }
+}
+
+namespace DynamicCS
+{
+    public class DynamicCalc: DynamicObject
+    {
+        public Calculator calc;
+        public DynamicCalc()
+        {
+            calc = new Calculator();
+        }
+        public double add(double argA, double argB)
+        {
+            return calc.add(argA, argB);
+        }
+        public double sub(double argA, double argB)
+        {
+            return calc.sub(argA, argB);
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object          result)
+        {
+            result = null;
+            switch (binder.Name)
+            {
+                case "add":
+                result = (Func<double, double ,double>) ((double a, double b)
+                       => calc.add(a, b));
+                    return true;
+                case "sub":
+                result = (Func<double, double, double>) ((double a, double b)
+                       => calc.sub(a, b));
+                    return true;
+            }
+            return false;
+        }
+    }
+}
+
+//namespace XCli
+//{
+//    using DynamicCS;
+//    public class Client
+//    {
+//
+//        public static void Main()
+//        {
+//            DynamicCS.DynamicCalc calc = new DynamicCS.DynamicCalc();
+//            var x = calc.calc.add(3.0, 4.0);
+//            Console.WriteLine("x = " + x );
+//            
+//        }
+//    }
+//}
+
---
+++ toys/py/com/calc.py
@@ -0,0 +1,23 @@
+import sys
+import clr
+
+sys.path.append(r"m:\code\com")
+clr.AddReference(r"m:\code\com\calc.dll")
+
+from DynamicCS import DynamicCalc
+
+calc=DynamicCalc()
+
+print calc.__class__.__name__
+# display the name of the class: 'DynamicCalc' 
+
+a=7.5
+b=2.5
+
+res = calc.add(a, b)
+print a, '+', b, '=', res
+
+res = calc.sub(a, b)
+print a, '-', b, '=', res
+
+raw_input('Press any key to finish...')
