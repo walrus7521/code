789b02af693c5838236dcf4b7e6a4e52e9dee062
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-05-29 06:51:20 -0700

    utils

--- toys/cs/utils/GetHostInfo.cs
+++ toys/cs/utils/GetHostInfo.cs
@@ -2,8 +2,24 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 
-class IPAddressExample {
-    static void PrintHostInfo(String host) {
+class Env_and_IPAddressExample {
+
+   static void ShowEnvDetails()
+   {
+       // print drives on this machine
+       foreach (var drive in Environment.GetLogicalDrives())
+       {
+           Console.WriteLine("Drive: {0}", drive);
+       }
+       Console.WriteLine("User Name: {0}", Environment.UserName);
+       Console.WriteLine("Machine Name: {0}", Environment.MachineName);
+       Console.WriteLine("OS: {0}", Environment.OSVersion);
+       Console.WriteLine("Num processors: {0}", Environment.ProcessorCount);
+       Console.WriteLine(".NET Version: {0}", Environment.Version);
+   }
+
+    static void PrintHostInfo(String host) 
+    {
         try {
             IPHostEntry hostInfo;
             hostInfo = Dns.GetHostEntry(host);
@@ -27,6 +43,8 @@ class IPAddressExample {
 
     static void Main(string[] args) {
         try {
+            Console.WriteLine("Show environment: ");
+            ShowEnvDetails();
             Console.WriteLine("Local host: ");
             String localHostName = Dns.GetHostName();
             Console.WriteLine("Host name:  " + localHostName);
