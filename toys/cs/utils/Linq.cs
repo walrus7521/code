be2531b38651a9f2c175916330465a8cf63eb2fb
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-05-30 11:04:36 -0700

    utils

---
+++ toys/cs/utils/Linq.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Client
+{
+    static void ReflectOverQueryResults(object resultSet)
+    {
+        Console.WriteLine("**** info about query ****");
+        Console.WriteLine("resultSet is of type: {0}", resultSet.GetType().Name);
+        Console.WriteLine("resultSet location: {0}", resultSet.GetType().Assembly.GetName().Name);
+    }
+
+    static void QueryOverInts()
+    {
+        Console.WriteLine("**** query over ints ****");
+        int[] numbers = {10, 20, 30, 40, 1, 2, 3, 8};
+        // print only items less than 10
+        //IEnumerable<int> subset = from i in numbers where i<10 select i;
+        var subset2 = from i in numbers where i<10 select i;
+        
+        foreach (var i in subset2) {
+            Console.WriteLine("Item: {0}", i);
+        }
+        ReflectOverQueryResults(subset2);
+    }
+
+    static void QueryOverStrings()
+    {
+        Console.WriteLine("**** query over strings ****");
+        string[] currentVideoGames = {"Morrowind", "Uncharted 2",
+                                      "Fallout 3", "Daxter", "System Shock 2"};
+        // build a query finding items with embedded spaces
+        IEnumerable<string> subset = from g in currentVideoGames
+                                     where g.Contains(" ") orderby g select g;
+        // print results
+        foreach (string s in subset) {
+            Console.WriteLine("Item: {0}", s);
+        }
+        ReflectOverQueryResults(subset);
+    }
+
+    static void Main()
+    {
+        QueryOverStrings();
+        QueryOverInts();
+    }
+
+
+
+}
