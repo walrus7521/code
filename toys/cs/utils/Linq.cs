9a8c5e912605984f93c07aa17dcb664f81e27978
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-05-30 19:22:34 -0700

    updates

--- toys/cs/utils/Linq.cs
+++ toys/cs/utils/Linq.cs
@@ -17,8 +17,14 @@ public class Client
         Console.WriteLine("**** query over ints ****");
         int[] numbers = {10, 20, 30, 40, 1, 2, 3, 8};
         // print only items less than 10
-        //IEnumerable<int> subset = from i in numbers where i<10 select i;
+        IEnumerable<int> subset = from i in numbers where i<10 select i;
         var subset2 = from i in numbers where i<10 select i;
+        int[] subsetAsIntArray = 
+            (from i in numbers where i<10 select i).ToArray<int>();
+
+        List<int> subsetAsListOfInts = 
+            (from i in numbers where i<10 select i).ToList<int>();
+
         
         foreach (var i in subset2) {
             Console.WriteLine("Item: {0}", i);
