969a2f595575ba861b53e80565a634e7b89173a2
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-06-01 07:29:05 -0700

    utils

--- toys/cs/utils/Linq.cs
+++ toys/cs/utils/Linq.cs
@@ -12,7 +12,17 @@ class Car
     public string Make {get;set;}
 }
 
-
+class ProductInfo
+{
+    public string Name {get;set;}
+    public string Description {get;set;}
+    public int NumberInStock {get;set;}
+    public override string ToString()
+    {
+        return string.Format("Name={0}, Description={1}, Number in Stock={2}",
+                Name, Description, NumberInStock);
+    }
+}
 
 public class Client
 {
@@ -105,7 +115,7 @@ public class Client
         }
     }
 
-    static void TestLinqOverCollections()
+    static void TestLinqOverGenericCollections()
     {
         Console.WriteLine("***** LINQ over Generic Collections *****");
         List<Car> myCars = new List<Car>() {
@@ -117,6 +127,139 @@ public class Client
         };
         GetFastCars(myCars);
         GetFastBMWs(myCars);
+    }
+
+    static void TestLinqOverNonGenericCollections()
+    {
+        Console.WriteLine("***** LINQ over Non Generic Collections *****");
+        List<Car> myCars = new List<Car>() {
+            new Car{PetName = "Henry",   Color = "Silver", Speed = 100, Make = "BMW"},
+            new Car{PetName = "Daisy",   Color = "Tan", Speed = 90, Make = "BMW"},
+            new Car{PetName = "Mary",    Color = "Black", Speed = 55, Make = "VW"},
+            new Car{PetName = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo"},
+            new Car{PetName = "Melvin",  Color = "White", Speed = 43, Make = "Ford"},
+        };
+        var myCarsEnum = myCars.OfType<Car>();
+        var fastCars = from c in myCarsEnum where c.Speed>55 select c;
+        foreach (var car in fastCars) {
+            Console.WriteLine("{0} is going too fast!", car.PetName);
+        }
+    }
+
+    static void FilteringDataUsingOfType()
+    {
+        Console.WriteLine("***** LINQ Filtering Data using OfType *****");
+        ArrayList myStuff = new ArrayList(); // non-generic holds objects
+        myStuff.AddRange(new object[] {10, 400, 8, false, new Car(), "string data"});
+        // now filter out the numeric data
+        var myInts = myStuff.OfType<int>();
+        foreach (var i in myInts) {
+            Console.WriteLine("Int value: {0}", i);
+        }
+    }
+
+    // use LINQ queries using ProductInfo
+    static void QueriesUsingLINQ()
+    {
+        Console.WriteLine("****** Queries with LINQ Expressions *****");
+        ProductInfo[] products = new[] {
+            new ProductInfo{ Name="Mac's Coffee", Description="Coffee with TEETH", NumberInStock=24 },
+            new ProductInfo{ Name="Milk Maid Mild", Description="Milk cows love", NumberInStock=100 },
+            new ProductInfo{ Name="Pure Silk Tofu", Description="Bland as possible", NumberInStock=120 },
+            new ProductInfo{ Name="Crunchy Pops", Description="Cheezy, peppery good", NumberInStock=2 },
+            new ProductInfo{ Name="RipOff Water", Description="From tap to wallet", NumberInStock=100 },
+            new ProductInfo{ Name="Classic Valpo Pizza", Description="Everyone's pizza", NumberInStock=73 },
+        };
+        // Basic selection syntax
+        // get everything
+        var allProducts = from p in products select p;
+        foreach (var prod in allProducts) {
+            Console.WriteLine(prod.ToString());
+        }
+        
+        // Just grab the Name
+        var names = from p in products select p.Name;
+        foreach (var n in names) {
+            Console.WriteLine("Name: {0}", n);
+        }
+
+        // Obtaining subsets of data - using "where"
+        // get items with 25 or more in stock
+        Console.WriteLine("overstock items");
+        var overstock = from p in products where p.NumberInStock > 25 select p;
+        foreach (var c in overstock) {
+            Console.WriteLine(c.ToString());
+        }
+
+        // query over names and descriptions and project results onto anonymous type
+        Console.WriteLine("project query to anonymous type");
+        var nameDesc = from p in products select new { p.Name, p.Description };
+        foreach (var item in nameDesc) {
+            Console.WriteLine(item.ToString());
+        }
+
+        // transform the anonymous projection into Array in case we need to return it
+        Console.WriteLine("transform anonymous projection to Array");
+        var nameDescArray = nameDesc.ToArray(); // note no use of <> as we don't know underlying type
+        foreach (var item in nameDescArray) {
+            Console.WriteLine(item.ToString());
+        }
+
+        // obtaining counts using Enumerable
+        int count = nameDescArray.Count();
+        Console.WriteLine("Query returned {0} items", count);
+
+        // Reverse the set
+        Console.WriteLine("Reverse the results");
+        foreach (var item in nameDesc.Reverse()) {
+            Console.WriteLine(item.ToString());
+        }
+
+        // Sort the results by alpha products
+        var sortProducts = from p in products orderby p.Name ascending select p;
+        Console.WriteLine("Query in sorted fashion");
+        foreach (var item in sortProducts) {
+            Console.WriteLine(item.ToString());
+        }
+
+        // LINQ as a better Venn diagramming tool
+        Console.WriteLine("LINQ as a better Venn diagramming tool");
+        List<string> myCars = new List<string> { "Yugo", "Aztec", "BMW" };
+        List<string> yourCars = new List<string> { "BMW", "Saab", "Aztec" };
+        var carDiff = (from c in myCars select c)
+            .Except(from c2 in yourCars select c2);
+        Console.WriteLine("Here is what you don't have, but I do:");
+        foreach (string s in carDiff) {
+            Console.WriteLine(s); // prints Yugo
+        }
+
+        var carIntersect = (from c in myCars select c)
+            .Intersect(from c2 in yourCars select c2);
+        Console.WriteLine("Here is what we have in common:");
+        foreach (string s in carIntersect) {
+            Console.WriteLine(s);
+        }
+
+        var carUnion = (from c in myCars select c)
+            .Union(from c2 in yourCars select c2);
+        Console.WriteLine("Here is what we both have, no duplicates (union):");
+        foreach (string s in carUnion) {
+            Console.WriteLine(s);
+        }
+
+        var carConcat = (from c in myCars select c)
+            .Concat(from c2 in yourCars select c2);
+        Console.WriteLine("Here is the concatenation of what we have:");
+        foreach (string s in carConcat) {
+            Console.WriteLine(s);
+        }
+
+        Console.WriteLine("Here is a dedup of what we have in common:");
+        foreach (string s in carConcat.Distinct()) {
+            Console.WriteLine(s);
+        }
+
+
     }
 
     static void Main()
@@ -132,7 +275,10 @@ public class Client
         foreach (string item in subset2) {
             Console.WriteLine(item);
         }
-        TestLinqOverCollections();
+        TestLinqOverGenericCollections();
+        TestLinqOverNonGenericCollections();
+        FilteringDataUsingOfType();
+        QueriesUsingLINQ();
     }
 
 
