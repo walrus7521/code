491b3a58e9c0c7ef31fd41eb83fb0083c33e9f09
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-06-01 22:23:04 -0700

    utils

--- toys/cs/utils/AwaitExpressionTest.cs
+++ toys/cs/utils/AwaitExpressionTest.cs
@@ -1,24 +1,36 @@
 // AwaitExpressionTest.cs
 // Copyright (c) 2015, HerongYang.com, All Rights Reserved.
 
+// mcs /r:System.Net.Http.dll AwaitExpressionTest.cs
+
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+
 public class AwaitExpressionTest {
-   static void Main() {
-      Console.WriteLine("Before 'await' ...");	
-      AwaitTest();
-      Console.WriteLine("After 'await' ...");	
-   } 
-   async static void AwaitTest() {
-      string uri = "http://www.herongyang.com/Service/Hello_REST.php";
-      HttpClient c = new HttpClient();
-      Console.WriteLine("Calling GetStringAsync()...");	
-      Task<string> t = c.GetStringAsync(uri);
-      Console.WriteLine("Evaluating await-expression ...");	
-      string s = await t;
-      Console.WriteLine("Writing task result ...");	
-      Console.WriteLine(s);
-   } 
+    static void Main() 
+    {
+        var t1 = Task.Factory.StartNew(new Func<Task>(async () => await DoMethod())).Unwrap();
+        Console.WriteLine("STARTED");
+        t1.Wait();
+    } 
+
+    static async Task DoMethod()
+    {
+        Console.WriteLine("Before 'await' ...");	
+        await AwaitTest();
+        Console.WriteLine("After 'await' ...");	
+    }
+
+    static async Task AwaitTest() {
+        string uri = "http://www.herongyang.com/Service/Hello_REST.php";
+        HttpClient c = new HttpClient();
+        Console.WriteLine("Calling GetStringAsync()...");	
+        Task<string> t = c.GetStringAsync(uri);
+        Console.WriteLine("Evaluating await-expression ...");	
+        string s = await t;
+        Console.WriteLine("Writing task result ...");	
+        Console.WriteLine(s);
+    } 
 }
 
