973c8eebb0b9fa1a78b8a71abb2bac0a8b4898a9
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-06-07 08:59:27 -0700

    utils

--- toys/cs/utils/Reflection.cs
+++ toys/cs/utils/Reflection.cs
@@ -1,32 +1,79 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
-using CarLibrary;
-
-// mcs ch14CarCli.cs /r:ch14CarLib.dll
-// or if the assembly is an .exe
-// mcs mcs Reflection.cs ch14CarLib.cs ch14CarDerived.cs 
-
-namespace CSharpCarClient
+public class Program
 {
-    public class Program
+    static void ListMethods(Type t)
     {
-        static void Main()
-        {
-            Console.WriteLine("****** c# CarLibrary client app ******");
-            SportsCar viper = new SportsCar("Viper", 240, 40);
-            viper.TurboBoost();
-
-            MiniVan mv = new MiniVan();
-            mv.TurboBoost();
-
-            Type t = viper.GetType(); // need instance of class here
-            Console.WriteLine("sports car type: {0}", t);
-            Type t2 = typeof(SportsCar); // no need to have instance of class here
-            Console.WriteLine("sports car type2: {0}", t2);
+        Console.WriteLine("**** Methods *****");
+        var methodNames = from n in t.GetMethods() select n.Name;
+        foreach (var name in methodNames) {
+            Console.WriteLine("->{0}", name);
         }
+    }
+
+    static void ListFields(Type t)
+    {
+        Console.WriteLine("***** Fields *****");
+        var fieldNames = from f in t.GetFields() select f.Name;
+        foreach (var name in fieldNames) {
+            Console.WriteLine("->{0}", name);
+        }
+    }
+
+    static void ListProps(Type t)
+    {
+        Console.WriteLine("***** Properties *****");
+        var propNames = from p in t.GetProperties() select p.Name;
+        foreach (var name in propNames) {
+            Console.WriteLine("->{0}", name);
+        }
+    }
+
+    static void ListInterfaces(Type t)
+    {
+        Console.WriteLine("***** Interfaces *****");
+        var ifaces = from i in t.GetInterfaces() select i;
+        foreach (var i in ifaces) {
+            Console.WriteLine("->{0}", i.Name);
+        }
+    }
+    
+    static void ListVariousStats(Type t)
+    {
+        Console.WriteLine("****** Various Statistics ******");
+        Console.WriteLine("Base class is: {0}", t.BaseType);
+        Console.WriteLine("Is type abstract? {0}", t.IsAbstract);
+        Console.WriteLine("Is type sealed? {0}", t.IsSealed);
+        Console.WriteLine("Is type generic? {0}", t.IsGenericTypeDefinition);
+        Console.WriteLine("Is type a class type? {0}", t.IsClass);
+    }
+
+    static void Main()
+    {
+        string typeName = "";
+        do {
+            Console.WriteLine("\nEnter a type name to evaluate");
+            Console.WriteLine("or enter Q to quit: ");
+            
+            typeName = Console.ReadLine();
+
+            if (typeName.ToUpper() == "Q") {
+                break;
+            }
+
+            try {
+                Type t = Type.GetType(typeName);
+                Console.WriteLine("");
+                ListVariousStats(t);
+                ListFields(t);
+                ListProps(t);
+                ListMethods(t);
+                ListInterfaces(t);
+            } catch {
+                Console.WriteLine("Sorry, can't find type {0}", typeName);
+            }
+        } while (true);
     }
 }
