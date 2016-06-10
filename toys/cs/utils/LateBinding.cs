654784cfaf874d5435fdeca0a5ea564793761723
Bart Bartel <BartB@helitrak.com>
2016-06-10 10:29:59 -0700

    csharp

--- toys/cs/utils/LateBinding.cs
+++ toys/cs/utils/LateBinding.cs
@@ -11,6 +11,7 @@ public class Program
 
     static void CreateUsingLateBinding(Assembly asm)
     {
+        Console.WriteLine("****** Call method using late binding ******");
         try
         {
             // get meta data
@@ -33,9 +34,30 @@ public class Program
         }
     }
 
+    // re-write using dynamic
+    static void InvokeMethodWithDynamicKeyword(Assembly asm)
+    {
+        Console.WriteLine("****** Call method using dynamics ******");
+        try
+        {
+            // get meta data
+            Type miniVan = asm.GetType("CarLibrary.MiniVan");
+            // create miniVan object on the fly
+            dynamic obj = Activator.CreateInstance(miniVan);
+            Console.WriteLine("Created a {0} using dynamics!", obj);
+            // invoke the method
+            obj.TurboBoost();
+            obj.TurnOnRadio(true, 0);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("****** Fun with Late Binding ******");
+        Console.WriteLine("****** Fun with Late Binding ******\n");
         // try loading a local copy of CarLibrary.dll
         Assembly a = null;
         try
@@ -50,6 +72,9 @@ public class Program
         if (a != null) 
         {
             CreateUsingLateBinding(a);
+            Console.WriteLine();
+            InvokeMethodWithDynamicKeyword(a);
+
         }
     }
 
