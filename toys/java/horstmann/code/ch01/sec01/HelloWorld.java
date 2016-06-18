c305ec0377842a152912a5ee69bc44bc40ddcf36
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-06-18 04:59:44 -0700

    java

---
+++ toys/java/horstmann/code/ch01/sec01/HelloWorld.java
@@ -0,0 +1,12 @@
+//package ch01.sec01;
+
+import java.util.*;
+
+
+// Our first Java program
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
