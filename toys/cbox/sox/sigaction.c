e8bb41c0bc524e82c4002a60dc8483d97f228ce6
Bart Bartel <BartB@helitrak.com>
2016-06-28 17:44:40 -0700

    sox

---
+++ toys/cbox/sox/sigaction.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+void InterruptSignalHandler(int signalType)
+{
+    printf("Interrupt received. exiting\n");
+    exit(1);
+}
+
+int main()
+{
+    struct sigaction handler;
+
+    handler.sa_handler = InterruptSignalHandler;
+    if (sigfillset(&handler.sa_mask) < 0) {
+        perror("sigfillset");
+        exit(1);
+    }
+
+    handler.sa_flags = 0;
+
+    if (sigaction(SIGINT, &handler, 0) < 0) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    for (;;) {
+        pause();
+    }
+
+    exit(0);
+}
