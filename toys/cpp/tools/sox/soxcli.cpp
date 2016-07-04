b8bae963754aba68e6667d620528ce67d05d6bf2
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-07-04 09:01:14 -0700

    stuff

--- toys/cpp/tools/sox/soxcli.cpp
+++ toys/cpp/tools/sox/soxcli.cpp
@@ -1,3 +1,9 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <netdb.h>
+
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -11,44 +17,45 @@ using namespace std;
 #define PORT 80
 #define USERAGENT "HTMLGET 1.0"
 
-void test()
+char *get_ip(char *host)
 {
-    const string host = "127.0.0.1";
-    const string page = PAGE;
-    const string getpage = "/";
-    const string useragent = USERAGENT;
-    const string tpl = "GET /%s HTTP/1.0\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n";
-
-    char query[256];
-    sprintf(query, tpl.c_str(), getpage.c_str(), host.c_str(), useragent.c_str());
-
-    string q(query);
-    cout << q << endl;
-
+  struct hostent *hent;
+  int iplen = 15; //XXX.XXX.XXX.XXX
+  char *ip = (char *)malloc(iplen+1);
+  memset(ip, 0, iplen+1);
+  if((hent = gethostbyname(host)) == NULL)
+  {
+    herror("Can't get IP");
+    exit(1);
+  }
+  if(inet_ntop(AF_INET, (void *)hent->h_addr_list[0], ip, iplen) == NULL)
+  {
+    perror("Can't resolve host");
+    exit(1);
+  }
+  return ip;
 }
 
 int main()
 {
-    //test();
-    //return 0;
 
-
-    sox_client *sc = new sox_client(sox_client::TCP, 5000, "127.0.0.1");
+    char *ip = get_ip((char *) HOST);
+    sox_client *sc = new sox_client(sox_client::TCP, 80, ip);
     sc->connect();
 
-    const string host = "127.0.0.1";
+    const string host = HOST;
     const string page = PAGE;
-    const string getpage = "/";
+    const string getpage = "";
     const string useragent = USERAGENT;
     const string tpl = "GET /%s HTTP/1.0\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n";
 
     char query[256];
+    memset(query, 0, 256);
     sprintf(query, tpl.c_str(), getpage.c_str(), host.c_str(), useragent.c_str());
 
-    string msg(query);
-    //string msg = "hello";
+    string get(query);
  
-    sc->setbuf(msg, msg.length());
+    sc->setbuf(get, get.length());
     sc->send();
     string rmsg = sc->recv();
     cout << "recv: " << rmsg << endl;
