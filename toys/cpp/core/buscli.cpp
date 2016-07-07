c37d4010da3a718aa5417965ef665c876b2f209d
Bart Bartel <BartB@helitrak.com>
2016-07-07 16:42:02 -0700

    cruft

---
+++ toys/cpp/core/buscli.cpp
@@ -0,0 +1,53 @@
+#include "bus.h"
+
+int main()
+{
+    try {
+        Interface link;
+
+        link.init   = dummy;
+        link.open   = dummy;
+        link.close  = dummy;
+        link.read   = dummy;
+        link.write  = dummy;
+        link.ioctl  = dummy;
+        link.notify = notify;
+        link.run    = run;
+
+        Class ahci(Bus::Ahci, link);
+        Class usb(Bus::Usb, link);
+        Class pci(Bus::Pci, link);
+    
+        Bus::subscribe(&ahci);
+        Bus::subscribe(&usb);
+        Bus::subscribe(&pci);
+
+        pci.start();
+        usb.start();
+        ahci.start();
+    
+        this_thread::sleep_for (chrono::seconds(3));
+        cout << "setting bus_ready" << endl;
+        bus_ready = true;
+    
+        Bus::list();
+
+        this_thread::sleep_for (chrono::seconds(3));
+
+        pci.stop();
+        usb.stop();
+        ahci.stop();
+
+        Bus::signal();
+        Bus::eject(&pci);
+        Bus::list();
+        Bus::eject(&usb);
+        Bus::list();
+
+    } catch (exception& ex) {
+        cout << "dude..." << endl;
+        cout << ex.what() << endl;
+    }
+
+
+}
--- toys/cpp/core/dev.h
+++ toys/cpp/core/dev.h
@@ -191,38 +191,4 @@ void create_devs()
 #define QUIT 2
 #define LIST 3
 
-void test()
-{
-    ofstream fout("devices.xml");
-    //xml_oarchive oa(fout);
-}
-
-int main()
-{
-    map<string, int> menu; // = new map<string, int>();
-    menu["help"] = HELP;
-    menu["quit"] = QUIT;
-    menu["list"] = LIST;
-    create_devs();
-    while (1) {
-        string cmd;
-        int cmdid;
-        cout << "$ ";
-        cin >> cmd;
-        for(int i = 0; i < cmd.length(); ++i) cmd[i] = tolower(cmd[i]);
-        cmdid = menu[cmd];
-        switch (cmdid) {
-            case HELP: 
-                cout << "Helping you" << endl;
-                break;
-            case QUIT:
-                return 0;
-            case LIST:
-                dm.ShowList();
-                break;
-            default:
-                break;
-        }
-    }
-}
 
