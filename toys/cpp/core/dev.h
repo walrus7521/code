fe94ddcd9c7ad76dd437e1658cfad97a2eda3efb
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-07-07 20:45:12 -0700

    cpp

--- toys/cpp/core/dev.h
+++ toys/cpp/core/dev.h
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <vector>
 #include <map>
 #include <string>
+#include <exception>
+#include <thread>
+#include <chrono>
+#include <atomic>
 #include <functional>
+
+std::atomic<bool> bus_ready (false);
+
+class Bus;
+
+class Context
+{
+public:
+    Bus *bus;
+};
+
+typedef void (*notification)(Context*);
+typedef void (*runnable)(Context*);
 
 class ParameterBlock
 {
@@ -11,12 +29,15 @@ public:
     int vid;
     int did;
     std::string name;
+    std::function<int(void)> init;
     std::function<int(void)> open;
     std::function<int(void)> close;
     std::function<int(void)> read;
     std::function<int(void)> write;
     std::function<int(void)> ioctl;
     std::function<int(void)> status;
+    notification notify;
+    runnable run;
 };
 
 class DeviceFramework
@@ -49,12 +70,162 @@ private:
 };
 
 template <typename T> int my_open1(T t)  { std::cout << "my_open" << t << std::endl; return 0; }
-int my_open()   { std::cout << "my_open"   << std::endl; return 0; }
-int my_close()  { std::cout << "my_close"  << std::endl; return 0; }
-int my_read()   { std::cout << "my_read"   << std::endl; return 0; }
-int my_write()  { std::cout << "my_write"  << std::endl; return 0; }
-int my_ioctl()  { std::cout << "my_ioctl"  << std::endl; return 0; }
-int my_status() { std::cout << "my_status" << std::endl; return 0; }
+
+class Bus
+{
+public:
+    enum Type {
+        Pci, Ohci, I2c, Can, Ahci, Usb, Firewire, Bluetooth
+    };
+    static std::string TypeDescriptor[];
+public:
+    Bus(bool throws_excpt, Type typ) : throws(throws_excpt), type(typ), cancel(false), ready(false) {
+        ident = id++;
+    }
+    Bus(bool throws_excpt, Type typ, const ParameterBlock ilink) : throws(throws_excpt), type(typ), link(ilink), cancel(false), ready(false) {
+        ident = id++;
+        runner = ilink.run;
+    }
+    ~Bus(){}
+    virtual void show() = 0;
+    virtual void start() = 0;
+    virtual void stop() = 0;
+    static void eject(Bus*);
+    std::string what() { return TypeDescriptor[type]; }
+    static int subscribe(Bus*);
+    static void list();
+    static void signal();
+    ParameterBlock link;
+    runnable runner;
+    int ident;
+    bool cancel;
+    bool ready;
+protected:
+    bool throws;
+    void error(const std::string& msg);
+    Type type;
+    static int id;
+private:
+    static std::vector<Bus*> members;
+};
+
+std::vector<Bus*> Bus::members;
+std::string Bus::TypeDescriptor[] = {"Pci", "Ohci", "I2c", "Can", "Ahci", "Usb", "Firewire", "Bluetooth" };
+int Bus::id = 1;
+
+void polymorph(Bus *bus)
+{
+    bus->show();
+}
+
+void broadcast(Bus *bus)
+{
+    Context *ctx = new Context();
+    ctx->bus = bus;
+    bus->link.notify(ctx);
+}
+
+void Bus::error(const std::string& message)
+{
+    if (throws) {
+        throw message.c_str();
+    } else {
+        std::cout << message << std::endl;
+    }
+}
+
+void Bus::eject(Bus *bus)
+{
+    std::cout << "Bus::ejecting(): " << bus->what() << " id: " << bus->ident << std::endl;
+    int i = 0;
+    for (std::vector<Bus*>::const_iterator bit = Bus::members.begin();
+            bit != Bus::members.end(); ++bit, ++i) {
+        if (*bit == bus) {
+            std::cout << "found it: " << i << std::endl;
+            break;
+        }
+        //polymorph(*bit);
+    }
+    members.erase(Bus::members.begin() + i);
+}
+
+int Bus::subscribe(Bus *bus) // plug (opposite of eject)
+{
+    members.push_back(bus);
+    return 0;
+}
+
+void Bus::list()
+{
+    for (std::vector<Bus*>::const_iterator bit = Bus::members.begin();
+            bit != Bus::members.end(); ++bit) {
+        polymorph(*bit);
+    }
+}
+
+void Bus::signal()
+{
+    for (std::vector<Bus*>::const_iterator bit = Bus::members.begin();
+            bit != Bus::members.end(); ++bit) {
+        broadcast(*bit);
+    }
+}
+
+class Class : public Bus
+{
+public:
+    Class(Type typ) : Bus(true, typ) {}
+    Class(Type typ, const ParameterBlock& ilink) : Bus(true, typ, ilink) {}
+    ~Class(){}
+    void show();
+    void start();
+    void stop();
+    std::thread run_thrd;
+};
+
+void Class::start()
+{
+    std::cout << "starting " << what() << std::endl;
+    cancel = false;
+    ready = false;
+    Context *ctx = new Context();
+    ctx->bus = this;
+    this->run_thrd = std::thread(link.run, ctx);
+}
+
+void Class::stop()
+{
+    std::cout << " stopping " << what() << std::endl;
+    cancel = true;
+    this->run_thrd.join();
+}
+
+void Class::show()
+{
+    std::cout << "Class::show() - type: " << what() << std::endl;
+}
+
+void notify(Context *ctx)
+{
+    std::cout << "got notify: " << ctx->bus->what() << std::endl;
+}
+
+void run(Context *ctx)
+{
+    std::cout << "thread started\n";
+    try {
+        while (!bus_ready) {
+            std::this_thread::yield();
+        }
+    } catch (std::exception& ex) { // const cancelled_error&
+        std::cout << "thread cancelled\n";
+    }
+    //std::cout << "cancelled? " << ctx->bus->cancel << std::endl;
+    while (!ctx->bus->cancel) {
+        // std::cout << "got cancal" << std::endl;
+    }
+    std::cout << "exiting " << ctx->bus->what() << " thread.." << std::endl;
+}
 
 // public 
 // serialize devices to backing file
@@ -120,43 +291,4 @@ std::map<int, Device*> DeviceManager::instmap; // list of Devices
 int DeviceManager::count = 0;
 int DeviceManager::UniqueId = 0;
 
-void overload(ParameterBlock *p)
-{
-    p->open   = my_open;
-    p->close  = my_close;
-    p->read   = my_read;
-    p->write  = my_write;
-    p->ioctl  = my_ioctl;
-    p->status = my_status;
-}
 
-void create_devs()
-{
-    ParameterBlock *p;
-    Device *dev;
-
-    p = new ParameterBlock();
-    p->name   = "bfs";
-    overload(p);
-    dev = dm.Create(p);
-
-    p = new ParameterBlock();
-    p->name   = "usb";
-    overload(p);
-    dev = dm.Create(p);
-
-    p = new ParameterBlock();
-    p->name   = "mouse";
-    overload(p);
-    dev = dm.Create(p);
-
-    p = new ParameterBlock();
-    p->name   = "kbd";
-    overload(p);
-    dev = dm.Create(p);
-
-    p = new ParameterBlock();
-    p->name   = "disk";
-    overload(p);
-    dev = dm.Create(p);
-}
--- toys/cpp/core/devcli.cpp
+++ toys/cpp/core/devcli.cpp
@@ -1,14 +1,118 @@
 #include "dev.h"
 
-#define HELP 1
-#define QUIT 2
-#define LIST 3
-
 using namespace std;
+
+int my_open()   { std::cout << "my_open"   << std::endl; return 0; }
+int my_close()  { std::cout << "my_close"  << std::endl; return 0; }
+int my_read()   { std::cout << "my_read"   << std::endl; return 0; }
+int my_write()  { std::cout << "my_write"  << std::endl; return 0; }
+int my_ioctl()  { std::cout << "my_ioctl"  << std::endl; return 0; }
+int my_status() { std::cout << "my_status" << std::endl; return 0; }
+
+void overload(ParameterBlock *p)
+{
+    p->open   = my_open;
+    p->close  = my_close;
+    p->read   = my_read;
+    p->write  = my_write;
+    p->ioctl  = my_ioctl;
+    p->status = my_status;
+}
+
+void create_devs()
+{
+    ParameterBlock *p;
+    Device *dev;
+
+    p = new ParameterBlock();
+    p->name   = "bfs";
+    overload(p);
+    dev = dm.Create(p);
+
+    p = new ParameterBlock();
+    p->name   = "usb";
+    overload(p);
+    dev = dm.Create(p);
+
+    p = new ParameterBlock();
+    p->name   = "mouse";
+    overload(p);
+    dev = dm.Create(p);
+
+    p = new ParameterBlock();
+    p->name   = "kbd";
+    overload(p);
+    dev = dm.Create(p);
+
+    p = new ParameterBlock();
+    p->name   = "disk";
+    overload(p);
+    dev = dm.Create(p);
+}
+
+int dummy()
+{
+    return 0;
+}
+
+void test_classes()
+{
+    try {
+        ParameterBlock link;
+        link.init = dummy;
+        link.open = dummy;
+        link.close = dummy;
+        link.read = dummy;
+        link.write = dummy;
+        link.ioctl = dummy;
+        link.notify = notify;
+        link.run = run;
+
+        Class ahci(Bus::Ahci, link);
+        Class usb(Bus::Usb, link);
+        Class pci(Bus::Pci, link);
+
+        // should be plug
+        Bus::subscribe(&ahci);
+        Bus::subscribe(&usb);
+        Bus::subscribe(&pci);
+
+        Bus::list();
+
+        pci.start();
+        usb.start();
+        ahci.start();
+
+        this_thread::sleep_for (chrono::seconds(3));
+        cout << "setting bus_ready" << endl;
+        bus_ready = true;
+
+        this_thread::sleep_for (chrono::seconds(3));
+
+        pci.stop();
+        usb.stop();
+        ahci.stop();
+
+        Bus::signal();
+        Bus::eject(&pci);
+        Bus::list();
+        Bus::eject(&usb);
+        Bus::list();
+
+    } catch (exception& ex) {
+        cout << ex.what() << endl;
+    }
+}
 
 int main()
 {
+    test_classes();
+    return 0;
+
     map<string, int> menu; // = new map<string, int>();
+#define HELP 1
+#define QUIT 2
+#define LIST 3
     menu["help"] = HELP;
     menu["quit"] = QUIT;
     menu["list"] = LIST;
