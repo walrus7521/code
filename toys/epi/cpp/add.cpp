e0847a8fff52394ca4e2206a73babc10cedd7178
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-07-09 08:06:21 -0700

    add

--- toys/epi/cpp/add.cpp
+++ toys/epi/cpp/add.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,16 +9,23 @@ void test()
 {
     string a = "125";
     string b = "456";
+    string c;
     int carry = 0;
     for (int i = a.length()-1; i >= 0; --i) {
-        cout << a[i] << " + " << b[i] << " = " << endl;
+        //cout << a[i] << " + " << b[i] << " = " << endl;
         int m = a[i] - '0';
         int n = b[i] - '0';
-        int p = m + n + carry;
-        carry = (p > 9) ? 1 : 0;
+        int s = m + n + carry;
+        carry = (s > 9) ? 1 : 0;
+        s %= 10;
         // ok now convert back to string.
-        cout << m << " + " << n << " + " << carry << " = " << p << endl;
+        ostringstream convert;
+        convert << s;
+        c += convert.str();
+        //cout << m << " + " << n << " + " << carry << " = " << s << endl;
     }
+    std::reverse(c.begin(), c.end());
+    cout << a << " + " << b << " = " << c << endl;
 }
 
 int main()
