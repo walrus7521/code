fd70f3bf26710dca3e7078634a12d20d4070e963
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-07-10 16:26:14 -0700

    epi

---
+++ toys/epi/cpp/06.04.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+ * Check if a board game is winnable
+ *
+ * In a particular board game, players have to try to advance
+ * through a sequence of positions. Each position has a nonnegative
+ * integer associated with it, representing the maximum you can 
+ * advance from that position in one move. For example, let
+ * A = <3,3,1,0,2,0,1> represent the board game, ie, the i-th
+ * entry in A is the maximum we can advance from i. Then the
+ * following sequence of advances leads to the last position
+ * <1,3,2>. If A = <3,2,0,0,2,0,1> it is not possible to advance
+ * past position 3.
+ *
+ * Write a function which takes an array of n integers, where 
+ * A[i] denotes the maximum you can advance from index i, and
+ * returns whether it is possible to advance to the last index
+ * starting from the beginning of the array.
+ *
+ */
+
+int main()
+{
+}
+
