5fdf69673cec4540222ac2d3dd3ba00ef6bd1685
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-07-09 21:30:13 -0700

    math

--- toys/epi/cpp/add.cpp
+++ toys/epi/cpp/add.cpp
@@ -29,15 +29,41 @@ string add(string a, string b)
     return c;
 }
 
+int power(int x, int power)
+{
+    int i;
+    if (power == 0) return 1;
+    for (i = 0; i < power-1; ++i) {
+        x *= 10;
+    }
+    return x;
+}
+
+int column(int x)
+{
+    int col = 0;
+    while (x) {
+        x /= 10;
+        col++;
+    }
+    return col-1;
+}
+
 string mult(string a, string b)
 {
-    //string a = "125";
-    //string b = "456";
+    int icol, jcol, col;
+    std::reverse(a.begin(), a.end());
+    std::reverse(b.begin(), b.end());
     string c = "boom";
     int carry = 0;
-    for (int i = a.length()-1; i >= 0; --i) {
-        for (int j = b.length()-1; j >= 0; --j) {
-           cout << a[i] << " * " << b[j] << " = " << endl;
+    for (int i = 0; i < a.length(); ++i) {
+        for (int j = 0; j < b.length(); ++j) {
+            icol = power(10, i);
+            jcol = power(10, j);
+            col = icol * jcol;
+            int cc = column(col);
+            cout << i << " * " << j << " col " << col << " cc " << cc << endl;
+            //cout << a[i] << " * " << b[j] << " = " << endl;
             int m = a[i] - '0';
             int n = b[j] - '0';
             int p = m * n;
@@ -48,7 +74,7 @@ string mult(string a, string b)
             //ostringstream convert;
             //convert << s;
             //c += convert.str();
-            cout << m << " * " << n  << " = " << p << endl;
+            //cout << m << " * " << n  << " = " << p << endl;
         }
     }
     //std::reverse(c.begin(), c.end());
@@ -58,11 +84,13 @@ string mult(string a, string b)
 
 int main()
 {
+
     string a = "123";
     string b = "456";
-    string c  = add(a, b);
-    cout << a << " + " << b << " = " << c << endl;
-    //mult();
+    string c;
+
+    //c  = add(a, b);
+    //cout << a << " + " << b << " = " << c << endl;
 
     c  = mult(a, b);
     cout << a << " * " << b << " = " << c << endl;
