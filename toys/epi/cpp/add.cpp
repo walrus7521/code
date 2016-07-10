8badb67990dd357ba16b103fd0be14c119215e11
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-07-10 13:58:15 -0700

    math

--- toys/epi/cpp/add.cpp
+++ toys/epi/cpp/add.cpp
@@ -51,49 +51,62 @@ int column(int x)
 
 string mult(string a, string b)
 {
-    int icol, jcol, col;
+    int col;
+    ostringstream convert;
+
+    cout << a << " * " << b << endl;
     std::reverse(a.begin(), a.end());
     std::reverse(b.begin(), b.end());
-    string c = "boom";
-    int carry = 0;
+    // initialize string with a.length() + b.length() zeros.
+    string c (a.length() + b.length(), '0');
     for (int i = 0; i < a.length(); ++i) {
         for (int j = 0; j < b.length(); ++j) {
-            icol = power(10, i);
-            jcol = power(10, j);
-            col = icol * jcol;
-            int cc = column(col);
-            cout << i << " * " << j << " col " << col << " cc " << cc << endl;
-            //cout << a[i] << " * " << b[j] << " = " << endl;
+            col = i + j;
+            cout << i << " * " << j << " col " << col << endl;
             int m = a[i] - '0';
             int n = b[j] - '0';
             int p = m * n;
-            //int s = m + n + carry;
-            //carry = (s > 9) ? 1 : 0;
-            //s %= 10;
-            // ok now convert back to string.
-            //ostringstream convert;
-            //convert << s;
-            //c += convert.str();
-            //cout << m << " * " << n  << " = " << p << endl;
+            cout << a[i] << " * " << b[j] << " = " << p << endl;
+            // need to do a RMW of result string 
+            // note: inside these if's need to check for overflow of
+            // each individual digit...oh crap
+            if (p > 9) {
+                // grab 2 digits
+                int q = c[col] - '0';
+                q += p % 10;
+                c[col] = q + '0';
+                int r = c[col+1] - '0';
+                r += p / 10;
+                c[col+1] = r + '0';
+                cout << "ov: " << q << " : " << r << " : " << c << endl;
+            } else {
+                // grab 1 digit
+                int q = c[col] - '0';
+                q += p % 10;
+                c[col] = q + '0';
+                cout << "nr: " << q << " : " << " : " << c << endl;
+            }
         }
     }
-    //std::reverse(c.begin(), c.end());
-    //cout << a << " + " << b << " = " << c << endl;
+    std::reverse(c.begin(), c.end());
+    //cout << a << " + " << b << " = " << result << endl;
     return c;
 }
 
 int main()
 {
 
-    string a = "123";
-    string b = "456";
+    string a = "111";
+    string b = "221";
     string c;
 
     //c  = add(a, b);
     //cout << a << " + " << b << " = " << c << endl;
 
     c  = mult(a, b);
+    string answer = "24531";
     cout << a << " * " << b << " = " << c << endl;
+    cout << "answer = " << answer << endl;
 
 }
 
