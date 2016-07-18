e490b40b0db9feadacd65a9f73bc90f13dabb888
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-07-18 09:09:53 -0700

    epi

---
+++ toys/epi/06.10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/*
+   Given an array A of length n whose entries are integers, compute
+   the largest product that can be made using n-1 entries in A.  You
+   cannot use an entry more than once.  Array entries may be positive
+   negative or zero.  Your algorithm cannot use the division operator,
+   explicitly or implicitly.
+ */
+
+
+int main()
+{
+    // A = <3,2,5,4> = <3,5,4> = 60
+    // A = <3,2,-1,4> = <3,2,4> = 24
+    // A = <3,2,-1,4,-1,6> = <3,-1,4,-1,6> = 72
+}
