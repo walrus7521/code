cb74b916e775e6dd956e5a70cde3bcdc81da564a
Bart Bartel <BartB@helitrak.com>
2016-07-19 13:38:31 -0700

    epi

--- toys/epi/06.11.cpp
+++ toys/epi/06.11.cpp
@@ -19,12 +19,37 @@ using namespace std;
 pair<int, int> LongestSubarray(vector<int> A)
 {
     pair<int, int> ret(0,0);
+    int max_len = 0, cur_len, max_idx;
+    int i, j;
+    for (i = 0; i < A.size(); ++i) {
+        cur_len = 1;
+        for (j = i; j < A.size(); ++j) {
+            if (A[j] < A[j+1]) {
+                cur_len++;
+             } else {
+                 break;
+             }
+        }
+        if (cur_len > max_len) {
+            max_len = cur_len;
+            max_idx = j;
+        }
+    }
+
+    if (max_idx == A.size()) {
+        --max_idx;
+        --max_len;
+    }
+    
+    cout << "len = " << max_len << " idx = " << max_idx << endl;
     return ret;
 }
 
 int main()
 {
-    int array[] = {2,11,3,5,13,7,19,17,23};
+    //int array[] = {2,11,3,5,13,7,19,17,23};
+    //int array[] = {2,3,5,7,19,17,23};
+    int array[] = {12,11,5,4,19,17,23};
     vector<int> A (array, array + sizeof(array)/sizeof(array[0]));    
     pair<int, int> ret = LongestSubarray(A);
 }
