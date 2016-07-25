2256fc79740a1f66cb7401cc428243319b1a7336
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-07-25 08:42:55 -0700

    epi

--- toys/epi/06.15.cpp
+++ toys/epi/06.15.cpp
@@ -7,50 +7,30 @@
 using namespace std;
 
 void show(vector<int>& A) {
+    cout << "<" ;
     for (int i = 0; i < A.size(); ++i) {
-        cout << "A[" << i << "] = " << A[i] << endl;
+        cout << A[i] << ", ";
     }
-}
-
-int ZeroOneRandom()
-{
-    double res = rand() / double(RAND_MAX);
-    return (res < 0.50 ? 0 : 1); 
-}
-
-int UniformRandom(int a, int b)
-{
-    int t = b - a + 1, res;
-    do {
-        res = 0;
-        for (int i = 0; (1 << i) < t; ++i) {
-            res = res * 2 | ZeroOneRandom();
-        }
-    } while (res >= t);
-    return (a + res);
+    cout << ">" << endl;
 }
 
 /*
- * Generate non-uniform random numbers
- *
- * Create an array to select from, and use uniform
- * random numbers to select.
+ * Rotate an array, A of n elements to the right
+ * by i positions.
  *
  */
 
-vector<int> SelectNonUniform(vector<int> A) {
-    vector<int> select;
-    int n = A.size();
-    for (int i = 0; i < n; ++i) {
-        select.push_back(UniformRandom(i, n));
+void RotateArray(vector<int> A, int i) {
+    for (int i = 0; i < A.size(); ++i) {
     }
-    return select;
 }
 
 int main()
 {
-    srand((unsigned)time(NULL));
+    int i = 4;
     vector<int> A = {22,3,5,4,8,1,34,76,23,98};
-    vector<int> R = SelectNonUniform(A);
-    show(R);
+    show(A);
+    RotateArray(A, i);
+    cout << "after rotation " << i << " positions right" << endl;
+    show(A);
 }
