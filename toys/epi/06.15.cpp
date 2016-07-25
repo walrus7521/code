b6c3befb391aa4fd925f0815d1f8d43b01594661
Bart Bartel <BartB@helitrak.com>
2016-07-25 10:21:06 -0700

    epi

--- toys/epi/06.15.cpp
+++ toys/epi/06.15.cpp
@@ -15,34 +15,37 @@ void show(vector<int>& A) {
 }
 
 /*
- * Rotate an array, A of n elements to the right
- * by i positions.
+ * Rotate an array, A of n elements to the right/left
+ * by k positions.
  *
  */
 
 /*
- * To rotate by one, store arr[0] in a temporary variable temp, 
+ * To rotate left by one, store arr[0] in a temporary variable temp, 
  * move arr[1] to arr[0], arr[2] to arr[1] …and finally temp to arr[n-1]
- *
  */
 
 void RotateArrayLeft(vector<int>& A, int k) {
-    int i, j;
+    int i, j, n = A.size();
     for (j = 0; j < k; ++j) {
         int tmp = A[0];
-        for (i = 0; i < A.size(); ++i) {
+        for (i = 0; i < n; ++i) {
             A[i] = A[i+1];
         }
         A[i-1] = tmp;
     }
 }
 
+/*
+ * To rotate left by one, store arr[n-1] in a temporary variable temp, 
+ * move arr[n-2] to arr[n-1], arr[n-3] to arr[n-2] …and finally temp to arr[0]
+ */
 void RotateArrayRight(vector<int>& A, int k) {
-    int i, j;
-    for (j = 0; j < k; ++j) {
-        int tmp = A[A.size()-1];
-        for (i = A.size(); i > 0; --i) {
-            A[i] = A[i+1];
+    int i, j, n = A.size();
+    for (j = 0; j < k; ++j) {   
+        int tmp = A[n-1];
+        for (i = n-1; i > 0; --i) {
+            A[i] = A[i-1];
         }
         A[0] = tmp;
     }
