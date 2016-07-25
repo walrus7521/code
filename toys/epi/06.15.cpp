6e2407415d37c64ce21fa584da58cae3aa7081f5
Bart Bartel <BartB@helitrak.com>
2016-07-25 09:47:50 -0700

    rotate

--- toys/epi/06.15.cpp
+++ toys/epi/06.15.cpp
@@ -20,17 +20,41 @@ void show(vector<int>& A) {
  *
  */
 
-void RotateArray(vector<int> A, int i) {
-    for (int i = 0; i < A.size(); ++i) {
+/*
+ * To rotate by one, store arr[0] in a temporary variable temp, 
+ * move arr[1] to arr[0], arr[2] to arr[1] …and finally temp to arr[n-1]
+ *
+ */
+
+void RotateArrayLeft(vector<int>& A, int k) {
+    int i, j;
+    for (j = 0; j < k; ++j) {
+        int tmp = A[0];
+        for (i = 0; i < A.size(); ++i) {
+            A[i] = A[i+1];
+        }
+        A[i-1] = tmp;
+    }
+}
+
+void RotateArrayRight(vector<int>& A, int k) {
+    int i, j;
+    for (j = 0; j < k; ++j) {
+        int tmp = A[A.size()-1];
+        for (i = A.size(); i > 0; --i) {
+            A[i] = A[i+1];
+        }
+        A[0] = tmp;
     }
 }
 
 int main()
 {
-    int i = 4;
-    vector<int> A = {22,3,5,4,8,1,34,76,23,98};
+    int i = 3;
+    vector<int> A = {2,0,1,3};
     show(A);
-    RotateArray(A, i);
+    //RotateArrayLeft(A, i);
+    RotateArrayRight(A, i);
     cout << "after rotation " << i << " positions right" << endl;
     show(A);
 }
