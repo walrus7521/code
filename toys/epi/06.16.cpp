2e0893e11cfea4c7a6f38b8373a1d13ef3bc4b5f
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-07-25 21:47:31 -0700

    epi

---
+++ toys/epi/06.16.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+ *  Sample offline data
+ *
+ *  Let A be an array whose entries are all distinct. Implement an
+ *  algorithm that takes A and an integer k and returns a subset of
+ *  k elements of A. All subsets should be equally likely. Use as few
+ *  calls to the random number generator (which returns random integers)
+ *  as possible and use O(1) additional storage. You can return the
+ *  result in the same array as input.
+ */
+
+void OfflineSample(vector<int> A, int k)
+{
+}
+
+int main()
+{
+}
