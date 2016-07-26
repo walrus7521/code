fd80528281fa4a8ad9e2fea91f180a25470c1a4b
Bart Bartel <BartB@helitrak.com>
2016-07-26 14:20:28 -0700

    stuff

--- toys/py/readcsv.py
+++ toys/py/readcsv.py
@@ -3,14 +3,23 @@ import numpy as np
 import csv
 import sys
 
+x = []
+y = []
 f = open("wave.csv", 'rt')
 try:
     reader = csv.reader(f)
     headers = reader.next()
-    print("headers: %s" % headers)
+    #print("headers: %s" % headers)
     for row in reader:
-        print row
+        #print row
+        y.append(row[1])
+        x.append(row[0])
+
+    plt.plot(x, y)
+    plt.xlabel('sample(n)')
+    plt.ylabel('voltage(V)')
+    plt.show()
+
 finally:
     f.close()
-
 
