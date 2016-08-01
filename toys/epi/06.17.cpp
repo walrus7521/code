0b5749a0f85f2a5fbc0922b25896fef48ec9ac97
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-08-01 09:14:43 -0700

    epi

--- toys/epi/06.17.cpp
+++ toys/epi/06.17.cpp
@@ -14,25 +14,12 @@ using namespace std;
  *  integers in the set [0,1,...,n-1] with equal probability; use as
  *  few calls to it as possible.
  *
- *  Basically, for each element from left to right, you generate all the 
- *  permutations of the remaining elements. You can do this recursively, 
- *  (or iteratively if you like pain) until you get to the last element 
- *  at which point there is only one possible order.
- *
- *  So, given a list: [1,2,3,4]
- *  You just generate all permutations that start with 1, then all the 
- *  permutations that start with 2, then with 3 and 4.
- * 
- *  This effectively reduces the problem from one of finding permutations 
- *  of a list of four elements to a list of three elements. Once you continue 
- *  reducing to 2 and then 1 element, you have all of them.
  *
  */
 
 void show(vector<int>& A) {
     cout << "<" ;
     for (int i = 0; i < A.size(); ++i) {
-        cout << A[i] << ", ";
     }
     cout << ">" << endl;
 }
@@ -54,11 +41,26 @@ int UniformRandom(int a, int b)
     } while (res >= t);
     return (a + res);
 }
-
+/*
+ *  Basically, for each element from left to right, you generate all the 
+ *  permutations of the remaining elements. You can do this recursively, 
+ *  (or iteratively if you like pain) until you get to the last element 
+ *  at which point there is only one possible order.
+ *
+ *  So, given a list: [1,2,3,4]
+ *  You just generate all permutations that start with 1, then all the 
+ *  permutations that start with 2, then with 3 and 4.
+ * 
+ *  This effectively reduces the problem from one of finding permutations 
+ *  of a list of four elements to a list of three elements. Once you continue 
+ *  reducing to 2 and then 1 element, you have all of them.
+ *
+ */
 void RandomPermutation(vector<int>& A, int k)
 {
-    for (int i = A.size()-1; i >= A.size()-1-k; --i) {
-        int r = UniformRandom(0, i);
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < k; ++i) {
+        int r = UniformRandom(i, A.size()-1);
         int tmp = A[r];
         A[r] = A[i];
         A[i] = tmp;
@@ -68,17 +70,51 @@ void RandomPermutation(vector<int>& A, int k)
 /* create a P vector of 0..k permutations,
  * and randomly select the element
  */
-void CreatePermutationVector(vector<int>& P, int k)
+vector<vector<int>> AllPerms;
+
+void dump_all_perms()
 {
+    int i, j;
+    for (i = 0; i < AllPerms.size(); ++i) {
+        for (j = 0; j < AllPerms[i].size(); ++j) {
+            cout << AllPerms[i][j] << ", ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void register_perm(vector<int>& array)
+{
+    static int perm = 0;
+    int i;
+    for (i = 0; i < array.size(); ++i) {
+        AllPerms[perm].push_back(array[i]);
+    }
+}
+
+void permute(vector<int>& array, int i, int length)
+{
+    if (length == i) {
+        show(array);
+        register_perm(array);
+        return;
+    }
+    int j = i;
+    for (j = i; j < length; j++) { 
+        swap(array[i],array[j]);
+        permute(array,i+1,length);
+        swap(array[i],array[j]);
+    }
+    return;
 }
 
 int main()
 {
-    srand((unsigned)time(NULL));
     vector<int> A = {1,2,3,4,5};
     int k = 2;
-    show(A);
-    OfflineSample(A, k);
-    show(A);
+    //RandomPermutation(A, k);
+    permute(A, 0, A.size());
+    dump_all_perms();
 }
 
