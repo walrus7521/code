2b993ca3a82c5a851188f9efdc267c4999501a71
Bart Bartel <BartB@helitrak.com>
2016-08-02 12:26:19 -0700

    stuff

---
+++ toys/cpp/hack/fib.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int x) {
+    if (x == 0)
+        return 0;
+    if (x == 1)
+        return 1;
+    return fib(x-1)+fib(x-2);
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << fib(n) << endl;
+}
