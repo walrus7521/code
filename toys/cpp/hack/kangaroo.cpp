72b03252a3e41f3900468a0c4236ebef7bd20fbe
Bart Bartel <BartB@helitrak.com>
2016-08-03 11:20:00 -0700

    hack

--- toys/cpp/hack/kangaroo.cpp
+++ toys/cpp/hack/kangaroo.cpp
@@ -3,22 +3,33 @@
 
 using namespace std;
 
-
-int main(){
+int main() {
     int x1;
     int v1;
     int x2;
     int v2;
+
     cin >> x1 >> v1 >> x2 >> v2;
 
-    if ((x2 > x1) && (v2 > v1)) {
-        cout << "NO" << endl;
-        return 0;
-    }
-    if ((x2 > x1) && (v1 > v2)) {
-        // check if multiple
-    }
+    while (1) {
+        if ((x2 > x1) && (v2 > v1) || 
+            (x1 > x2) && (v1 > v2) || 
+            (v1 == v2)) 
+        {
+            cout << "NO" << endl;
+            break;
+        }
 
+        if (x1 == x2) 
+        {
+            cout << "YES" << endl;
+            break;
+        }
+
+        x1 += v1;
+        x2 += v2;
+    }
     return 0;
 }
+
 
