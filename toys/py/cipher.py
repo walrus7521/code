6ee037e477a720a6fc7e9d06b53ceb2b9b763487
Bart Bartel <bartbartel@cindy-gregorys-imac.local>
2016-08-06 15:14:25 -0700

    hack

---
+++ toys/py/cipher.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import base64
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc))
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc)
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+
+str = "bart"
+key = "12345678"
+str1 = encode(key, str)
+print str1
+str2 = decode(key, str1)
+print str2
+
