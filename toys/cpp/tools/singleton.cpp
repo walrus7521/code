d1adda49b07c848a02b456cf0004a4483c1a36ba
Bart Bartel <BartB@helitrak.com>
2016-08-09 15:33:43 -0700

    tools

--- toys/cpp/tools/singleton.cpp
+++ toys/cpp/tools/singleton.cpp
@@ -7,17 +7,8 @@
 #include <cstdio>
  
 /*
- * create a singleton class to implement these methods
+ * create a singleton class to implement methods
  *
-  I M N   Create a new M x N image with all pixels initially colored white (O).
-  C   Clear the table by setting all pixels white (O). The size remains unchanged.
-  L X Y C Colors the pixel (X, Y) in color (C).
-  V X Y1 Y2 C Draw a vertical segment of color (C) in column X, between the rows Y1 and Y2 inclusive.
-  H X1 X2 Y C Draw a horizontal segment of color (C) in the row Y, between the columns X1 and X2 inclusive.
-  K X1 Y1 X2 Y2 C Draw a filled rectangle of color C, where (X1, Y1) is the upper-left and (X2, Y2) the lower right corner.
-  F X Y C Fill the region R with the color C, where R is defined as follows. Pixel (X, Y) belongs to R. Any other pixel which is the same color as pixel (X, Y) and shares a common side with any pixel in R also belongs to this region.
-  S Name  Write the file name in MSDOS 8.3 format followed by the contents of the current image.
-  X   Terminate the session.
 */
 
 using namespace std;
@@ -25,11 +16,22 @@ using namespace std;
 class Singleton
 {
     public:
-        static Singleton& getInstance() {
-            static Singleton instance; // Guaranteed to be destroyed.
+        static Singleton* getInstance() {
+            static Singleton *instance; // Guaranteed to be destroyed.
             // Instantiated on first use.
+            if (!instance) {
+                cout << "new singleton..." << endl;
+                instance = new Singleton;
+            } else {
+                cout << "old singleton..." << endl;
+            }
             return instance;
         }
+
+        void pulse() {
+            cout << "pulsing singleton..." << endl;
+        }
+        
     private:
         Singleton() {}; // Constructor? (the {} brackets) are needed here.
         // C++ 03
@@ -47,7 +49,52 @@ class Singleton
         //void operator=(S const&)  = delete;
 };
 
+class GlobalClass
+{
+    int m_value;
+    static GlobalClass *s_instance;
+    GlobalClass(int v = 0)
+    {
+        m_value = v;
+    }
+  public:
+    int get_value()
+    {
+        return m_value;
+    }
+    void set_value(int v)
+    {
+        m_value = v;
+    }
+    static GlobalClass *instance()
+    {
+        if (!s_instance)
+          s_instance = new GlobalClass;
+        return s_instance;
+    }
+};
+
+GlobalClass *GlobalClass::s_instance = 0;
+Singleton *s = Singleton::getInstance();
+
+void foo(void)
+{
+  GlobalClass::instance()->set_value(1);
+  cout << "foo: global_ptr is " << GlobalClass::instance()->get_value() << '\n';
+}
+
+void bar(void)
+{
+  GlobalClass::instance()->set_value(2);
+  cout << "bar: global_ptr is " << GlobalClass::instance()->get_value() << '\n';
+}
+
 int main() {
+    Singleton *s = Singleton::getInstance();
+    s->pulse();
+    cout << "main: global_ptr is " << GlobalClass::instance()->get_value() << '\n';
+    foo();
+    bar();
     return 0;
 }
 
