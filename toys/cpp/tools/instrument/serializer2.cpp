ba2c6c8b15b81c765e4e903bca3c4e50ff44a3fb
Bart Bartel <BartB@helitrak.com>
2016-08-09 14:27:32 -0700

    parse

--- toys/cpp/tools/instrument/serializer2.cpp
+++ toys/cpp/tools/instrument/serializer2.cpp
@@ -2,14 +2,16 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cstdlib>
 #include "structs.h"
 
 using namespace std;
+
 class Serializer_Alias
 {
 public:
     virtual ~Serializer_Alias(){}
-    virtual void Serialize_Packet(void *packet) = 0;
+    virtual void Serialize_Packet(std::ofstream& os, void *packet) = 0;
 protected:
     Serializer_Alias(){}
 };
@@ -18,10 +20,25 @@ template <class Data_Type> class Serializer : public Serializer_Alias {
 public:
     Serializer(string name) : output_file_name(name) {}
     ~Serializer(){}
-    void Serialize_Packet(void * packet) {
+    void Serialize_Packet(std::ofstream& os, void * packet) {
+        Data_Type *t = (Data_Type *) packet;
+        cout << hex << t->h.tag << " : " << t->b.id << endl;
+        int bytes_to_write = sizeof(packet_type_1);
+        while (bytes_to_write)
+        {
+            auto i = os.tellp();
+            os.write((char *) t, sizeof(packet_type_1));
+            auto j = os.tellp();
+            int bytes_written = j - i;
+            bytes_to_write -= bytes_written;
+            std::cout << "bytes written " << bytes_written << std::endl;
+            this->bytes_written = bytes_written;
+        }
     }
 private:
     string output_file_name;
+    int bytes_written;
+
 };
 
 class Writer {
@@ -31,31 +48,36 @@ public:
     void AddSerializer(int tag, Serializer_Alias *serializer) {
         serializers.push_back(std::make_pair(tag, serializer));
     }
-    void Write() {
+
+    void Write(int tag) {
         output_file_stream.open(output_file_name.c_str(), std::ofstream::out | std::ofstream::binary);
-        // We are starting...
-        std::cout << std::endl << "Serializing packets " << output_file_name << std::endl;
-        int bytes_to_write = sizeof(packet_type_1);
-        packet_type_1 p; // header is common_header
-        p.h.tag = PACKET_TYPE_1;
-        p.b.id = 0x103;
-        while (bytes_to_write)
-        {
-            // this should be done by the serializer
-            auto i = output_file_stream.tellp();
-            output_file_stream.write((char *) &p, sizeof(packet_type_1));
-            auto j = output_file_stream.tellp();
-            int bytes_written = j - i;
-            bytes_to_write -= bytes_written;
-            std::cout << "bytes written " << bytes_written << std::endl;
-            this->bytes_written = bytes_written;
+        for (int i = 0; i < serializers.size(); ++i) {
+            if (serializers[i].first == tag) {
+                for (int j = 0; j < queue.size(); ++j) {
+                    serializers[i].second->Serialize_Packet(output_file_stream, queue[j]);
+                    //packet_type_1 *p = (packet_type_1 *) queue[i];
+                    //cout << hex << ' ' << p->h.tag << endl;
+                }
+            }
         }
         output_file_stream.close();
         output_file_stream.clear();
-      }
+    }
+    void Queue(vector<void *> pv) {
+        queue = pv;
+    }
+
+    void Show() {
+        vector<void *>::iterator it;
+        for (it = queue.begin() ; it != queue.end(); ++it) {
+            packet_type_1 *p = (packet_type_1 *) *it;
+            cout << hex << ' ' << p->h.tag << endl;
+        }
+    }
 
 private:
     std::vector<std::pair<int, Serializer_Alias *> > serializers;    
+    std::vector<void *> queue;
     string output_file_name;
     int bytes_written;
     std::ofstream output_file_stream;
@@ -64,11 +86,26 @@ private:
 
 int main()
 {
-     string filename = "file.dat";
-     Writer writer(filename);
-     Serializer<packet_type_1> *serializer = new Serializer<packet_type_1>(filename);
-     writer.AddSerializer(77, serializer);
-     writer.Write();
-     delete serializer;
+    string filename = "file.dat";
+    Writer writer(filename);
+    Serializer<packet_type_1> *serializer = new Serializer<packet_type_1>(filename);
+    writer.AddSerializer(PACKET_TYPE_1, serializer);
+
+    // generate packets
+    vector<packet_type_1 *> pp;
+    for (int i = 0; i < 4; ++i) {
+        packet_type_1 *p = (packet_type_1 *) malloc(sizeof(packet_type_1)); // header is common_header
+        p->h.tag = PACKET_TYPE_1;
+        p->b.id = i;
+        pp.push_back(p);
+    }
+
+    // queue packets
+    vector<void *> pv(pp.begin(), pp.end());
+    writer.Queue(pv);
+
+    //writer.Show();
+    writer.Write(PACKET_TYPE_1);
+    delete serializer;
 }
 
