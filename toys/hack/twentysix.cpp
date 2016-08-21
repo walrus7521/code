d23e1225c54afac7a3ae6b604708f678a96eae15
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-08-21 15:05:45 -0700

    much

--- toys/hack/twentysix.cpp
+++ toys/hack/twentysix.cpp
@@ -10,31 +10,37 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int Da, Ma, Ya;
     int De, Me, Ye;
-    int cost = 0;
+    int fine = 0;
 
     cin >> Da >> Ma >> Ya;
     cin >> De >> Me >> Ye;
 
-    if (Ya == Ye) {
-        if (Ma == Me) {
-            if (Da == De) {
-                cost = 0;
-            } else
-            if (Da > De) {
-                cost = (Da - De) * 15;
-            } else {
-                cost = 0;
-            }
-        } else
-        if (Ma > Me) {
-        } else {
-        }
-    } else
-    if (Ya > Ye) {
-    } else {
-        cost = 0;
+    if (Ya < Ye) {
+        cout << fine << endl;
+        return 0;
     }
-    cout << cost << endl;
+
+    // on or before
+    if ((Ya <= Ye) &&
+        (Ma <= Me) &&
+        (Da <= De)) {
+        fine = 0;
+        cout << fine << endl;
+        return 0;
+    }
+
+    if (Ya <= Ye) {
+        if (Ma <= Me) {
+            if (Da > De) {
+                fine = (Da - De) * 15;
+            }
+        } else  {
+            fine = 500 * (Ma - Me);
+        }
+    } else {
+        fine = 10000;
+    }
+    cout << fine << endl;
 
     return 0;
 }
