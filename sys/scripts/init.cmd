dd272d39fe61c532a43d2824aec3cf2ed71e34bf
Bart Bartel <BartB@helitrak.com>
2016-08-24 11:48:52 -0700

    init

--- sys/scripts/init.cmd
+++ sys/scripts/init.cmd
@@ -1,4 +1,46 @@
-doskey dev=cd \helitrak
-doskey home=cd \users\bartb
+@echo off
+setlocal enabledelayedexpansion
+
+title=authorized hacker
+
+rem Create vector with names of days
+set i=0
+for %%d in (Sunday Monday Tuesday Wednesday Thrusday Friday Saturday) do (
+   set /A i=i+1
+   set day[!i!]=%%d
+   REM echo %%d
+)
+
+set quotes[0]=authorized hacker
+set quotes[1]=arrest this man
+set quotes[2]=vulnerable
+set quotes[3]=victim
+set quotes[4]=dialup
+set quotes[5]=compromise
+set quotes[6]=aethernet
+set quotes[7]=insecure
+
+set /a num=%random% %%7
+REM echo %num%
+REM echo !quotes[%num%]!
+title !quotes[%num%]!
+
+doskey dev=cd /d %SystemDrive%\helitrak
+doskey fdev=cd /d F:\helitrak
+doskey code=cd /d %SystemDrive%\code
+doskey prj=cd /d %SystemDrive%\helitrak\Projects
+doskey ddk=cd /d %SystemDrive%\\WinDDK\7600.16385.1
+doskey repo=cd /d %SystemDrive%\code
+doskey home=cd /d %USERPROFILE%
+doskey share=cd /d %HOMEDRIVE%%HOMEPATH%
 doskey pur=del *~
-doskey ll=dir
+doskey ll=dir $*
+doskey l=dir $*
+doskey ls=dir $*
+doskey cp=xcopy $*
+doskey mv=move $*
+doskey rm=del $*
+doskey grep=findstr $*
+doskey alias=doskey /macros
+
+@echo on
