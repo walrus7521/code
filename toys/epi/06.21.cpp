068399ed2db29fba9b034c6c263a49021e38a2a7
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-08-29 16:23:36 -0700

    epi

--- toys/epi/06.21.cpp
+++ toys/epi/06.21.cpp
@@ -5,20 +5,6 @@
 
 using namespace std;
 
-int **malloc2d(int r, int c)
-{ 
-    int i;
-    int **t = (int **) malloc(r * sizeof(int *));
-    for (i = 0; i < r; i++) {
-        t[i] = (int *) malloc(c * sizeof(int));
-    }
-    return t;
-}
-
-bool hasDuplicate(vector<int> A) {
-    return true;
-}
-
 void show_sudoku(vector<vector<int> > A) {
     for (int i = 0; i < A.size(); ++i) {
         for (int j = 0; j < A[i].size(); ++j) {
@@ -30,171 +16,154 @@ void show_sudoku(vector<vector<int> > A) {
 }
 
 void init_sudoku(vector<vector<int> >& A) {
-    A.push_back({ 1,1,1, 2,2,2, 3,3,3 });
-    A.push_back({ 1,1,1, 2,2,2, 3,3,3 });
-    A.push_back({ 1,1,1, 2,2,2, 3,3,3 });
+    A.push_back({ 5,3,0, 0,7,0, 0,0,0 });
+    A.push_back({ 6,0,0, 1,9,5, 0,0,0 });
+    A.push_back({ 0,9,8, 0,0,0, 0,6,0 });
 
-    A.push_back({ 4,4,4, 5,5,5, 6,6,6 });
-    A.push_back({ 4,4,4, 5,5,5, 6,6,6 });
-    A.push_back({ 4,4,4, 5,5,5, 6,6,6 });
+    A.push_back({ 8,0,0, 0,6,0, 0,0,3 });
+    A.push_back({ 4,0,0, 8,0,3, 0,0,1 });
+    A.push_back({ 7,0,0, 0,2,0, 0,0,6 });
     
-    A.push_back({ 7,7,7, 8,8,8, 9,9,9 });
-    A.push_back({ 7,7,7, 8,8,8, 9,9,9 });
-    A.push_back({ 7,7,7, 8,8,8, 9,9,9 });
+    A.push_back({ 0,6,0, 0,0,0, 2,8,0 });
+    A.push_back({ 0,0,0, 4,1,9, 0,0,5 });
+    A.push_back({ 0,0,0, 0,8,0, 0,7,9 });
 }
 
-void print_spiral_outer_ring(int **a, int size, int n)
-{
-    int r, c, start=size-n, end=n-1;
-    //printf("outer: size=%d, n=%d, start=%d, end=%d\n", size, n, start, end);
-    if (start == end) {
-        printf("%d ", a[start][end]);
-        return;
-    }
-    for (c = start; c < end; ++c) { // east
-        printf("%02d ", a[start][c]);
-    }
-    for (r = start; r < end; ++r) { // south
-        printf("%02d ", a[r][end]);
-    }
-    for (c = end; c > start; --c) { // west
-        printf("%02d ", a[end][c]);
-    }
-    for (r = end; r > start; --r) { // north
-        printf("%02d ", a[r][start]);
-    }
-}
-
-enum {NORTH, SOUTH, EAST, WEST};
-void print_spiral(int **a, int n) {
-    int dir = EAST, spaces = n;
-    int row=0, col=0, r, c;
-    for (int i = 0; i < n*n; i+=spaces) {
-        switch (dir) {
-        case NORTH:
-        //printf("NORTH: %02d, spaces %d, row %d, col %d\n", i, spaces, row, col);
-            for (r = row; r > row-spaces; --r) {
-                printf("%02d ", a[r][col]);
+bool has_duplicate(vector<int> A) {
+    for (int i = 0; i < A.size(); ++i) {
+        if (A[i] == 0) continue;
+        for (int j = i+1; j < A.size(); ++j) {
+            if (A[i] == A[j]) {
+                cout << "dup" << endl;
+                return true;
             }
-            row -= spaces;
-            row++;
-            col++;
-            dir = EAST;
-            break;
-        case SOUTH:
-        //printf("SOUTH: %02d, spaces %d, row %d, col %d\n", i, spaces, row, col);
-            for (r = row; r < row+spaces; ++r) {
-                printf("%02d ", a[r][col]);
-            }
-            row += spaces;
-            row--;
-            col--;
-            dir = WEST;
-            break;
-        case EAST:
-        //printf(" EAST: %02d, spaces %d, row %d, col %d\n", i, spaces, row, col);
-            for (c = col; c < col+spaces; ++c) {
-                printf("%02d ", a[row][c]);
-            }
-            col += spaces;
-            row++;
-            col--;
-            spaces--;
-            dir = SOUTH;
-            break;
-        case WEST:
-        //printf(" WEST: %02d, spaces %d, row %d, col %d\n", i, spaces, row, col);
-            for (c = col; c > col-spaces; --c) {
-                printf("%02d ", a[row][c]);
-            }
-            col -= spaces;
-            row--;
-            col++;
-            spaces--;
-            dir = NORTH;
-            break;
-        }
-        if (spaces == 0) {
-            break;
         }
     }
-    printf("\n");
+    return false;
 }
 
-void print_square(int **a, int n) {
-    int i, j;
-    for (i = 0; i < n; ++i) {
-        for (j = 0; j < n; ++j) {
-            printf("%02d ", a[i][j]);
+bool has_dup_row(vector<vector<int> > A) {
+    vector<int> vi;
+    for (int r = 0; r < 9; ++r) {
+        vi.clear();
+        for (int c = 0; c < 9; ++c) {
+            vi.push_back(A[r][c]);
         }
-        //printf("\n");
+        if (has_duplicate(vi)) return true;
     }
-    printf("\n");
+    return false;
 }
 
-void fill7x7(int **a) {
-    a[0][0] = 1;
-    a[0][1] = 2;
-    a[0][2] = 3;
-    a[0][3] = 4;
-    a[0][4] = 5;
-    a[0][5] = 6;
-    a[0][6] = 7;
-    a[1][0] = 8;
-    a[1][1] = 9;
-    a[1][2] = 10;
-    a[1][3] = 11;
-    a[1][4] = 12;
-    a[1][5] = 13;
-    a[1][6] = 14;
-    a[2][0] = 15;
-    a[2][1] = 16;
-    a[2][2] = 17;
-    a[2][3] = 18;
-    a[2][4] = 19;
-    a[2][5] = 20;
-    a[2][6] = 21;
-    a[3][0] = 22;
-    a[3][1] = 23;
-    a[3][2] = 24;
-    a[3][3] = 25;
-    a[3][4] = 26;
-    a[3][5] = 27;
-    a[3][6] = 28;
-    a[4][0] = 29;
-    a[4][1] = 30;
-    a[4][2] = 31;
-    a[4][3] = 32;
-    a[4][4] = 33;
-    a[4][5] = 34;
-    a[4][6] = 35;
-    a[5][0] = 36;
-    a[5][1] = 37;
-    a[5][2] = 38;
-    a[5][3] = 39;
-    a[5][4] = 40;
-    a[5][5] = 41;
-    a[5][6] = 42;
-    a[6][0] = 43;
-    a[6][1] = 44;
-    a[6][2] = 45;
-    a[6][3] = 46;
-    a[6][4] = 47;
-    a[6][5] = 48;
-    a[6][6] = 49;
+bool has_dup_col(vector<vector<int> > A) {
+    vector<int> vi;
+    for (int c = 0; c < 9; ++c) {
+        vi.clear();
+        for (int r = 0; r < 9; ++r) {
+            vi.push_back(A[r][c]);
+        }
+        if (has_duplicate(vi)) return true;
+    }
+    return false;
+}
+
+bool has_dup_blk(vector<vector<int> > A) {
+    vector<int> vi;
+    int row_offset = 0, col_offset = 0;
+
+// column 0
+    row_offset = 0, col_offset = 0;
+    for (int i = row_offset; i < row_offset+3; ++i) {
+        for (int j = col_offset; j < col_offset+3; ++j) {
+            vi.push_back(A[i][j]);
+        }
+    }
+    if (has_duplicate(vi)) return true;
+
+    row_offset = 3, col_offset = 0;
+    vi.clear();
+    for (int i = row_offset; i < row_offset+3; ++i) {
+        for (int j = col_offset; j < col_offset+3; ++j) {
+            vi.push_back(A[i][j]);
+        }
+    }
+    if (has_duplicate(vi)) return true;
+ 
+    row_offset = 6, col_offset = 0;
+    vi.clear();
+    for (int i = row_offset; i < row_offset+3; ++i) {
+        for (int j = col_offset; j < col_offset+3; ++j) {
+            vi.push_back(A[i][j]);
+        }
+    }
+    if (has_duplicate(vi)) return true;
+ 
+// column 3
+    row_offset = 0, col_offset = 3;
+    for (int i = row_offset; i < row_offset+3; ++i) {
+        for (int j = col_offset; j < col_offset+3; ++j) {
+            vi.push_back(A[i][j]);
+        }
+    }
+    if (has_duplicate(vi)) return true;
+
+    row_offset = 3, col_offset = 3;
+    vi.clear();
+    for (int i = row_offset; i < row_offset+3; ++i) {
+        for (int j = col_offset; j < col_offset+3; ++j) {
+            vi.push_back(A[i][j]);
+        }
+    }
+    if (has_duplicate(vi)) return true;
+ 
+    row_offset = 6, col_offset = 3;
+    vi.clear();
+    for (int i = row_offset; i < row_offset+3; ++i) {
+        for (int j = col_offset; j < col_offset+3; ++j) {
+            vi.push_back(A[i][j]);
+        }
+    }
+    if (has_duplicate(vi)) return true;
+ 
+// column 6
+    row_offset = 0, col_offset = 6;
+    for (int i = row_offset; i < row_offset+3; ++i) {
+        for (int j = col_offset; j < col_offset+3; ++j) {
+            vi.push_back(A[i][j]);
+        }
+    }
+    if (has_duplicate(vi)) return true;
+
+    row_offset = 3, col_offset = 6;
+    vi.clear();
+    for (int i = row_offset; i < row_offset+3; ++i) {
+        for (int j = col_offset; j < col_offset+3; ++j) {
+            vi.push_back(A[i][j]);
+        }
+    }
+    if (has_duplicate(vi)) return true;
+ 
+    row_offset = 6, col_offset = 6;
+    vi.clear();
+    for (int i = row_offset; i < row_offset+3; ++i) {
+        for (int j = col_offset; j < col_offset+3; ++j) {
+            vi.push_back(A[i][j]);
+        }
+    }
+    if (has_duplicate(vi)) return true;
+ 
+    return false;
 }
 
 int main()
 {
-    int n = 9;
-    int **a = malloc2d(n,n);
     vector<vector<int> > A;
     init_sudoku(A);
     show_sudoku(A);
-    //for (int i = n; i > n/2; --i) {
-    //    print_spiral_outer_ring(a, n, i);
-    //}
-    printf("\n");
+    if ((has_dup_col(A)) || (has_dup_row(A)) || (has_dup_blk(A))) {
+        cout << "bad sudoku" << endl;
+    } else {
+        cout << "valid sudoku" << endl;
+    }
     return 0;
 }
 
