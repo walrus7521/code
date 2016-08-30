b143b63024da4eb52758f3e4cc617beac7dfda2a
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-08-29 20:07:25 -0700

    epi

--- toys/epi/06.25.cpp
+++ toys/epi/06.25.cpp
@@ -15,6 +15,18 @@ void show_board(vector<vector<int> > A) {
     cout << endl;
 }
 
+vector<pair<int, int> > find_rooks(vector<vector<int> > A) {
+    vector<pair<int, int> > ret;
+    for (int r = 0; r < A.size(); ++r) {
+        for (int c = 0; c < A[r].size(); ++c) {
+            if (0 == A[r][c]) {
+                ret.push_back(make_pair(r,c));
+            }
+        }
+    }
+    return ret;
+ }
+
 void init_attack(vector<vector<int> >& A) {
     A.push_back({ 1,0, 1,1, 1,1, 1,1 });
     A.push_back({ 1,1, 1,1, 1,1, 1,1 });
@@ -31,6 +43,10 @@ int main()
     vector<vector<int> > A;
     init_attack(A);
     show_board(A);
+    vector<pair<int, int> > vp = find_rooks(A);
+    for (int i = 0; i < vp.size(); ++i) {
+        cout << vp[i].first << " " << vp[i].second << endl;
+    }
     return 0;
 }
 
