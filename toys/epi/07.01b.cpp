6cc8fdae0d2c065ad1fd5fc8a1f287ec661ec612
Bart Bartel <bartb7@gmail.com>
2016-08-31 08:13:19 -0700

    epi

---
+++ toys/epi/07.01b.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int atoi(string a)
+{
+    return 0;
+}
+
+string itoa(int i)
+{
+    string s = "hello";
+    return s;
+}
+
+int main()
+{
+}
