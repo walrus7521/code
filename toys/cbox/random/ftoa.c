604ae4d6688c4b85693eca816414dcae67d385e5
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-09-02 18:25:09 -0700

    cbox

--- toys/cbox/random/ftoa.c
+++ toys/cbox/random/ftoa.c
@@ -19,24 +19,31 @@ static int ftoa(float f, int significant, char *buf)
         f = -f;
     }
     dp = 0;
+    // force all but single digit left of decimal
+    // and accumulate the number of places shifted
+    printf("chk1: f %f\n", f);
     while ( f >= DECIMAL_MULTIPLE) 
     {
         f = f / DECIMAL_MULTIPLE;
         dp++;
     }
+    printf("chk2: f %f, dp %d\n", f, dp);
     for (ix = 1; ix < significant; ix++)
     {
         num = f;
         f = f - num;
-        if (num > SINGLE_DIGIT_MAX)
+        printf("chk3: f %f, num %d\n", f, num);
+        if (num > SINGLE_DIGIT_MAX) // invalid digit
         {
-            buf[pos++]='#';
+            printf("woops1: dp %d\n", dp);
+            buf[pos++]='#'; // NaN
         }
         else
         {
-            buf[pos++]='0'+num;
+            printf("woops2: dp %d\n", dp);
+            buf[pos++]='0'+num; // number to char conversion
         }
-        if (dp == 0) 
+        if (dp == 0) // finished processing digits to left of decimal place
         {
             buf[pos++]='.';
         }
@@ -53,18 +60,8 @@ int main()
     float f = 22.0 / 7.0; //3.14;
     char buffer[20];
 
-#if 0
-    itoa(a, buffer, 2);   // here 2 means binary
-    printf("Binary value = %s\n", buffer);
- 
-    itoa(a, buffer, 10);   // here 10 means decimal
-    printf("Decimal value = %s\n", buffer);
- 
-    itoa(a, buffer, 16);   // here 16 means Hexadecimal
-    printf("Hexadecimal value = 0x%s\n", buffer);
-#endif
-
     memset(buffer, 0, 20);
+    f = 123.4;
     ftoa(f, 8, buffer);
     printf("Float value = %s\n", buffer);
 
