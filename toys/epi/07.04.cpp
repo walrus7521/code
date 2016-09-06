048ebd89e6c23796201193ffc9cd65b360ec691d
Bart Bartel <bartb7@gmail.com>
2016-09-05 18:20:51 -0700

    epi

--- toys/epi/07.04.cpp
+++ toys/epi/07.04.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 vector<string> split(string s) {
-    int i = 0, j;
+    int i = 0;
     vector<string> vs;
-    while (true) {
-        while (!isalpha(s[i])) {
-            i++;
+    while (i != s.size()) {
+        while (i != s.size() && isspace(s[i])) {
+            ++i;
         }
-        j = i;
-        while (isalpha(s[j])) {
-            j++;
+        int j = i;
+        while (j != s.size() && !isspace(s[j])) {
+            ++j;
         }
-        vs.push_back(s.substr(i, j));
+        if (i != j) {
+            vs.push_back(s.substr(i, j-i));
+            i = j;
+        }
     }
     return vs;
 }
 
 void reverse(string s) {
+    vector<string> vs = split(s);
+    string rev;
+    for (vector<string>::reverse_iterator it = vs.rbegin();
+            it != vs.rend(); ++it) {
+        cout << *it << endl;
+        rev += *it;
+        rev += " ";
+    }
+    cout << "reversed:: " << rev << endl;    
 }
 
 int main()
 {
-    string s = "hello, my name is bart";
-    vector<string> vs = split(s);
-    for (vector<string>::const_iterator it = vs.begin();
-            it != vs.end(); ++it) {
-        cout << *it << endl;
-    }
+    string s = "hello my name is bart\0";
+    reverse(s);
 }
