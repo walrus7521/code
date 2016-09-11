1f689512660dd8f57879a9df71c1d5ddf2cf99ba
Bart Bartel <bartb7@gmail.com>
2016-09-11 12:49:18 -0700

    epi

--- toys/epi/07.05.cpp
+++ toys/epi/07.05.cpp
@@ -1,116 +1,97 @@
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <string>
-#include <map>
+#include <array>
 
 using namespace std;
 
-struct _dial {
-    int size;
-    string letters;
-    int setting;
-};
-
-struct _dial dials[10];
-int current_dial = 0;
-
-void init() {
-
-    dials[0].size = 0;
-    dials[1].size = 0;
-
-    dials[2].size    = 3;
-    dials[2].letters = "abc";
-    dials[2].setting = 0;
-
-    dials[3].size    = 3;
-    dials[3].letters = "def";
-    dials[3].setting = 0;
-
-    dials[4].size    = 3;
-    dials[4].letters = "ghi";
-    dials[4].setting = 0;
-
-    dials[5].size    = 3;
-    dials[5].letters = "jkl";
-    dials[5].setting = 0;
-
-    dials[6].size    = 3;
-    dials[6].letters = "mno";
-    dials[6].setting = 0;
-
-    dials[7].size    = 4;
-    dials[7].letters = "pqrs";
-    dials[7].setting = 0;
-
-    dials[8].size    = 3;
-    dials[8].letters = "tuv";
-    dials[8].setting = 0;
-
-    dials[9].size    = 4;
-    dials[9].letters = "wxyz";
-    dials[9].setting = 0;
-
-    current_dial = 0;
-
+vector<string> letterCombinations(string digits) {
+    vector<string> lettComb;
+    lettComb.push_back("");
+    string dict[] = {"0", "1", "abc", "def", "ghi", 
+                     "jkl", "mno", "pqrs", "tuv", 
+                     "wxyz"};
+    // loop over input digits
+    for(int i = 0; i < digits.size(); ++i) {
+        // current number of digits in lettComb
+        int n = lettComb.size();
+        // get letters corresponding to current digit
+        string letters = dict[digits[i]-'0'];
+        cout << "<" << letters << ">" << endl;
+        // loop over size of lettComb
+        for(int j = 0; j < n; ++j) {
+            // loop over letters of digit
+            for(int k = 1; k < letters.size(); ++k) {
+                string comb = lettComb[j];  //clone lettComb[j]
+                comb.push_back(letters[k]);
+                lettComb.push_back(comb);
+            }
+            lettComb[j].push_back(letters[0]);
+        }
+    }
+    return lettComb;
 }
 
-int step()
-{
-    if (dials[9].setting == dials[9].size) {
-        return 0; // we're done
-    }    
-    printf("step: %c\n", dials[current_dial].letters[dials[current_dial].setting]);
-    if (dials[current_dial].setting == dials[current_dial].size) {
-        dials[current_dial].setting = 0;
+const int kNumTelDigits = 10;
+const array<string, kNumTelDigits> M = {{"0", "1", "abc", 
+                                         "def", "ghi",
+                                         "jkl", "mno", 
+                                         "pqrs", "tuv",
+                                         "wxyz"}};
+
+void PhoneMnemonicHelper(const string& phone_number, int digit,
+                         string* partial_mnemonic,
+                         vector<string>* mnemonics) {
+    if (digit == phone_number.size()) {
+        // all digits are processed so we add partial_mnemonics 
+        // to mnemoics
+        mnemonics->emplace_back(*partial_mnemonic);
     } else {
-        dials[current_dial].setting++;
-    }
-#if 0
-    switch (current_dial) {
-        case 0: 
-        case 1:
-            current_dial++;
-            break;
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-            current_dial++;
-            break;
-        case 9:
-            current_dial = 0;
-            break;
-        default: break;
-    }
-#endif
-    return 1;
-}
-
-void display()
-{
-    //printf("display...\n");
-}
-
-void mnemonic(string num) {
-    for (int i = 0; i < num.size(); ++i) {
-        int v = num[i] - '0';
-        printf("num[%d] = %c => %d => %s\n", i, num[i], v, dials[v].letters.c_str());
-        current_dial = v;
-        while (step()) {
-            display();
+        // try all possible characters for this digit
+        for (const char &c : M[phone_number[digit] - '0']) {
+            (*partial_mnemonic)[digit] = c;
+            PhoneMnemonicHelper(phone_number, digit+1, partial_mnemonic,
+                    mnemonics);
         }
     }
 }
 
+vector<string> PhoneMnemonic(const string& phone_number) {
+    string partial_mnemonic(phone_number.size(), 0);
+    vector<string> mnemonics;
+    PhoneMnemonicHelper(phone_number, 0, &partial_mnemonic, &mnemonics);
+    return mnemonics;
+}
+
+
+vector<string> phone(string digits) {
+    string dial[] = {"0", "1", "abc", "def", "ghi", 
+                     "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> mnems;
+    mnems.push_back(""); // prime the pump
+    for(int i = 0; i < digits.size(); ++i) {
+        int n = mnems.size(); // snapshot of n cuz mnems grows
+        string letters = dial[digits[i]-'0']; // select dial position
+        for (int j = 0; j < n; ++j) { // loop over current mnems
+            for(int k = 1; k < letters.size(); ++k) {
+                string comb = mnems[j];  //clone lettComb[j]
+                comb.push_back(letters[k]);
+                mnems.push_back(comb);
+            }
+            mnems[j].push_back(letters[0]);
+        }
+    }
+    return mnems;
+}
 
 int main()
 {
-    init();
-    mnemonic("4568519");
+    //vector<string> vs = letterCombinations("2276696");
+    //vector<string> vs = PhoneMnemonic("2276696");
+    vector<string> vs = phone("2276696");
+    for (int i = 0; i < vs.size(); ++i) {
+        cout << vs[i] << endl;
+    }
+
 }
+
