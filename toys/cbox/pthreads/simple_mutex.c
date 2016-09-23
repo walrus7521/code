02b50f1739e792abe64055301e0a9dcdae348996
Bart Bartel <BartB@helitrak.com>
2016-09-23 12:31:53 -0700

    pth

--- toys/cbox/pthreads/simple_mutex.c
+++ toys/cbox/pthreads/simple_mutex.c
@@ -16,10 +16,17 @@ int main(int argc, char *argv[])
 {
     pthread_t thread1, thread2;
 
-    r3 = atoi(argv[1]);
+    //if (argc < 2) {
+    //    printf("need input douche!!\n");
+    //    return -1;
+    //}
 
     pthread_create(&thread1, NULL, (void *) do_one_thing, (void *) &r1);
     pthread_create(&thread2, NULL, (void *) do_another_thing, (void *) &r2);
+
+    printf("please input something asshole: ");
+    scanf("%d", &r3);
+    //r3 = atoi(argv[1]);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
@@ -34,10 +41,12 @@ void do_one_thing(int *pnum_times) {
     int i, j, x;
     pthread_mutex_lock(&r3_mutex);
     if (r3 > 0) {
+        printf("1: r3 > 0: %d\n", r3);
         x = r3;
         r3--;
     } else {
-        x = 1;
+        printf("1: r3 <= 0: %d\n", r3);
+        x = 19;
     }
     pthread_mutex_unlock(&r3_mutex);
     for (i = 0; i < 4; i++) {
@@ -51,6 +60,16 @@ void do_one_thing(int *pnum_times) {
 
 void do_another_thing(int *pnum_times) {
     int i, j, x;
+    pthread_mutex_lock(&r3_mutex);
+    if (r3 > 0) {
+        printf("2: r3 > 0: %d\n", r3);
+        x = r3;
+        r3--;
+    } else {
+        printf("1: r3 <= 0: %d\n", r3);
+        x = 42;
+    }
+    pthread_mutex_unlock(&r3_mutex);
     for (i = 0; i < 4; i++) {
         printf("do another thing\n");
         for (j = 0; j < 10000; j++) {
