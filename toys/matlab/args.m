0372578491634bf247b33d13f0bc9bf8b43448b8
Bart Bartel <BartB@helitrak.com>
2016-09-23 16:35:59 -0700

    tools

--- toys/matlab/args.m
+++ toys/matlab/args.m
@@ -1,8 +1,8 @@
 #!/usr/local/bin/octave -qf
 
-printf ("%s", program_name ());
+printf ("dude: %s", program_name ());
 arg_list = argv ();
 for i = 1:nargin
   printf (" %s", arg_list{i});
-  endfor
-  printf ("\n");
+endfor
+printf ("\n");
