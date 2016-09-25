8fc67df01b31a7f5d6c0088a2531d80b8ed786f0
Bart Bartel <bartb7@gmail.com>
2016-09-25 16:29:39 -0700

    list

--- toys/cbox/phd/list.c
+++ toys/cbox/phd/list.c
@@ -81,8 +81,92 @@ void list_show(list *head)
     printf("\n");
 }
 
+/* merge 2 sorted lists 
+   only use nodes in lists, no 
+   allocating new nodes, except
+   for a new head
+ */
+list *merge(list *l, list *f)
+{
+    list dummy;
+    list *m = list_new();
+    //list *m = &dummy;
+    list *t = m;
+    list *p = l->next;
+    list *r = f->next;
+    while (p && r) {
+        if (p->value < r->value) {
+            t->next = p;
+            p = p->next;
+        } else {
+            t->next = r;
+            r = r->next;
+        }
+        t = t->next;
+    }
+    if (p != NULL) {
+        t->next = p;
+    }
+    if (r != NULL) {
+        t->next = r;
+    }
+    return m;
+}
+
+list *merge2(list *l, list *f)
+{
+    list *m = list_new();
+    while (l->next && f->next) {
+        if (l->next->value < f->next->value) {
+            list_push_back(m, l->next->value);
+            l = l->next;
+        } else {
+            list_push_back(m, f->next->value);
+            f = f->next;
+        }
+    }
+    if (l->next != NULL) {
+        while (l->next) {
+            list_push_back(m, l->next->value);
+            l = l->next;
+        }
+    }
+    if (f->next != NULL) {
+        while (f->next) {
+            list_push_back(m, f->next->value);
+            f = f->next;
+        }
+    }
+    return m;
+}
+
+void test_merge()
+{
+    list *l = list_new();
+    list *f = list_new();
+    list_push_back(l, 1);
+    list_push_back(l, 3);
+    list_push_back(l, 5);
+    list_push_back(l, 7);
+    list_push_back(l, 9);
+    list_push_back(l, 21);
+    list_push_back(l, 22);
+    list_push_back(l, 23);
+    list_push_back(f, 2);
+    list_push_back(f, 4);
+    list_push_back(f, 6);
+    list_push_back(f, 8);
+    list_push_back(f, 10);
+    list_show(l);
+    list_show(f);
+    list *m = merge(l, f);
+    list_show(m);
+}
+
 int main()
 {
+    test_merge();
+#if 0
     int i;
     list *l = list_new();
     list *r = list_new();
@@ -98,6 +182,7 @@ int main()
         link *n = list_pop_front(l);
         printf("pop: %d\n", n->value);
     }
+#endif
     return 0;
 }
 
