b2ca2f1ec6d44ce2c0a273b6d49e743441b675ee
Bart Bartel <BartB@helitrak.com>
2016-10-03 11:35:32 -0700

    xml

--- toys/tinyxml/prj/XmlTest/XmlTest.cpp
+++ toys/tinyxml/prj/XmlTest/XmlTest.cpp
@@ -4,10 +4,18 @@
 #include <fstream>
 #include <tuple>
 #include <map>
+#include <vector>
 #include <cstdlib>
+
+#include <tinyxml.h>
+
+// http://www.grinninglizard.com/tinyxmldocs/tutorial0.html
+//
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+    TiXmlDocument doc;
     return 0;
 
 }
