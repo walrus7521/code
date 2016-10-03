e86bc8b9d485742068f75d4ed99bb27ffc56706d
Bart Bartel <BartB@helitrak.com>
2016-10-03 11:17:27 -0700

    xml

---
+++ toys/tinyxml/prj/README.md
@@ -0,0 +1,6 @@
+build with OPENCV_DIR=H:\opencv31\build\x64\vc12
+
+Notes:
+    findContours appears to be broken in opencv31\build\x64\vc14
+
+
---
+++ toys/tinyxml/prj/XmlTest/XmlTest.cpp
@@ -0,0 +1,14 @@
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <tuple>
+#include <map>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+
+}
+
