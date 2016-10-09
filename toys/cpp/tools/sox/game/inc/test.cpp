92618924b6703261ae75e0dac79dc4eefe212bdd
Bart Bartel <bartb7@gmail.com>
2016-10-08 20:04:00 -0700

    game

---
+++ toys/cpp/tools/sox/game/inc/test.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+#ifdef _WIN32
+	#define WIN32_LEAN_AND_MEAN
+	#define NOMINMAX
+
+	#include "Windows.h"
+	#include "WinSock2.h"
+	#include "Ws2tcpip.h"
+	typedef int socklen_t;
+	//typedef char* receiveBufer_t;
+#else
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <sys/types.h>
+	#include <netdb.h>
+	#include <errno.h>
+	#include <fcntl.h>
+	#include <unistd.h>
+	//typedef void* receiveBufer_t;
+	typedef int SOCKET;
+	const int NO_ERROR = 0;
+	const int INVALID_SOCKET = -1;
+	const int WSAECONNRESET = ECONNRESET;
+	const int WSAEWOULDBLOCK = EAGAIN;
+	const int SOCKET_ERROR = -1;
+#endif
+
+
+#include <memory>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <list>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <cassert>
+
+#include "SocketAddress.h"
+//#include "RoboCatPCH.h"
+
+#if _WIN32
+int WINAPI WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow )
+{
+	UNREFERENCED_PARAMETER( hPrevInstance );
+	UNREFERENCED_PARAMETER( lpCmdLine );
+	
+}
+#else
+const char** __argv;
+int __argc;
+int main(int argc, const char** argv)
+{
+	__argc = argc;
+	__argv = argv;
+	
+}
+#endif
+
