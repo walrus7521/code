09074dd672689509f3d27d4db08c6fd025f7a4df
Bart Bartel <BartB@helitrak.com>
2016-10-10 17:08:48 -0700

    sox

--- toys/cbox/sox/chat/tcpserver2.cpp
+++ toys/cbox/sox/chat/tcpserver2.cpp
@@ -7,9 +7,42 @@
 #include "arpa/inet.h"
 #include "netinet/in.h"
 #include "pthread.h"
+#include <string>
+
 #define PORT 4444
 #define BUF_SIZE 2000
 #define CLADDR_LEN 100
+
+
+class sox_api
+{
+public:
+    sox_api(){
+        this->sockfd = ::socket(AF_INET, SOCK_STREAM, 0);
+        if (this->sockfd < 0) {
+            printf("Error creating socket!\n");
+            exit(1);
+        }
+        printf("Socket created...\n");    
+    }
+    ~sox_api(){
+        ::close(this->sockfd);
+    }
+    int connect() { return 0; }
+    int bind(){ return 0; }
+    int listen(){ return 0; }
+    int recv(){ return 0; }
+    int send(){ return 0; }
+    int accept(){ return 0; }
+    char buffer[BUF_SIZE]; 
+    unsigned long sockfd;
+    pthread_t rThread;
+    struct sockaddr_in addr, cl_addr;  
+//private:
+    std::string srv_addr;
+};
+
+
 // gcc tcpserver2.c -o server2 -pthread 
 // ./server 
 struct chat_t {
@@ -48,25 +81,20 @@ void * sendMessage(void * chat) {
     }   
 }
 
-int main() {
-    struct sockaddr_in addr, cl_addr;
-    unsigned long sockfd, ret, newsockfd;
+int test() {
+
+    sox_api srv;
+
+    unsigned long ret, newsockfd;
     socklen_t len;
     char buffer[BUF_SIZE];
-    pid_t childpid;
     char clientAddr[CLADDR_LEN];
     pthread_t rThread, sThread;
-    sockfd = ::socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        printf("Error creating socket!\n");
-        exit(1);
-    }
-    printf("Socket created...\n");
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = PORT; 
-    ret = ::bind(sockfd, (struct sockaddr *) &addr, sizeof(addr));
+    memset(&srv.addr, 0, sizeof(srv.addr));
+    srv.addr.sin_family = AF_INET;
+    srv.addr.sin_addr.s_addr = INADDR_ANY;
+    srv.addr.sin_port = PORT; 
+    ret = ::bind(srv.sockfd, (struct sockaddr *) &srv.addr, sizeof(srv.addr));
     if (ret < 0) {
         printf("Error binding!\n");
         exit(1);
@@ -74,15 +102,16 @@ int main() {
     printf("Binding done...\n");
 listener:
     printf("Waiting for a connection...\n");
-    listen(sockfd, 5);
-    len = sizeof(cl_addr);
-    newsockfd = ::accept(sockfd, (struct sockaddr *) &cl_addr, &len);
+    listen(srv.sockfd, 5);
+    len = sizeof(srv.cl_addr);
+    newsockfd = ::accept(srv.sockfd, (struct sockaddr *) &srv.cl_addr, &len);
     if (newsockfd < 0) {
         printf("Error accepting connection!\n");
         exit(1);
     } 
-    inet_ntop(AF_INET, &(cl_addr.sin_addr), clientAddr, CLADDR_LEN);
+    inet_ntop(AF_INET, &(srv.cl_addr.sin_addr), clientAddr, CLADDR_LEN);
     printf("Connection accepted from %s...\n", clientAddr); 
+
     memset(buffer, 0, BUF_SIZE);
     printf("Enter your messages one by one and press return key!\n");
     //creating a new thread for receiving messages from the client
@@ -93,7 +122,7 @@ listener:
     }
     struct chat_t *ch = (struct chat_t *) malloc(sizeof(struct chat_t));
     ch->socket = (void *) newsockfd;
-    ch->cl_addr = cl_addr;
+    ch->cl_addr = srv.cl_addr;
     //creating a new thread for sending messages to the client
     ret = pthread_create(&sThread, NULL, sendMessage, (void *) ch);
     if (ret) {
@@ -103,7 +132,11 @@ listener:
     goto listener;
     while (1) ;
     close(newsockfd);
-    close(sockfd);
     pthread_exit(NULL);
     return 0;
+}
+
+int main()
+{
+    test();
 }
