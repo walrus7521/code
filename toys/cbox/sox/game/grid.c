bf3c58ef3134c99ab560ae5200e966f94300ea9a
Bart Bartel <BartB@helitrak.com>
2016-10-12 14:36:09 -0700

    game

--- toys/cbox/sox/game/grid.c
+++ toys/cbox/sox/game/grid.c
@@ -2,8 +2,11 @@
 
 #define ROWS    (30)
 #define COLS    (40)
-#define VSPACE   (3)
+#define VSPACE   (4)
 #define HSPACE   (2)
+
+char points[ROWS][COLS] = {0};
+char ships[ROWS][COLS] = {0};
 
 int draw_grid()
 {
@@ -11,14 +14,14 @@ int draw_grid()
     for (r = 0; r < ROWS; r++) {
         if (r % HSPACE == 0) {
             for (c = 0; c < COLS; c++) {
-                //if (c % VSPACE == 1) {
-                //    printf("|");
-                //}
                 printf("-");
             }
         } else {
             for (c = 0; c < COLS; c++) {
-                if (c % VSPACE == 1) {
+                if (points[r][c]) {
+                    printf("%c", points[r][c]);
+                }
+                else if (c % VSPACE == 1) {
                     printf("|");
                 } else {
                     printf(" ");
@@ -30,7 +33,29 @@ int draw_grid()
     printf("\n");
 }
 
+int place_ships()
+{
+    int r, c;
+    for (r = 0; r < ROWS; r++) {
+        for (c = 0; c < COLS; c++) {
+            points[r][c] = ships[r][c];
+        }
+    }
+}
+
+int boom()
+{
+    printf("%c", 0x7);
+}
+
 int main()
 {
+    ships[3][3] = '#';
+    ships[3][7] = '#';
+    ships[3][11] = '#';
+    place_ships();
+    points[5][3] = 'o';
+    points[5][7] = 'x';
     draw_grid();
+    boom();
 }
