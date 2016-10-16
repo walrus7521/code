b65e00305fd493cee83bb6522cfcab2ecb636a3d
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-10-16 14:17:22 -0700

    moo

--- toys/cpp/moo/practice/Sales_data.cpp
+++ toys/cpp/moo/practice/Sales_data.cpp
@@ -2,35 +2,15 @@
 #include <string>
 #include "Sales_data.h"
 
+
 using namespace std;
 
 int main()
 {
-    Sales_data data1, data2;
+    Sales_data data1(cin), data2(cin);
+    print(cout, data1);
+    cout << endl;
 
-    double price = 0.0;
-
-    cin >> data1.bookNo >> data1.units_sold >> price;
-    data1.revenue = data1.units_sold * price;
-
-    cin >> data2.bookNo >> data2.units_sold >> price;
-    data2.revenue = data2.units_sold * price;
-
-    if (data1.bookNo == data2.bookNo) {
-        unsigned totalCnt = data1.units_sold + data2.units_sold;
-        double totalRevenue = data1.revenue + data2.revenue;
-
-        cout << data1.bookNo << " " << totalCnt
-                  << "  " << totalRevenue << " ";
-        if (totalCnt != 0) {
-            cout << totalRevenue/totalCnt << endl;
-        } else {
-            cout << "(no sales)" << endl;
-        }
-    } else {
-        cerr << "Data must refer to the same ISBN" << endl;
-        return -1;
-    }
     return 0;
    
 }
