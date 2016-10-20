15a2fe84ad0ed0b134e38156b27aadf0dcba734e
Bart Bartel <bartb7@gmail.com>
2016-10-20 07:31:38 -0700

    moo

--- toys/cpp/moo/practice/Sales_data.cpp
+++ toys/cpp/moo/practice/Sales_data.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "Sales_data.h"
 
@@ -8,9 +9,9 @@ using namespace std;
 int main()
 {
     string null_book = "9-999-99999-9";
+    ifstream in("data");
 
-
-    Sales_data data1(cin), data2;
+    Sales_data data1(in), data2;
     print(cout, data1);
     cout << endl;
 
@@ -18,7 +19,7 @@ int main()
     //data2.combine("9-999-99999-9"); // illegal double conversion
     ///data2.combine(string("9-999-99999-9")); // okay - nope explicit
     //data2.combine(Sales_data("9-999-99999-9")); // okay - nope explicit
-    //data2.combine(cin); // okay - nope explicit
+    //data2.combine(in); // okay - nope explicit
 
     print(cout, data2);
     cout << endl;
