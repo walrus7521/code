abe455c01dbe3e8bb17faacc00e5f9ce29d77788
Bart Bartel <BartB@helitrak.com>
2016-10-21 09:26:19 -0700

    csv

--- toys/cpp/tools/csv.cpp
+++ toys/cpp/tools/csv.cpp
@@ -15,66 +15,59 @@ struct data_format {
     double value;
 };
 
-// get rid of comma
-data_format formatter(string line)
+data_format deserialize(string line)
 {
     std::stringstream ss(line); // create a stream of string
-    string name, number, value;
+    string item;
     data_format data;
-    ss >> name >> number >> value;
-    data.name   = name;
-    data.number = atoi(number.c_str());
-    data.value  = atof(value.c_str());
+
+    // need to use getline to delete the comma
+    std::getline(ss, item, ','); data.name   = item;
+    std::getline(ss, item, ','); data.number = atoi(item.c_str());
+    std::getline(ss, item, ','); data.value  = atof(item.c_str());
+
     return data;
+}
+
+string serialize(data_format data)
+{
+    std::ostringstream ss;
+    std::ostringstream name, number, value;
+
+    name   << data.name;
+    number << data.number;
+    value  << data.value;
+    ss << name.str() << ", " << number.str() << ", " << value.str();
+    std::string line = ss.str();
+
+    return line;
 }
 
 vector<data_format> csv_in(string file)
 {
-    std::ifstream in(file);
+    std::ifstream csv_in(file);
     vector<data_format> csv_data;
     string line;
-    while(getline(in, line)) {
-        data_format data = formatter(line);
+    while(getline(csv_in, line)) {
+        data_format data = deserialize(line);
         csv_data.push_back(data);
     }
     return csv_data;
 }
 
-vector<data_format> csv_in2()
+void csv_out(const vector<data_format>& data, string file)
 {
-    std::ifstream readFile("testin.csv");
-    vector<string> csv_line;
-    vector<data_format> csv_data;
-    string line;
-
-    while(getline(readFile,line))   {
-        std::stringstream ss(line); // create a stream of string
-        csv_line.clear(); // clear out the csv line
-        std::string item; // individual items in the line
-        while(std::getline(ss, item, ',')) // split string into items
-        {
-            csv_line.push_back(item);
-            cout << item << endl;
-        }
-        data_format data;
-        data.name   = csv_line[0];
-        data.number = atoi(csv_line[1].c_str());
-        data.value  = atof(csv_line[2].c_str());
-        csv_data.push_back(data);
-    }
-    return csv_data;
-}
-
-void csv_out(const vector<string>& vs)
-{
-    std::ofstream writeFile("testout.csv");
-    for (auto s : vs) {
-        writeFile << s << std::endl;
+    std::ofstream csv_out(file);
+    for (auto d : data) {
+        string s = serialize(d);
+        cout << s << endl;
+        //csv_out << s << std::endl;
     }
 }
 
 void dummy(string& s, int& i, double& d)
 {
+    std::cout << s << ", " << i << ", " << d << std::endl;
     s += ".sup";
     i += 42;
     d *= 2.0;
@@ -82,29 +75,13 @@ void dummy(string& s, int& i, double& d)
 
 void test_csv()
 {
-    vector<data_format> vs = csv_in("testin.csv");
-    for (auto s : vs) {
-        std::cout << s.name << std::endl;
-        std::cout << s.number << std::endl;
-        std::cout << s.value << std::endl;
+    vector<data_format> data_in = csv_in("testin.csv");
+    vector<data_format> data_out;
+    for (auto d : data_in) {
+        dummy(d.name, d.number, d.value);
+        data_out.push_back(d); // modified by dummy() function
     }
-#if 0
-    string s = vs[0];
-    int    i = atoi(vs[1].c_str());
-    char* pEnd;
-    double d = atof(vs[2].c_str());
-    std::cout << "str: " << s << ", i: " << i << ", d: " << d << std::endl;
-    dummy(s, i, d);
-    std::cout << "str: " << s << ", i: " << i << ", d: " << d << std::endl;
-
-    std::ostringstream stringStream;
-    stringStream << s << ", " << i << ", " << d;
-    std::string copyOfStr = stringStream.str();
-
-    vector<string> vs_out;
-    vs_out.push_back(copyOfStr);
-    csv_out(vs_out);
-#endif
+    csv_out(data_out, "testout.csv");
 }
 
 int main()
