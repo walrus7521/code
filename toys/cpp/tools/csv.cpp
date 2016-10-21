ded841e8be000c2b1f546e5f0baa1c82bb9b2a4b
Bart Bartel <BartB@helitrak.com>
2016-10-21 08:47:46 -0700

    csv

--- toys/cpp/tools/csv.cpp
+++ toys/cpp/tools/csv.cpp
@@ -18,6 +18,24 @@ struct data_format {
 vector<data_format> csv_in()
 {
     std::ifstream readFile("testin.csv");
+    vector<data_format> csv_data;
+    string line;
+    while(getline(readFile, line)) {
+        //cout << line.length() << endl;
+        std::stringstream ss(line); // create a stream of string
+        data_format data;
+        ss >> data.name >> data.number >> data.value;
+        std::cout << data.name   << std::endl;
+        std::cout << data.number << std::endl;
+        std::cout << data.value  << std::endl;
+        csv_data.push_back(data);
+    }
+    return csv_data;
+}
+
+vector<data_format> csv_in2()
+{
+    std::ifstream readFile("testin.csv");
     vector<string> csv_line;
     vector<data_format> csv_data;
     string line;
@@ -36,8 +54,6 @@ vector<data_format> csv_in()
         data.number = atoi(csv_line[1].c_str());
         data.value  = atof(csv_line[2].c_str());
         csv_data.push_back(data);
-        //dummy(s, i, d);
-        //std::cout << "str: " << s << ", i: " << i << ", d: " << d << std::endl;
     }
     return csv_data;
 }
@@ -59,10 +75,12 @@ void dummy(string& s, int& i, double& d)
 
 void test_csv()
 {
-    vector<data_format> vs = csv_in();
+    vector<data_format> vs = csv_in2();
 #if 0
     for (auto s : vs) {
-        std::cout << s << std::endl;
+        std::cout << s.name << std::endl;
+        std::cout << s.number << std::endl;
+        std::cout << s.value << std::endl;
     }
     string s = vs[0];
     int    i = atoi(vs[1].c_str());
