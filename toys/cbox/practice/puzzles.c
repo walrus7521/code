069c0960562fda396c41c691eca18d7e5d7c5fe0
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-10-30 17:07:05 -0700

    cbox

--- toys/cbox/practice/puzzles.c
+++ toys/cbox/practice/puzzles.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+// anagram
+int is_anagram(char *s1, char *s2)
+{
+    return 0;
+}
+
+// palindrome
+int is_palindrome(char *s)
+{
+    return 0;
+}
+
 int main()
 {
 }
