61ee34fd9986705344369686d38a1ead2eb245c6
Bart Bartel <BartB@helitrak.com>
2016-11-03 16:56:12 -0700

    cbox

--- toys/cbox/random/typedef.h
+++ toys/cbox/random/typedef.h
@@ -1,6 +1,18 @@
 #ifndef _TYPEDEF_H_
 #define _TYPEDEF_H_
 
+/*
+ *  Data ranges:
+ *
+ *  u8  :       0  -  255
+ *  s8  :    -128  -  127
+ *  u16 :       0  -  65535
+ *  s16 :  -32768  -  32767
+ *  u32 :       0  -  4.294967295e9
+ *  s32 : -2.147483648e9  -  2.147483647e9
+ *
+*/
+
 typedef unsigned char  u8;
 typedef signed char    s8;
 typedef unsigned short u16;
