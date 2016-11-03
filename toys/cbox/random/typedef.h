0c19c64d7946023c38d1de58c5a491ad241b8183
Bart Bartel <BartB@helitrak.com>
2016-11-03 15:34:46 -0700

    cbos

---
+++ toys/cbox/random/typedef.h
@@ -0,0 +1,11 @@
+#ifndef _TYPEDEF_H_
+#define _TYPEDEF_H_
+
+typedef unsigned char  u8;
+typedef signed char    s8;
+typedef unsigned short u16;
+typedef signed short   s16;
+typedef unsigned long  u32;
+typedef signed long    s32;
+
+#endif // _TYPEDEF_H_
