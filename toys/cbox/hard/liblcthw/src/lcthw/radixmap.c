2b807ab8ac23572fc91351a52aa9d353a292dd1b
Bart Bartel <bartb7@gmail.com>
2016-11-13 18:16:04 -0800

    hard

--- toys/cbox/hard/liblcthw/src/lcthw/radixmap.c
+++ toys/cbox/hard/liblcthw/src/lcthw/radixmap.c
@@ -9,12 +9,30 @@ RadixMap *RadixMap_create(size_t max)
     RadixMap *map = calloc(sizeof(RadixMap), 1);
     check_mem(map);
 
+    map->contents = calloc(sizeof(RMElement), max + 1);
+    check_mem(map->contents);
 
+    map->temp = calloc(sizeof(RMElement), max + 1);
+    check_mem(map->temp);
+
+    map->max = max;
+    map->end = 0;
+
+    return map;
+error:
+    return NULL;
 }
 
 void RadixMap_destroy(RadixMap *map)
 {
+    if (map) {
+        free(map->contents);
+        free(map->temp);
+        free(map);
+    }
 }
+
+// here is where we stopped
 
 void RadixMap_sort(RadixMap *map)
 {
