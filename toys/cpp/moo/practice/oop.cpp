828f8714554db2c4ffc0c50810e2c06ba2a3e2f2
Bart Bartel <bartb7@gmail.com>
2016-11-24 12:02:09 -0800

    moo

--- toys/cpp/moo/practice/oop.cpp
+++ toys/cpp/moo/practice/oop.cpp
@@ -18,21 +18,36 @@ public:
         bookNo(book), price(sales_price) {}
     std::string isbn() const { return bookNo; } // same for all classes
     virtual double net_price(std::size_t n) const { // specific to each class, dynamic binding
+        std::cout << "Quote::net_price()" << std::endl;
         return n * price;
     }
     virtual ~Quote() = default; // dynamic binding for destructor
+    static int num_instances;
 private:
     std::string bookNo; // ISBN
 protected:
     double price = 0.0; // normal, undiscounted price
 };
 
+int Quote::num_instances = 0;
+
 // represents books with quantity discount
 class Bulk_quote : public Quote {
 public:
     Bulk_quote() = default;
-    Bulk_quote(const std::string&, double, std::size_t, double);
-    double net_price(std::size_t n) const override;
+    // initializing base class using it's constructor
+    Bulk_quote(const std::string& book, double price, std::size_t qty, double disc) :
+        Quote(book, price), min_qty(qty), discount(disc) {
+            Quote::num_instances++;
+        }
+    double net_price(std::size_t cnt) const override {
+        std::cout << "Bulk_quote::net_price()" << std::endl;
+        if (cnt >= min_qty) {
+            return cnt * (1 - discount) * price;
+        } else {
+            return cnt * price;
+        }
+    }
 private:
     std::size_t min_qty = 0;
     double discount = 0.0;
@@ -56,7 +71,8 @@ double print_total(ostream &os, const Quote &item, size_t n)
 int main()
 {
     Quote q;
-    Bulk_quote b;
+    Bulk_quote b("123-456-78", 39.95, 4, 0.2);
     print_total(cout, q, 1);
-    print_total(cout, b, 1);
+    print_total(cout, b, 4);
+    cout << "num quotes: " << Quote::num_instances << endl;
 }
