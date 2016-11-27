e8893199720d5679acb6cdbfe419c222c1ad3ebd
Bart Bartel <bartb7@gmail.com>
2016-11-26 16:39:37 -0800

    moo

--- toys/cpp/moo/query.cpp
+++ toys/cpp/moo/query.cpp
@@ -98,26 +98,83 @@ private:
 
 
 class WordQuery : public Query_base {
-};
-
-class NotQuery : public Query_base {
-};
-
-class BinaryQuery : public Query_base {
-};
-
-class AndQuery : public BinaryQuery {
-};
-
-class OrQuery : public Query_base {
+    friend class Query;
+    WordQuery(const string &s) : query_word(s){}
+    // concrete class WordQuery defines all inherited pure virtuals
+    QueryResult eval(const TextQuery &t) const
+        { return t.query(query_word); }
+    string rep() const { return query_word; }
+    string query_word;
 };
 
 // interface class
 class Query {
-
+    friend Query operator~(const Query&);
+    friend Query operator|(const Query&, const Query&);
+    friend Query operator&(const Query&, const Query&);
+public:
+    Query(const string &s) : q(new WordQuery(s)) {}
+    QueryResult eval(const TextQuery &t) const
+    { return q->eval(t); }
+    string rep() const { return q->rep(); }
 private:
-    Query_base *qb;
+    Query(shared_ptr<Query_base> query) : q(query) {}
+    shared_ptr<Query_base> q;
 };
+
+
+class NotQuery : public Query_base {
+    friend Query operator~(const Query &);
+    NotQuery(const Query &q): query(q){}
+    string rep() const { return "~(" + query.rep() + ")"; }
+    QueryResult eval(const TextQuery&) const;
+    Query query;
+};
+inline Query operator~(const Query &operand)
+{
+    return shared_ptr<Query_base>(new NotQuery(operand));
+}
+
+// abstract base class
+class BinaryQuery : public Query_base {
+protected:
+    BinaryQuery(const Query &l, const Query &r, string s) :
+        lhs(l), rhs(r), opSym(s) {}
+    string rep() const { return "(" + lhs.rep() + " "
+                                    + opSym + " "
+                                    + rhs.rep() + ")"; }
+    Query lhs, rhs;
+    string opSym;
+};
+
+class AndQuery : public BinaryQuery {
+    friend Query operator&(const Query&, const Query&);
+    AndQuery(const Query &left, const Query &right) :
+        BinaryQuery(left, right, "&") {}
+    QueryResult eval(const TextQuery&) const;
+};
+inline Query operator&(const Query &lhs, const Query &rhs)
+{
+    return shared_ptr<Query_base>(new AndQuery(lhs, rhs));
+}
+
+class OrQuery : public BinaryQuery {
+    friend Query operator|(const Query&, const Query&);
+    OrQuery(const Query &left, const Query &right) :
+        BinaryQuery(left, right, "|") {}
+    QueryResult eval(const TextQuery&) const;
+};
+inline Query operator|(const Query &lhs, const Query &rhs)
+{
+    return shared_ptr<Query_base>(new OrQuery(lhs, rhs));
+}
+
+ostream& operator<<(ostream &os, const Query &query)
+{
+    // Query::rep makes a virtual call through its Query_base
+    // pointer to rep()
+    return os << query.rep();
+}
 
 int main()
 {
