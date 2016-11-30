d60458cbec881ac59c58997d3cd4c02e344ab642
Bart Bartel <bartb7@gmail.com>
2016-11-30 08:10:18 -0800

    meta

--- toys/cpp/templates/meta.cpp
+++ toys/cpp/templates/meta.cpp
@@ -27,12 +27,61 @@ template <typename T> struct Head;
 // http://tismith.id.au/2014/template-metaprogramming-fun.html
 
 template< int a, int b > struct GCD {
- static const int RESULT = GCD< b, a % b >::RESULT;
+    static const int RESULT = GCD< b, a % b >::RESULT;
+};
+template< int a > struct GCD< a, 0 > {
+    static const int RESULT = a;
 };
 
-template< int a > struct GCD< a, 0 > {
- static const int RESULT = a;
+template< int i > struct FIB {
+    static const int RESULT = FIB< i - 1 >::RESULT + FIB< i - 2 >::RESULT;
 };
+template< > struct FIB< 1 > {
+    static const int RESULT = 1;
+};
+template< > struct FIB< 2 > {
+    static const int RESULT = 1;
+};
+
+//create the conditional -- may belong .hpp file
+template< bool CONDITION, class THEN, class ELSE > struct IF {};
+template<class THEN, class ELSE> struct IF< false, THEN, ELSE > {
+    typedef ELSE TEST;
+};
+template<class THEN, class ELSE> struct IF< true, THEN, ELSE > {
+    typedef THEN TEST;
+};
+// use the conditional
+struct A {
+    static const int RESULT = 1;
+};
+struct B {
+    static const int RESULT = 0;
+};
+struct A1 {
+    static inline void EXEC(void) {
+        std::cout << "TRUE!";
+    }
+};
+struct B1 {
+    static inline void EXEC(void) {
+        std::cout << "FALSE!";
+    }
+};
+
+void test_conditional()
+{
+    std::cout << "true = " << IF<true, A, B>::TEST::RESULT << std::endl;
+    std::cout << "true = " << IF<false, A, B>::TEST::RESULT << std::endl;
+ 
+    std::cout << "true = ";
+    IF<true, A1, B1>::TEST::EXEC();
+    std::cout << std::endl;
+
+    std::cout << "false = ";
+    IF<false, A1, B1>::TEST::EXEC();
+    std::cout << std::endl;
+}
 
 
 int main()
@@ -40,7 +89,9 @@ int main()
     std::cout << Head<IntList<7,IntList<8, NullType>>>::value << std::endl;
 
     std::cout << "GCD (25,50) == " << GCD<25, 50>::RESULT << std::endl;
+    std::cout << "Fib 11: " << FIB<11>::RESULT << std::endl;
 
+    test_conditional();
 
     return 0;
 }
