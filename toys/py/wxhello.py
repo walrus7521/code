7b3b61053d9b58875a51345b8a584587a5ba84bd
Bart Bartel <bartb7@gmail.com>
2016-12-14 01:37:27 -0800

    py

--- toys/py/wxhello.py
+++ toys/py/wxhello.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import wx
  
+# https://wiki.wxpython.org/Getting%20Started
+
 app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
 frame = wx.Frame(None, wx.ID_ANY, "dude, wusup") # A Frame is a top-level window.
 frame.Show(True)     # Show the frame.
