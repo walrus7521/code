c31db43da1c73768bdc1e3acb748f42e7b869128
Bart Bartel <bartb7@gmail.com>
2016-12-19 08:13:51 -0800

    algos

---
+++ toys/algos/test_poly.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Geometry.hpp"
+
+
+int main()
+{
+    Point points[] = {{0, 3}, {2, 2}, {1, 1}, {2, 1},
+                      {3, 0}, {0, 0}, {3, 3}};
+    int n = sizeof(points)/sizeof(points[0]);
+    //convexHull(points, n);
+     
+    Polygon p(points, n);
+    p.showPolygon();
+}
