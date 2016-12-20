028779badc4534bf8a09c1d453e7f08871cebe0f
Bart Bartel <BartB@helitrak.com>
2016-12-19 17:08:42 -0800

    hash

--- toys/cpp/algos/Hash.hpp
+++ toys/cpp/algos/Hash.hpp
@@ -1,33 +1,50 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <utility>
 
 using namespace std;
 
 class Hash {
 public:
     Hash(int nB) : B(nB) {
-        table = new list<int>[B];
+        table = new list<pair<string, int>>[B];
     }
-    void addHash(int v) {
-        int b = hash(v);
-        table[b].push_back(v);
+    virtual ~Hash(){}
+    void insert(string k, int v) {
+        int b = hash2(k);
+        pair<string, int> p = std::make_pair(k, v);
+        table[b].push_back(p);
     }
-    bool findHash(int v) {
-        int b = hash(v);
-        list<int>::iterator j; // used for bucket list entries
+    bool lookup(string k, int v) {
+        int b = hash2(k);
+        list<pair<string, int>>::iterator j; // used for bucket list entries
         for(j = table[b].begin(); j != table[b].end(); ++j) {
-            if (v == *j) {
+            if (k == j->first) {
                 return true;
             }
         }
         return false;
     }
+
+    pair<string, int> fetch(string k, int v) {
+        int b = hash2(k);
+        list<pair<string, int>>::iterator j; // used for bucket list entries
+        for(j = table[b].begin(); j != table[b].end(); ++j) {
+            if (k == j->first) {
+                return *j;
+            }
+        }
+        //string s("error");
+        pair<string, int> err = std::make_pair(string("err"), 0);
+        return err;
+    }
+
     void show() {
-        list<int>::iterator j; // used for bucket list entries
+        list<pair<string, int>>::iterator j; // used for bucket list entries
         for (int i = 0; i < B; ++i) {
             for(j = table[i].begin(); j != table[i].end(); ++j) {
-               cout << *j << endl;
+               cout << j->first << " => " << j->second << endl;
             }
         }
     }
@@ -35,8 +52,19 @@ private:
     int hash(int v) {
         return v % B;
     }
+
+    int hash2(string str)
+    {
+        unsigned int h = 0;
+        string::iterator it;
+        for ( it = str.begin(); it < str.end(); ++it) {
+            h = (31 * h) + *it;
+        }        
+        return (h % B);
+    }
+
     int B;     // No. of hash buckets
-    list<int> *table; // Pointer to an array containing buckets of hash entries
+    list<pair<string, int>> *table; // Pointer to an array containing buckets of hash entries
         
 };
 
--- toys/cpp/algos/test_hash.cpp
+++ toys/cpp/algos/test_hash.cpp
@@ -7,18 +7,23 @@ using namespace std;
 int main()
 {
     Hash h(13);
-    h.addHash(0);
-    h.addHash(1);
-    h.addHash(2);
-    h.addHash(3);
-    h.addHash(4);
-    h.addHash(5);
+    h.insert("abe", 0);
+    h.insert("boa", 1);
+    h.insert("cod", 2);
+    h.insert("dia", 3);
+    h.insert("eat", 4);
+    h.insert("fat", 5);
  
     h.show();
     cout << endl;
 
-    cout << "h(7)?: " << h.findHash(7) << endl;
-    cout << "h(4)?: " << h.findHash(4) << endl;
+    cout << "h(7)?: " << h.lookup("goa", 7) << endl;
+    cout << "h(4)?: " << h.lookup("eat", 4) << endl;
+
+    pair<string, int> p = h.fetch("eat", 4);
+    cout << "f(4): " << p.first << ": " << p.second << endl;
+    pair<string, int> p2 = h.fetch("goe", 7);
+    cout << "f(7): " << p2.first << ": " << p2.second << endl;
  
     return 0;
 }
