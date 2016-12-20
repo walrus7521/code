339ba0126cca66e1287269fef4fea8334e501f22
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2016-12-20 01:49:34 -0800

    mak

---
+++ toys/makadoo/test_graph.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Graph.hpp"
+
+using namespace std;
+ 
+// Driver program to test methods of graph class
+int main()
+{
+    // Create a graph given in the above diagram
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+ 
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 2) \n";
+    g.Bfs(2);
+    cout << endl;
+ 
+    return 0;
+}
+
