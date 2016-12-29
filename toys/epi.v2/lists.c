803f1e6d52afa2ce80c11088056364f0519e7cd2
Bart Bartel <BartB@helitrak.com>
2016-12-29 10:17:49 -0800

    epi

--- toys/epi.v2/lists.c
+++ toys/epi.v2/lists.c
@@ -3,11 +3,14 @@
 
 int main()
 {
-    struct list *head = list_create(42);
+    int arr[32];
+    struct heap hp = { .A = arr, .length = 1, .size = 32 };
+    struct list head = { .next = NULL, .last = NULL, .key = 42, .max = &hp };
+
     int i;
     for (i = 0; i < 8; i++) {
-        list_push(head, i);
+        list_push(&head, i);
     }
-    list_show(head);
+    list_show(&head);
 }
 
