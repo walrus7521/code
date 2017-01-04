d35548d69b2ba4cf6a5809260783e6fd3692da99
Bart Bartel <bartb7@gmail.com>
2017-01-04 07:33:53 -0800

    epi

--- toys/cpp/epi.cc/pch.hpp
+++ toys/cpp/epi.cc/pch.hpp
@@ -1,15 +1,19 @@
 #ifndef _pch_h_
 #define _pch_h_
 
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <list>
 #include <array>
+#include <deque>
 #include <utility>
 #include <typeinfo>
 #include <type_traits>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <condition_variable>
 
 #endif // _pch_h_
 
