2135496b6b211a1bf4d387d33f13ad0c1829e3d8
Bart Bartel <bartb7@gmail.com>
2017-01-14 18:38:49 -0800

    hask

--- toys/haskell/play.hs
+++ toys/haskell/play.hs
@@ -1,7 +1,10 @@
 import System.Environment
+import System.IO
+import Control.Monad
+import System.IO.Unsafe 
 
-myNum :: Int                 -- int myNum() {
-myNum = 12+13                --   return 12+13; }
+-- myNum :: Int                 -- int myNum() {
+--myNum = 12+13                --   return 12+13; }
 
 -- square :: Int -> Int         -- int square(int n) {
 -- square n = n*n               --   return = n*n;  }
@@ -17,9 +20,39 @@ myNum = 12+13                --   return 12+13; }
 
  
 -- | 'main' runs the main program
-main :: IO ()
-main = getArgs >>= print . haqify . head
+--main :: IO ()
+--main = getArgs >>= print . haqify . head
  
-haqify s = "Haq! " ++ s
+--haqify s = "Haq! " ++ s
+
+--module Main where
+
+-- import BinaryTree
+
+-- foo :: Integer -> Integer
+-- foo 0 = 0
+-- foo n = n + foo (n-1)
+-- main = print $ foo 1
+
+-- main = do  
+--         let list = []
+--         handle <- openFile "test.txt" ReadMode
+--         contents <- hGetContents handle
+--         let singlewords = words contents
+--             list = f singlewords
+--             --list = singlewords
+--         print list
+--         hClose handle   
+-- 
+-- f :: [String] -> [Int]
+-- f = map read
+
+
+main = do
+   args <- getArgs
+   content <- readFile (args !! 0)
+   let linesOfFiles = lines content
+   print linesOfFiles
+
 
 
