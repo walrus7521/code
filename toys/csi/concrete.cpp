a8238355afc53010ed587b77951a85353206a84a
Bart Bartel <BartB@helitrak.com>
2017-01-20 12:41:04 -0800

    emb

--- toys/csi/concrete.cpp
+++ toys/csi/concrete.cpp
@@ -10,6 +10,14 @@
 
 using namespace std;
 
+void show(int a[], size_t len)
+{
+    for (size_t i = 0; i < len; ++i) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
 /*
  * max first element is 1. 2nd element is 2, and 2 is less
  * than or equal to 1 + 1, so we can produce all values up
@@ -23,27 +31,26 @@ using namespace std;
  */
 int max_subset_sum(int sorted[], int len)
 {
+    int running = 0, max;
     for (size_t i = 0; i < len; ++i) {
-        printf("max[%zu] = %d\n", i, sorted[i]);
+        max = running+1;
+        running += sorted[i];
+        if (sorted[i] > max) {
+            printf("smallest number we cannot produce is: %d\n", max);
+            return max;
+        }
+        printf("max[%d] = %d => sum %d\n", i, sorted[i], running);
     }
     return 0;
 }
 
-void show(int a[], size_t len)
-{
-    for (size_t i = 0; i < len; ++i) {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-}
 
 int main()
 {
     int a[] = {12,2,1,15,2,4};
     size_t len = sizeof(a) / sizeof(a[0]);
 
-    show(a, len);
     std::sort(a, a+len);
-    max_subset_sum(a, len);
     show(a, len);
+    max_subset_sum(a, len);
 }
