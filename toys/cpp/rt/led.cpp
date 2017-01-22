3746178f5e1ca81d1527b73dffba6e882e0f9502
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2017-01-21 19:57:28 -0800

    rt

---
+++ toys/cpp/rt/led.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdint>
+#include <unistd.h>
+#include "mcal_reg.h"
+
+template <typename port_type,
+          typename bval_type,
+          const port_type port,
+          const bval_type bval>
+class led_template
+{
+public:
+    //typedef std::uint8_t port_type;
+    //typedef std::uint8_t bval_type;
+
+    // constructor
+    led_template()
+    {
+        // set port pin to low
+        //*reinterpret_cast<volatile bval_type*>(port)
+        //    &= static_cast<bval_type>(~bval);
+
+        // set the port pin direction to output
+        //*reinterpret_cast<volatile bval_type*>(port - 1U)
+        //    |= bval;
+    }
+
+    static void toggle() const
+    {
+        //*reinterpret_cast<volatile bval_type*>(port)
+        //    ^= bval;
+        std::cout << "toggle" << std::endl;
+    }
+
+private:
+    static constexpr port_type pdir = port - 1U;
+    //const port_type port;
+    //const bval_type bval;
+};
+
+namespace
+{
+    // create led_b5 on portb.5
+    const led_template<std::uint8_t,
+                       std::uint8_t,
+                       mcal::reg::portb,
+                       mcal::reg::bval5> led_b5;
+}
+
+constexpr uint32_t one_second = 1000000;
+int main()
+{
+    // toggle led_b5 forever
+    for (;;)
+    {
+        led_b5.toggle();
+        usleep(one_second);
+
+    }
+}
+
+
+
---
+++ toys/cpp/rt/mcal_reg.h
@@ -0,0 +1,18 @@
+namespace mcal
+{
+    namespace reg
+    {
+        constexpr std::uint8_t portb = 0x25U;
+
+        constexpr std::uint8_t bval0 = 1U;
+        constexpr std::uint8_t bval1 = 1U << 1U;
+        constexpr std::uint8_t bval2 = 1U << 2U;
+        constexpr std::uint8_t bval3 = 1U << 3U;
+        constexpr std::uint8_t bval4 = 1U << 4U;
+        constexpr std::uint8_t bval5 = 1U << 5U;
+        constexpr std::uint8_t bval6 = 1U << 6U;
+        constexpr std::uint8_t bval7 = 1U << 7U;
+
+    }
+
+}
