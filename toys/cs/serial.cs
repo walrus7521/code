cbaea1fd8633153c4058d36a8d33899ba0736db9
Bart Bartel <BartB@helitrak.com>
2017-01-27 11:12:30 -0800

    cs

--- toys/cs/serial.cs
+++ toys/cs/serial.cs
@@ -6,37 +6,56 @@ using System.IO.Ports;
 
 namespace proto {
     public class serial {
-        //public const string COM_PORT = "COM31"; // ADP
+        public const string COM_PORT = "COM35"; // ADP
         //public const string COM_PORT = "COM32"; // MCPA
         //public const string COM_PORT = "COM33"; // MCPB
-        public const string COM_PORT = "COM34"; // MCPC
+        //public const string COM_PORT = "COM34"; // MCPC
         public serial() {
+            port_name = COM_PORT;
             serialPort = new SerialPort();
             serialPort.BaudRate = 115200;
             serialPort.DataBits = 8;
             serialPort.Parity = Parity.None;
             serialPort.StopBits = StopBits.One;
             serialPort.DataReceived += port_rx;
-            serialPort.PortName = COM_PORT;
+            serialPort.PortName = port_name;
         }
         ~serial()
         {
             serialPort.Close();
         }
+        public void setport(string com)
+        {
+            port_name = com;
+            serialPort.Close();
+            serialPort.PortName = port_name;
+            System.Console.WriteLine("setport {0}", port_name);
+            connect();
+        }
+        public void getports()
+        {
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            for (int i = 0; i < ports.Length; ++i) {
+                System.Console.WriteLine("port {0}", ports[i]);
+            }
+        }
         public bool connect() {
             try {
                 serialPort.Open();
             } catch (Exception) {
-                System.Console.WriteLine("unable to open port {0}", COM_PORT);                
+                System.Console.WriteLine("unable to open port {0}", port_name);
                 return false;
             }
             return true;
+        }
+        public void read() {
+            //return serialPort.Read();
         }
         public void write(string s) {
             //byte[] buf = Encoding.ASCII.GetBytes(s);
             //System.Console.WriteLine("sending {0} to serial port", buf);
             //serialPort.Write(buf, 0, s.Length);
-            System.Console.WriteLine("sending {0} to serial port", s);
+            //System.Console.WriteLine("sending {0} to serial port", s);
             serialPort.Write(s);
         }
         private void port_rx(object sender, System.IO.Ports.SerialDataReceivedEventArgs e) {
@@ -52,6 +71,7 @@ namespace proto {
                 }
             }
         }
+        public string port_name;
         private SerialPort serialPort;
         private string rx_str = string.Empty;
     }
@@ -64,12 +84,24 @@ namespace proto {
                 return;
             }
             while (true) {
-                System.Console.Write("$ ");
+                System.Console.Write("{0}> ", port.port_name);
                 string cmd = System.Console.ReadLine();
+                if (cmd == "") continue;
                 if (cmd == "quit") {
                     return;
                 }
-                port.write(cmd);
+                if (cmd == "list") {
+                    port.getports();
+                }
+                if (cmd.Substring(0,3) == "com") {
+                    port.setport(cmd);
+                }
+                if (cmd.Substring(0,3) == "put") {
+                    port.write(cmd.Substring(4));
+                }
+                if (cmd.Substring(0,3) == "get") {
+                    port.read();
+                }
 
             }
         }
