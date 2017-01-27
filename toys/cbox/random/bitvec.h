6033754ea11b1a550c241b071d194bab07efd030
Bart Bartel <bartb7@gmail.com>
2017-01-27 08:06:48 -0800

    cbo

--- toys/cbox/random/bitvec.h
+++ toys/cbox/random/bitvec.h
@@ -47,18 +47,44 @@ void insert(int x, bitvec *vector)
     dprintf("insert bit=%x => [%d]:%08x\n", bit, idx, vector->vector[idx]);
 }
 
+int fast_find_first_zero_bit(bitvec *vector)
+{
+    int i;
+    for (i = 0; i < vector->size; i++) {
+        unsigned int x = vector->vector[i];
+        unsigned int y = x ^ 0xffffffff;
+        if (y) {
+            int lowest_set_bit_mask = x & ~(x -1); // isolate lowest set bit as a mask
+            printf("found zero @ vec[%d] = %08X => bit %d\n", i, y, lowest_set_bit_mask);
+            //vector->vector[i]);
+            break;
+        }
+    }
+    return i;
+}
+
 void printvector(bitvec *vector)
 {
     int i;
     for (i = 0; i < vector->size; i++) {
-        dprintf("vec[%d] = %08X\n", i, vector->vector[i]);
+        printf("vec[%d] = %08X\n", i, vector->vector[i]);
     }
 }
 
 int bitvec_test()
 {
+    int i;
     bitvec *vector;
     vector = create(256);
+
+    //int x = 0x2c;
+    //printf("%x => %x\n", x, x & ~(x-1));
+    //return 0;
+
+    for (i = 0; i < 13; i++) {
+        insert(i, vector);
+    }
+    
     insert(2, vector);
     insert(5, vector);
     insert(240, vector);
@@ -66,13 +92,14 @@ int bitvec_test()
     lookup(5, vector);
     lookup(7, vector);
     lookup(240, vector);
-    delete(2, vector);
+    //delete(2, vector);
     lookup(2, vector);
     printvector(vector);
-    for (int i = 0; i < 256; i++) {
-        if (lookup(i, vector)) {
-            printf("%07d\n", i);
-        }
-    }
+    //for (int i = 0; i < 256; i++) {
+    //    if (lookup(i, vector)) {
+    //        printf("%07d\n", i);
+    //    }
+    //}
+    fast_find_first_zero_bit(vector);
     return 0;
 }
