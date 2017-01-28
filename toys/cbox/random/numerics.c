7cb92d468c8fdc3fb5db864b5288cc4306897e12
Bart Bartel <bartb7@gmail.com>
2017-01-28 15:40:50 -0800

    cbo

--- toys/cbox/random/numerics.c
+++ toys/cbox/random/numerics.c
@@ -26,11 +26,39 @@ uint32_t rotate_left(uint32_t vector, int n)
     return vector;
 }
 
-int main()
+int test_bit_rot()
 {
     uint32_t vector = 0x1;
     int n = 4;
     //printf("rotate 0x%x left  %d => 0x%08x\n", vector, n, rotate_left(vector, n));
     printf("rotate %x right %d => 0x%08x\n", vector, n, rotate_right(vector, n));
     return 0;
+}
+
+int min(int a, int b, int c) {
+    if (a <= b && a <= c) return a;
+    if (b <= a && b <= c) return b;
+    return c;
+}
+
+void test_primes_235()
+{
+    int p0[3] = {1,1,1};
+    int p1[3] = {2,3,5};
+    for (int i = min(p0[0], p0[1], p0[2]); i <= 1024; i = min(p1[0], p1[1], p1[2])) {
+        printf("i:%04d> %04d %04d %04d\n", i, p0[0], p0[1], p0[2]);
+        p0[0] = p1[0];
+        p0[1] = p1[1];
+        p0[2] = p1[2];
+        p1[0] = 2 * i;
+        p1[1] = 3 * i;
+        p1[2] = 5 * i;
+    }
+    
+}
+
+int main()
+{
+    test_primes_235();
+    test_bit_rot();
 }
