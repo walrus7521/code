ae527e0af8a381b4b7445d3eab816d9e0100292b
Bart Bartel <bartb7@gmail.com>
2017-02-04 07:27:28 -0800

    alg

--- toys/algos/csi/Sort.hpp
+++ toys/algos/csi/Sort.hpp
@@ -55,4 +55,48 @@ void Sort_bubble(T a[], int n)
     }
 }
 
+template <typename T>
+void merge(T b[], int p, T c[], int q, T a[], int r)
+{
+    // assert: r = p+q
+    // assert: b and c are sorted
+
+    // 1. i <- 0; j <- 0; k <- 0;
+    // 2. while i < p and j < q do // while there are elements in b & c
+    // 3.     if b[i] <= c[j]
+    // 4.         a[k] <- b[i]; i <- i+1
+    // 5.      else a[k] <- c[j]; j <- j+1
+    // 6.      k <- k+1
+    // 7. if i = p // array b is exhausted so copy remainder of c to a
+    // 8.     copy c[j..q-1] to a[k..p+q-1]
+    // 9. else // array c is exhausted so copy remainder of b to a
+    //        copy b[i..p-1] to a[k..p+q-1]
+}
+
+template <typename T>
+void copy(T *a, int sp, int plen, T *b, int sq, int qlen)
+{
+    if (plen == qlen) {
+        for (int i = sp, j = sq; i < plen; ++i, ++j) {
+            a[i] = b[j];
+        }
+    }
+}
+
+template <typename T>
+void Sort_mergesort(T a[], int n)
+{
+    T *b = new T[n];
+    T *c = new T[n];
+    // 1. if n > 1
+    if (n > 1) {
+    // 2.     copy a[0..n/2-1] to b[0..n/2-1]
+        copy(b, 0, n/2-1, a, 0, n/2-1);
+    // 3.     copy a[n/2..n-1] to c[0..n/2-1]
+    // 4.     mergesort(b[0..n/2-1])
+    // 5.     mergesort(c[0..n/2-1])
+    // 6.     merge(b, c, a)
+    }
+}
+
 #endif // _Sort_h_
