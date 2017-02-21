3860895ba478f88ece701725071f1a744d201e4d
Bart Bartel <bartb7@gmail.com>
2017-02-21 07:34:53 -0800

    py

--- toys/algos/csi/algos.py
+++ toys/algos/csi/algos.py
@@ -102,7 +102,7 @@ def quicksort(arr):
 
 def print_items(list):
     for item in list:
-        sleep(1)
+        #sleep(1)
         print item
 
 from collections import deque
@@ -117,32 +117,71 @@ graph["peggy"]  = [""]
 graph["thom"]   = [""]
 graph["jonny"]  = [""]
 
-def person_is_seller(name):
-    return name[-1] == 'm'
+graph2 = {}
+graph2["wake up"] = [""]
+graph2["shower"] = ["wake up"]
+graph2["brush teeth"] = ["wake up"]
+graph2["eat breakfast"] = ["brush teeth"]
 
-def gsearch():
+def activity_is_wakey(activity):
+    print "activity: " + activity
+    return activity == "wake up"
+
+def person_is_seller(name):
+    print "person seller? " + name
+    return name[-1] == 'm' # silly, use last letter as flag for mango dealer
+
+def graph_search(g, start, condition, notice):
     search_queue = deque()
-    search_queue += graph["you"]
+    search_queue += g[start]
+    searched = []
     while search_queue:
-        person = search_queue.popleft()
-        if person_is_seller(person):
-            print person + " is a mango seller"
-            return True
-        else:
-            search_queue += graph[person]
+        item = search_queue.popleft()
+        if not item in searched:
+            if condition(item):
+                print item + notice
+                return True
+            else:
+                search_queue += g[item]
+                searched.append(item)
     return False
 
+def top_sort(graph_unsorted):
+    graph_sorted = []
+    # convert unsorted graph to hash table
+    print graph_unsorted
+    graph_unsorted = dict(graph_unsorted)
+    print graph_unsorted
+    while graph_unsorted:
+        acyclic = False
+        for node, edges in list(graph_unsorted.items()):
+            for edge in edges:
+                if edge in graph_unsorted:
+                    break
+            else:
+                acyclic = True
+            del graph_unsorted[node]
+            graph_sorted.append((node, edges))
+        if not acyclic:
+            print "error, cyclic"
+    return graph_sorted
 
-print selectionSort([5,3,6,2,10])
-countdown(5)
-greet("jack")
-print "fact: {}".format(fact(7))
-print "gcd: {}".format(gcd(1680,640))
-print "sum: {}".format(rec_sum([2,4,6]))
-print "countem: {}".format(countem([2,4,6,3,1,9,1]))
-print "maxit: {}".format(maxit([2,4,6,3,42,1,9,1]))
-print "binsearch: {}".format(binsearch([2,3,4,9,42,99,132,763,1023],42))
-print "quicksort: {}".format(quicksort([33,15,10,42,99,6]))
-print_items([33,15,42,199])
-gsearch()
+
+#print selectionSort([5,3,6,2,10])
+#countdown(5)
+#greet("jack")
+#print "fact: {}".format(fact(7))
+#print "gcd: {}".format(gcd(1680,640))
+#print "sum: {}".format(rec_sum([2,4,6]))
+#print "countem: {}".format(countem([2,4,6,3,1,9,1]))
+#print "maxit: {}".format(maxit([2,4,6,3,42,1,9,1]))
+#print "binsearch: {}".format(binsearch([2,3,4,9,42,99,132,763,1023],42))
+#print "quicksort: {}".format(quicksort([33,15,10,42,99,6]))
+#print_items([33,15,42,199])
+                
+#graph_search(graph, "you", person_is_seller, " => is a mango seller")
+#graph_search(graph2, "eat breakfast", activity_is_wakey, " => is wakey")
+tsort = top_sort(graph2)
+for i in tsort:
+    print i
 
