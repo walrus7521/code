334117ec507bea4398ba8f5a93eb89dd00949b04
Bart Bartel <BartB@helitrak.com>
2017-03-02 09:30:53 -0800

    py

--- toys/py/busp/sc750.py
+++ toys/py/busp/sc750.py
@@ -1,8 +1,8 @@
+import sys
 import time
 from pyBusPirateLite.I2Chigh import *
 
 # http://www.nxp.com/documents/application_note/AN10462.pdf
-
 # https://courses.engr.illinois.edu/ece390/resources/serial.html
 # https://github.com/juhasch/pyBusPirateLite/blob/master/tests/test_bitbang.py
 
@@ -16,6 +16,8 @@ SC700_WRGPIO      = 0x4F # O 0x4F write to GPIO port
 SC700_PWRDN       = 0x5A # Z 0x5A power down
 
 # SC700 register addresses
+# register          addr    RW    reset
+#=========          ====    ==    =====
 SC700_BRG0        = 0x00  # RW    0xF0
 SC700_BRG1        = 0x01  # RW    0x02
 SC700_PortConf1   = 0x02  # RW    0x55
@@ -52,40 +54,41 @@ UART_MODEM_STAT   = 0x06  # R
 #UART_SCRATCH      = 0x07  # RW
 
 
-SC750_RHR         = 0x00 #Receive Holding Register (RHR) 
-SC750_THR         = 0x00 #Transmit Holding Register (THR)
-SC750_IER         = 0x01 #Interrupt Enable Register (IER) Interrupt Enable Register
-SC750_IIR         = 0x02 #Interrupt Identification Register (IIR) 
-SC750_FCR         = 0x02 #FIFO Control Register (FCR)
-SC750_LCR         = 0x03 #Line Control Register (LCR) Line Control Register
-SC750_MCR         = 0x04 #Modem Control Register (MCR)[1] Modem Control Register[1]
-SC750_LSR         = 0x05 #Line Status Register (LSR) n/a
-SC750_MSR         = 0x06 #Modem Status Register (MSR) n/a
-SC750_SPR         = 0x07 #Scratchpad Register (SPR) Scratchpad Register
-SC750_TCR         = 0x06 #Transmission Control Register (TCR)[2] Transmission Control Register[2]
-SC750_TLR         = 0x07 #Trigger Level Register (TLR)[2] Trigger Level Register[2]
-SC750_TXLVL       = 0x08 #Transmit FIFO Level Register n/a
-SC750_RXLVL       = 0x09 #Receive FIFO Level Register n/a
-SC750_IODir       = 0x0A #I/O pin Direction Register I/O pin Direction Register
-SC750_IOState     = 0x0B #I/O pin States Register n/a
-SC750_IOIntEna    = 0x0C #I/O Interrupt Enable Register I/O Interrupt Enable Register
+# SC750 register addresses
+# register          addr    description
+#=========          ====    ===============================
+SC750_RHR         = 0x00    #Receive Holding Register (RHR) 
+SC750_THR         = 0x00    #Transmit Holding Register (THR)
+SC750_IER         = 0x01    #Interrupt Enable Register (IER) Interrupt Enable Register
+SC750_IIR         = 0x02    #Interrupt Identification Register (IIR) 
+SC750_FCR         = 0x02    #FIFO Control Register (FCR)
+SC750_LCR         = 0x03    #Line Control Register (LCR) Line Control Register
+SC750_MCR         = 0x04    #Modem Control Register (MCR)[1] Modem Control Register[1]
+SC750_LSR         = 0x05    #Line Status Register (LSR) n/a
+SC750_MSR         = 0x06    #Modem Status Register (MSR) n/a
+SC750_SPR         = 0x07    #Scratchpad Register (SPR) Scratchpad Register
+SC750_TCR         = 0x06    #Transmission Control Register (TCR)[2] Transmission Control Register[2]
+SC750_TLR         = 0x07    #Trigger Level Register (TLR)[2] Trigger Level Register[2]
+SC750_TXLVL       = 0x08    #Transmit FIFO Level Register n/a
+SC750_RXLVL       = 0x09    #Receive FIFO Level Register n/a
+SC750_IODir       = 0x0A    #I/O pin Direction Register I/O pin Direction Register
+SC750_IOState     = 0x0B    #I/O pin States Register n/a
+SC750_IOIntEna    = 0x0C    #I/O Interrupt Enable Register I/O Interrupt Enable Register
 SC750_reserved2   = 0x0D
-SC750_IOControl   = 0x0E #I/O pins Control Register I/O pins Control Register
-SC750_EFCR        = 0x0F #Extra Features Register Extra Features Register
-SC750_DLL         = 0x00 #divisor latch LSB (DLL)[4] divisor latch LSB[4]
-SC750_DLH         = 0x01 #divisor latch MSB (DLH)[4] divisor latch MSB[4]
-SC750_EFR         = 0x02 #Enhanced Feature Register (EFR)[5] Enhanced Feature Register[5]
-SC750_XON1        = 0x04 #Xon1 word[5] Xon1 word[5]
-SC750_XON2        = 0x05 #Xon2 word[5] Xon2 word[5]
-SC750_XOFF1       = 0x06 #Xoff1 word[5] Xoff1 word[5]
-SC750_XOFF2       = 0x07 #Xoff2 word[5] Xoff2 word[5]
-
-
+SC750_IOControl   = 0x0E    #I/O pins Control Register I/O pins Control Register
+SC750_EFCR        = 0x0F    #Extra Features Register Extra Features Register
+SC750_DLL         = 0x00    #divisor latch LSB (DLL)[4] divisor latch LSB[4]
+SC750_DLH         = 0x01    #divisor latch MSB (DLH)[4] divisor latch MSB[4]
+SC750_EFR         = 0x02    #Enhanced Feature Register (EFR)[5] Enhanced Feature Register[5]
+SC750_XON1        = 0x04    #Xon1 word[5] Xon1 word[5]
+SC750_XON2        = 0x05    #Xon2 word[5] Xon2 word[5]
+SC750_XOFF1       = 0x06    #Xoff1 word[5] Xoff1 word[5]
+SC750_XOFF2       = 0x07    #Xoff2 word[5] Xoff2 word[5]
 
 
 def outw(register, channel, word):
     reg = (register << 3 | channel) & 0xFF
-    #print "write: {:02X}, {:02X}".format(reg, word)
+    print "WRITEW: {:02X}, {:02X}".format(reg, word)
     i2c.set_word(i2c_addr, reg, word)
 
 def outb(register, channel, value):
@@ -115,12 +118,10 @@ def i2c_init():
     global i2c
     global i2c_addr
     global port
-
     i2c_addr = 0x48
     port = "COM13"
     bitrate = 1
     timeout = 5
-
     i2c = I2Chigh(port, bitrate, timeout)
     i2c.resetBP()
     i2c.BBmode()    # bit bang mode
@@ -128,9 +129,6 @@ def i2c_init():
     # turn on the power supply and enable the pullups
     i2c.cfg_pins(I2CPins.POWER | I2CPins.PULLUPS)
     print i2c
-
-#print i2c.get_byte(0x48, 0x00)
-#print i2c.set_byte(0x48, 0x0E, 0x08)
 
 def uart_init():
     # configure the UART
@@ -207,12 +205,20 @@ def dump(a, len):
     
 def scan():
     print "Scanning for i2c devices:"
+    devices = []
     for addr in range(0,128):
         if i2c.detect(addr, 0x5A) == True:
-            #print "NACK addr {:02X}".format(addr)
-            pass
+            sys.stdout.write('.')
         else:
-            print "  => found device at addr {:02X}h".format(addr)
+            devices.append(addr)
+    print ""
+    if (len(devices) > 0):
+        print "found {} device(s): ".format(len(devices))
+        for d in devices:
+            print "  i2c addr: {:02X}h".format(d)
+    else:
+        print "no devices found"
+    print ""
 
 def scratch_pad_ping(value):
     channel = TX_CHANNEL
@@ -221,11 +227,11 @@ def scratch_pad_ping(value):
     v = inb(SC750_SPR, channel)
     return v
     
-def sc700_fetch(reg):
-    outb(SC750_THR, TX_CHANNEL, SC700_START);
-    outb(SC750_THR, TX_CHANNEL, SC700_READ);
-    outb(SC750_THR, TX_CHANNEL, reg);
-    outb(SC750_THR, TX_CHANNEL, SC700_STOP);
+def sc700_fetch_register(reg):
+    #outb(SC750_THR, TX_CHANNEL, SC700_START);
+    outb(SC750_THR, TX_CHANNEL, SC700_READ)
+    outb(SC750_THR, TX_CHANNEL, reg)
+    outb(SC750_THR, TX_CHANNEL, SC700_STOP)
     #rx = inb(SC750_LSR, RX_CHANNEL)
     #print "LSR {:02X}".format(rx)
     data = inb(SC750_RHR, RX_CHANNEL)
@@ -238,13 +244,13 @@ BMP085_READTEMPCMD       = 0x2E
 BMP085_READPRESSURECMD   = 0x34
 def sc700_slave_read(slave_addr, num_bytes):
     outb(SC750_THR, TX_CHANNEL, SC700_START)
-    outb(SC750_THR, TX_CHANNEL, (slave_addr << 1))
+    outb(SC750_THR, TX_CHANNEL, (slave_addr<<1)) # write
     outb(SC750_THR, TX_CHANNEL, 2) # num bytes to write
     outb(SC750_THR, TX_CHANNEL, BMP085_CONTROL)
     outb(SC750_THR, TX_CHANNEL, BMP085_READTEMPCMD)
     outb(SC750_THR, TX_CHANNEL, SC700_STOP)
     outb(SC750_THR, TX_CHANNEL, SC700_START)
-    outb(SC750_THR, TX_CHANNEL, (slave_addr << 1) | 0x01)
+    outb(SC750_THR, TX_CHANNEL, (slave_addr<<1)|1) # read
     outb(SC750_THR, TX_CHANNEL, 1) # num bytes to read
     outb(SC750_THR, TX_CHANNEL, SC700_STOP)
     data = inb(SC750_RHR, RX_CHANNEL)
@@ -253,12 +259,12 @@ def sc700_slave_read(slave_addr, num_bytes):
 def sc750_init():
     outb(SC750_IOControl, TX_CHANNEL, 0x80) # reset sc750
     outb(SC750_FCR, TX_CHANNEL, 0x07) # enable fifo (C7)
-    outb(SC750_LCR, TX_CHANNEL, 0x87) # enable special registers, and line control (8:N:1)
-    # divisor = 1152d = 480h = 11.0592 MHz / 9600 baud
-    outb(SC750_DLH, TX_CHANNEL, 0x04) # baud rate divisor (high byte)
-    outb(SC750_DLL, TX_CHANNEL, 0x80) # baud rate divisor (low byte)
-    outb(SC750_LCR, TX_CHANNEL, 0x07) # disable special registers
-    outb(SC750_MCR, TX_CHANNEL, 0x03) # modem control - set RTS/DTR high
+    outb(SC750_LCR, TX_CHANNEL, 0x83) # enable special registers, and line control (8:N:1)
+    # divisor = 72d = 48h = 11.0592 MHz / (9600 baud * 16) see section 7.8
+    outb(SC750_DLH, TX_CHANNEL, 0x00) # baud rate divisor (high byte)
+    outb(SC750_DLL, TX_CHANNEL, 0x48) # baud rate divisor (low byte)
+    outb(SC750_LCR, TX_CHANNEL, 0x03) # disable special registers
+    outb(SC750_MCR, TX_CHANNEL, 0x00) # modem control - set RTS/DTR high
     x = inb(SC750_MSR, RX_CHANNEL)
     print "MSR {:02X}".format(x)
     x = inb(SC750_LSR, RX_CHANNEL)
@@ -267,7 +273,7 @@ def sc750_init():
     print "RHR {:02X}".format(x)
     x = inb(SC750_IIR, RX_CHANNEL)
     print "IIR {:02X}".format(x)
-
+    outb(SC750_MCR, TX_CHANNEL, 0x00) # modem control - set RTS/DTR low
 
 def main():
     i2c_init()
@@ -290,18 +296,17 @@ def main():
         print "failed scratch register test"
 
     while True:
-        #x = sc700_fetch(SC700_I2CAdr)
-        #x = sc700_fetch(SC700_I2CO)
-        #print "700: {:02X}".format(x)
-
+        #scratch_pad_ping(sp)
+        #outb(SC750_THR, TX_CHANNEL, 0x01)
+        #outb(SC750_THR, TX_CHANNEL, 0xFF)
+        #for reg in range (0,11):
+        #    x = sc700_fetch_register(reg)
+        #    print "700: reg {:02X} = {:02X}".format(reg, x)
         slave_addr = 0x77
         y = sc700_slave_read(slave_addr, 1)
-        print "700 temp: {}".format(y)
-
-        
+        print "BMP temp: {}".format(y)
+        print ""
         time.sleep(1)
-
-    uart_exit()
 
 if __name__ == '__main__':
     main()
--- toys/py/ser.py
+++ toys/py/ser.py
@@ -1,13 +1,84 @@
-#!/usr/bin/env python
+#"""python3 control of buspirate (SWHarden.com)"""
+# http://www.swharden.com/wp/2016-07-14-controlling-bus-pirate-with-python/
+# http://dangerousprototypes.com/docs/1-Wire
 
+# M:\code\buspy>ser.py
+# send #
+# #
+# RESET
+# 
+# Bus Pirate v3b
+# Firmware v5.10 (r559)  Bootloader v4.4
+# DEVID:0x0447 REVID:0x3046 (24FJ64GA002 B8)
+# http://dangerousprototypes.com
+# HiZ>
+# send m
+# m
+# 1. HiZ
+# 2. 1-WIRE
+# 3. UART
+# 4. I2C
+# 5. SPI
+# 6. 2WIRE
+# 7. 3WIRE
+# 8. LCD
+# 9. DIO
+# x. exit(without change)
+
+
+import time
 import serial
 
-ser = serial.Serial()
-ser.baudrate = 19200
-ser.port = 'COM11'
-ser.open()
-print ser.is_open
-ser.close()
-print ser.is_open
+commands = {
+        'BBIO1': '\x00',    # Enter reset binary mode
+        'SPI1':  '\x01',    # Enter binary SPI mode
+        'I2C1':  '\x02',    # Enter binary I2C mode
+        'ART1':  '\x03',    # Enter binary UART mode
+        '1W01':  '\x04',    # Enter binary 1-Wire mode
+        'RAW1':  '\x05',    # Enter binary raw-wire mode
+        'RESET': '\x0F',    # Reset Bus Pirate
+        'STEST': '\x10',    # Bus Pirate self-tests
+}
 
-print ser
+BUSPIRATE_PORT = 'com13' #customize this! Find it in device manager.
+
+def send(ser,cmd):
+    """send the command and listen to the response."""
+    #print "send {}".format(cmd)
+    ser.write(str(cmd+'\n').encode('ascii')) # send our command
+    for line in ser.readlines(): # while there's a response
+        print(line.decode('utf-8').strip()) # show it
+
+ser=serial.Serial(BUSPIRATE_PORT, 115200, timeout=1) # is com free?
+assert ser.isOpen() #throw an exception if we aren't connected
+send(ser,'#') # reset bus pirate (slow, maybe not needed)
+send(ser,'m') # change mode (goal is to get away from HiZ)
+send(ser,'4') # mode 9 is i2c
+#send(ser,'0') # mode 9 is DIO
+#send(ser,'w') # turn power supply to OFF. Uppercase W for ON, Lowercase w for OFF.
+send(ser,'W') # turn power supply to ON. Uppercase W for ON, Lowercase w for OFF.
+send(ser,'P') # pullups
+send(ser,"(1)"
+#send(ser,'6') # mode 9 is DIO
+#send(ser,'v') # show current voltages
+#for i in range (0,4):
+#    mode = chr(ord('0') + i)
+#    send(ser, 'm')
+#    send(ser, mode)
+#    time.sleep(1)
+
+#ser.write(commands.get('RESET'))
+while True:
+    cmd = raw_input()
+    print "cmd: {}".format(cmd)
+    send(ser,'?') # pullups
+
+#    got = ser.readline()
+#    if not got:
+#        break
+#    print(got),
+#    pass
+
+print "hello"
+ser.close() # disconnect so we can access it from another app
+#print("disconnected!") # let the user know we're done.
