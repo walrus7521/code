f310fbba8ce74bcbd8c79d24b2a0a277b3beca65
Bart Bartel <BartB@helitrak.com>
2017-03-08 11:48:29 -0800

    ph

--- toys/algos/phd/ringb.c
+++ toys/algos/phd/ringb.c
@@ -7,10 +7,11 @@
 typedef int e_v;
 #include "ring.inc"
 
-int main()
+void test_ring()
 {
     int i, n;
     init_ring();
+    printf("space avail %d\n", rngspace());
     for (i = 0; i < 8; i++) {
         if (!rngfull()) {
             rngput(i);
@@ -18,16 +19,48 @@ int main()
     }
 reload:
     while (!rngempty()) {
+        printf("peek: %d\n", rngpeek());
+        printf("bytes avail %d\n", rngdata());
+        printf("space avail %d\n", rngspace());
         n = rngget();
         printf("n=%d\n", n);
     }
-
     for (; i < n+8; i++) {
         if (!rngfull()) {
             rngput(i);
         }
     }
     goto reload;
-    
+}
+
+#include <string.h>
+#include "ring2.inc"
+
+void test_ring2()
+{
+    char buffer[256];
+    int i, id = 7;
+    fifo_init(id, buffer, 256);
+    for (i = 0; i < 26; i++) {
+        fifo_enqueue(id, 'a'+i);
+    }
+    char c;
+    fifo_peek_relative(id, 2, &c);
+    printf("peek: %c\n", c);
+    char tmp[8] = "";
+    memset(tmp, 8, 0);
+    fifo_peek_data(id, (void *) tmp, 8);
+    printf("peek data: %s\n", tmp);
+    fifo_enqueue_data(id, tmp, 8);
+    while (fifo_get_num_data_avail(id)) {
+        fifo_dequeue_data(id, tmp, 1);
+        printf("dequeued: %c\n", tmp[0]);
+    }
+}
+
+int main()
+{
+    test_ring();
+    //test_ring2();    
     return 0;
 }
