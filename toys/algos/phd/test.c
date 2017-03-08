9f5136e1564e819d0d221b513992d3661f55bd68
Bart Bartel <BartB@helitrak.com>
2017-03-08 11:27:05 -0800

    ph

--- toys/algos/phd/test.c
+++ toys/algos/phd/test.c
@@ -6,21 +6,21 @@
 int test()
 {
     char buffer[256];
-    int i;
-    fifo_init(0x42, buffer, 256);
+    int i, id = 7;
+    fifo_init(id, buffer, 256);
     for (i = 0; i < 26; i++) {
-        fifo_enqueue(0x42, 'a'+i);
+        fifo_enqueue(id, 'a'+i);
     }
     char c;
-    fifo_peek_relative(0x42, 2, &c);
+    fifo_peek_relative(id, 2, &c);
     printf("peek: %c\n", c);
     char tmp[8] = "";
     memset(tmp, 8, 0);
-    fifo_peek_data(0x42, (void *) tmp, 8);
+    fifo_peek_data(id, (void *) tmp, 8);
     printf("peek data: %s\n", tmp);
-    fifo_enqueue_data(0x42, tmp, 8);
-    while (fifo_get_num_data_avail(0x42)) {
-        fifo_dequeue_data(0x42, tmp, 1);
+    fifo_enqueue_data(id, tmp, 8);
+    while (fifo_get_num_data_avail(id)) {
+        fifo_dequeue_data(id, tmp, 1);
         printf("dequeued: %c\n", tmp[0]);
     }
 }
