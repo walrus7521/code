b51fd44f500420df27260243f831d818828ccfa5
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2017-03-08 19:25:36 -0800

    ph

--- toys/algos/phd/seasort.c
+++ toys/algos/phd/seasort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 #define key(A) (A)
@@ -19,6 +20,71 @@ void show(int a[], int n)
     }
     printf("\n\n\n ");
 }
+
+int match(char *string, char *pattern)
+/*
+ * return position of the 1st occurence of pattern p
+ * in the string, and -1 if not found.
+ */
+{
+    int i, j;
+    int plen, tlen;
+
+    plen = strlen(pattern);
+    tlen = strlen(string);
+
+    for (i = 0; i <= (tlen-plen); ++i) {
+        j = 0;
+        while ((j < plen) && (string[i+j] == pattern[j]))
+            ++j;
+        if (j == plen) return (i);
+    }
+    return (-1);
+}
+
+int sequential_search(char *str, int len, char k)
+{
+    int i = 0;
+    while (i < len && str[i] != k) i++;
+    if (i < len) return i;
+    return -1;
+}
+
+int palindrome(char *s)
+{
+    int len = strlen(s);
+    int n = len-1;
+    int i;
+    for (i = 0; i < len/2; i++, n--) {
+        if (s[i] != s[n]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int anagram(char *s1, int len1, char *s2, int len2)
+{
+    int ascii[256];
+    int i;
+    if (len1 != len2) {
+        return 0;
+    }
+    for (i = 0; i < 256; i++) ascii[i] = 0;
+    for (i = 0; i < len1; i++) ascii[s1[i]]++;
+    for (i = 0; i < len1; i++) {
+        if (ascii[s2[i]]-- == 0) {
+            return 0;
+        }
+    }
+    for (i = 0; i < 256; i++) {
+        if (ascii[i] != 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 
 int findmax(int a[], int from) {
     int i, max = 0, maxval = 0;
@@ -264,7 +330,27 @@ void merge_sort(int arr[], int l, int r)
     }
 }
 
-int main() {
+void test_strings()
+{
+
+    int f;
+    char data[]  = "sledbobber\0";
+    char data2[] = "sleddels";
+    char data3[] = "abbaz";
+    char data4[] = "yaabb";
+    
+    //f = match("bob", data);
+    //if (f != -1) printf("found match at %d -> %s\n", f, &data[f]);
+
+    printf("is match %d\n", match(data, "bob"));
+    printf("is pal   %d\n", palindrome(data2));
+    printf("is ana   %d\n", anagram(data3, strlen(data3), data4, strlen(data4)));
+    printf("search   %d\n", sequential_search(data, 10, 'z'));
+
+}
+
+void test_sort()
+{
     int a[] = {26,33,35,29,19,12,22,15,42,69,1};
     int sz = sizeof(a)/sizeof(a[0]);
     show(a, sz);
@@ -279,6 +365,12 @@ int main() {
     //quicksort(a, 0, sz-1);
     //show(a, sz);
     //printf("binsearch %d\n", binsearch(42, a, sz));
+  
+}
+
+int main() {
+    //test_strings();
+    test_sort();
   
     return 0;
 }
