9fc5518691c174da81d218e71dcfefc832d208b1
Bart Bartel <BartB@helitrak.com>
2017-03-16 15:16:21 -0700

    em

--- toys/embed/click.cs
+++ toys/embed/click.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.IO.Ports;
 using System.Threading;
 
+
 namespace proto {
     public class serial {
-        public const string COM_PORT = "COM13"; // ADP
-        //public const string COM_PORT = "COM32"; // MCPA
-        //public const string COM_PORT = "COM33"; // MCPB
-        //public const string COM_PORT = "COM34"; // MCPC
+        public const string COM_PORT = "COM13";
+        private int counter = 0;
+        private int n_loop = 0;
+        private string[] loop = new string[8];
+        
+        
         public serial() {
             port_name = COM_PORT;
             serialPort = new SerialPort();
@@ -52,8 +55,9 @@ namespace proto {
             return true;
         }
         public void read() {
-            //return serialPort.Read();
-        }
+            string message = serialPort.ReadLine();
+            System.Console.WriteLine("read: {0}", message);
+       }
         public void write(string s) {
             //byte[] buf = Encoding.ASCII.GetBytes(s);
             //System.Console.WriteLine("sending {0} to serial port", buf);
@@ -62,17 +66,25 @@ namespace proto {
             serialPort.Write(s);
         }
         private void port_rx(object sender, System.IO.Ports.SerialDataReceivedEventArgs e) {
-            
-            System.Console.WriteLine("got indication");
+            counter++;
+            //System.Console.WriteLine("{0:X02} got indication", counter % 0xff);
             if (serialPort.IsOpen == true) {
                 // Accumulate all data in a string buffer. Always append, so we never lose any data.
                 rx_str += serialPort.ReadExisting();
                 int idx;
                 while ((idx = rx_str.IndexOf('\n')) >= 0) {
-                    System.Console.WriteLine("received: {0}", rx_str.Substring(0, idx));
+                    System.Console.WriteLine("{0:X02}: received: {1}", counter % 0xff, rx_str.Substring(0, idx));
                     rx_str = rx_str.Substring(idx + 1);
                 }
             }
+        }
+        public void bus_pirate_reset()
+        {
+            write("#");
+        }
+        public void spi_reset()
+        {
+            write("0xC0\n");
         }
         public void spi_init()
         {
@@ -99,23 +111,38 @@ namespace proto {
             write("\n\n");
             Thread.Sleep(300);
         }
-        public void stream()
+        public void load()
         {
+            string[] lines = System.IO.File.ReadAllLines(@"spi.pi");
+            n_loop = 0;
+            //foreach (string line in lines) {
+            //    loop[n_loop++] = line + "\n";
+            //    //loop[n_loop++] = line;
+            //    //loop[n_loop++] = line.TrimEnd( '\r', '\n' );
+            //    //System.Console.WriteLine("line = {0}", line);
+            //}
+            n_loop = 2;
+            loop[0] = "{0xC0\n";
+            loop[1] = "r]";
+            System.Console.WriteLine("n_loop = {0}", n_loop);
+            for (int i = 0; i < n_loop; i++) {
+                System.Console.WriteLine("loop[{0}] = {1}", i, loop[i]);
+            }
+            //loop[0] = "[3,1]\n";
+            //loop[1] = "{0xC0\n";
+            //n_loop = 2;
+            //loop[0] = "{0xC0\n";
+            //loop[1] = "r]";
+        }
+        public void run()
+        {
+            int pc = 0;
             while (true) {
-                //write("[3,1]\n");
-                //write("{0xC0\n");
-                
-                //write("{0xC0\n");
-                //Thread.Sleep(300);
-                //write("r]");
-                //Thread.Sleep(300);
-
-                write("{3,1\n");
-                Thread.Sleep(30);
-                write("r]");
+                for (pc = 0; pc < n_loop; pc++) {
+                    write(loop[pc]);
+                    Thread.Sleep(30);
+                }
                 Thread.Sleep(300);
-
-                
             }
         }
         public string port_name;
@@ -132,6 +159,10 @@ namespace proto {
             if (!port.connect()) {
                 return;
             }
+            port.bus_pirate_reset();
+            port.spi_init();
+            port.spi_reset();
+            port.load();
             while (true) {
                 System.Console.Write("{0}> ", port.port_name);
                 string cmd = System.Console.ReadLine();
@@ -145,6 +176,9 @@ namespace proto {
                 else if (cmd == "reset") {
                     port.write("0xC0\n");
                 }
+                else if (cmd == "load") {
+                    port.load();
+                }
                 else if (cmd == "cs") {
                     port.write("[]\n");
                 }
@@ -154,14 +188,11 @@ namespace proto {
                 else if (cmd == "hi") {
                     port.write("]\n");
                 }
-                else if (cmd == "go") {
-                    port.stream();
+                else if (cmd == "run") {
+                    port.run();
                 }
                 else if (cmd == "list") {
                     port.getports();
-                }
-                else if (cmd.Length > 4 && cmd.Substring(0,3) == "com") {
-                    port.setport(cmd);
                 }
                 else if (cmd.Length > 4 && cmd.Substring(0,3) == "put") {
                     string io_cmd = cmd.Substring(4).ToString();
@@ -172,6 +203,7 @@ namespace proto {
                     port.read();
                 }
                 else {                    
+                    //System.Console.Write("Huh??? {0} \n", cmd);
                     string io_cmd = cmd;
                     io_cmd += "\n";
                     System.Console.Write("writing {0} to port\n", io_cmd);
