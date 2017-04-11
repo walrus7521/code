8002c88efaca801120e40585279f2f4b515aecfb
Bart Bartel <bartb7@gmail.com>
2017-04-11 07:13:53 -0700

    ph

--- toys/algos/phd/algos.c
+++ toys/algos/phd/algos.c
@@ -2,12 +2,25 @@
 
 #define exchg(A, B) { int t = A; A = B; B = t; }
 
+// binary search
+// selection sort
+// divide and conquer: gcd
+
 int gcd(int m, int n)
 {
     if (n > m) exchg(m,n);
     printf("m:%d, n:%d => %d\n", m, n, m % n);
     if (m % n == 0) return n;
     return gcd(n,m-n);
+}
+
+void show(int a[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d, ", a[i]);
+    }
+    printf("\n");
 }
 
 int main()
