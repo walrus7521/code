f036d612f2fd84110b378d2fa8ca850058eeaa1a
Bart Bartel <bartb7@gmail.com>
2017-04-11 07:19:29 -0700

    ph

--- toys/algos/phd/algos.c
+++ toys/algos/phd/algos.c
@@ -6,6 +6,20 @@
 // selection sort
 // divide and conquer: gcd
 
+void selection(int a[], int n)
+{
+    int i, j, min;
+    for (i = 0; i < n; i++) {
+        min = i;
+        for (j = i+1; j < n; j++) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
+        }
+        exchg(a[i], a[min]);
+    }
+}
+
 int gcd(int m, int n)
 {
     if (n > m) exchg(m,n);
@@ -27,4 +41,10 @@ int main()
 {
     int g = gcd(1680, 640);
     printf("gcd: %d\n", g);
+
+    int a[] = {4,1,8,3,42,2};
+    int n = sizeof(a)/sizeof(a[0]);
+    show(a,n);
+    selection(a,n);
+    show(a,n);
 }
