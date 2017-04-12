4b86e7feec246d31243d56b1ae678d653b51a2d2
Bart Bartel <BartB@helitrak.com>
2017-04-12 10:04:00 -0700

    ph

--- toys/algos/phd/feedback.c
+++ toys/algos/phd/feedback.c
@@ -3,6 +3,20 @@
 
 #define SAMPLE_SIZE 16
 
+float integ(float gain, float input, float ic, float sat, int hold)
+{
+    static float state;
+    if (hold == 0) {
+        state += input * gain;
+        if (state > sat) {
+            state = sat;
+        }
+    } else {
+        state = ic;
+    }
+    return state;
+}
+
 unsigned long cal_running_avg(unsigned long avg, unsigned long new_sample) 
 {
     if (avg != 0) {
@@ -12,7 +26,7 @@ unsigned long cal_running_avg(unsigned long avg, unsigned long new_sample)
     return avg;
 }
 
-int main()
+void test_avg()
 {
     unsigned long avg = 0;
     unsigned long v = 256;
@@ -24,4 +38,28 @@ int main()
     v = 32;
     avg = cal_running_avg(avg, v);
     printf("%ld -> %ld\n", v, avg);
+}
+
+void test_integ()
+{
+    float gain, input, ic, output, sat;
+    int hold;
+    hold = 0;
+    ic = 1.0f;
+    gain = 1.0f;
+    input = 0.01f;
+    sat = 42.0f;
+    int i;
+    for (i = 0; i < 16; i++) {
+        output = integ(gain, input, ic, sat, hold);
+        printf("integ: %f\n", output);
+        input = output;
+    }
+
+}
+
+int main()
+{
+    test_avg();
+    test_integ();
 }
