bdecc949d8860d7aaf9b104f1f758e4ef2100fca
Bart Bartel <BartB@helitrak.com>
2017-05-04 13:25:10 -0700

    sq

--- toys/algos/sqlapp/src/orm.cpp
+++ toys/algos/sqlapp/src/orm.cpp
@@ -4,6 +4,10 @@
  *  Created on: Jan 20, 2010
  *      Author: dweller
  */
+// need to abstract anything schema specific: s_record
+// make s_record a class to do it's own alloc and free
+// as well as comparisons.
+//
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,33 +19,8 @@
 extern "C" {
     #include <sqlite3.h>
 }
-/* T defines minimum number of children in non-root nodes
- * 2*T is maximum number of children in all nodes
- * 2*T-1 is maximum number of keys in all nodes */
-#define T 3
 
-typedef char * nodekey;
-
-/* record structure */
-typedef struct s_record {
-	unsigned int irecord; /* index of record in main database */
-	char * name; /* movie name used as key */
-	char * category;
-	unsigned int year;
-	char * format;
-	char * language;
-	char * url;
-} nodevalue;
-
-/* B-tree node structure */
-typedef struct s_tnode {
-	nodekey keys[2*T-1]; /* keys */
-	nodevalue * values[2*T-1]; /* values */
-	unsigned int nkeys; /* number of keys, < 2*T */
-
-	struct s_tnode * parent; /* pointer to parent */
-	struct s_tnode * children[2*T]; /* pointers to children */
-} * p_tnode;
+#include "btree.h"
 
 /* static variables for the root of the tree and number of records */
 static p_tnode ptreeroot = NULL;
