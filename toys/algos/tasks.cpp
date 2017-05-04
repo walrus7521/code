de74c1bee99437f531d37e93fbfba588d247dc91
Bart Bartel <BartB@helitrak.com>
2017-05-04 12:31:37 -0700

    al

--- toys/algos/tasks.cpp
+++ toys/algos/tasks.cpp
@@ -7,7 +7,7 @@
 // todo, launch a long running task and wait for completion
 
 std::mutex mx;
-std::condition_variable cv;
+std::condition_variable event_signal;
 std::queue<int> q;
 
 bool finished = false;
@@ -17,17 +17,17 @@ void producer(int n) {
         std::lock_guard<std::mutex> lk(mx);
         q.push(i);
         std::cout << "pushing " << i << std::endl;
-        cv.notify_all();
+        event_signal.notify_all();
     }
     std::lock_guard<std::mutex> lk(mx);
     finished = true;
-    cv.notify_all();
+    event_signal.notify_all();
 }
 
 void consumer() {
     while (true) {
         std::unique_lock<std::mutex> lk(mx);
-        cv.wait(lk, []{ return finished || !q.empty(); });
+        event_signal.wait(lk, []{ return finished || !q.empty(); });
         while (!q.empty()) {
             std::cout << "consuming " << q.front() << std::endl;
             q.pop();
@@ -40,13 +40,13 @@ void slave(int n)
 {
     for(int i=0; i<n; ++i) {
         std::lock_guard<std::mutex> lk(mx);
+        std::cout << "do task: " << i << std::endl;
         q.push(i);
-        std::cout << "grinding " << i << std::endl;
-        cv.notify_all();
+        event_signal.notify_all();
     }
     std::lock_guard<std::mutex> lk(mx);
     finished = true;
-    cv.notify_all();
+    event_signal.notify_all();
 }
 
 void master()
@@ -54,9 +54,9 @@ void master()
     std::thread t1(slave, 10);
     while (true) {
         std::unique_lock<std::mutex> lk(mx);
-        cv.wait(lk, []{ return finished || !q.empty(); });
+        event_signal.wait(lk, []{ return finished || !q.empty(); });
         while (!q.empty()) {
-            std::cout << "scarfing " << q.front() << std::endl;
+            std::cout << "cleanup task: " << q.front() << std::endl;
             q.pop();
         }
         if (finished) break;
@@ -65,11 +65,16 @@ void master()
     std::cout << "finished!" << std::endl;
 }
 
+void dual_procon()
+{
+    std::thread t1(producer, 10);
+    std::thread t2(consumer);
+    t1.join();
+    t2.join();
+    std::cout << "finished!" << std::endl;
+}
+
 int main() {
-    master();
-    //std::thread t1(producer, 10);
-    //std::thread t2(consumer);
-    //t1.join();
-    //t2.join();
-    //std::cout << "finished!" << std::endl;
+    //master();
+    dual_procon();
 }
