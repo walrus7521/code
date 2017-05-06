936ccd79dde4b17d297e0d47be35c18bcfe657ed
Bart Bartel <bartb7@gmail.com>
2017-05-06 02:45:25 -0700

    al

--- toys/algos/grok.cpp
+++ toys/algos/grok.cpp
@@ -6,34 +6,54 @@
 
 using namespace std;
 
-int test()
-{
-    map<string, set<int> > concordance;
+class vbase {
+public:
+    vbase(){cout << "ctor: vbase" << endl;}
+    ~vbase(){cout << "dtor: vbase" << endl;}
+    virtual void f() = 0;
+private:
+};
 
-    concordance["newcastle"].insert(98056);
-    concordance["newcastle"].insert(98057);
-    concordance["renton"].insert(98054);
-    concordance["renton"].insert(98055);
-    concordance["bellevue"].insert(98035);
-    concordance["issaquah"].insert(98027);
-
-    map<string, set<int> >::const_iterator iter =
-        concordance.begin();
-    while (iter != concordance.end()) {
-        cout << (*iter).first << endl;
-        set<int>::const_iterator siter = (*iter).second.begin();
-        while (siter != (*iter).second.end()) {
-            cout << " : " << (*siter) << endl;
-            siter++;
-        }
-        iter++;
+class derived : public vbase {
+public:
+    derived(){cout << "ctor: derived" << endl;
+              /*concordance = new map<string, set<int>>();*/}
+    ~derived(){cout << "dtor: derived" << endl;}
+    virtual void f() {}
+    void add(string s, int a) { concordance[s].insert(a); }
+    void show() {
+        map<string, set<int> >::const_iterator iter =
+            concordance.begin();
+        while (iter != concordance.end()) {
+            cout << (*iter).first << endl;
+            set<int>::const_iterator siter = (*iter).second.begin();
+            while (siter != (*iter).second.end()) {
+                cout << " : " << (*siter) << endl;
+                siter++;
+            }
+            iter++;
+        }        
     }
-}
+private:
+    map<string, set<int>> concordance;
+};
 
+
+void classes()
+{
+    derived d;
+    d.add("newcastle",98057);
+    d.add("renton",98054);
+    d.add("renton",98055);
+    d.add("bellevue",98035);
+    d.add("issaquah",98027);
+    d.show();
+}
 
 int main()
 {
-    test();
+    classes();
+    //test();
     return 0;
 }
 
