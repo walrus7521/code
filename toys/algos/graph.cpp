acc0d3fb8e21e31cd4e6370ece8aae4806c08999
Bart Bartel <bartb7@gmail.com>
2017-05-20 18:37:24 -0700

    al

--- toys/algos/graph.cpp
+++ toys/algos/graph.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 void graph_show(vector<vector<int>> p, string name) {
     int i, j,n = p.size();
-    printf("graph: %s\n", name.c_str());
+    cout << "graph: " << name << endl;
     for (i=0; i < n; i++) {
         for (j = 0; j < n; j++) 
-            printf("%d\t", p[i][j]);
-        printf("\n"); 
+            cout << p[i][j] << '\t';
+        cout << endl;
     }
-    printf("\n");
+    cout << endl;
 }
 
 // is "v" reachable
@@ -21,7 +21,7 @@ int reach[20];
 void dfs_r(int v, vector<vector<int>> a) {
     int i, n = a.size();
     reach[v] = 1;
-    printf("dfs: %d\n", v);
+    cout << "dfs: " << v << endl;
     for (i = 0; i < n; i++) {
         if (a[v][i] && !reach[i]) { 
             dfs_r(i, a);
@@ -39,20 +39,17 @@ void test_dfs()
                     { 1, 1, 0, 0, 0, 1 }, 
                     { 0, 1, 1, 0, 1, 0 } };
 
-    
     graph_show(p,"dfs");
     dfs_r(0, p);
     int i, count;
     for (i = 0; i < n; i++) { 
         if(reach[i]) count++; 
     }
-    printf("count: %d, n: %d\n", count, n);
-    printf("\n"); 
+    cout << "count: " << count << ", n: " << n << endl;
     if(count == n)
-        printf("\n Graph is connected"); 
+        cout << "Graph is connected" << endl; 
     else
-        printf("\n Graph is not connected");
-    printf("\n");
+        cout << "Graph is not connected" << endl;
 }
 
 int main()
@@ -60,3 +57,4 @@ int main()
     test_dfs();
     return 0;
 }
+
