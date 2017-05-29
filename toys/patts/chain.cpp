674a7f40a1906f57db65e848b9b64a6e1d3dd0ef
Bart Bartel <bartb7@gmail.com>
2017-05-29 11:47:17 -0700

    pa

--- toys/patts/chain.cpp
+++ toys/patts/chain.cpp
@@ -1,72 +1,69 @@
 #include <iostream>
-#include <vector>
 #include <ctime>
+#include <memory>
 
 using namespace std;
 
-class Base {
-    Base *next; // 1. "next" pointer in the base class
+class Handler { // abstract
+    shared_ptr<Handler> successor; // 1. "successor" pointer in the base class
 public:
-    Base() { next = 0; }
-    void setNext(Base *n) { next = n; }
-    void add(Base *n) {
-        if (next) next->add(n);
-        else next = n;
+    Handler() { successor = nullptr; }
+    void setNext(shared_ptr<Handler> n) { successor = n; }
+    void Add(shared_ptr<Handler> n) {
+        if (successor) successor->Add(n);
+        else successor = n;
     }
-    // 2. The "chain" method in the base class always 
-    // delegates to the next obj
-    virtual void handle(int i) { next->handle(i); }
+    // "chain" method in base class always delegates to successor obj
+    virtual void HandleRequest(int i) { successor->HandleRequest(i); }
 };
 
-class Handler1: public Base {
+class ConcreteHandler1: public Handler {
 public:
-    virtual void handle(int i) {
+    virtual void HandleRequest(int i) {
         if (rand() % 3) {
-            // 3. Don't handle requests 3 times out of 4
             cout << "H1 passed " << i << "  ";
-            Base::handle(i); // 3. Delegate to the base class
+            Handler::HandleRequest(i); // 3. Delegate to the base class
         } else {
           cout << "H1 handled " << i << "  ";
         }
     }
 };
 
-class Handler2: public Base {
+class ConcreteHandler2: public Handler {
 public:
-    virtual void handle(int i) {
+    virtual void HandleRequest(int i) {
         if (rand() % 3) {
             cout << "H2 passed " << i << "  ";
-            Base::handle(i);
+            Handler::HandleRequest(i);
         } else {
           cout << "H2 handled " << i << "  ";
         }
     }
 };
 
-class Handler3: public Base {
+class ConcreteHandler3: public Handler {
 public:
-     virtual void handle(int i) {
-        if (rand() % 3)
-        {
+     virtual void HandleRequest(int i) {
+        if (rand() % 3) {
             cout << "H3 passed " << i << "  ";
-            Base::handle(i);
-        }
-        else
+            Handler::HandleRequest(i);
+        } else {
           cout << "H3 handled " << i << "  ";
+        }
     }
 };
 
 int main()
 {
   srand(time(0));
-  Handler1 root;
-  Handler2 two;
-  Handler3 three;
-  root.add(&two);
-  root.add(&three);
-  three.setNext(&root);
+  shared_ptr<ConcreteHandler1> root(new ConcreteHandler1());
+  shared_ptr<ConcreteHandler2> two(new ConcreteHandler2());
+  shared_ptr<ConcreteHandler3> three(new ConcreteHandler3());
+  root->Add(two);
+  root->Add(three);
+  three->setNext(root);
   for (int i = 1; i < 10; i++) {
-    root.handle(i);
+    root->HandleRequest(i); // prints handlers
     cout << '\n';
   }
 }
