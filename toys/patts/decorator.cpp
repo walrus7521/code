163d27a3f1d27407ca1d10e18521ecdf3037db29
Bart Bartel <BartB@helitrak.com>
2017-06-01 11:47:10 -0700

    pa

--- toys/patts/decorator.cpp
+++ toys/patts/decorator.cpp
@@ -2,104 +2,61 @@
 
 using namespace std;
 
-// https://sourcemaking.com/design_patterns/decorator/cpp/2
-// compile w/g14
-
-// 1. "lowest common denominator", abstract component class
-class Component // abstract component
-{
+class Component { // abstract
 public:
-    virtual void draw() = 0; // pure virtual
-    virtual void resize() = 0; // pure virtual
+    virtual void Operation() = 0;
 };
-
-// concrete component class
-class TextField: public Component
-{
-private:
-    // 3. "Core" class & "is a"
-    int width, height;
+ 
+class ConcreteComponent : public Component {
 public:
-    TextField(int w, int h) {
-        width = w;
-        height = h;
-    }
-    virtual void draw() override {
-        cout << "TextField: draw " << width << ", " << height << '\n';
-    }
-    virtual void resize() override {
-        cout << "TextField: resize" << width << ", " << height << '\n';
+    void Operation() { // override
+        cout << "ConcreteComponent.Operation()" << endl;
     }
 };
-
-// 2. 2nd level base class
-// decorator subclasses component
-// abstract wrapper class, to be subclassed to different decorations
-// wraps any component and conforms to component interface
-class Decorator: public Component  // 4. "is a" relationship
-{
-private:
-    Component *_component; // 4. "has a" relationship
-public:
-    Decorator(Component *w) {
-        _component = w;
+ 
+class Decorator : public Component { // abstract 
+protected:
+    Component *_component;
+public: 
+    void SetComponent(Component *component) {
+        _component = component;
     }
-    virtual void draw() override {
-        _component->draw(); // 5. Delegation
-    }
-    virtual void resize() override {
-        _component->resize(); // 5. Delegation
+    void Operation() { // override 
+        if (_component != NULL) {
+            _component->Operation();
+        }
     }
 };
-
-// concrete wrapper class
-class BorderDecorator: public Decorator
-{
+ 
+class ConcreteDecoratorA : public Decorator {
 public:
-    // 6. Optional embellishment
-    BorderDecorator(Component *w): Decorator(w){}
-    virtual void draw() override {
-        // 7. Delegate to base class and add extra stuff
-        Decorator::draw();
-        // add extra stuff here
-        cout << "   BorderDecorator draw" << '\n';
-    }
-    virtual void resize() override {
-        // 7. Delegate to base class and add extra stuff
-        Decorator::resize();
-        // add extra stuff here
-        cout << "   BorderDecorator resize" << '\n';
+    void Operation() { // override 
+        Decorator::Operation();
+        cout << "ConcreteDecoratorA.Operation()" << endl;
     }
 };
-
-// concrete wrapper class
-class ScrollDecorator: public Decorator
-{
+ 
+class ConcreteDecoratorB : public Decorator {
 public:
-    // 6. Optional embellishment
-    ScrollDecorator(Component *w): Decorator(w){}
-    virtual void draw() override {
-        // 7. Delegate to base class and add extra stuff
-        Decorator::draw();
-        // add extra stuff here
-        cout << "   ScrollDecorator draw" << '\n';
+    void Operation() { // override 
+        Decorator::Operation();
+        cout << "ConcreteDecoratorB.Operation()" << endl;
+        AddedBehavior();
     }
-    virtual void resize() override {
-        // 7. Delegate to base class and add extra stuff
-        Decorator::resize();
-        // add extra stuff here
-        cout << "   ScrollDecorator resize" << '\n';
-    }
+    void AddedBehavior() { cout << "yo added" << endl; }
 };
 
 int main()
-{ // Client has the responsibility to compose desired configurations
-    Component *aComponent = // top level abstract component wrapper
-        new BorderDecorator( // concrete wrapper 1
-            new BorderDecorator( // concrete wrapper 2
-                new ScrollDecorator( // concrete wrapper 3
-                    new TextField(80, 24)))); // concreate component
-
-    aComponent->draw();
-    aComponent->resize();
+{
+    // Create ConcreteComponent and two Decorators
+    ConcreteComponent *c = new ConcreteComponent();
+    ConcreteDecoratorA *d1 = new ConcreteDecoratorA();
+    ConcreteDecoratorB *d2 = new ConcreteDecoratorB();
+ 
+    // Link decorators
+    d1->SetComponent(c);
+    d2->SetComponent(d1);
+ 
+    d2->Operation();
 }
+ 
