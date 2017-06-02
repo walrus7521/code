35b70992f1a8c2324d38c4a3c51938144a941c1c
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2017-06-01 21:23:32 -0700

    pa

--- toys/patts/strategy.cpp
+++ toys/patts/strategy.cpp
@@ -1,113 +1,56 @@
 #include <iostream>
 
 using namespace std;
- 
-class SortBehavior 
-{
-    public:
-        virtual void sort() const = 0;
+
+class Strategy { // abstract 
+public:
+    virtual void AlgorithmInterface() = 0;
 };
  
-class Merge: public SortBehavior 
-{
-    public:
-        virtual void sort() const {
-            cout << "Merge sort()\n";
-        }
+class ConcreteStrategyA : public Strategy {
+public:
+    void AlgorithmInterface() { // override 
+        cout << "Called ConcreteStrategyA.AlgorithmInterface()" << endl;
+    }
 };
  
-class Quick: public SortBehavior {
-    public:
-        virtual void sort() const {
-            cout << "Quick sort()\n";
-        }
+class ConcreteStrategyB : public Strategy {
+public:
+    void AlgorithmInterface() { // override 
+        cout << "Called ConcreteStrategyB.AlgorithmInterface()" << endl;
+    }
 };
  
-class Heap: public SortBehavior
-{
-    public:
-        virtual void sort() const {
-            cout << "Heap sort()\n";
-        }
+class ConcreteStrategyC : public Strategy {
+public:
+    void AlgorithmInterface() { // override 
+        cout << "Called ConcreteStrategyC.AlgorithmInterface()" << endl;
+    }
 };
  
-class SearchBehavior 
-{
-    public:
-        virtual void search() const = 0;
-};
- 
-class Sequential: public SearchBehavior 
-{
-    public:
-        virtual void search() const {
-            cout << "Sequential search()\n";
-        }
+class Context {
+private:
+    Strategy *_strategy;
+public: 
+    Context(Strategy *strategy) {
+        _strategy = strategy;
+    }
+    void ContextInterface() {
+        _strategy->AlgorithmInterface();
+    }
 };
 
-class BinaryTree: public SearchBehavior 
+int main()
 {
-    public:
-        virtual void search() const {
-            cout << "BinaryTree search()\n";
-        }
-};
-
-class HashTable: public SearchBehavior 
-{
-    public:
-        virtual void search() const {
-            cout << "HashTable search()\n";
-        }
-};
-
-// Context
-class Collection 
-{
-    private:
-        SortBehavior* m_sort;
-        SearchBehavior* m_search;
-    public:
-        Collection(){}
-        void set_sort(SortBehavior* s){
-            m_sort = s;
-        }
-        void set_search(SearchBehavior* s){
-            m_search = s;
-        }
-        void sort() const {
-            m_sort->sort();
-        }
-        void search() const {
-            m_search->search();
-        }
-};
+    Context *context;
+    // Three contexts following different strategies
+    context = new Context(new ConcreteStrategyA());
+    context->ContextInterface();
  
+    context = new Context(new ConcreteStrategyB());
+    context->ContextInterface();
  
-int main(int argc, char *argv[])
-{
-    // instantiate concrete sort algorithm classes
-    Merge merge;
-    Quick quick;
-    Heap heap;
-
-    // create a context class for sorting strategies
-    // and assign one of the concrete sort algorithms
-    Collection colA;
-    colA.set_sort(&merge);
-    colA.sort(); // run the concrete sort algorithm via the context
-
-    // instantiate concreate search algorithm classes
-    Sequential sequential;
-    BinaryTree binaryTree;
-    HashTable hashTable;
- 
-
-    // create a context class for searching strategies
-    // and assign one of the concrete search algorithms
-    Collection colB;
-    colB.set_search(&binaryTree);
-    colB.search();
-
-    return 0;
+    context = new Context(new ConcreteStrategyC());
+    context->ContextInterface();
 }
+
