aa2f5e7f0fe4e1c0d8a4292e26ee305e5f3df347
Bart Bartel <bartb7@gmail.com>
2017-06-04 13:41:10 -0700

    pa

--- toys/patts/absfact4.cpp
+++ toys/patts/absfact4.cpp
@@ -7,127 +7,108 @@ using namespace std;
 class AbstractProductA;
 class AbstractProductB;
 
-class AbstractFactory
-{
-protected:
+class AbstractFactory {
 public:
     string _name;
     virtual AbstractProductA *CreateProductA() = 0;
     virtual AbstractProductB *CreateProductB() = 0;
 };
  
-class ConcreteFactory1 : public AbstractFactory
-{
+class ConcreteFactory1 : public AbstractFactory {
 public:
     AbstractProductA *CreateProductA();
     AbstractProductB *CreateProductB();
 };
  
-class ConcreteFactory2 : public AbstractFactory
-{
+class ConcreteFactory2 : public AbstractFactory {
 public:
     AbstractProductA *CreateProductA();
     AbstractProductB *CreateProductB();
 };
  
-//class ProductA1;
-//class ProductB1;
-//class ProductA2;
-//class ProductB2;
-
-class AbstractProductA
-{
+class AbstractProductA {
 public:
     string _name;
+    virtual void Interact(AbstractProductB *a) = 0;
 };
  
-class AbstractProductB
-{
+class AbstractProductB {
 public:
     string _name;
     virtual void Interact(AbstractProductA *a) = 0;
 };
  
-class ProductA1 : public AbstractProductA
-{
-};
- 
-class ProductB1 : public AbstractProductB
-{
+class ProductA1 : public AbstractProductA {
 public:
-    void Interact(AbstractProductA *a)
-    {
-        cout << "Product B1 -> " << a->_name << endl;
-        cout << a->_name << endl;
+    ProductA1() { _name = "ProductA1"; }
+    void Interact(AbstractProductB *b) {
+        cout << _name << " -> " << b->_name << endl;
     }
 };
  
-class ProductA2 : public AbstractProductA
-{
-};
- 
-class ProductB2 : public AbstractProductB
-{
+class ProductB1 : public AbstractProductB {
 public:
-    void Interact(AbstractProductA *a)
-    {
-        cout << "Product B2 -> " << a->_name << endl;
+    ProductB1() { _name = "ProductB1"; }
+    void Interact(AbstractProductA *a) {
+        cout << _name << " -> " << a->_name << endl;
     }
 };
  
+class ProductA2 : public AbstractProductA {
+public:
+    ProductA2() { _name = "ProductA2"; }
+    void Interact(AbstractProductB *b) {
+        cout << _name << " -> " << b->_name << endl;
+    }
+};
+ 
+class ProductB2 : public AbstractProductB {
+public:
+    ProductB2() { _name = "ProductB2"; }
+    void Interact(AbstractProductA *a) {
+        cout << _name << " -> " << a->_name << endl;
+    }
+};
 
-AbstractProductA *ConcreteFactory1::CreateProductA()
-{
-    _name = "A1";
+AbstractProductA *ConcreteFactory1::CreateProductA() {
+    _name = "Factory1 for A";
     return new ProductA1();
 }
-AbstractProductB *ConcreteFactory1::CreateProductB()
-{
-    _name = "B1";
+AbstractProductB *ConcreteFactory1::CreateProductB() {
+    _name = "Factory1 for B";
     return new ProductB1();
 }
-
-AbstractProductA *ConcreteFactory2::CreateProductA()
-{
-    _name = "A2";
+AbstractProductA *ConcreteFactory2::CreateProductA() {
+    _name = "Factory2 for A";
     return (AbstractProductA *) new ProductA2();
 }
-AbstractProductB *ConcreteFactory2::CreateProductB()
-{
-    _name = "B2";
+AbstractProductB *ConcreteFactory2::CreateProductB() {
+    _name = "Factory2 for B";
     return (AbstractProductB *) new ProductB2();
 }
 
-class Client
-{
+class Client {
 private:
     AbstractProductA *_abstractProductA;
     AbstractProductB *_abstractProductB;
 public:
-    // Constructor
-    Client(AbstractFactory *factory)
-    {
+    Client(AbstractFactory *factory) {
       _abstractProductB = factory->CreateProductB();
       _abstractProductA = factory->CreateProductA();
     }
- 
-    void Run()
-    {
+    void Run() {
       _abstractProductB->Interact(_abstractProductA);
     }
 };
  
 int main()
 {
-    // Abstract factory #1
     AbstractFactory *factory1 = new ConcreteFactory1();
     Client *client1 = new Client(factory1);
     client1->Run();
  
-    // Abstract factory #2
     AbstractFactory *factory2 = new ConcreteFactory2();
     Client *client2 = new Client(factory2);
     client2->Run();
- 
 }
  
