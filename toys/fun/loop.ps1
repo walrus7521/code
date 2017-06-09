937c512d0df30f6b02d13a900bf2c5e1a86b988e
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2017-06-09 08:08:14 -0700

    fu

--- toys/fun/loop.ps1
+++ toys/fun/loop.ps1
@@ -1,5 +1,16 @@
 
 
+# https://ss64.com/ps/syntax-hash-tables.html
+
+[int[]] $myArray = @()
+
 for ($i=0; $i -lt 4; $i++) {
-    Write-Host "i = " + $i -ForegroundColor Magenta    
+    $myArray += $i
+    Write-Host "i = " + $myArray[$i] -ForegroundColor Magenta    
+}
+
+Write-Host "length = " + $myArray.length -ForegroundColor Blue    
+
+for ($i=0; $i -lt $myArray.length; $i++) {
+    Write-Host "i = " + $myArray[$i] -ForegroundColor Yellow    
 }
