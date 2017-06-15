92eeee8e4a2fe04e7c86405dcff7603dbb1d06d8
Bart Bartel <bartb7@gmail.com>
2017-06-14 20:58:15 -0700

    pa

--- toys/patts/dispatch.cpp
+++ toys/patts/dispatch.cpp
@@ -8,25 +8,24 @@ public:
     typedef int (Base::*GetNumStepsFn)( void* );
     typedef int (Base::*GetNextStepFn)( void* );
     typedef int (Base::*ExecuteStepFn)( void*, int );
+
     virtual void registerFn1( GetNumStepsFn fn ) = 0;
     virtual void registerFn2( GetNextStepFn fn ) = 0;
     virtual void registerFn3( ExecuteStepFn fn ) = 0;
 };
+
+class DLL;
 
 class Dispatch : public Base
 {
 private:
     int step = 0;
 public:
-    Dispatch()
-    {
-        //registerFn1( static_cast<GetNumStepsFn>(&DispatchGetNumSteps) );
-        //registerFn2( static_cast<GetNextStepFn>(&DispatchGetNextStep) );
-        //registerFn3( static_cast<ExecuteStepFn>(&DispatchExecuteStep) );
-    }
-    //int DispatchGetNumSteps(void *context) { GetNumStepsFn(nullptr); return 0; };
-    //int DispatchGetNextStep(void *context) { return ++step; };
-    //int DispatchExecuteStep(void *context, int step) { std::cout << "exec: " << step << std::endl; return 0; };
+    int dude = 42;
+    Dispatch() {}
+
+
+    void registerDLL( DLL *dll);
     void registerFn1( GetNumStepsFn fn ) {
         getNumSteps = fn;
     }
@@ -36,33 +35,45 @@ public:
     void registerFn3( ExecuteStepFn fn ) {
         executeStep = fn;
     }
-    GetNumStepsFn getNumSteps;
-    GetNextStepFn getNextStep;
-    ExecuteStepFn executeStep;
 
+    Base::GetNumStepsFn getNumSteps;
+    Base::GetNextStepFn getNextStep;
+    Base::ExecuteStepFn executeStep;
 };
 
-class DLL// : public Dispatch
+class DLL : public Dispatch
 {
 private:
     int step = 0;
     Dispatch *disp;
 public:
     DLL(Dispatch *d) : disp{d} {
-        _d->registerFn1( static_cast<GetNumStepsFn>(&DispatchGetNumSteps) );
-        _d->registerFn2( static_cast<GetNextStepFn>(&DispatchGetNextStep) );
-        _d->registerFn3( static_cast<ExecuteStepFn>(&DispatchExecuteStep) );
+        registerFn1( static_cast<Base::GetNumStepsFn>(&DLL::DispatchGetNumSteps) );
+        //d->registerFn2( static_cast<Dispatch::GetNextStepFn>(&DLL::DispatchGetNextStep) );
+        //d->registerFn3( static_cast<Dispatch::ExecuteStepFn>(&DLL::DispatchExecuteStep) );
     }
-    int DispatchGetNumSteps(void *context) { return 5; };
-    int DispatchGetNextStep(void *context) { return ++step; };
-    int DispatchExecuteStep(void *context, int step) { std::cout << "exec: " << step << std::endl; return 0; };
+    int DispatchGetNumSteps(void *context) { return 5; }
+    int DispatchGetNextStep(void *context) { return ++step; }
+    int DispatchExecuteStep(void *context, int step) { 
+        std::cout << "exec: " << 
+        step << std::endl; return 0; 
+    }
 };
+
+void Dispatch::registerDLL( DLL *dll ) {
+    this->getNumSteps = dll->DispatchGetNumSteps;
+}
+
+using namespace std;
 
 int main()
 {
     Dispatch *d = new Dispatch();
     DLL *dll = new DLL(d);
-    int num_steps = d->getNumSteps(nullptr);
+    int num_steps = d->dude; //getNumSteps(nullptr);
+    //auto steps = d->getNumSteps(nullptr);
+    //steps(nullptr);
+    cout << "num steps: " << num_steps << endl;
     //int next_step = 0;
     //for (int i = 0; i < num_steps; ++i) {
     //    d.DispatchExecuteStep(nullptr, next_step);
