54298150f5f6bf88bc7288dec9d2f9c2678d43a7
Bart Bartel <BartB@helitrak.com>
2017-06-26 16:31:24 -0700

    py

--- stuff/embed/fit.py
+++ stuff/embed/fit.py
@@ -24,7 +24,7 @@ for i in range(len(SPEEDS)):
     air_accum    = 0;
     air_avg      = 0;
     count        = 0;
-    csvfile = open(CSV_FILENAME, 'rb')
+    csvfile = open(CALIBRATION_FILENAME, 'rb')
     csv_rows = csv.reader(csvfile, delimiter=',', quotechar='|')
     if (SKIP_HEADER):
         next(csv_rows) # skip first line header
@@ -39,6 +39,7 @@ for i in range(len(SPEEDS)):
             air_array = np.append(air_array, [[int(speed),int(air_avg)]], axis=0)
             break;
 
+
 # airspeed calculation
 x = air_array[1:,0] # start on 1st element not zero-th
 y = air_array[1:,1] 
@@ -47,8 +48,46 @@ y = air_array[1:,1]
 z = np.polyfit(x, y, POLYNOMIAL_DEGREE) # 
 f = np.poly1d(z)
 
+print air_array
+#print x
+#print y
 
-# pick 9 speed values
+# now pick a speed and get a pressure
+spd = 40
+press = int(np.interp([spd], x, y))
+print "speed {0} => pressure {1}".format(spd, press)
+
+press = 4800
+spd = int(np.interp([press], y, x))
+print "pressure {0} => speed {1}".format(press, spd)
+
+# now interpolate a CSV based on the CAL data
+interpFile = open(INTERP_FILENAME, 'rb')
+csv_rows = csv.reader(interpFile, delimiter=',', quotechar='|')
+if (SKIP_HEADER):
+    next(csv_rows) # skip first line header
+for line in csv_rows:       
+    frame   = int(line[0].strip())
+    mode    = int(line[1].strip())
+    state   = int(line[2].strip())
+    trigger = int(line[3].strip())
+    air_raw0 = float(line[AIRSPEED_INDEX])
+    air_raw1 = float(line[AIRSPEED_INDEX+1])
+    air_cal0 = int(np.interp([air_raw0], y, x))
+    air_cal1 = int(np.interp([air_raw1], y, x))
+    alt_raw0 = int(line[8])
+    alt_raw1 = int(line[9])
+    alt_cal0 = int(line[10])
+    alt_cal1 = int(line[11])
+    print "{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}".format(
+            frame, mode, state, trigger, int(air_raw0), int(air_raw1), air_cal0, air_cal1, 
+            alt_raw0, alt_raw1, alt_cal0, alt_cal1)
+    #print "frame: {0} pressure {1} speed {2}".format(frame, air_raw0, air_cal0)
+
+exit()
+
+
+# pick data points
 min_value = SPEEDS[0]
 max_value = SPEEDS[len(SPEEDS)-1]
 print max_value
@@ -61,6 +100,8 @@ for v in my_range(0, max_value, value_spacing):
     interp = int(np.interp([v], x, y))
     air_linterp_array = np.append(air_linterp_array, [[interp,v,0]], axis=0)
     print "speed: {}, press: {}".format(v, interp)
+
+
 
 print air_linterp_array
 
@@ -77,6 +118,8 @@ for i in my_range(0, len(air_linterp_array)-1, 1):
 
 print air_linterp_array
 
+exit()
+
 print "LINTERP_STRUCT airspeed_0_linterp  = {{ {0}, {{".format(NUM_DATA_POINTS);
 print "                                     {{ {0}, {1}, {2:f} }},".format(air_linterp_array[1][0], air_linterp_array[1][1], air_linterp_array[1][2])
 print "                                     {{ {0}, {1}, {2:f} }},".format(air_linterp_array[2][0], air_linterp_array[2][1], air_linterp_array[2][2])
@@ -89,7 +132,6 @@ print "                                     {{ {0}, {1}, {2:f} }},".format(air_l
 print "                                     {{ {0}, {1}, {2:f} }},".format(air_linterp_array[9][0], air_linterp_array[9][1], air_linterp_array[9][2])
 print "                                     {{ {0}, {1}, {2:f} }} ".format(0.0, 0.0, 0.0)
 print "                                     }};"
-
 
 #exit()
 
