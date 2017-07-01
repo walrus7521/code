4060b564e531b0165251d48af4ce14e5619fdd70
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2017-06-30 21:23:23 -0700

    ap

--- toys/app/lispy/lispy.c
+++ toys/app/lispy/lispy.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "mpc.h"
+
 
 // linux
 // gcc -std=c99 -Wall lispy.c mpc.c -ledit -lreadline -o lispy
@@ -33,7 +35,78 @@ void add_history(char* unused) {
 //#include <editline/history.h>
 #endif
 
+enum { LVAL_NUM, LVAL_ERR };
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+
+int number_of_nodes(mpc_ast_t *t)
+{
+    if (t->children_num == 0) { return 1; }
+    if (t->children_num >= 1) {
+        int total = 1;
+        int i;
+        for (i = 0; i < t->children_num; i++) {
+            total = total + number_of_nodes(t->children[i]);
+        }
+        return total;
+    }
+    return 0;
+}
+
+long eval_op(long x, char *op, long y)
+{
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t)
+{
+    /* if tagged as number return it directly - base case */
+    if (strstr(t->tag, "number")) {
+        return (atoi(t->contents));
+    }
+
+    /* the operator is always the 2nd child, first is ( */
+    char *op = t->children[1]->contents;
+
+    /* store 3rd child in x */
+    long x = eval(t->children[2]);
+
+    /* iterate remaining children and combine */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+
+}
+
 int main(int argc, char** argv) {
+
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Lispy    = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                   \
+            number    : /-?[0-9]+/ ;                            \
+            operator  : '+' | '-' | '*' | '/' | '%' ;           \
+            expr      : <number> | '(' <operator> <expr>+ ')' ; \
+            lispy     : /^/ <operator> <expr>+ /$/ ;            \
+            ",
+        Number, Operator, Expr, Lispy);
 
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
@@ -49,11 +122,36 @@ int main(int argc, char** argv) {
         printf("bye bye\n");
         exit(0);
     }
-    printf("No you're a %s\n", input);
+
+    mpc_result_t r;
+    if (mpc_parse("<stdin", input, Lispy, &r)) {
+#if 0
+        mpc_ast_t* a = r.output;
+        printf("Tag: %s\n", a->tag);
+        printf("Contents: %s\n", a->contents);
+        printf("Num children: %d\n", a->children_num);
+        mpc_ast_t* c0 = a->children[0];
+        printf("First child tag: %s\n", c0->tag);
+        printf("First child contents: %s\n", c0->contents);
+        printf("First child Num children: %d\n", c0->children_num);
+
+        printf("number of nodes: %d\n", number_of_nodes(a));
+
+        mpc_ast_print(r.output);
+#endif
+        long result = eval(r.output);
+        printf("%li\n", result);
+        mpc_ast_delete(r.output);
+    } else {
+        mpc_err_print(r.error);
+        mpc_err_delete(r.error);
+    }
 
     free(input);
 
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
   return 0;
 }
