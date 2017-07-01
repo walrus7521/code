cb4c9040e8537e9dc1eb686d9075416feb5dff9f
Bart Bartel <bartb7@gmail.com>
2017-07-01 07:12:51 -0700

    li

--- toys/app/lispy/lispy.c
+++ toys/app/lispy/lispy.c
@@ -59,27 +59,72 @@ int number_of_nodes(mpc_ast_t *t)
     return 0;
 }
 
-long eval_op(long x, char *op, long y)
+lval lval_num(long x)
 {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    return 0;
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
 }
 
-long eval(mpc_ast_t* t)
+lval lval_err(int x)
+{
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+void lval_print(lval v)
+{
+    switch (v.type) {
+    case LVAL_NUM: printf("%li", v.num); break;
+    case LVAL_ERR: 
+        if (v.err == LERR_DIV_ZERO) {
+            printf("Error: Division by zero.");
+        } else if (v.err == LERR_BAD_OP) {
+            printf("Error: Invalid operand.");
+        } else if (v.err == LERR_BAD_NUM) {
+            printf("Error: Invalid number.");
+        }
+        break;
+    }
+}
+
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+lval eval_op(lval x, char *op, lval y)
+{
+    if (x.type == LVAL_ERR) { return x; }
+    if (y.type == LVAL_ERR) { return y; }
+
+    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+    if (strcmp(op, "/") == 0) { 
+        return y.num == 0 
+            ? lval_err(LERR_DIV_ZERO)
+            : lval_num(x.num / y.num); 
+    }
+
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t)
 {
     /* if tagged as number return it directly - base case */
     if (strstr(t->tag, "number")) {
-        return (atoi(t->contents));
+        /* chk if error in conversion */
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
     }
 
     /* the operator is always the 2nd child, first is ( */
     char *op = t->children[1]->contents;
 
     /* store 3rd child in x */
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     /* iterate remaining children and combine */
     int i = 3;
@@ -139,8 +184,8 @@ int main(int argc, char** argv) {
 
         mpc_ast_print(r.output);
 #endif
-        long result = eval(r.output);
-        printf("%li\n", result);
+        lval result = eval(r.output);
+        lval_println(result);
         mpc_ast_delete(r.output);
     } else {
         mpc_err_print(r.error);
