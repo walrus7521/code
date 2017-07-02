c1f170c843b2c767cdcfd7e561eead4508650926
Bart Bartel <bartb7@gmail.com>
2017-07-01 17:30:28 -0700

    li

--- toys/app/lispy/lispy.c
+++ toys/app/lispy/lispy.c
@@ -35,13 +35,19 @@ void add_history(char* unused) {
 //#include <editline/history.h>
 #endif
 
-enum { LVAL_NUM, LVAL_ERR };
+enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR };
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
-typedef struct {
+typedef struct lval {
     int type;
     long num;
-    int err;
+
+    char *err;
+    char *sym;
+
+    int count;
+    struct lval** cell;
+
 } lval;
 
 
@@ -59,20 +65,95 @@ int number_of_nodes(mpc_ast_t *t)
     return 0;
 }
 
-lval lval_num(long x)
+lval *lval_num(long x)
 {
-    lval v;
-    v.type = LVAL_NUM;
-    v.num = x;
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_NUM;
+    v->num = x;
     return v;
 }
 
-lval lval_err(int x)
+lval *lval_err(char *m)
 {
-    lval v;
-    v.type = LVAL_ERR;
-    v.err = x;
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_ERR;
+    v->err = malloc(srlen(m)+1);
+    strcpy(v->err, m);
     return v;
+}
+
+lval *lval_sym(char *s)
+{
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_SYM;
+    v->sym = malloc(srlen(s)+1);
+    strcpy(v->sym, s);
+    return v;
+}
+
+lval *lval_sexpr(void)
+{
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_SEXPR;
+    v->count = 0;
+    v->cell = NULL;
+    return v;
+}
+
+void lval_del(lval *v)
+{
+    switch (v->type) {
+    case LVAL_NUM: break;
+    case LVAL_ERR: free(v->err); break;
+    case LVAL_SYM: free(v->sym); break;
+    case LVAL_SEXPR:
+        for (int i = 0; i < v->count; i++) {
+            lval_del(v->cell[i]);
+        }
+        free(v->cell);
+        break;
+    }
+    free(v);
+}
+
+lval* lval_read_num(mpc_ast_t *t)
+{
+    errno = 0;
+    long x = sgtrtol(t->contents, NULL, 10);
+    return errno != ERANGE ?
+        lval_num(x) : lval_err("invalid number");
+}
+
+lval* lval_add(lval *v, lval *x)
+{
+    v->count++;
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    v->cell[v->count-1] = x;
+    return v;
+}
+
+lval* lval_read(mcp_ast_t *t)
+{
+    // sym or num return conversion to that type
+    if (strstr(t->tag, "number")) { return lval_read_num(t); }
+    if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+    // if root (>) or sexpr then create empty list
+    lval *x = NULL;
+    if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+    if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
+
+    // fill this list with any valid expression contained within
+    for (int i = 0; i < t->children_num; i++) {
+        if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
+        if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
+        x = lval_add(x, lval_read(t->children[i]));
+    }
+
+    return x;
 }
 
 void lval_print(lval v)
