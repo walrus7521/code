d9ad2ab06d9ad1abb623d3e958980d4fad4276d1
Bart Bartel <bartb7@gmail.com>
2017-07-02 12:52:12 -0700

    li

--- toys/app/lispy/lispy.c
+++ toys/app/lispy/lispy.c
@@ -35,7 +35,7 @@ void add_history(char* unused) {
 //#include <editline/history.h>
 #endif
 
-enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR };
+enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR };
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
 typedef struct lval {
@@ -106,12 +106,22 @@ lval *lval_sexpr(void)
     return v;
 }
 
+lval *lval_qexpr(void)
+{
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_QEXPR;
+    v->count = 0;
+    v->cell = NULL;
+    return v;
+}
+
 void lval_del(lval *v)
 {
     switch (v->type) {
     case LVAL_NUM: break;
     case LVAL_ERR: free(v->err); break;
     case LVAL_SYM: free(v->sym); break;
+    case LVAL_QEXPR:
     case LVAL_SEXPR:
         for (int i = 0; i < v->count; i++) {
             lval_del(v->cell[i]);
@@ -148,6 +158,7 @@ lval* lval_read(mpc_ast_t *t)
     lval *x = NULL;
     if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
     if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
+    if (strstr(t->tag, "qexpr"))  { x = lval_qexpr(); }
 
     // fill this list with any valid expression contained within
     for (int i = 0; i < t->children_num; i++) {
@@ -169,6 +180,7 @@ void lval_print(lval *v)
     case LVAL_ERR:   printf("Error: %s", v->err); break;
     case LVAL_SYM:   printf("%s", v->sym); break;
     case LVAL_SEXPR: lval_expr_print(v, '(', ')'); break;
+    case LVAL_QEXPR: lval_expr_print(v, '{', '}'); break;
     }
 }
 
@@ -291,23 +303,26 @@ lval *builtin_op(lval *a, char *op)
 }
 
 
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv) 
+{
     mpc_parser_t* Number   = mpc_new("number");
     mpc_parser_t* Symbol   = mpc_new("symbol");
     mpc_parser_t* Sexpr    = mpc_new("sexpr");
+    mpc_parser_t* Qexpr    = mpc_new("qexpr");
     mpc_parser_t* Expr     = mpc_new("expr");
     mpc_parser_t* Lispy    = mpc_new("lispy");
 
     mpca_lang(MPCA_LANG_DEFAULT,
-            "                                           \
-            number    : /-?[0-9]+/ ;                    \
-            symbol    : '+' | '-' | '*' | '/' | '%' ;   \
-            sexpr     : '(' <expr>* ')' ;               \
-            expr      : <number> | <symbol> | <sexpr> ; \
-            lispy     : /^/ <expr>* /$/ ;               \
+            "                                                         \
+            number    : /-?[0-9]+/ ;                                  \
+            symbol    : \"list\" | \"head\" | \"tail\" ;              \
+                      | \"join\" | \"eval\" | '+' | '-' | '*' | '/' ; \
+            sexpr     : '(' <expr>* ')' ;                             \
+            qexpr     : '{' <expr>* '}' ;                             \
+            expr      : <number> | <symbol> | <sexpr> | <qexpr> ;     \
+            lispy     : /^/ <expr>* /$/ ;                             \
             ",
-        Number, Symbol, Sexpr, Expr, Lispy);
+        Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
 
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
@@ -319,7 +334,7 @@ int main(int argc, char** argv) {
 
         add_history(input);
 
-        if (strncmp(input, "exit", 4) == 0) {
+        if (strncmp(input, "quit", 4) == 0) {
             printf("bye bye\n");
             exit(0);
         }
@@ -339,7 +354,7 @@ int main(int argc, char** argv) {
 
     }
 
-    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
+    mpc_cleanup(6, Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
 
     return 0;
 }
