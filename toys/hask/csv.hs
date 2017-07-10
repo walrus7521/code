b66c8dab424e8910cb6447316ce4abfa4c9fbad8
Bart Bartel <BartB@helitrak.com>
2017-07-10 16:44:20 -0700

    ha

---
+++ toys/hask/csv.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE BangPatterns, ScopedTypeVariables #-}
+
+-- uses: cabal install cassava
+--
+
+-- sample CSV content:
+-- sumSalaries rs = sum [salary | Right (_ :: String, salary :: Int) <- rs]
+-- John Doe,50000
+-- Jane Doe,60000
+
+import Control.Monad
+import qualified Data.ByteString as B
+import Data.Csv.Incremental
+import System.Exit
+import System.IO
+
+main :: IO ()
+main = withFile "test.csv" ReadMode $ \ csvFile -> do
+    let loop !_ (Fail _ errMsg) = putStrLn errMsg >> exitFailure
+        loop acc (Many rs k)    = loop (acc + sumPullDowns rs) =<< feed k
+        loop acc (Done rs)      = putStrLn $ "Total pulldowns: " ++
+                                  show (sumPullDowns rs + acc)
+
+        feed k = do
+            isEof <- hIsEOF csvFile
+            if isEof
+                then return $ k B.empty
+                else k `fmap` B.hGetSome csvFile 4096
+    loop 0 (decode NoHeader)
+  where
+    sumPullDowns rs = sum [pulldown | Right (frame :: Int, bogus1 :: Int, pulldown :: Int, bogus2 :: Int) <- rs]
+
+
