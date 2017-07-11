741f052e395a06b8a78217e83ecb047c56b3b4e3
Bart Bartel <BartB@helitrak.com>
2017-07-11 09:55:55 -0700

    ha

--- toys/hask/part1.hs
+++ toys/hask/part1.hs
@@ -2,6 +2,11 @@
 
 -- main = putStrLn "Hello, world!"
 
+x1 :: Int
+x1 = 3
+y1 :: Int --Float
+y1 = 4
+
 main = do
   putStrLn "What is your name?"
   name <- getLine
@@ -13,7 +18,11 @@ main = do
   print $ g 3 2
   print $ h 3 2
   print (f 2.3 4.2)
-  print $ j 1 2
+  print $ j 1
+  print $ k 1 2
+  print $ m 1
+
+  print (f2 x1 y1)
 
 -- this is a function, if you don't declare type
 -- it essentially becomes polymorphic
@@ -24,4 +33,17 @@ g x y = x*x - y*y + x - y
 h :: Int -> Int -> Int
 h x y = x + y
 
-j x y "hello"
+-- From Int to Float
+j :: Int -> Float
+j x = 3.14
+
+k :: Num a => a -> a -> a
+k x y = x+y
+m :: Num a => a -> a
+-- m = k 3
+m = \y -> 3 + y
+-- m y <=> 3*3 + y*y
+
+f2 :: Num a => a -> a -> a
+f2 x y = x*x + y*y
+
