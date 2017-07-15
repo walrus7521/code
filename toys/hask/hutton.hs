3059e20a43e553ec1ba709ad3dc14c3021c167f2
Bart Bartel <bartb7@gmail.com>
2017-07-15 04:42:07 -0700

    ha

--- toys/hask/hutton.hs
+++ toys/hask/hutton.hs
@@ -2,4 +2,31 @@
 
 double x = x + x
 
-main = print $ double 4
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+    where
+        smaller = [a | a <- xs, a <= x]
+        larger  = [b | b <- xs, b > x]
+
+seqn :: Monad m => [m a] -> m [a]
+seqn []         = return []
+seqn (act:acts) = do x <- act
+                     xs <- seqn acts
+                     return (x:xs)
+
+main = do
+    print $ double 4
+    print $ qsort [3,5,1,4,2]
+    print $ qsort ['n','x','e','b','w']
+    print $ qsort ["yes","no","maybe"]
+    print $ [7,8,9] ++ [42,43,44]
+    print $ seqn [qsort [1,3,2], qsort [7,2,4]]
+
+
+    
+    
+
+
+
+
