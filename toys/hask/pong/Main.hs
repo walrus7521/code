5f65c3cb245beed93a0144c035b959b7f5e065b8
Bart Bartel <BartB@helitrak.com>
2017-07-26 13:06:58 -0700

    ha

--- toys/hask/pong/Main.hs
+++ toys/hask/pong/Main.hs
@@ -17,16 +17,79 @@ drawing :: Picture
 drawing = pictures [ball, walls,
                     mkPaddle rose 120 (-20),
                     mkPaddle orange (-120) 40]
-
-  [ translate (-20) (-100) $ color ballColor $ circleSolid 30
-  , translate (30) (50) $ color paddleColor $ rectangleSolid 10 50
-  ]
   where
-    ballColor = dark red 
+    -- the pong ball
+    ball = translate (-10) 40 $ color ballColor $ circleSolid 10
+    ballColor = dark red
+
+    -- bottom and top walls
+    wall :: Float -> Picture
+    wall offset =
+        translate 0 offset $
+            color wallColor $
+                rectangleSolid 270 10
+
+    wallColor = greyN 0.5
+    walls = pictures [wall 150, wall (-150)]
+
+    -- make a paddle of a given border and vert offset
+    mkPaddle :: Color -> Float -> Float -> Picture
+    mkPaddle col x y = pictures
+        [ translate x y $ color col $ rectangleSolid 26 86
+        , translate x y $ color paddleColor $ rectangleSolid 20 80
+        ]
     paddleColor = light (light blue)
 
+-- | Data describing the state of the pong game.
+data PongGame = Game
+    { ballLoc :: (Float, Float) -- ^ Pong ball (x,y) location.
+    , ballVel :: (Float, Float) -- ^ Pong ball (x,y) velocity.
+    , player1 :: Float          -- ^ Left player paddle height.
+                                -- Zero is middle of the screen.
+    , player2 :: Float          -- ^ Right player paddle height.
+    } deriving Show
+
+-- | The starting state for the game of Pong.
+initialState :: PongGame
+initialState = Game
+    { ballLoc = (-10, 30)
+    , ballVel = (1, -3)
+    , player1 = 40
+    , player2 = (-80)
+    }
+
+-- | Convert a game state into a picture.
+render :: PongGame  -- ^ The game state to render.
+        -> Picture  -- ^ A picture of this game state.
+render game = 
+  pictures [balls, walls,
+            mkPaddle rose 120 $ player1 game,
+            mkPaddle orange (-120) $ player2 game]
+  where
+    -- The pong ball.
+    ball = uncurry translate (ballLoc game) $ color ballColor $
+    circleSolid 10
+    ballColor = dark red
+
+    -- The bottom and top walls.
+    wall :: Float -> Picture
+    wall offset = 
+      translate 0 offset $
+        color wallColor $
+          rectangleSolid 270 10
+
+    wallColor = greyN 0.5
+    walls = pictures [wall 150, wall (-150)]
+
+    -- Make a paddle of a given border and vertical offset.
+    mkPaddle :: Color -> Float -> Float -> Picture
+    mkPaddle col x y = pictures
+      [ translate x y $ color col $ rectangleSolid 26 86
+      , translate x y $ color paddleColor $ rectangleSolid 20 80
+      ]
+
+    paddleColor = light (light blue)
 
 main :: IO ()
 main = display window background drawing
 
---main = putStrLn "Hello, Haskell!"
