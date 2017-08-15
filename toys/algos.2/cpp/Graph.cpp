abde069fea17d209bee2519d632df05d79a8dcb5
Bart Bartel <bartb7@gmail.com>
2017-08-15 07:56:48 -0700

    cpt

--- toys/algos.2/cpp/Graph.cpp
+++ toys/algos.2/cpp/Graph.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <limits>
 
 using namespace std;
 
@@ -76,9 +77,62 @@ void test_dfs()
         cout << "Graph is not connected" << endl;
 }
 
+template <typename T>
+bool bellman_ford(GraphPtr<T> G, int source)
+{
+    vector<double> dis_to_source(G->mat.size(), numeric_limits<double>::max());
+    dis_to_source[source] = 0;
+
+    for (size_t times = 1; times < G->mat.size(); ++times) {
+        bool have_update = false;
+        for (size_t i = 0; i < G->mat.size(); ++i) {
+            for (size_t j = 0; j < G->mat[i].size(); ++j) {
+                if (dis_to_source[i] != numeric_limits<double>::max() &&
+                    dis_to_source[j] > dis_to_source[i] + G->mat[i][j]) {
+                    have_update = true;
+                    dis_to_source[j] = dis_to_source[i] + G->mat[i][j];
+                }
+            }
+        }
+        if (have_update == false) {
+            return false;
+        }
+    }
+    // detects cycle if there is any further update
+    for (size_t i = 0; i < G->mat.size(); ++i) {
+        for (size_t j = 0; j < G->mat[i].size(); ++j) {
+            if (dis_to_source[i] != numeric_limits<double>::max() &&
+                dis_to_source[j] > dis_to_source[i] + G->mat[i][j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+
+}
+
+void test_bell()
+{
+    GraphPtr<double> g = create<double>(6);
+    g->mat = { { 0, 0, 1, 1, 1, 0 }, 
+               { 0, 0, 0, 0, 1, 1 }, 
+               { 1, 0, 0, 1, 0, 1 }, 
+               { 1, 0, 1, 0, 0, 0 }, 
+               { 1, 1, 0, 0, 0, 1 }, 
+               { 0, 1, 1, 0, 1, 0 } };
+
+    graph_show(g, "bellman");
+    int source = 3;
+    for (int i = 0; i < 6; ++i) {
+        printf("bell[%d]: %d\n", i, bellman_ford(g, i));
+    }
+
+}
+
 int main()
 {
-    test_dfs();
+    //test_dfs();
+    test_bell();
     return 0;
 }
 
