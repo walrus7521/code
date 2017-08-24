17d1791bc76000f2fc02fbf02fcafad66168b22e
Bart Bartel <bartb7@gmail.com>
2017-08-24 04:24:09 -0700

    agc

--- toys/algos.2/grokkage/knap.c
+++ toys/algos.2/grokkage/knap.c
@@ -1,47 +1,8 @@
 #include <stdio.h>
 
+// add collection of items chosen as output
+
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
-#define WEIGHT (0)
-#define VALUE  (1)
-
-int sack[3][3];
-int items[3][3] = {{1,1500},{4,3000},{3,2000}};
-int sizes[3] = {1,2,3};
-
-void show()
-{
-    int r, c;
-    for (r = 0; r < 3; r++) {
-        printf("(%d,%d)\n", items[r][WEIGHT], items[r][VALUE]);
-    }
-    for (r = 0; r < 3; r++) {
-        for (c = 0; c < 3; c++) {
-            printf("%d ", sack[r][c]);
-        }
-        printf("\n");
-    }
-}
-
-// for a given row, find max for a given column
-int get_max(int row, int col)
-{
-    static int max = 0;
-    int r, c;
-    printf("find max for row %d, col %d\n", row, col);
-    for (r = 0; r <= row; r++) {
-        int wt_of_row = items[r][WEIGHT];
-        int val = items[r][VALUE];
-        for (c = 0; c <= col; c++) {
-            int wt_cap_of_col = sizes[c];
-            if (wt_of_row <= wt_cap_of_col && val > max) {
-                max = val;
-                printf("setting max = %d\n", max);
-            }
-        }
-    }
-    printf("max[%d,%d] = %d\n", row, col, max);
-    return max;
-}
 
 // Returns the maximum value that can be put in a knapsack of capacity W
 int knapper(int W, int wt[], int val[], int n)
@@ -56,63 +17,69 @@ int knapper(int W, int wt[], int val[], int n)
        return knapper(W, wt, val, n-1);
    }
  
+   else {
    // Return the maximum of two cases: 
    // (1) nth item included 
    // (2) not included
-   else return MAX( val[n-1] + knapper(W-wt[n-1], wt, val, n-1),
-                    knapper(W, wt, val, n-1)
-                  );
+       int val1 = val[n-1] + knapper(W-wt[n-1], wt, val, n-1);
+       int val2 = knapper(W, wt, val, n-1);
+       return MAX(val1, val2);
+   }
 }
 
-int max(int a, int b) { return (a > b)? a : b; }
-// Returns the maximum value that can be put in a knapsack of capacity W
-int knappy2(int W, int wt[], int val[], int n)
+
+ // Returns the maximum value that can be put in a knapsack of capacity W
+int knappy(int W, int wt[], int val[], int n)
 {
    int i, w;
    int K[n+1][W+1];
    // Build table K[][] in bottom up manner
-   for (i = 0; i <= n; i++)
-   {
-       for (w = 0; w <= W; w++)
-       {
-           if (i==0 || w==0) K[i][w] = 0;
-           else if (wt[i-1] <= w) K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
-           else K[i][w] = K[i-1][w];
+   for (i = 0; i <= n; i++) {
+       for (w = 0; w <= W; w++) {   
+           // Base case
+           if (i == 0 || w == 0) {
+               K[i][w] = 0;
+           }
+
+           // if weight of ith item if more than knapsack capacity, then
+           // this item can't be included in optimal solution
+           else if (wt[i-1] > w) {
+               K[i][w] = K[i-1][w];
+           } 
+           
+           else {
+           // pick max of
+           // (1) ith item included
+           // (2) not included
+               int val1 = val[i-1] + K[i-1][w-wt[i-1]];
+               int val2 = K[i-1][w];
+               K[i][w] = MAX(val1, val2);
+           }
        }
    }
+   // looks like its always the last entry
    return K[n][W];
 }
  
-void knap()
-{
-    int mx = get_max(0,0);
-        //mx = get_max(0,1);
-        //mx = get_max(0,2);
-}
+int val[] = {60, 100, 120, 150, 200};
+int wt[]  = {10,  20,  30,   4,  32};
+int n = sizeof(val)/sizeof(val[0]);
 
 void knap2()
 {
-    int val[] = {60, 100, 120};
-    int wt[] = {10, 20, 30};
-    int W = 50;
-    int n = sizeof(val)/sizeof(val[0]);
-    printf("%d\n", knapper(W, wt, val, n));
+    int W = 50; // capacity of knapsack
+    printf("knapper = %d\n", knapper(W, wt, val, n));
 }
 
 void knap3()
 {
-    int val[] = {60, 100, 120};
-    int wt[] = {10, 20, 30};
-    int  W = 50;
-    int n = sizeof(val)/sizeof(val[0]);
-    printf("\nValue = %d", knappy2(W, wt, val, n));
+    int  W = 50; // capacity of knapsack
+    printf("knappy  = %d\n", knappy(W, wt, val, n));
 }
 
 int main()
 {
-    knap();
     knap2();
-    knap3(); // best code so far
-    show();
+    knap3();
 }
 
