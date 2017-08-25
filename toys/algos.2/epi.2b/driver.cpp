542062ec0e5c96ba700d2494692eb259f88a797f
Bart Bartel <BartB@helitrak.com>
2017-08-25 09:58:02 -0700

    dr

--- toys/algos.2/epi.2b/driver.cpp
+++ toys/algos.2/epi.2b/driver.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 #include "Backtrack.hpp"
 void test_backtrack()
 {
@@ -32,11 +34,63 @@ void test_dynamic()
  
 }
 
+#include "Graph.hpp"
+void test_graph()
+{
+    // Create a graph given in the above diagram
+    Graph g(4);
+    //       v1 v2  data   wt
+    g.addEdge(0, 1, "abe", 1);
+    g.addEdge(0, 2, "boe", 1);
+    g.addEdge(1, 2, "cal", 1);
+    g.addEdge(2, 0, "doe", 1);
+    g.addEdge(2, 3, "egg", 1);
+    g.addEdge(3, 3, "fog", 1);
+ 
+    g.show();
+
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 2) \n";
+    g.Bfs(2);
+    cout << endl;
+}
+
+#if 0
+#include "hash.h"
+void test_hash()
+{
+    hash *h = createHash(13);
+
+    insertHash(h, "abe", 0);
+    insertHash(h, "boa", 1);
+    insertHash(h, "cod", 2);
+    insertHash(h, "dia", 3);
+    insertHash(h, "eat", 4);
+    insertHash(h, "fat", 5);
+
+    showHash(h);
+#if 0
+
+    cout << "h(7)?: " << h.lookup("goa", 7) << endl;
+    cout << "h(4)?: " << h.lookup("eat", 4) << endl;
+
+    pair<string, int> p = h.fetch("eat", 4);
+    cout << "f(4): " << p.first << ": " << p.second << endl;
+    pair<string, int> p2 = h.fetch("goe", 7);
+    cout << "f(7): " << p2.first << ": " << p2.second << endl;
+#endif
+
+}
+#endif
+
 // driver program to test above function
 int main()
 {
     test_backtrack();
     test_bits();
     test_dynamic();
+    test_graph();
+
+    
     return 0;
 }
