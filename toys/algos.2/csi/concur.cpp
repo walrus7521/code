ec4f2986bfd62e53232c5675b097895d0b791af5
Bart Bartel <bartb7@gmail.com>
2017-08-26 13:39:23 -0700

    csi

--- toys/algos.2/csi/concur.cpp
+++ toys/algos.2/csi/concur.cpp
@@ -1,11 +1,55 @@
-#include "pch.hpp"
-#include "Concur.hpp"
+#ifndef _CONCUR_H
+#define _CONCUR_H
+
+#include <thread>
+#include <iostream>
+#include <deque>
+
+struct Buffer
+{
+    // Add them as member variables here
+    std::mutex mu, cout_mu;
+    std::condition_variable cond;
+
+    // Your normal variables here
+    std::deque<int> buffer_;
+    const unsigned int size_ = 10;
+
+};
+
+void Buffer_add(Buffer *buf, int num) {
+    while (true) {
+        std::unique_lock<std::mutex> locker(buf->mu);
+
+        buf->cond.wait(locker, [buf](){return buf->buffer_.size() < buf->size_;});
+        buf->buffer_.push_back(num);
+        locker.unlock();
+        buf->cond.notify_all();
+        return;
+    }
+}
+
+int Buffer_remove(Buffer *buf) {
+    while (true)
+    {
+        std::unique_lock<std::mutex> locker(buf->mu);
+        buf->cond.wait(locker, [buf](){return buf->buffer_.size() > 0;});
+        int back = buf->buffer_.back();
+        buf->buffer_.pop_back();
+        locker.unlock();
+        buf->cond.notify_all();
+        return back;
+    }
+}
+
+
+
+#endif // _CONCUR_H
 
 // interface-ize this
 // http://codereview.stackexchange.com/questions/84109/a-multi-threaded-producer-consumer-with-c11
 
 using namespace std;
-
 
 void producer(Buffer *b)
 {
