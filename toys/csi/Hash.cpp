a4d9383293feacf55ca03d43303cf67141fe60a1
Bart Bartel <bartb7@gmail.com>
2017-08-27 14:50:41 -0700

    ha

--- toys/csi/Hash.cpp
+++ toys/csi/Hash.cpp
@@ -7,32 +7,32 @@ using namespace std;
 
 struct ContactList {
     // Equal function for hash.
-    bool operator==(const ContactList & that) const {
-        return unordered_set <string >( names.begin (), names.end ()) ==
-        unordered_set <string >( that.names.begin (), that.names.end ());
+    bool operator==(const ContactList& that) const {
+        return unordered_set<string>(names.begin(), names.end()) ==
+        unordered_set<string>(that.names.begin(), that.names.end());
     }
     vector <string > names;
 };
 
 // Hash function for ContactList .
 struct HashContactList {
-    size_t operator()(const ContactList & contacts) const {
+    size_t operator()(const ContactList& contacts) const {
         size_t hash_code = 0;
-        for (const string& name : unordered_set <string >( contacts .names.begin (),
-            contacts.names.end ())) {
-                hash_code ^= hash <string >()(name);
+        for (const string& name : unordered_set<string>(contacts.names.begin(),
+            contacts.names.end())) {
+                hash_code ^= hash<string>()(name); // std::hash<T> built in library!!
         }
         return hash_code ;
     }
 };
 
-vector <ContactList > MergeContactLists (const vector <ContactList >& contacts) {
-    unordered_set <ContactList , HashContactList > unique_contacts (
-    contacts.begin (), contacts.end ());
-    return { unique_contacts .begin (), unique_contacts .end ()};
+vector <ContactList> MergeContactLists (const vector <ContactList>& contacts) {
+    unordered_set<ContactList, HashContactList> unique_contacts (
+        contacts.begin(), contacts.end());
+    return {unique_contacts.begin(), unique_contacts.end()};
 }
 
-void list(ContactList& cl)
+void Show(ContactList& cl)
 {
     for (std::vector<string>::iterator si = cl.names.begin();
             si != cl.names.end(); ++si) {
@@ -54,14 +54,13 @@ void test()
     vcl.push_back(cl);
     vcl.push_back(cl2);
 
-    //list(cl);
-    //list(cl2);
-    //cout << endl;
+    //Show(cl);
+    //Show(cl2);
 
     vector<ContactList> nvcl = MergeContactLists(vcl);
     for (std::vector<ContactList>::iterator si = nvcl.begin();
              si != nvcl.end(); ++si) {
-        list(*si);
+        Show(*si);
     }
 
 }
