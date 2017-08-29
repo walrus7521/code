e86da3efd23c38a4114922715b90a6bc55f6863a
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2017-08-29 08:38:51 -0700

    csi

---
+++ toys/csi/libs.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class libs
+{
+    public static void main (String[] args)
+    {
+        // Type safe array list, stores only string
+        List<String> l = new ArrayList<String>(5);
+ 
+        l.add("GeeksforGeeks");
+        l.add("Practice");
+        l.add("GeeksQuiz");
+        l.add("IDE");
+        l.add("Courses");
+ 
+        List<String> range = new ArrayList<String>();
+ 
+        // return List between 2nd(including)
+        // and 4th element(excluding)
+        range = l.subList(2, 4);
+ 
+        System.out.println(range);  // [GeeksQuiz, IDE]
+    }
+}
