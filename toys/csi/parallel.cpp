eceee3c057b648be402fc64a3df2019f5b26f343
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2017-08-29 20:43:59 -0700

    cs

--- toys/csi/parallel.cpp
+++ toys/csi/parallel.cpp
@@ -3,15 +3,17 @@
 #include <mutex>              // std::mutex, std::unique_lock
 #include <condition_variable> // std::condition_variable
 
+using namespace std;
+
 std::mutex mtx;
 std::condition_variable cv;
 bool ready = false;
 
-void print_id (int id) {
+void print_id (int id, string s) {
   std::unique_lock<std::mutex> lck(mtx);
   while (!ready) cv.wait(lck);
   // ...
-  std::cout << "thread " << id << '\n';
+  std::cout << "thread: " << s << " id: " << id << '\n';
 }
 
 void go() {
@@ -24,10 +26,13 @@ int main ()
 {
   std::thread threads[10];
   // spawn 10 threads:
-  for (int i=0; i<10; ++i)
-    threads[i] = std::thread(print_id,i);
+  for (int i=0; i<10; ++i) threads[i] = std::thread(print_id,i,"dude");
 
   std::cout << "10 threads ready to race...\n";
+  for (int i = 0; i < 4; ++i) {
+      std::cout << "dude    . \r";// << endl;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
   go();                       // go!
 
   for (auto& th : threads) th.join();
