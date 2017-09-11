5d8c7b673e5bc4220028f16a46cdd7a440103222
Bart Bartel <BartB@helitrak.com>
2017-09-11 10:57:20 -0700

    st

--- toys/grokkage/ring2.c
+++ toys/grokkage/ring2.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
 
 
 // adt
-uint8_t capacity = 8;
-int array[8];
+// private
+uint8_t capacity; // = 8;
+int *array; //int array[8];
 uint32_t read;
 uint32_t write;
+// public methods
 int size()     { return write - read; }
 int empty()    { return read == write; }
 int full()     { return size() == capacity; }
@@ -15,6 +18,11 @@ int mask(val)  { return val & (capacity - 1); }
 void push(val) { assert(!full()); array[mask(write++)] = val; }
 int shift()    { assert(!empty()); return array[mask(read++)]; }
 
+void init(int size)
+{
+    capacity = size;
+    array = (int *) malloc(size * sizeof(int));
+}
 
 void dump()
 {
@@ -24,9 +32,11 @@ void dump()
     }
 }
 
+#define RING_SIZE (8)
 int main()
 {
     int i;
+    init(RING_SIZE); // must be power-of-two size
     for (i = 0; i < 32; i++) {
         printf("mask(%02d) = %02d\n", i, mask(i));
         if (!full()) {
@@ -36,7 +46,6 @@ int main()
     printf("full : %d\n", full());
     printf("empty: %d\n", empty());
     printf("size : %d\n", size());
-    printf("mask : %d\n", mask(8));
     while (!empty()) {
         printf("shift: %d\n", shift());
     }
