e12c758af175521b816c49509584bfc0660d4c83
Bart Bartel <BartB@helitrak.com>
2017-09-11 21:09:45 -0700

    st

--- toys/grokkage/ring2.c
+++ toys/grokkage/ring2.c
@@ -5,65 +5,97 @@
 
 #include "ring2.h"
 
+struct ring2 {
+    uint32_t capacity;
+    uint32_t read;
+    uint32_t write;
+    void **arr;
+};
 
 struct ring2* create(int size)
 {
-    capacity = size;
-    array = (int *) malloc(size * sizeof(int));
+    struct ring2 *r = (struct ring2 *) malloc(sizeof(struct ring2));
+    r->capacity = size;
+    r->write = r->read = 0;
+    r->arr = (void **) malloc(size * sizeof(void *));
+}
+
+void destroy(struct ring2 *r)
+{
+    free(r->arr);
+    free(r);
 }
 
 int size(struct ring2 *r)
 {
+    return (r->write - r->read);
 }
 
 int empty(struct ring2 *r)
 {
+    return (r->read == r->write);
 }
 
 int full(struct ring2 *r)
 {
+    return (size(r) == r->capacity);
 }
 
-int mask(struct ring2 *r, e_type val)
+int mask(struct ring2 *r, uint32_t index)
 {
+    return (index & (r->capacity - 1));
 }
 
-void push(struct ring2 *r, e_type val)
+void push(struct ring2 *r, void *val)
 {
+    assert(!full(r)); r->arr[mask(r, r->write++)] = val; 
 }
 
 e_type shift(struct ring2 *r)
 {
+    assert(!empty(r)); return r->arr[mask(r, r->read++)];
 }
 
 void dump(struct ring2 *r)
 {
     int i;
-    for (i = 0; i < capacity; i++) {
-        printf("a[%d] = %d\n", i, array[i]);
+    for (i = 0; i < r->capacity; i++) {
+        printf("a[%02d] = %08x\n", i, r->arr[i]);
     }
 }
 
-
-#if 0
 #define RING_SIZE (8)
-int main()
+void ring2_test()
 {
     int i;
-    init(RING_SIZE); // must be power-of-two size
+    struct ring2 *r1 = create(8);
+    struct ring2 *r2 = create(16);
     for (i = 0; i < 32; i++) {
-        printf("mask(%02d) = %02d\n", i, mask(i));
-        if (!full()) {
-            push(i);
+        printf("mask1(%02d) = %02d\n", i, mask(r1, i));
+        if (!full(r1)) {
+            push(r1, (void *) i);
+        }
+        printf("mask2(%02d) = %02d\n", i, mask(r2, i));
+        if (!full(r2)) {
+            push(r2, (void *) i);
         }
     }
-    printf("full : %d\n", full());
-    printf("empty: %d\n", empty());
-    printf("size : %d\n", size());
-    while (!empty()) {
-        printf("shift: %d\n", shift());
+    printf("full : %d\n", full(r1));
+    printf("empty: %d\n", empty(r1));
+    printf("size : %d\n", size(r1));
+
+    printf("full : %d\n", full(r2));
+    printf("empty: %d\n", empty(r2));
+    printf("size : %d\n", size(r2));
+    
+    while (!empty(r1)) {
+        printf("shift1: %d\n", shift(r1));
     }
-    dump();
+    while (!empty(r2)) {
+        printf("shift2: %d\n", shift(r2));
+    }
+
+    dump(r1);
+    dump(r2);
 }
-#endif
 
--- toys/grokkage/ring2.h
+++ toys/grokkage/ring2.h
@@ -6,14 +6,8 @@
 #include <stdint.h>
 #include <assert.h>
 
-typedef int e_type;
-struct ring2 {
-    uint8_t capacity;
-    e_type *array;
-    int32_t read;
-    uint32_t write;
-};
 
+typedef void * e_type;
 // adt
 // private
 // public methods
@@ -21,8 +15,8 @@ struct ring2* create(int size);
 int size(struct ring2 *r);
 int empty(struct ring2 *r);
 int full(struct ring2 *r);
-int mask(struct ring2 *r, e_type val);
-void push(struct ring2 *r, e_type val);
+int mask(struct ring2 *r, uint32_t index);
+void push(struct ring2 *r, void *val);
 e_type shift(struct ring2 *r);
 void dump(struct ring2 *r);
 
