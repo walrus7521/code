2dba27aa9c8318b3fb02953ff18fa5f70913f802
Bart Bartel <bartb7@gmail.com>
2017-09-24 17:19:17 -0700

    pu

--- toys/grokkage/serdes.c
+++ toys/grokkage/serdes.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct _object {
-    int member1;
-    int member2;
-    int member3;
-    int member4;
-};
-
-void write(char *file_name, struct _object *data)
+//void write(char *file_name, struct _object *data)
+void write(char *file_name, void *data, int len)
 {
     FILE *f;
     f = fopen(file_name,"wb");
@@ -16,11 +10,13 @@ void write(char *file_name, struct _object *data)
         printf("Unable to open file!");
         return;
     }
-    fwrite(data, sizeof(struct _object), 1, f);    
+    //fwrite(data, sizeof(struct _object), 1, f);    
+    fwrite(data, len, 1, f);    
     fclose(f);
 };
 
-void read(const char *file_name, struct _object *data)
+//void read(const char *file_name, struct _object *data)
+void read(const char *file_name, void *data, int len)
 {
     FILE *f;
     f = fopen(file_name,"rb");
@@ -28,9 +24,17 @@ void read(const char *file_name, struct _object *data)
         printf("Unable to open file!");
         return;
     }
-    fread(data, sizeof(struct _object), 1, f);    
+    //fread(data, sizeof(struct _object), 1, f);    
+    fread(data, len, 1, f);    
     fclose(f);
 }
+
+struct _object {
+    int member1;
+    int member2;
+    int member3;
+    int member4;
+};
 
 void show(struct _object *data)
 {
@@ -47,9 +51,9 @@ int main()
     o.member2 = 43;
     o.member3 = 44;
     o.member4 = 45;
-    write("test.bin", &o);
+    write("test.bin", (void *) &o, sizeof(struct _object));
     struct _object o2;
-    read("test.bin", &o2);
+    read("test.bin", (void *) &o2, sizeof(struct _object));
     show(&o2);
 }
 
