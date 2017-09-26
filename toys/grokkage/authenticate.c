485718e290c6f47733e28c5841780debf1a19886
Bart Bartel <BartB@helitrak.com>
2017-09-26 09:30:39 -0700

    st

--- toys/grokkage/authenticate.c
+++ toys/grokkage/authenticate.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//
+// OSI: https://en.wikipedia.org/wiki/OSI_model
+// 7. Application Data: High-level APIs, including resource sharing, remote file access
+// 6. Presentation: Translation of data between a networking service and an application; including character encoding, data compression and encryption/decryption
+// 5. Session:  Managing communication sessions, i.e. continuous exchange of information in the form of multiple back-and-forth transmissions between two nodes
+// 4. Transport Segment (TCP) / Datagram (UDP): Reliable transmission of data segments between points on a network, including segmentation, acknowledgement and multiplexing
+// 3. Network:  Packet  Structuring and managing a multi-node network, including addressing, routing and traffic control
+// 2. Data link Frame:  Reliable transmission of data frames between two nodes connected by a physical layer
+// 1. Physical:
+//
+// presentation: code 
+// session: auth
+// transport: order,status
+// network: msg
+
 // https://yakking.branchable.com/posts/state-machines-in-c/
 
 enum states {
