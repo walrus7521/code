710ec5626b527d8c915f9fdd7735157bf356f62f
Bart Bartel <bartb7@gmail.com>
2017-10-01 01:03:21 -0700

    pu

--- toys/grokkage/hanoi.c
+++ toys/grokkage/hanoi.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void Hanoi(int n, int from, int to, int use) {
+void hanoi(int n, int from, int to, int use) {
     if (n > 0) {
-        Hanoi(n - 1, from, use, to);
+        hanoi(n - 1, from, use, to);
         printf("Move from peg %d to %d\n", from, to);
-        Hanoi(n - 1, use, to, from);
+        hanoi(n - 1, use, to, from);
     }
 }
 
 int main()
 {
-    Hanoi(4, 1, 3, 2);
+    hanoi(4, 1, 3, 2);
     return 0;
 }
