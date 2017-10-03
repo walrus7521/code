4a8de51709cf8576399bd30f46a7b1fa36e2ae90
Bart Bartel <BartB@helitrak.com>
2017-10-03 09:42:14 -0700

    st

--- toys/grokkage/math.c
+++ toys/grokkage/math.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-#include "linalg.h"
+#include <math.h>
+#include "math.h"
+
+double log_base_n(double x, double n)
+{
+    //logN (x) = logy (x) / logy (N)
+    double lognX = log(x) / log(n);
+    return lognX;
+}
 
 int dotProduct(int x[], int y[], int n) {
     int i, sum;
@@ -20,10 +28,19 @@ void matrix_show(struct matrix *m)
     vector_show(&m->row3);
 }
 
+#if 0
 int main()
 {
     int a[] = {1,2,3,4};
     int b[] = {5,6,7,8};
     int sum = dotProduct(a, b, 4);
     printf("sum: %d\n", sum);
+
+    double n, x, h;
+    n = 4;
+    x = 64;
+    h = log_base_n(x,n) + 1;
+    printf("height of %.0f-way tree with %.0f nodes is %.0f\n", n, x, h);
 }
+#endif
+
