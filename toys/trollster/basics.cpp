39ead52460c47f2bec1822df0ad223b6eff8ec55
Bart Bartel <bartb7@gmail.com>
2017-10-08 05:42:16 -0700

    pu

--- toys/trollster/basics.cpp
+++ toys/trollster/basics.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <stdexcept> // out_of_range
+#include <vector>
 
 namespace troll {
+    enum class Type { Vector, List };
 
     // abstract type - pure interface
     class Container {
     public:
         virtual double& operator[] (int i) = 0;
         virtual int size() const = 0;
+        virtual void show() const = 0;
+
+        // delete the copy and move defaults -- broken
+        //Container(const Container&) = delete;
+        //Container& operator=(const Container&) = delete;
+        //Container(const Container&&) = delete;
+        //Container operator=(const Container&&) = delete;
+
         virtual ~Container() {}
     private:
     };
@@ -37,33 +47,157 @@ namespace troll {
     class Vector : public Container {
     public:
         Vector() :elem{ nullptr }, sz{ 0 } {}
-        Vector(int s) :elem{ new double[s] }, sz{ s } 
+        explicit Vector(int s) :elem{ new double[s] }, sz{ s } 
         {
-            for (int i = 0; i < size(); ++i)  elem[i] = 0;
+            for (int i = 0; i != size(); ++i)  elem[i] = 0;
         }
         Vector(std::initializer_list<double> lst)
             : elem{ new double[lst.size()] }, sz{ static_cast<int>(lst.size()) }
         {
             std::copy(lst.begin(), lst.end(), elem);
         }
-        ~Vector() { delete[] elem; }
+        ~Vector() { 
+            std::cout << "destroy\n";
+            if (elem == nullptr) return;
+            delete[] elem; 
+        }
+        
+        Vector(const Vector& a) // copy constructor
+            : elem { new double[a.sz] }, sz{ a.sz }
+        {
+            std::cout << "copy constructor\n";
+            for (int i = 0; i != sz; ++i) elem[i] = a.elem[i];
+        }
+        Vector& operator=(const Vector& lhs) // copy assignment
+        {
+            std::cout << "copy assignment\n";
+            double *rhs = new double[lhs.sz];
+            for (int i = 0; i != lhs.sz; ++i) rhs[i] = lhs.elem[i];
+            delete[] elem;
+            elem = rhs;
+            sz = lhs.sz;
+            return *this;
+        }
+
+        Vector(Vector&& rval) // move constructor
+            : elem { rval.elem }, sz{ rval.sz }
+        {
+            std::cout << "move constructor\n";
+            rval.elem = nullptr;
+            rval.sz = 0;
+        }
+        Vector& operator=(Vector&& rval) // move assignment
+        {
+            std::cout << "move assignment\n";
+            elem = std::move(rval.elem); // vacates rval
+            this->sz = rval.sz;
+            rval.elem = nullptr;
+            rval.sz = 0;
+            return *this;
+        }
+
+#if 1
+        // this requires a move operation
+        // lhs is implicitly "this"
+        Vector operator+(Vector& rhs)
+        {
+            if (this->size() != rhs.size()) throw std::length_error("Vector::operator+");
+
+            Vector res(rhs.size());
+            for (int i = 0; i != rhs.sz; ++i) res[i] = this->elem[i] + rhs[i];
+
+            return res;
+        }
+#endif
+
         void resize(int n) 
         {
             delete[] elem;
             elem = new double[n];
             sz = n;
-            for (int i = 0; i < size(); ++i)  elem[i] = 0;
+            for (int i = 0; i != size(); ++i)  elem[i] = 0;
         }
-        double& operator[] (int i) 
+        double& operator[] (int i) override
         { 
             if (i < 0 || size() <= i)
                 throw std::out_of_range("Vector::operator[]");
             return elem[i]; 
         }
-        int size() const noexcept { return sz; }
-        void show() 
+        int size() const noexcept override { return sz; }
+        void show() const override
         {
-            for (int i = 0; i < size(); ++i) {
+            std::cout << "show: Vector\n";
+            for (int i = 0; i != size(); ++i) {
+                std::cout << elem[i] << std::endl;
+            }
+        }
+    private:
+        double *elem;
+        int sz;
+    };
+
+#if 0
+    // must be non-member if using 2 parameters
+    Vector operator+(Vector& lhs, Vector& rhs)
+    {
+        std::cout << "Vector operator+\n";
+        if (rhs.size() != lhs.size()) throw std::length_error("Vector::operator+");
+        Vector res(lhs.size());
+        for (int i = 0; i != rhs.size(); ++i) res[i] = rhs[i] + lhs[i];
+        return res;
+    }
+#endif
+
+    class List : public Container {
+    public:
+        List(int s) :elem{ new double[s] }, sz{ s } 
+        {
+            for (int i = 0; i != sz; ++i)  elem[i] = 0;
+        }
+
+        List(const List& a) // copy constructor
+            : elem { new double[a.sz] }, sz{ a.sz }
+        {
+            std::cout << "copy constructor\n";
+            for (int i = 0; i != sz; ++i) elem[i] = a.elem[i];
+        }
+        List& operator=(const List& lhs) // copy assignment
+        {
+            std::cout << "copy assignment\n";
+            double *rhs = new double[lhs.sz];
+            for (int i = 0; i != lhs.sz; ++i) rhs[i] = lhs.elem[i];
+            delete[] elem;
+            elem = rhs;
+            sz = lhs.sz;
+            return *this;
+        }
+
+        List(List&& rval) // move constructor
+            : elem { rval.elem }, sz{ rval.sz }
+        {
+            std::cout << "move constructor\n";
+            rval.elem = nullptr;
+            rval.sz = 0;
+        }
+        List& operator=(List&& rval) // move assignment
+        {
+            std::cout << "move assignment\n";
+            elem = std::move(rval.elem); // vacates rval
+            this->sz = rval.sz;
+            rval.elem = nullptr;
+            rval.sz = 0;
+            return *this;
+        }
+
+
+        
+        ~List() { delete[] elem; }
+        double& operator[] (int i) override { return elem[i]; }
+        int size() const override { return sz; };
+        void show() const override
+        {
+            std::cout << "show: List\n";
+            for (int i = 0; i != size(); ++i) {
                 std::cout << elem[i] << std::endl;
             }
         }
@@ -100,11 +234,41 @@ double read_and_sum(int s)
     return sum;
 }
 
+void use2(troll::Vector v)
+{
+}
+
 void use(troll::Container& c)
 {
     const int sz = c.size();
-    for (int i = 0; i < sz; ++i) {
+    for (int i = 0; i != sz; ++i) {
         std::cout << c[i] << std::endl;
+    }
+}
+
+std::unique_ptr<troll::Container> factory(troll::Type t, int sz)
+{
+    switch (t) {
+        case troll::Type::Vector:
+            return std::unique_ptr<troll::Container>{ new troll::Vector(sz)};
+        case troll::Type::List:
+            return std::unique_ptr<troll::Container>{ new troll::List(sz)};
+    }
+    return nullptr;
+}
+
+void iterate(const std::vector<std::unique_ptr<troll::Container>>& vc)
+{
+    for (auto& p : vc) { // needs copy constructor
+#if 0 // broken
+        if (std::unique_ptr<troll::Vector> pv = dynamic_cast<std::unique_ptr<troll::Vector>>(p)) {
+            std::cout << "got a Vector ...\n";
+        }
+        if (std::unique_ptr<troll::List> pl = dynamic_cast<std::unique_ptr<troll::List>>(p)) {
+            std::cout << "got a List ...\n";
+        }
+#endif
+        p->show();
     }
 }
 
@@ -115,11 +279,24 @@ int main()
 
     troll::Complex a, b;
 
-    troll::Vector v = {1,2,3,4};
-    use(v); // polymorphic function
-    v.show();
-    v.resize(4);
-    v.show();
+    troll::Vector v1 = {1,2,3,4};
+    //troll::Vector v2 = 4; // explicit so not ok, defines v(4);
+    troll::Vector v2 = {1,2,3,4};
+    troll::Vector v3 = {1,2,3,4};
+    use(v1); // polymorphic function
+    use2(v1); // copy constructor
+    v1.show();
+    v1.resize(4);
+    v1.show();
+    v1 = v2; // copy assignment
+    std::cout << "doing add\n";
+    v3 = v1 + v2; //broken
+    use(v3);
+
+    std::vector<std::unique_ptr<troll::Container>> vc;
+    vc.push_back(factory(troll::Type::Vector, 6));
+    vc.push_back(factory(troll::Type::List, 4));
+    iterate(vc);
 
 }
 
