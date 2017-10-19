c8e6e5f3414e8f6b082bd4d0878fe66effeb14fd
Bart Bartel <BartB@helitrak.com>
2017-10-19 15:44:09 -0700

    st

---
+++ toys/grokkage/cpp/tq.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "queue.hpp"
+
+int main()
+{
+    msgQueue<int> ibuf;
+    int i;
+
+    for (i = 0; i < 8; i++) {
+        ibuf.push(i);
+    }
+
+    while (ibuf.size() > 0) {
+        i = ibuf.front();
+        ibuf.pop();
+        std::cout << i << std::endl;
+    }
+}
