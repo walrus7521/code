0e523a5165b470ae5dba23bc5968748e2d9c2be8
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2017-11-22 17:59:00 -0800

    auto

--- toys/misc/embed/split_tranny.c
+++ toys/misc/embed/split_tranny.c
@@ -8,16 +8,16 @@ void split(int size)
 {
     int n_trans = size / FIFO_LIMIT;
     int n_bytes = MIN(size, FIFO_LIMIT);
-    int n_offset = 0;
+    int buffer_offset = 0;
 
     n_trans = (size % FIFO_LIMIT == 0) ? (size / FIFO_LIMIT) : (size / FIFO_LIMIT) + 1;
 
     printf("split[%d] => %d\n", size, n_trans);
     do {
         n_bytes = MIN(size, FIFO_LIMIT);
-        printf("transmit[%02d] => %02d off: %02d\n", n_trans, n_bytes, n_offset);
+        printf("transmit[%02d] => %02d off: %02d\n", n_trans, n_bytes, buffer_offset);
         size -= n_bytes;
-        n_offset += n_bytes;
+        buffer_offset += n_bytes;
     } while (--n_trans > 0);
 }
 
