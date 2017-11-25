2ab792717bcaa481dff153cff304eb5fbb3a3f78
Bart Bartel <bartb7@gmail.com>
2017-11-25 11:51:57 -0800

    pu

--- toys/grokkage/scratch/btree.c
+++ toys/grokkage/scratch/btree.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct record {
+} record_t;
+
+typedef struct node {
+} node_t;
+
+node_t *insert(node_t *root, int key, int value)
+{
+    node_t *node = malloc(sizeof(node_t));
+    return node;
+}
 
 int main()
 {
+    node_t *root = NULL;
+    root = insert(root, 42, 17);
 }
 
