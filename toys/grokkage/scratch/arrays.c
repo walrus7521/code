db36fa8e4ffba89ad0fd5826159fd3dcf83e5ff4
Bart Bartel <bartb7@gmail.com>
2017-11-26 08:28:00 -0800

    pu

--- toys/grokkage/scratch/arrays.c
+++ toys/grokkage/scratch/arrays.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "types.h"
 
 /*
     sorting
@@ -7,7 +7,22 @@
     strings
  */
 
+int anagram(char *a, char *b)
+{
+    char dict[256];
+    int len = strlen(a);
+    if (len != strlen(b)) return 0;
+    for (int i = 0; i < len; i++) dict[i] = 0;
+    for (int i = 0; i < len; i++) dict[a[i]]++;
+    for (int i = 0; i < len; i++) {
+        if (dict[b[i]]-- == 0) return 0;
+    }
+    return 1;
+}
+
 int main()
 {
+    printf("is_ana: %d\n", anagram("dude", "eddu"));
+    
 }
 
--- toys/grokkage/scratch/list.c
+++ toys/grokkage/scratch/list.c
@@ -9,12 +9,33 @@ list_t *new()
     return list;
 }
 
+int size(list_t *list)
+{
+    int count = 0;
+    link_t *t = list->head->next;
+    while (t) {
+        t = t->next;
+        count++;
+    }
+    return count;
+}
+
 link_t *link(int key)
 {
     link_t *link = malloc(sizeof(link_t));
     link->key = key;
     link->next = NULL;
     return link;
+}
+
+int empty(list_t *list)
+{
+    return (list->tail ? 0 : 1);
+}
+
+link_t *peek_front(list_t *list)
+{
+    return list->head->next;
 }
 
 void push_front(list_t *list, int key)
@@ -29,7 +50,7 @@ void push_front(list_t *list, int key)
 
 link_t *pop_front(list_t *list)
 {
-    link_t *t;
+    link_t *t = NULL;
     if (list->head->next) {
         t = list->head->next;
         list->head->next = t->next;
@@ -40,9 +61,15 @@ link_t *pop_front(list_t *list)
     return t;
 }
 
-void push_back(link_t *list, int key)
+void push_back(list_t *list, int key)
 {
     link_t *l = link(key);
+    if (list->tail) {
+        list->tail = (list->tail->next = l);
+    } else {
+        l->next = list->head->next;
+        list->tail = list->head->next = l;
+    }
 }
 
 
@@ -60,5 +87,14 @@ int main()
     list_t *list = new();
     push_front(list, 42);
     push_front(list, 17);
+    push_front(list, 2);
+    push_front(list, 11);
     show(list);
+    printf("size: %d\n", size(list));
+    while (!empty(list)) {
+        link_t *t = peek_front(list);
+        pop_front(list);
+        printf("t->%d\n", t->key);
+    }
 }
+
