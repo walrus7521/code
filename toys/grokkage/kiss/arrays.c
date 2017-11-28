6e1bc2f79da642e140011bea71bbc7cae6c4726b
Bart Bartel <BartB@helitrak.com>
2017-11-28 08:49:13 -0800

    st

--- toys/grokkage/kiss/arrays.c
+++ toys/grokkage/kiss/arrays.c
@@ -83,6 +83,50 @@ int is_palindrome(char *a)
     return 1;
 }
 
+int count_bits(uint32_t x)
+{
+    int count = 0;
+    while (x) {
+        count++;
+        x &= (x-1);
+    }
+    return count;
+}
+
+int parity(uint32_t x)
+{
+    int count = count_bits(x);
+    //return (count % 2) ? 0 : 1; // even parity
+    return (count & 0x01); // odd parity
+}
+
+uint64_t bitsort(array_t *a)
+{
+    uint64_t x = 0;
+    for (int i = 0; i < a->size; i++) {
+        x |= (1 << a->a[i]);
+    }
+    return x;
+}
+
+void test_bits()
+{
+    int x = 0x5a;
+    printf("count:  %x => %d\n", x, count_bits(x));
+    printf("parity: %x => %d\n", x, parity(x));
+
+    array_t a = {.size=6, {4,3,1,2,7,31}};
+    show(&a);
+    uint32_t y = bitsort(&a);
+    printf("y: %08x\n", y);
+    for (int i = 0; i < 64; i++) {
+        if (y & 1) {
+            printf("%d\n", i);
+        }
+        y >>= 1;
+    }
+}
+
 int main()
 {
     printf("is_ana: %d\n", is_anagram("dude", "eddu"));
@@ -93,5 +137,7 @@ int main()
     selection(&a);
     show(&a);
 
+    printf("test bit stuff\n");
+    test_bits();
 }
 
