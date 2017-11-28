a9b11779950012b7813542ad122f2a215d6a8282
Bart Bartel <BartB@helitrak.com>
2017-11-27 17:05:46 -0800

    st

--- toys/grokkage/kiss/graph.c
+++ toys/grokkage/kiss/graph.c
@@ -21,14 +21,54 @@ void show(graph_t *g)
     }
 }
 
+void find_path(int start, int end, int parents[])
+{
+    printf("start %d, end %d\n", start, end);
+    if ((start == end) || (end == -1))
+        printf("{start => %02d\n", start);
+    else {
+        find_path(start, parents[end], parents);
+        printf("          %02d <=\n", end);
+    }
+}
+
+void init(graph_t *g)
+{
+    for (int i = 0; i < g->n_vert; i++) {
+        g->parent[i] = -1;
+        g->visited[i] = 0;
+    }
+}
+
 void bfs()
 {
-    graph_t g = {.n_vert = 4, 
-                  { { 0, 1, 0, 0 }, 
-                    { 0, 0, 0, 1 }, 
-                    { 0, 0, 0, 0 }, 
-                    { 1, 0, 1, 0 } } 
-                };
+    int start = 2;
+    int v;
+    ring_t ring = {{0},.head=0,.tail=0};
+    graph_t g = {.n_vert = 4, /* directed graph */
+                  { { 0, 1, 1, 0 }, 
+                    { 0, 0, 1, 0 }, 
+                    { 1, 0, 0, 1 }, 
+                    { 0, 0, 0, 1 } } };
+    init(&g);
+    ring.q[ring.tail++] = start;
+    while (ring.tail != ring.head) {
+        v = ring.q[ring.head++];
+        printf("v: %d\n", v);
+        for (int i = 0; i < g.n_vert; i++) {
+            if (g.m[v][i] == 1 && !g.visited[i]) {
+                ring.q[ring.tail++] = i;
+                g.visited[i] = 1;
+                g.parent[i] = v;
+            }
+        }
+    }
+    printf("\n The vertices which are reachable from %d are:\n\n", start); 
+    for (int i=0;i<g.n_vert;i++)
+        if(g.visited[i]) 
+            printf("%d\t",i);
+    printf("\n");
+    find_path(0, 4, g.parent);
     show(&g);
 }
 
@@ -36,5 +76,4 @@ int main()
 {
     bfs();
 }
-
 
--- toys/grokkage/kiss/types.h
+++ toys/grokkage/kiss/types.h
@@ -4,9 +4,12 @@
 #include <string.h>
 #include <limits.h>
 
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
 #define exchg(X, Y) { \
-    (X) = (X) ^ (Y);   \
-    (Y) = (Y) ^ (X);   \
+    (X) = (X) ^ (Y);  \
+    (Y) = (Y) ^ (X);  \
     (X) = (X) ^ (Y); }
 
 /* array
@@ -32,13 +35,21 @@ typedef struct {
     link_t *tail;
 } list_t;
 
+#define MAX_RING (32)
+typedef struct ring {
+    int q[MAX_RING];
+    int head;
+    int tail;    
+} ring_t;
+
 /* graph
  */
 #define MAX_VERT (16)
 typedef struct graph {
     int n_vert;
     int m[MAX_VERT][MAX_VERT];
-    int ring[MAX_VERT];
+    int parent[MAX_VERT];
+    int visited[MAX_VERT];
 } graph_t;
 
 /* tree
@@ -77,4 +88,6 @@ typedef struct hash {
     int size;
     pair_t *p[MAX_HASH];
 } hash_t;
+
+#endif // _TYPES_H_
 
