70bec7de22a0d0574bb82f7107ae38cc3357be67
Bart Bartel <BartB@helitrak.com>
2017-11-27 17:18:02 -0800

    st

--- toys/grokkage/kiss/bits.c
+++ toys/grokkage/kiss/bits.c
@@ -17,11 +17,38 @@ int parity(uint32_t x)
     return (count & 0x01); // odd parity
 }
 
+uint64_t bitsort(array_t *a)
+{
+    uint64_t x = 0;
+    for (int i = 0; i < a->size; i++) {
+        x |= (1 << a->a[i]);
+    }
+    return x;
+}
+
+void show(array_t *a)
+{
+    for (int i = 0; i < a->size; i++) {
+        printf("[%d] => %d\n", i, a->a[i]);
+    }
+}
 
 int main()
 {
     int x = 0x5a;
     printf("count:  %x => %d\n", x, count_bits(x));
     printf("parity: %x => %d\n", x, parity(x));
+
+    array_t a = {.size=6, {4,3,1,2,7,32}};
+    show(&a);
+    uint64_t y = bitsort(&a);
+    printf("y: %x\n", y);
+    for (int i = 0; i < 64; i++) {
+        if (y & 1) {
+            printf("%d\n", i);
+        }
+        y >>= 1;
+    }
+
 }
 
