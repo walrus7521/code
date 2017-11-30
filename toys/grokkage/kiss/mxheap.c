aed95960a65e9a3d938ff69f2e5acd4bf537cf34
Bart Bartel <bartb7@gmail.com>
2017-11-30 08:14:25 -0800

    pu

--- toys/grokkage/kiss/mxheap.c
+++ toys/grokkage/kiss/mxheap.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include <limits.h>
 
-int g_heap[256];
-int g_size = 0;
+int g_heap[256]={INT_MAX}; int g_size = 0;
 
-void heap_show()
-{
-    for (int i = 1; i <= g_size; i++) printf("a[%02d] = %02d\n", i, g_heap[i]);
-}
-
-void down_max(int k) // k = 1
-{
-    int child, last = g_heap[k]; /* grab max */
+void down_max() {
+    int k=1, child, last = g_heap[k]; /* grab max */
+    printf("downing %02d from %02d ", last, k);
     while (k <= g_size/2) {
         child = 2 * k;
         if (child < g_size && g_heap[child] < g_heap[child+1]) child++;
@@ -19,41 +13,35 @@ void down_max(int k) // k = 1
         g_heap[k] = g_heap[child];
         k = child;
     }
-    printf("down k: %d size: %d\n", k, g_size);
+    printf(" => %02d size: %02d : ", k, g_size);
     g_heap[k] = last;
 }
 
-void up_max(int k) // k = size
-{
-    int v = g_heap[k];
-    printf("upping: %d\n", v);
-    while (g_heap[k/2] <= v) {
+void up_max() {
+    int k = g_size, v = g_heap[k]; // new item @ bottom
+    printf("insert(upping): %02d ", v);
+    while (g_heap[k/2] <= v) { // has sentinal @[0]
         g_heap[k] = g_heap[k/2];
         k = k/2;
-    }
-    printf("up k: %d\n", k);
-    g_heap[k] = v;
+    } // stop search when v is not larger than parent
+    printf(" => %02d\n", k);
+    g_heap[k] = v; // place new item @k
 }
 
-int main()
-{
+int main() {
     int a[] = { 34, 5, 23, 12, 33, 98, 4, 13, 44, 37, 1, 86, 8};
     int i, len=sizeof(a) / sizeof(a[0]);
     printf("max heap - len %d\n", len);
-    g_size = 0;
-    g_heap[0] = INT_MAX;
     for (i = 0; i < len; i++) {
         g_heap[++g_size] = a[i]; // insert at bottom
-        up_max(g_size); // bubble up
+        up_max(); // bubble up
     }
-    heap_show();
+    for (i=1;i<=g_size;i++)printf("a[%02d] = %02d\n",i,g_heap[i]);
     while (g_size != 0) {
         int max = g_heap[1]; // save top
         g_heap[1] = g_heap[g_size--]; // copy bottom to top
-        down_max(1); // sink down
-        printf("%d ", max);
+        down_max(); // sink down
+        printf("mx => %02d\n", max);
     }
-    printf("\n");
-    return 0;
 }
 
