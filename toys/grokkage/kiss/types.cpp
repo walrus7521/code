dc3fa5456d60b9a937cec8a92f1509276675fbf8
Bart Bartel <BartB@helitrak.com>
2017-12-04 13:11:44 -0800

    st

--- toys/grokkage/kiss/types.cpp
+++ toys/grokkage/kiss/types.cpp
@@ -8,24 +8,49 @@ unique_ptr<Trie> new_trie() {
     return trie;
 }
 
-void insert(unique_ptr<Trie> root, char *key) {
-    printf("insert: %s\n", key);
-    int len = strlen(key);
-    unique_ptr<Trie> t = root;
+void insert(unique_ptr<Trie> &root, string &key) {
+    printf("insert: %s\n", key.c_str());
+    int len = key.size();
+    Trie *t = root.get();
     for (int i = 0; i < len; i++) {
         int idx = key[i] - 'a';
-        //if (t->m[idx] == nullptr) { t->m[idx] = new_trie<T>(); }
-        //t = t->m[idx]; // advance to next trie
+        if (t->m[idx] == nullptr) { t->m[idx] = new_trie(); }
+        t = t->m[idx].get(); // advance to next trie
     }
     int idx = TERMINATOR; // terminate all strings here
-    //t->m[idx] = new_trie<T>();
+    t->m[idx] = new_trie();
+}
+
+bool find(const unique_ptr<Trie> &root, string &key) {
+    printf("find: %s => ", key.c_str());
+    int len = key.size();
+    Trie *t = root.get();
+    for (int i = 0; i < len; i++) {
+        int idx = key[i] - 'a';
+        //printf("check %c => %d\n", key[i], idx);
+        if (t->m[idx] == nullptr) {
+            printf("not found\n");
+            return false;
+        }
+        t = t->m[idx].get();
+    }
+    int idx = TERMINATOR; //'\0' - 'a';
+    if (t->m[idx] == nullptr) { // was key terminated?
+        printf("not found\n");
+        return false;
+    } else {
+        printf("found\n");
+        return true;
+    }
 }
 
 int main()
 {
     unique_ptr<Trie> root = new_trie();
-    char key[] = "bart";
-    insert(root, key);
-    
+    string key;
+    key = "bart"; insert(root, key);
+    key = "cindy"; insert(root, key);
+    key = "douche"; find(root, key);
+    key = "bar"; find(root, key);
 }
 
--- toys/grokkage/kiss/types.hpp
+++ toys/grokkage/kiss/types.hpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <string>
 #include <memory>
-#include <cstring>
 
 // array
 // vector<int> a;
