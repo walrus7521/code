4a830689438fd22c9007fb8788cf1cf6d7e4fd31
Bart Bartel <BartB@helitrak.com>
2017-12-04 16:11:02 -0800

    st

--- toys/grokkage/kiss/arrays.c
+++ toys/grokkage/kiss/arrays.c
@@ -7,29 +7,23 @@
     strings
  */
 
-array_t *new()
+void show(int *a, int len)
 {
-    array_t *a = malloc(sizeof(array_t));
-    return a;
-}
-
-void show(array_t *a)
-{
-    for (int i = 0; i < a->size; i++) {
-        printf("%d, ", a->a[i]);
+    for (int i = 0; i < len; i++) {
+        printf("%d, ", a[i]);
     }
     printf("\n");
 }
 
 // each largest element bubbles down to end
-void bubble(array_t *a)
+void bubble(int *a, int len)
 {
-    for (int i = 0; i < a->size; i++) {
-        int sorted = 1;
-        for (int j = 0; j < a->size-i-1; j++) {
-            if (a->a[j] > a->a[j+1]) {
+    for (int i = 0; i < len; i++) {
+        int sorted = 1; // short circuit
+        for (int j = 0; j < len-i-1; j++) { // move the bottom
+            if (a[j] > a[j+1]) {
                 sorted = 0;
-                exchg(a->a[j], a->a[j+1]);
+                exchg(a[j], a[j+1]);
             }
         }
         if (sorted) { // no swaps
@@ -40,25 +34,34 @@ void bubble(array_t *a)
 }
 
 // puts item in correct location each iteration
-void insertion(array_t *a)
+void insertion(int *a, int len)
 {
-    for (int i = 1; i <= a->size-1; i++) {
+    for (int i = 1; i <= len-1; i++) {
         int j = i;
-        while (j > 0 && a->a[j] < a->a[j-1]) {
-            exchg(a->a[j], a->a[j-1]);
+        while (j > 0 && a[j] < a[j-1]) {
+            exchg(a[j], a[j-1]);
             j--;
         }
     }
 }
 
 // find smallest and exchg with advancing first position
-void selection(array_t *a)
+void selection(int *a, int len)
 {
-    for (int i = 0; i < a->size; i++) {
-        for (int j = i; j < a->size; j++) {
-            if (a->a[i] > a->a[j]) exchg(a->a[i], a->a[j]);
+    for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+            if (a[i] > a[j]) exchg(a[i], a[j]);
         }
     }
+}
+
+uint64_t bitsort(int *a, int len)
+{
+    uint64_t x = 0;
+    for (int i = 0; i < len; i++) {
+        x |= (1 << a[i]);
+    }
+    return x;
 }
 
 int is_anagram(char *a, char *b)
@@ -100,24 +103,16 @@ int parity(uint32_t x)
     return (count & 0x01); // odd parity
 }
 
-uint64_t bitsort(array_t *a)
-{
-    uint64_t x = 0;
-    for (int i = 0; i < a->size; i++) {
-        x |= (1 << a->a[i]);
-    }
-    return x;
-}
-
 void test_bits()
 {
     int x = 0x5a;
     printf("count:  %x => %d\n", x, count_bits(x));
     printf("parity: %x => %d\n", x, parity(x));
 
-    array_t a = {.size=6, {4,3,1,2,7,31}};
-    show(&a);
-    uint32_t y = bitsort(&a);
+    int a[] = {4,3,1,2,7,31};
+    int len = sizeof(a) / sizeof(a[0]);
+    show(a, len);
+    uint32_t y = bitsort(a, len);
     printf("y: %08x\n", y);
     for (int i = 0; i < 64; i++) {
         if (y & 1) {
@@ -129,15 +124,17 @@ void test_bits()
 
 int main()
 {
-    printf("is_ana: %d\n", is_anagram("dude", "eddu"));
-    printf("is_pal: %d\n", is_palindrome("duiud"));
-    array_t a = {.size=6, {2,9,1,3,6,4}};
-    //bubble(&a);
-    //insertion(&a);
-    selection(&a);
-    show(&a);
+    //printf("is_ana: %d\n", is_anagram("dude", "eddu"));
+    //printf("is_pal: %d\n", is_palindrome("duiud"));
+    int a[] = {2,9,1,3,6,4};
+    int len = sizeof(a) / sizeof(a[0]);
+    show(a, len);
+    //bubble(a, len);
+    //insertion(a, len);
+    selection(a, len);
+    show(a, len);
 
-    printf("test bit stuff\n");
-    test_bits();
+    //printf("test bit stuff\n");
+    //test_bits();
 }
 
