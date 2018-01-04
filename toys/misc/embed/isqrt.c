5cff219540f8b3796a3fa1a1e10a3337edc40c27
Bart Bartel <BartB@helitrak.com>
2018-01-04 09:41:22 -0800

    st

--- toys/misc/embed/isqrt.c
+++ toys/misc/embed/isqrt.c
@@ -5,19 +5,21 @@
 
 float Q_rsqrt( float number )
 {
-	long i;
-	float x2, y;
-	const float threehalfs = 1.5F;
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
 
-	x2 = number * 0.5F;
-	y  = number;
-	i  = * ( long * ) &y;                       // evil floating point bit level hacking
-	i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
-	y  = * ( float * ) &i;
-	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
-//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+    x2 = number * 0.5F;
+    y  = number;
+    i  = * ( long * ) &y;                       // evil floating point bit level hacking
+    printf("hexf 1: %0lx\n", i);
+    i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
+    printf("hexf 2: %0lx\n", i);
+    y  = * ( float * ) &i;
+    y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//  y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
 
-	return y;
+    return y;
 }
 
 int main()
@@ -26,5 +28,5 @@ int main()
     x = 4;
     y = sqrt(x);
     z = Q_rsqrt(x);
-    printf("inv sqrt(%f) = %f\n", x, 1.0/Q_rsqrt(x));
+    printf("inv sqrt(%f) = %f\n", x, z);
 }
