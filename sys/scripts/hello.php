121848bb3e14bf4594fae50184ae2e97e759db4a
Bart Bartel <bartb7@gmail.com>
2018-01-29 06:51:33 -0800

    pu

---
+++ sys/scripts/hello.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+
+<?php print "Hello World!\n"; ?>
+<?php 
+    $i = 0; 
+    while ( $i < 4 ) { 
+        print $i."n\n"; 
+        sleep(1); 
+        $i++; 
+    } 
+?>
+
+<?php fwrite(STDOUT, "Please enter your name:"); ?>
+
+<?php
+    $STDOUT = fopen("php://stdout", "w");
+    fwrite($STDOUT, "Output #1.");
+    fclose($STDOUT);
+?>
+
+<?php
+    $lines = split("\n", file_get_contents("php://stdin", "r"));
+    shuffle($lines);
+    foreach ($lines as $line) {
+        if ($line !== "") {
+            echo "$line\n";
+        }
+    }
+?>
+
+
