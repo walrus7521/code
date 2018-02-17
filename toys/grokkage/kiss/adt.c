fa0f2b93563cb79b36753fa3213f90dd12a2bf00
Bart Bartel <BartB@helitrak.com>
2018-02-16 17:10:03 -0800

    st

---
+++ toys/grokkage/kiss/adt.c
@@ -0,0 +1,22 @@
+#include "adt.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+// foo.c
+struct _foo {
+    int x;
+    int y;
+};
+
+foo_t *create()
+{
+    foo_t *f = malloc(sizeof(foo_t));
+    f->x = 42;
+    f->y = 17;
+    return f;
+}
+
+void show(foo_t *f)
+{
+    printf("%d - %d\n", f->x, f->y);
+}
---
+++ toys/grokkage/kiss/adt.h
@@ -0,0 +1,7 @@
+
+// foo.h
+typedef struct _foo foo_t;
+
+foo_t *create();
+void show(foo_t *f);
+
