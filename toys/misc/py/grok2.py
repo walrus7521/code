7fdfc8bb0d42865e55bc1603e788dfc08a0adc35
Bart Bartel <BartB@helitrak.com>
2018-02-19 10:53:04 -0800

    st

--- toys/misc/py/grok2.py
+++ toys/misc/py/grok2.py
@@ -17,10 +17,29 @@ def binary_search(arr, item):
 
     return None
 
+
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range (1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selection_sort(arr):
+    new_arr = []
+    for i in range (len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
 def main():
     my_arr = [1,3,5,7,9]
     print binary_search(my_arr, 3)
     print binary_search(my_arr, -1)
+    print my_arr[find_smallest(my_arr)]
+    print selection_sort([5,3,6,2,10])
 
 
 if __name__ == '__main__':
