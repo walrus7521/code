918e867fdb240e2399275f9e9795e3d1843e2a11
Bart Bartel <BartB@helitrak.com>
2018-02-20 12:57:26 -0800

    st

--- toys/misc/py/grok2.py
+++ toys/misc/py/grok2.py
@@ -70,6 +70,10 @@ def make_plots(rect):
 
 # summing numbers using D&C
 def sum(arr):
+    if len(arr) == 1:
+        return arr[0]
+    else:
+        return arr[0] + sum(arr[1:])
     None
 
 # quicksort
