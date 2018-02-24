c24f45057f731107a05e877ff5da9eb1218b45ae
Bart Bartel <BartB@helitrak.com>
2018-02-23 16:01:06 -0800

    st

--- toys/misc/py/grok2.py
+++ toys/misc/py/grok2.py
@@ -115,11 +115,18 @@ from collections import deque
 def bfs(graph):
     search_queue = deque()
     search_queue += graph["you"]
+    searched = []
     while search_queue:
         person = search_queue.popleft()
-        if person_is_seller(person):
-            print person + " is a mango seller"
-            search_queue += graph[person]       
+        if not person in searched:
+            if person_is_seller(person):
+                print person + " is a mango seller"
+                return True
+            else:
+                print person + " is not a mango seller"
+                search_queue += graph[person]
+                searched.append(person)
+    return False
 
 
 def main():
