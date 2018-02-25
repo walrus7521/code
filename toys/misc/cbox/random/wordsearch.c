cc7a4a7c2949ddaa66063d46a5bb5e58c02421ba
Bart Bartel <bartbartel@Barts-MacBook-Pro.local>
2018-02-24 19:34:39 -0800

    auto

--- toys/misc/cbox/random/wordsearch.c
+++ toys/misc/cbox/random/wordsearch.c
@@ -8,11 +8,11 @@
 #define min(a,b) ((a)<(b)?(a):(b))
 
 // create a dictionary
-char dictionary[64][36];
+char dictionary[128][36];
 int dict_index = 0;
 
 char words[20][32] = {
-    "AVENTIL",       "GREATHELM",
+    "AVENTAIL",      "GREATHELM",
     "BALLISTICVEST", "GREAVE",
     "BASCINET",      "HAUBERK",
     "BURGONET",      "HELMET",
@@ -40,7 +40,7 @@ void add(char *sym, int len)
 void dump()
 {
     int i;
-    for (i = 0; i < 64; i++) {
+    for (i = 0; i < 128; i++) {
         printf("dict[%02d] : %s\n", i, dictionary[i]);
     }
     for (i = 0; i < 20; i++) {
@@ -48,9 +48,21 @@ void dump()
     }
 }
 
+void show(char pz[15][15], int wid)
+{
+    int i, j;
+    for (i = 0; i < wid; i++) {
+        for (j = 0; j < wid; j++) {
+            printf("%c ", pz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 char puzzle[15][15] = {
     {'G','T','E','N','O','G','R','U','B','F','P','J','Z','P','A'},
-    {'Q','T','E','N','E','C','S','A','B','A','A','R','B','C','I'},
+    {'Q','T','E','N','I','C','S','A','B','A','A','R','B','C','I'},
     {'R','R','C','L','I','O','R','K','R','E','B','U','A','H','Q'},
     {'V','O','A','K','T','C','L','O','S','E','H','E','L','M','Z'},
     {'U','T','R','F','M','N','G','U','P','R','C','C','L','D','P'},
@@ -66,7 +78,9 @@ char puzzle[15][15] = {
     {'G','E','L','P','K','A','T','F','W','U','V','E','N','E','E'},
 };
 
-void diagonal(int rows, int cols)
+char transpose[15][15];
+
+void diagonal(int rows, int cols, char pz[15][15])
 {
     int d, j, k;
     int hgt; // row of lowest point
@@ -86,11 +100,11 @@ void diagonal(int rows, int cols)
             if (   l1 >= 0 && l1 < rows
                 && l2 >= 0 && l2 < cols) {
                 //printf("[%2d][%2d]=(%02d) ", l1, l2, a[l1][l2]);
-                //printf("(%c) ", puzzle[l1][l2]);
+                //printf("(%c) ", pz[l1][l2]);
                 if (l1 + l2 > 14) {
-                    diag[j-1] = puzzle[l1][l2];
+                    diag[j-1] = pz[l1][l2];
                 } else {
-                    diag[j] = puzzle[l1][l2];
+                    diag[j] = pz[l1][l2];
                 }
             }
         }
@@ -98,6 +112,31 @@ void diagonal(int rows, int cols)
         //printf("\n");
     }
     printf("\n");
+}
+
+void do_transpose(char in[15][15], char trans[15][15], int wid)
+{
+    int i, j;
+    for (i = 0; i < wid; i++) {
+        for (j = 0; j < wid; j++) {
+            trans[j][i] = in[i][j];
+        }
+    }
+}
+
+// 90 deg clockwise
+void rotate(char input[15][15], char output[15][15])
+{
+    int n =15; //input.length();
+    int m = 15; //input[0].length();
+    //int [][] output = new int [m][n];
+
+    int i, j;
+    for (i=0; i<n; i++) {
+	    for (j=0;j<m; j++) {
+		    output [j][n-1-i] = input[i][j];
+        }
+    }
 }
 
 void traverse()
@@ -122,7 +161,11 @@ void traverse()
         add(col, strlen(col));
     }
 
-    diagonal(15,15);
+    diagonal(15,15, puzzle);
+    show(puzzle, 15);
+    rotate(puzzle, transpose);
+    diagonal(15,15, transpose);
+    show(transpose, 15);
 }
 
 void reverse(char *word, char *rev, int len)
@@ -136,15 +179,20 @@ void reverse(char *word, char *rev, int len)
 int search(char *word)
 {
     int i;
-    printf("find: %s\n", word);
-    for (i = 0; i < 64; i++) {
+    //printf("find: %s\n", word);
+    for (i = 0; i < 128; i++) {
         char *dict = dictionary[i];
         char rev[36];
         memset(rev, '\0', 36);
         reverse(dict, rev, strlen(dict));
         //printf("check => %s, rev => %s\n", dict, rev);
         if (strstr(dict, word)) {
-            printf("found one: %s => %s\n", word, dictionary[i]);
+            //printf("found it: %s => %s\n", word, dictionary[i]);
+            return 1;
+        }
+        if (strstr(rev, word)) {
+            //printf("found it: %s => %s\n", word, dictionary[i]);
+            return 1;
         }
     }
     return 0;
@@ -153,9 +201,19 @@ int search(char *word)
 int main()
 {
     int i;
+    //do_transpose(puzzle, transpose, 15);
+    //show(transpose, 15);
+    //return 0;
     traverse();
+    //dump();
+    //return 0;
+
     for (i = 0; i < 20; i++) {
-        search(words[i]);
+        if (search(words[i])) {
+            printf("found: %s\n", words[i]);
+        } else {
+            printf("not found: %d %s\n", i, words[i]);
+        }
     }
 
 }
