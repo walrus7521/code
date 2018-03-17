4b2093533607077f8506aa274d2ddcd8da16b8ad
Bart Bartel <bartb7@gmail.com>
2018-03-17 14:46:08 -0700

    pu

--- toys/misc/cbox/lisp/repl.c
+++ toys/misc/cbox/lisp/repl.c
@@ -16,12 +16,12 @@ static char buffer[2048];
 
 /* Fake readline function */
 char* readline(char* prompt) {
-  fputs(prompt, stdout);
-  fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
-  strcpy(cpy, buffer);
-  cpy[strlen(cpy)-1] = '\0';
-  return cpy;
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
 }
 
 /* Fake add_history function */
@@ -37,21 +37,53 @@ void add_history(char* unused) {
 
 int main(int argc, char** argv) {
 
-  puts("Polish Version 0.0.0.0.1");
-  puts("Press Ctrl+c to Exit\n");
+    puts("Polish Version 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
 
-  while (1) {
+    mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Expr      = mpc_new("expr");
+    mpc_parser_t* Lispy     = mpc_new("lispy");
 
-    /* Now in either case readline will be correctly defined */
-    char* input = readline("polish> ");
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                           \
+            number      : /-?[0-9]+/ ;                              \
+            operator    : '+' | '-' | '*' | '/' ;                   \
+            expr        : <number> | '(' <operator> <expr>+ ')' ;   \
+            lispy       : /^/ <operator> <expr>+ /$/ ;              \
+        ",
+        Number, Operator, Expr, Lispy);
 
-    add_history(input);
+    while (1) {
 
-    printf("%s\n", input);
+        /* Now in either case readline will be correctly defined */
+        char* input = readline("polish> ");
 
-    free(input);
+        add_history(input);
 
-  }
+        printf("%s\n", input);
+        if (strcmp(input, "quit") == 0) {
+            free(input);
+            break;
+        }
 
-  return 0;
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            // on success print the ast
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            // on error print the error
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+        free(input);
+
+    }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+
+    return 0;
 }
+
