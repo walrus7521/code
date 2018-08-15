181b376670cdd327ad26571e481753d5b5d150f2
Bart Bartel <BartB@Corp.helitrak.com>
2018-08-15 14:44:36 -0700

    st

--- toys/grokkage/kiss/suffix.c
+++ toys/grokkage/kiss/suffix.c
@@ -2,44 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int pstrcmp(const void *, const void *);
-static int comlen(const char *p, const char *q);
-
-int
-main(void)
-{
-    char buf[BUFSIZ];
-    if (!fgets(buf, sizeof buf, stdin)) return 1;
-
-    size_t len = strlen(buf);
-    const char *a[len];
-
-    size_t i, maxi, maxlen;
-    for (i = 0; i < len; i++) a[i] = &buf[i];
-
-    //qsort(a, len, sizeof *a, pstrcmp);
-    qsort(a, len, sizeof(char *), pstrcmp);
-
-    maxlen = -1;
-    maxi = 0;
-    for (i = 0; i < len; i++) {
-        //if (comlen(a[i],a[i+1]) > maxlen) {
-        //    maxlen = comlen(a[i],a[i+1]);
-        //    maxi = i;
-        //}
-        printf("%d\n", a[i] - buf);
-    }
-    printf("%d\n", maxi);
-
-    return 0;
-}
-
-int
-pstrcmp(const void *p1, const void *p2)
+int pstrcmp(const void *p1, const void *p2)
 {
     const char *const *s1 = p1;
     const char *const *s2 = p2;
-
     return strcmp(*s1, *s2);
 }
 
@@ -49,4 +15,43 @@ static int comlen(const char *p, const char *q)
     while (*p && (*p++ == *q++)) i++;
     return i;
 }
+
+int main(void)
+{
+    char buf[BUFSIZ];
+    char dude[BUFSIZ];
+    if (!fgets(buf, sizeof buf, stdin)) return 1;
+
+    size_t len = strlen(buf);
+    const char *a[len];
+
+    size_t i;
+    int maxi, maxlen;
+    for (i = 0; i < len; i++) a[i] = &buf[i];
+
+    qsort(a, len, sizeof(char *), pstrcmp);
+
+    int nlen;
+    maxlen = -1;
+    maxi = 0;
+    for (i = 0; i < len-1; i++) {
+        nlen = comlen(a[i],a[i+1]);
+        //printf("len: %d\n", nlen);
+        if (nlen > maxlen) {
+            maxlen = nlen;
+            maxi = i;
+            //printf("max: %d\n", maxlen);
+        }
+        //printf("%x\n", a[i] - buf);
+        //printf("%x => %x\n", &buf[i], a[i]);
+    }
+    //printf("max[%d] = %d\n", maxi, maxlen);
+
+    strncpy(dude, a[maxi], maxlen);
+    dude[maxlen] = '\0';
+    printf("dude: '%s'\n", dude);
+
+    return 0;
+}
+
 
