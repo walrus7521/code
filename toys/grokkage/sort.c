7e4177b1b6551e1e853ad08a0543016c0e6e9035
Bart Bartel <BartB@Corp.helitrak.com>
2018-09-14 13:02:21 -0700

    st

--- toys/grokkage/sort.c
+++ toys/grokkage/sort.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// sedgewick
+
 #define MAXN 100
 
 void swap(int a[], int i, int j)
@@ -45,14 +47,34 @@ void inssort(int a[], int n)
     }
 }
 
+void indirect_sort(int a[], int p[], int n)
+{
+    int i, j, v;
+    for (i = 0; i < n; i++) p[i] = i;
+    for (i = 1; i < n; i++) {
+        v = p[i]; j = i;
+        while (a[p[j-1]] > a[v]) {
+            p[j] = p[j-1];
+            j--;
+        }
+        p[j] = v;
+    }
+}
+
+
 int main()
 {
-    int n=0, i, a[MAXN+1];
-    while (scanf("%d", &a[n]) != EOF) n++;
+    int n=0, i; //, a[MAXN+1];
+    //while (scanf("%d", &a[n]) != EOF) n++;
+    int a[] = {'a','s','o','r','t','i','n','g','e','x','a','m','p','l','e'};
+    int p[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+    n = 15;
     //selsort(a, n);
     //inssort(a, n);
-    bubsort(a, n);
-    for (i = 0; i < n; i++) printf("%d ", a[i]);
+    //bubsort(a, n);
+    //bubsort(a, n);
+    indirect_sort(a, p, n);
+    for (i = 0; i < n; i++) printf("%c ", a[p[i]]);
     printf("\n");
 }
 
