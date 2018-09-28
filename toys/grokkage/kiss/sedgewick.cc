82315fa5e71dc22a22f64b9c48ff83325a331bbc
Bart Bartel <BartB@Corp.helitrak.com>
2018-09-28 14:28:02 -0700

    st

--- toys/grokkage/kiss/sedgewick.cc
+++ toys/grokkage/kiss/sedgewick.cc
@@ -132,14 +132,26 @@ int smallest(int a[], int n)
     return min_idx;
 }
 
+void append(int tape[], int i, int buf[], int j, int n)
+{
+    int it, ib;
+    printf("append: tape %d <= buf %d len %d\n", i, j, n);
+    for (it = i, ib = j; ib < n; it++, ib++) {
+        tape[it] = buf[ib];
+    }
+}
+
 #define MIN(a, b) (((a)>(b)) ? (b) : (a))
 
 int tape_in[] = {'A','S','O','R','T','I','N','G','A','N','D','M','E','R','G','I','N','G','E','X','A','M','P','L','E'};
 int tape_out[32] = {0};
 void three_way_merge_sort()
 {
-    int n, t1, t2, t3, j;
+    int n, t1, t2, t3, j, k;
+    int t1_lim, t2_lim, t3_lim;
     int len = sizeof(tape_in) / sizeof(tape_in[0]);
+    int remainder;
+    int tmp[3];
     int tape1[32] = {0};
     int tape2[32] = {0};
     int tape3[32] = {0};
@@ -152,25 +164,49 @@ void three_way_merge_sort()
     // fix this loop to manage the non-multiple of 3 size of input
     // this should be reflected on the outputs: tape1, tape2, tape3
     for (j = n = 0; n < len; n+=9) {
-        printf("n: %d n/3: %d j: %d min: %d\n", n, n/3, j, MIN(n,n/3+3));
-        for (t1 = n/3; t1 < n/3+3; t1++, j++) {
-            tape1[t1] = tape_in[j];
+        remainder = (25 - n) % 25;
+        t1_lim = t2_lim = t3_lim = n/3+3;
+        if (remainder > 0 && remainder < 9) {
+            printf("gotta fix up this loop\n");
+            t1_lim = t2_lim = t3_lim = 0;
+            if (remainder < 3) {
+                t1_lim = remainder;
+            } else
+            if (remainder < 6) {
+                t1_lim = n/3+3;
+                t2_lim = remainder;
+                t3_lim = 0;
+            } else
+            if (remainder < 9) {
+                t1_lim = n/3+3;
+                t2_lim = n/3+3;
+                t3_lim = remainder;
+            }
         }
-        //arr_show(&tape1[n/3], 3);
-        selsort(&tape1[n/3], 3);
-        arr_show(&tape1[n/3], 3);
-        for (t2 = n/3; t2 < n/3+3; t2++, j++) {
-            tape2[t2] = tape_in[j];
+
+        //printf("n: %d n/3: %d j: %d min: %d rem: %d\n", n, n/3, j, MIN(n,n/3+3), remainder);
+        //printf("t1_lim: %d t2_lim: %d t3_lim: %d\n", t1_lim, t2_lim, t3_lim);
+        for (k=0,t1 = n/3; t1 < t1_lim; t1++, j++, k++) {
+            //printf("t1: %02d j: %02d :%c\n", t1, j, tape_in[j]);
+            tmp[k] = tape_in[j];
         }
-        //arr_show(&tape2[n/3], 3);
-        selsort(&tape2[n/3], 3);
-        arr_show(&tape2[n/3], 3);
-        for (t3 = n/3; t3 < n/3+3; t3++, j++) {
-            tape3[t3] = tape_in[j];
+        selsort(tmp, t1_lim-n/3);
+        append(tape1, n/3, tmp, 0, t1_lim-n/3);
+        arr_show(&tape1[n/3], t1_lim-n/3);
+        for (k=0,t2 = n/3; t2 < t2_lim; t2++, j++, k++) {
+            //printf("t2: %02d j: %02d :%c\n", t2, j, tape_in[j]);
+            tmp[k] = tape_in[j];
         }
-        //arr_show(&tape3[n/3], 3);
-        selsort(&tape3[n/3], 3);
-        arr_show(&tape3[n/3], 3);
+        selsort(tmp, t2_lim-n/3);
+        append(tape2, n/3, tmp, 0, t2_lim-n/3);
+        arr_show(&tape2[n/3], t2_lim-n/3);
+        for (k=0,t3 = n/3; t3 < t3_lim; t3++, j++, k++) {
+            //printf("t3: %02d j: %02d :%c\n", t3, j, tape_in[j]);
+            tmp[k] = tape_in[j];
+        }
+        selsort(tmp, t3_lim-n/3);
+        append(tape3, n/3, tmp, 0, t3_lim-n/3);
+        arr_show(&tape3[n/3], t3_lim-n/3);
     }
     arr_show(tape1, n);
     arr_show(tape2, n);
@@ -184,7 +220,6 @@ void three_way_merge_sort()
     // process the 3rd 3x3 block into tape6
     for (n = 0; n < 9; n++) {
         int s1, s2, s3;
-        int tmp[3];
         if (first == 1) {
             tmp[0] = tape1[n];
             tmp[1] = tape2[n];
