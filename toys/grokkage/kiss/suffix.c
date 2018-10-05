bbcb2cd1dde6efdf141677bab1b58bdb6beabacf
Bart Bartel <BartB@Corp.helitrak.com>
2018-10-05 12:16:06 -0700

    st

--- toys/grokkage/kiss/suffix.c
+++ toys/grokkage/kiss/suffix.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// source: pearls: bentley, pg 165 LCS
+#include "utils.h"
 
-/*
- * source: pearls: bentley, pg 165
- */
 int pstrcmp(const void *p1, const void *p2)
-{
+{ // needs to handle one level of indirection
     const char *const *s1 = p1;
     const char *const *s2 = p2;
     return strcmp(*s1, *s2);
@@ -21,40 +17,26 @@ static int comlen(const char *p, const char *q)
 
 int main(void)
 {
-    char buf[BUFSIZ];
-    char dude[BUFSIZ];
+    char buf[BUFSIZ], dude[BUFSIZ];
+    int i, maxi, maxlen, len, nlen;
+
     if (!fgets(buf, sizeof buf, stdin)) return 1;
-
-    size_t len = strlen(buf);
+    len = strlen(buf);
     const char *a[len];
+    
+    for (i = 0; i < len; i++) a[i] = &buf[i]; // setup suffix array
+    qsort(a, len, sizeof(char *), pstrcmp); // sort the strings
 
-    size_t i;
-    int maxi, maxlen;
-    for (i = 0; i < len; i++) a[i] = &buf[i];
-
-    qsort(a, len, sizeof(char *), pstrcmp);
-
-    int nlen;
-    maxlen = -1;
-    maxi = 0;
-    for (i = 0; i < len-1; i++) {
+    for (i = maxi = 0, maxlen = -1; i < len-1; i++) {
         nlen = comlen(a[i],a[i+1]);
-        //printf("len: %d\n", nlen);
         if (nlen > maxlen) {
             maxlen = nlen;
             maxi = i;
-            //printf("max: %d\n", maxlen);
         }
-        //printf("%x\n", a[i] - buf);
-        //printf("%x => %x\n", &buf[i], a[i]);
     }
-    //printf("max[%d] = %d\n", maxi, maxlen);
-
     strncpy(dude, a[maxi], maxlen);
     dude[maxlen] = '\0';
     printf("dude: '%s'\n", dude);
-
     return 0;
 }
-
 
