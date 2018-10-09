486abdb7dce77e8bfec5d855441a27055d23e53a
Bart Bartel <bartb7@gmail.com>
2018-10-09 07:39:55 -0700

    pu

--- toys/misc/algo/comp/test.cc
+++ toys/misc/algo/comp/test.cc
@@ -15,14 +15,15 @@ void dump()
 
 int rsq1(int n)
 {
-    printf("rsq(%d):\n", n);
+    printf("rsq1(%02x)\n", n);
     int sum = 0;
-    printf("\tft[%d] = %d\n", n, ft[n]);
+    printf("\tft[%02x] = %d\n", n, ft[n]);
     sum += ft[n];
+    n -= LSOne(n);
     while (n > 0) {
-        n -= (n & (-n));
-        printf("\tft[%d] = %d\n", n, ft[n]);
+        printf("\tft[%02x] = %d\n", n, ft[n]);
         sum += ft[n];
+        n -= LSOne(n);
     }
     return sum;
 }
@@ -39,16 +40,48 @@ void adjust(int k, int v)
     }
 }
 
+void test_lsone()
+{
+    int n=90, s;
+    s = LSOne(90);
+    printf("%d + LSOne(%d) = %d\n", n, s, n+s);
+    printf("%d - LSOne(%d) = %d\n", n, s, n-s);
+    return;
+
+    while (n) {
+        s = n & (-n);
+        printf("%02X => %02X\n", n, s);
+        n-=s;
+    }
+}
+
+void uva725()
+{
+    int N = 2;
+    for (int fghij = 1234; fghij <= 98765/N; fghij++) {
+        int abcde = fghij * N;
+        int tmp, used = (fghij < 10000); // if digit f=0, then flag
+        tmp = abcde; while (tmp) { used |= 1 << (tmp % 10); tmp /= 10; }
+        tmp = fghij; while (tmp) { used |= 1 << (tmp % 10); tmp /= 10; }
+        if (used == (1<<10) - 1)
+            printf("%0.5d / %0.5d = %d\n", abcde, fghij, N);
+    }
+}
+
 int main()
 {
+    uva725();
+    //test_lsone();
+/*
     for (int i = 0; i < 11; i++) {
         adjust(scores[i], 1);
     }
-    dump();
+    //dump();
     int n = 6;
-    int s = rsq1(n);
-    printf("rsq(%d) = %d\n", n, s);
+    //int s = rsq1(n);
+    //printf("rsq(%02x) = %d\n", n, s);
     int t = rsq2(3,10);
     printf("rsq2 = %d\n", t);
+*/
 }
 
