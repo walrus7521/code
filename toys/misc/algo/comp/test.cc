1159f4c9293380d283c2a830845fe91a6c782979
Bart Bartel <bartb7@gmail.com>
2018-10-15 07:44:49 -0700

    pu

--- toys/misc/algo/comp/test.cc
+++ toys/misc/algo/comp/test.cc
@@ -131,12 +131,54 @@ void uva12455()
     }
 }
 
+// uva11450
+int M, C, price[25][25];
+int memo[210][25];
+int shop(int money, int g)
+{
+    if (money < 0) return -1000000000;
+    if (g == C) return M - money;
+    if (memo[money][g] != -1) return memo[money][g];
+    int ans = -1;
+    for (int model = 1; model <= price[g][0]; model++) {
+        ans = max(ans, shop(money - price[g][model], g+1));
+    }
+    return memo[money][g] = ans;
+}
+
+void uva11450()
+{
+    int i, j, score;
+    scanf("%d %d", &M, &C);
+    for (i = 0; i < C; i++) {
+        scanf("%d", &price[i][0]);
+        for (j = 1; j <= price[i][0]; j++) scanf("%d", &price[i][j]);
+    }
+    memset(memo, -1, sizeof(memo));
+    score = shop(M, 0);
+    if (score < 0) printf("no solution\n");
+    else           printf("%d\n", score);
+}
+
+void max_1D_range_sum()
+{
+    int n = 9, A[] = {4, -5, 4, -3, 4, 4, -4, 4, -5};
+    int sum = 0, ans = 0;
+    for (int i = 0; i < n; i++) {
+        sum += A[i]; // greedy
+        ans = max(ans, sum);
+        if (sum < 0) sum = 0;
+    }
+    printf("Max 1D Range Sum = %d\n", ans);
+}
+
 int main()
 {
     //printf("%x\n", (1<<10)-1);
     //uva725();
-    //uva11565();
-    ex123t8();
+    //uva11450();
+    max_1D_range_sum();
+    //ex123t8();
     //uva12455();
     //test_lsone();
 /*
