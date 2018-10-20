44b75933456035011af9215a2dda122721c4f94e
Bart Bartel <bartb7@gmail.com>
2018-10-19 17:39:04 -0700

    pu

--- toys/misc/cpp/google/composer.h
+++ toys/misc/cpp/google/composer.h
@@ -1,7 +1,10 @@
 #ifndef _COMPOSER_H_
 #define _COMPOSER_H_
 
+#include <iostream>
 #include <string>
+
+using namespace std;
 
 // composer.h, Maggie Johnson
 // Description: The class for a Composer record.
@@ -12,34 +15,80 @@ const int kDefaultRanking = 10;
 class Composer {
  public:
   // Constructor
-  Composer();
+  Composer(){}
   // Here is the destructor which has the same name as the class
   // and is preceded by ~. It is called when an object is destroyed
   // either by deletion, or when the object is on the stack and
   // the method ends.
-  ~Composer();
+  ~Composer(){}
 
   // Accessors and Mutators
-  void set_first_name(std::string in_first_name);
-  std::string first_name();
-  void set_last_name(std::string in_last_name);
-  std::string last_name();
-  void set_composer_yob(int in_composer_yob);
-  int composer_yob();
-  void set_composer_genre(std::string in_composer_genre);
-  std::string composer_genre();
-  void set_ranking(int in_ranking);
-  int ranking();
-  void set_fact(std::string in_fact);
-  std::string fact();
+  void set_first_name(std::string in_first_name) {
+    first_name_ = in_first_name;
+  }  
+  std::string first_name() {
+    return first_name_;
+  }  
+  void set_last_name(std::string in_last_name) {
+    this->last_name_ = in_last_name;
+    cout << "setting last name: " << this->last_name_ << endl;
+  }
+  
+  std::string last_name() {
+    return last_name_;
+  }
+  
+  void set_composer_yob(int in_composer_yob) {
+    this->composer_yob_ = in_composer_yob;
+  }
+  
+  int composer_yob() {
+    return composer_yob_; // year of birth
+  }
+  
+  void set_composer_genre(std::string in_composer_genre) {
+    this->composer_genre_ = in_composer_genre;
+  }
+
+  std::string composer_genre() {
+    return composer_genre_; // baroque, classical, romantic, etc.
+  }
+  void set_ranking(int in_ranking) {
+    this->ranking_ = in_ranking;
+  }
+  
+  int ranking() {
+    return ranking_;
+  }
+  
+  void set_fact(std::string in_fact) {
+    this->fact_ = in_fact;
+  }
+
+  std::string fact() {
+    return fact_;
+  }
 
   // Methods
   // This method increases a composer's rank by increment.
-  void Promote(int increment);
+  void Promote(int increment) {
+    this->ranking_ += increment;
+  }  
   // This method decreases a composer's rank by decrement.
-  void Demote(int decrement);
+  void Demote(int decrement) {
+    this->ranking_ -= decrement;
+  }
+  
   // This method displays all the attributes of a composer.
-  void Display();
+  void Display() {
+    cout << "first name:    : " << this->first_name_ << endl;
+    cout << "last name:     : " << this->last_name_ << endl;
+    cout << "composer yob   : " << this->composer_yob_ << endl;
+    cout << "composer genre : " << this->composer_genre_ << endl;
+    cout << "fact           : " << this->fact_ << endl;
+    cout << "ranking        : " << this->ranking_ << endl;
+  }
+  
 
  private:
   std::string first_name_;
--- toys/misc/cpp/google/database.h
+++ toys/misc/cpp/google/database.h
@@ -7,25 +7,56 @@
 #include <string>
 #include "composer.h"
 
+using namespace std;
+
 // Our database holds 100 composers, and no more.
 const int kMaxComposers = 100;
 
 class Database {
  public:
-  Database();
-  ~Database();
+  Database() {
+      next_slot_ = 0;
+  }
+  ~Database(){};
 
   // Add a new composer using operations in the Composer class.
   // For convenience, we return a reference (pointer) to the new record.
   Composer& AddComposer(std::string in_first_name, std::string in_last_name,
-                        std::string in_genre, int in_yob, std::string in_fact);
+                        std::string in_genre, int in_yob, std::string in_fact) {
+    cout << "adding composer..." << endl;
+    if (next_slot_ < kMaxComposers) {
+        composers_[next_slot_].set_first_name(in_first_name);
+        composers_[next_slot_].set_last_name(in_last_name);
+        composers_[next_slot_].set_composer_yob(in_yob);
+        composers_[next_slot_].set_composer_genre(in_genre);
+        composers_[next_slot_].set_fact(in_fact);
+        return composers_[next_slot_++];
+    }
+
+    return *(Composer*) nullptr;
+  //return NULL;
+  }
+  
   // Search for a composer based on last name. Return a reference to the
   // found record.
-  Composer& GetComposer(std::string in_last_name);
+  Composer& GetComposer(std::string in_last_name) {
+    for (int i = 0; i < next_slot_; ++i) {
+        if (in_last_name == composers_[i].last_name()) {
+            return composers_[i];
+        }
+    }
+    return *(Composer*) nullptr;
+  }
+  
   // Display all composers in the database.
-  void DisplayAll();
+  void DisplayAll() {
+    for (int i = 0; i < next_slot_; ++i) {
+        composers_[i].Display();
+    }
+  }
+  
   // Sort database records by rank and then display all.
-  void DisplayByRank();
+  void DisplayByRank() {}
 
  private:
   // Store the individual records in an array.
