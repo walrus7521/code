c0862962690ebc01af182b6096420f821848f953
Bart Bartel <bartbartel@Barts-MBP.hsd1.wa.comcast.net>
2018-10-28 10:07:45 -0700

    auto

---
+++ toys/grokkage/puma/util.h
@@ -0,0 +1,14 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+
+#include <assert.h>
+#include <stdbool.h>
+
+
+typedef char *string;
+string String(char *);
+
+void *checked_malloc(int);
+
+#endif // _UTIL_H_
