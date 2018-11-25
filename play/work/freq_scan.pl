0eb7e0284d941d243f5d9263d5dba8bbfc95d1cb
Bart Bartel <bartb7@gmail.com>
2018-11-25 14:55:41 -0800

    pu

--- play/work/freq_scan.pl
+++ play/work/freq_scan.pl
@@ -5,7 +5,7 @@ use Text::CSV_XS;
 
 
 sub read_log {
-    my $fh = shift;
+    my $fh = shift; # file handle
     $count = 0;
     @frames;
     while (<$fh>) {
@@ -13,7 +13,7 @@ sub read_log {
         if ($_ =~ /FA*B*A*B*(\d+)sinfreq = (\d+\.\d+)/) {
             my $item = {}; # need this for unique instance
             $count++;
-            $item->{Frame}     = $1;
+            $item->{StartFrame} = $1;
             $item->{Frequency} = $2;
             push @frames, $item;
           }
@@ -22,9 +22,11 @@ sub read_log {
 }
 
 sub read_csv {
-    my $fh = shift;
+    my $fh = shift; # file handle
     my $csv = Text::CSV_XS->new;
     my @data;
+    $hdr = $csv->getline($fh); # skip header
+    #foreach (@$hdr) { printf("> %s\n", $_); }
     while (<$fh>) {
         $csv->parse($_);
         push @data, [$csv->fields];
@@ -33,28 +35,12 @@ sub read_csv {
 }
 
 sub write_csv {
-}
-
-sub main {
-    my $file;
-    my $fh;
-    $file = "20181116_MCPA_STDIO.log";
-    open($fh, '<:encoding(UTF-8)', $file)
-        or die "Could not open file '$file' $!";
-
-    my $frames = read_log($fh);
+    my $frames = shift;
+    my $data = shift;
 
     foreach (@$frames) {
-        printf("> %s => %s\n", $_->{Frame}, $_->{Frequency});
+        printf("> %s => %s\n", $_->{StartFrame}, $_->{Frequency});
     }
-    close($fh);
-
-    $file = "20181116_FreqSweep0_25Hz_no_header.csv";
-    open($fh, '<:encoding(UTF-8)', $file)
-        or die "Could not open file '$file' $!";
-
-    my $data = read_csv($fh);
-    close($fh);
 
     $transition = 0;
     foreach (@$data) {
@@ -67,8 +53,36 @@ sub main {
     #   }
         print "Frame: $_->[0], $_->[3]\n";
     }
+}
+
+sub main {
+
+    if (scalar @ARGV[0] < 1) {
+        print "usage: freq_scan 20181116\n";
+        exit;
+    }
+
+    my $file_prefix = @ARGV[0];
+    my $file, $fh;
+
+    $file = $file_prefix . "_MCPA_STDIO.log";
+    open($fh, '<:encoding(UTF-8)', $file)
+        or die "Could not open file '$file' $!";
+
+    my $frames = read_log($fh); # returns array of frames
+    close($fh);
+
+    $file = $file_prefix . "_FreqSweep0_25Hz.csv";
+    open($fh, '<:encoding(UTF-8)', $file)
+        or die "Could not open file '$file' $!";
+
+    my $data = read_csv($fh); # returns array of csv rows
+    close($fh);
+
+    write_csv(\@$frames, \@$data);
 
 }
 
 main();
+
 
