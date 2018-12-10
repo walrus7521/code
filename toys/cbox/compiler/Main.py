09e25301e7411177aa7a77ff94b4e9f48e6c0eb6
Bart Bartel <bartb7@gmail.com>
2018-12-09 17:09:55 -0800

    pu

--- toys/cbox/compiler/Main.py
+++ toys/cbox/compiler/Main.py
@@ -6,10 +6,11 @@ from antlr4.tree.Trees import Trees
 from ExprLexer import ExprLexer
 from ExprParser import ExprParser
 from ExprListener import ExprListener
+from ExprVisitor import ExprVisitor
 
 import sys
  
-class ExprPrinter(ExprListener):     
+class Listen(ExprListener):     
     def enterProg(self, ctx):         
         print("enterProg baby!") 
 
@@ -28,6 +29,55 @@ class ExprPrinter(ExprListener):
     def exitExpr(self, ctx):         
         print("exitExpr baby!") 
 
+class Visit(ExprVisitor):
+    def visitAssign(self, ctx):
+        print("visitAssign: ")
+        print(ctx.ID())
+        val = self.visit(ctx.expr())
+        print(val)
+        return val
+
+    def visitPrintExpr(self, ctx):
+        val = ctx.expr()
+        print("visitPrintExpr: ")
+        print(val)
+        return 0
+
+    def visitInt(self, ctx):
+        val = ctx.INT()
+        print("visitInt: ")
+        print(val)
+        return val
+
+    def visitId(self, ctx):
+        id = ctx.ID()
+        print("visitId: ")
+        print(id)
+        return 0
+
+    def visitMulDiv(self, ctx):
+        left = ctx.expr(0)
+        right = ctx.expr(1)
+        print("visitMulDiv: ")
+        val = 0
+        if (ctx.op == MUL):
+            val = left*right
+        else:
+            val = left*right
+        print (val)
+        return val
+
+    def visitAddSub(self, ctx):
+        left = ctx.expr(0)
+        right = ctx.expr(1)
+        print("visitAddSub: ")
+        print (left+right)
+        return left+right
+
+    def visitParens(self, ctx):
+        print("visitParens: ")
+
+
 def main(argv):
     input = FileStream(argv[1])
     lexer = ExprLexer(input)
@@ -37,9 +87,15 @@ def main(argv):
 
 #   print(Trees.toStringTree(tree, None, parser))    
 
-    printer = ExprPrinter()
+    print("start listener...")
+    listener = Listen()
     walker = ParseTreeWalker()
-    walker.walk(printer, tree)
+    walker.walk(listener, tree)
+
+    print("start visitor...")
+    evalV = Visit()
+    evalV.visit(tree)
+
 
  
 if __name__ == '__main__':
