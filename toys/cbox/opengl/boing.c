d8e17905afbeab1ed3b21d8bef48853141842c4b
Bart Bartel <bartb7@gmail.com>
2018-12-18 08:17:51 -0800

    pu

---
+++ toys/cbox/opengl/boing.c
@@ -0,0 +1,42 @@
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit()) {
+        printf("dude you suck\n");
+        return -1;
+    }
+    printf("dude you don't suck\n");
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
---
+++ toys/cbox/opengl/gluttage.cpp
@@ -0,0 +1,10 @@
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
+
+
+int main()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
