7b712e1a747d1866625c9cc9f58786bb1fc10d80
Bart Bartel <bartb7@gmail.com>
2018-12-22 13:35:49 -0800

    pu

--- toys/cbox/lisp/puma.c
+++ toys/cbox/lisp/puma.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mpc.h"
 
-// gcc repl0.c mpc.c -ledit
+
+// linux
+// gcc -std=c99 -Wall polish.c mpc.c -ledit -lreadline -o polish
+// windows
+// gcc -std=c99 -Wall polish.c mpc.c -o polish
 
 /* If we are compiling on Windows compile these functions */
 #ifdef _WIN32
@@ -12,12 +17,12 @@ static char buffer[2048];
 
 /* Fake readline function */
 char* readline(char* prompt) {
-    fputs(prompt, stdout);
-    fgets(buffer, 2048, stdin);
-    char* cpy = malloc(strlen(buffer)+1);
-    strcpy(cpy, buffer);
-    cpy[strlen(cpy)-1] = '\0';
-    return cpy;
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
 }
 
 /* Fake add_history function */
@@ -30,61 +35,168 @@ void add_history(char* unused) {
 //#include <editline/history.h>
 #endif
 
+enum { LVAL_NUM, LVAL_ERR };
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
-int startup_puma()
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+
+int number_of_nodes(mpc_ast_t *t)
 {
-    mpc_parser_t *Adjective = mpc_new("adjective");
-    mpc_parser_t *Noun      = mpc_new("noun");
-    mpc_parser_t *Phrase    = mpc_new("phrase");
-    //mpc_parser_t *Number    = mpc_new("number");
-    mpc_parser_t *Puma      = mpc_new("puma");
-
-    mpca_lang(MPCA_LANG_DEFAULT,
-            "                               \
-            adjective : \"wow\"             \
-                      | \"many\"            \
-                      | \"so\"              \
-                      | \"such\";           \
-            noun      : \"puma\"            \
-                      | \"language\"        \
-                      | \"book\"            \
-                      | \"build\"           \
-                      | \"c\";              \
-            phrase    : <adjective> <noun>; \
-            puma      : <phrase>*;          \
-            ",
-            Adjective, Noun, Phrase, Puma);
-
-
-    while (1) {
-        char *input = readline("puma> "); // prompt
-        add_history(input);
-        if (strcmp(input, "quit") == 0) break;
-        printf("%s\n", input);
-        mpc_result_t r;
-        if (mpc_parse("<stdin", input, Puma, &r)) {
-            mpc_ast_print(r.output);
-            mpc_ast_delete(r.output);
-        } else {
-            // there was an error
-            mpc_err_print(r.error);
-            mpc_err_delete(r.error);
+    if (t->children_num == 0) { return 1; }
+    if (t->children_num >= 1) {
+        int total = 1;
+        int i;
+        for (i = 0; i < t->children_num; i++) {
+            total = total + number_of_nodes(t->children[i]);
         }
-        free(input);
+        return total;
     }
-
-    mpc_cleanup(1, Adjective, Noun, Phrase, Puma);
-
     return 0;
 }
 
-int main(int argc, char *argv[])
+lval lval_num(long x)
 {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
 
-    puts("puma ver 0.01");
-    puts("press ^-C to exit\n");
+lval lval_err(int x)
+{
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
 
-    startup_puma();
+void lval_print(lval v)
+{
+    switch (v.type) {
+    case LVAL_NUM: printf("%li", v.num); break;
+    case LVAL_ERR: 
+        if (v.err == LERR_DIV_ZERO) {
+            printf("Error: Division by zero.");
+        } else if (v.err == LERR_BAD_OP) {
+            printf("Error: Invalid operand.");
+        } else if (v.err == LERR_BAD_NUM) {
+            printf("Error: Invalid number.");
+        }
+        break;
+    }
+}
 
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+lval eval_op(lval x, char *op, lval y)
+{
+    if (x.type == LVAL_ERR) { return x; }
+    if (y.type == LVAL_ERR) { return y; }
+
+    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+    if (strcmp(op, "/") == 0) { 
+        return y.num == 0 
+            ? lval_err(LERR_DIV_ZERO)
+            : lval_num(x.num / y.num); 
+    }
+
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t)
+{
+    /* if tagged as number return it directly - base case */
+    if (strstr(t->tag, "number")) {
+        /* chk if error in conversion */
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+    }
+
+    /* the operator is always the 2nd child, first is ( */
+    char *op = t->children[1]->contents;
+
+    /* store 3rd child in x */
+    lval x = eval(t->children[2]);
+
+    /* iterate remaining children and combine */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+
+}
+
+int main(int argc, char** argv) {
+
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Function = mpc_new("function");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Puma     = mpc_new("puma");
+
+    //        expr      : <number> | '(' <operator> <expr>+ ')' ;
+    mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                   \
+            number    : /-?[0-9]+/ ;                            \
+            operator  : '+' | '-' | '*' | '/' | '%' ;           \
+            function  : \"mode\"                                \
+                      | \"scenario\"                            \
+                      | \"activate\"                            \
+                      | \"associate\"                           \
+                      | \"wait\"                                \
+                      | \"send\"                                \
+                      | \"ring\"                                \
+                      | \"signal\"                              \
+                      | \"log\"                                 \
+                      | \"free\"                                \
+                      | \"exec\" ;                              \
+            expr      : <function> '(' <number>* ')' ;          \
+            puma      : /^/ <expr>+ /$/ ;                       \
+            ",
+        Number, Operator, Function, Expr, Puma);
+
+  puts("Puma Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+
+  while (1) {
+
+    /* Now in either case readline will be correctly defined */
+    char* input = readline("puma> ");
+
+    add_history(input);
+
+    if (strncmp(input, "quit", 4) == 0) {
+        printf("bye bye\n");
+        free(input);
+        break;
+    }
+
+    mpc_result_t r;
+    if (mpc_parse("<stdin", input, Puma, &r)) {
+        //lval result = eval(r.output);
+        //lval_println(result);
+        mpc_ast_print(r.output);
+        mpc_ast_delete(r.output);
+    } else {
+        mpc_err_print(r.error);
+        mpc_err_delete(r.error);
+    }
+    free(input);
+  }
+
+  mpc_cleanup(4, Number, Operator, Expr, Puma);
+
+  return 0;
 }
 
