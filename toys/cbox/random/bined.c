414962a4a302eff779c0137864a6cf58a9cbf2c9
Bart Bartel <bartb7@gmail.com>
2018-12-22 09:06:53 -0800

    pu

---
+++ toys/cbox/random/bined.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+// https://www.codingunit.com/c-tutorial-binary-file-io
+
+/* Our structure */
+struct rec
+{
+    int x,y,z;
+};
+
+int write_bin()
+{
+    int counter;
+    FILE *ptr_myfile;
+    struct rec my_record;
+
+    ptr_myfile=fopen("test.bin","wb");
+    if (!ptr_myfile)
+    {
+        printf("Unable to open file!");
+        return 1;
+    }
+    for ( counter=1; counter <= 10; counter++)
+    {
+        my_record.x= counter;
+        fwrite(&my_record, sizeof(struct rec), 1, ptr_myfile);
+    }
+    fclose(ptr_myfile);
+    return 0;
+}
+
+int read_bin()
+{
+    int counter;
+    FILE *ptr_myfile;
+    struct rec my_record;
+
+    ptr_myfile=fopen("test.bin","rb");
+    if (!ptr_myfile)
+    {
+        printf("Unable to open file!");
+        return 1;
+    }
+    for ( counter=1; counter <= 10; counter++)
+    {
+        fread(&my_record,sizeof(struct rec),1,ptr_myfile);
+        printf("%d\n",my_record.x);
+    }
+    fclose(ptr_myfile);
+    return 0;
+}
+
+int main()
+{
+    write_bin();
+    read_bin();
+}
+
