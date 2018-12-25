006429334346138211d09b9976bddf4479c37d42
Bart Bartel <bartb7@gmail.com>
2018-12-25 05:00:21 -0800

    pu

--- toys/misc/py/bined.py
+++ toys/misc/py/bined.py
@@ -2,4 +2,11 @@
 
 # https://stackoverflow.com/questions/11462466/read-and-write-binary-file-in-python
 
+with open("test.bin", "rb") as f:
+    byte = f.read(1)
+    while byte:
+        # Do stuff with byte.
+        byte = f.read(1)
+        print(byte)
+
 
