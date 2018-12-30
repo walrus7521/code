bf2fb13f50fd41067f766468100b5ac9b6cc9ced
Bart Bartel <bartb7@gmail.com>
2018-12-29 19:57:37 -0800

    pu

--- toys/cbox/graphics/ncurses/pong.c
+++ toys/cbox/graphics/ncurses/pong.c
@@ -6,70 +6,85 @@ int main()
   object scr; int i = 0,cont=0; bool end=false;
   initscr();
   start_color();
+
   init_pair(1,COLOR_YELLOW, COLOR_BLACK);
+  init_pair(2,COLOR_RED, COLOR_BLACK);
+
   keypad(stdscr,true);
   noecho();
   curs_set(0);
   getmaxyx(stdscr,scr.y,scr.x);
-  object b1={scr.x-2,scr.y/2,0,false,false},b2={1,scr.y/2,0,false,false},b={scr.x/2,scr.y/2,0,false,false};
-  mvprintw(4,0,"\t           oooooooooo                                        \n"
-               "\t           888    888  ooooooo    ooooooo    oooooooo8       \n"
-               "\t           888oooo88 888     888 888   888  888    88o       \n"
-               "\t           888       888     888 888   888   888oo888o       \n"
-               "\t          o888o        88ooo88  o888o o888o 888     888      \n"
-               "\t                                             888ooo888     \n\n"
+
+  object paddle_right = {scr.x-2,scr.y/2,0,false,false};
+  object paddle_left  = {1,scr.y/2,0,false,false};
+  object ball         = {scr.x/2,scr.y/2,0,false,false};
+
+  mvprintw(4,0,"\t           oooooooooo                     o88   88o          \n"
+               "\t           888    888  ooooooo  o8888888  888 88o            \n"
+               "\t           888    88 888     888 888   8  888o               \n"
+               "\t           888    88 888     888 888      888 88o            \n"
+               "\t          o888o88888   88ooo88   oooo     8o8  888o          \n"
+               "\t                                                             \n\n"
                "\t Any questions please send me at vicente.bolea@gmail.com     \n"
                "\t \t\t\tPlayer 1 your controls are 'a' and 'q'                \n"
                "\t \t\t\tPlayer 2 your controls are the arrows of the keyboard \n"
                "\t \t\t\tPush ANY key to start, 'p' for pause and ESC to quit" ); 
   getch();
+
   for (nodelay(stdscr,1); !end; usleep(4000)) {
     //beep();
     //flash();      
     if (++cont%16==0){
-      if ((b.y==scr.y-1)||(b.y==1))
-        b.movver=!b.movver;
-      if ((b.x>=scr.x-2)||(b.x<=2)){
-        b.movhor=!b.movhor;
-        if ((b.y==b1.y-1)||(b.y==b2.y-1)) {
-          b.movver=false;
-        } else if ((b.y==b1.y+1)||(b.y==b2.y+1)) {
-          b.movver=true;
-        } else if ((b.y != b1.y) && (b.y != b2.y)) {
-          (b.x>=scr.x-2) ? b1.c++: b2.c++;
-          b.x=scr.x/2;
-          b.y=scr.y/2;
+      if ((ball.y==scr.y-1)||(ball.y==1))
+        ball.movver=!ball.movver;
+      if ((ball.x>=scr.x-2)||(ball.x<=2)){
+        ball.movhor=!ball.movhor;
+        if ((ball.y==paddle_right.y-1)||(ball.y==paddle_left.y-1)) {
+          ball.movver=false;
+        } else if ((ball.y==paddle_right.y+1)||(ball.y==paddle_left.y+1)) {
+          ball.movver=true;
+        } else if ((ball.y != paddle_right.y) && (ball.y != paddle_left.y)) {
+          (ball.x>=scr.x-2) ? paddle_right.c++: paddle_left.c++;
+          ball.x=scr.x/2;
+          ball.y=scr.y/2;
         }
       }
-      b.x=b.movhor ? b.x+1 : b.x-1;
-      b.y=b.movver ? b.y+1 : b.y-1;
 
-      if (b1.y<=1)
-        b1.y=scr.y-2;
-      if (b1.y>=scr.y-1)
-        b1.y=2;
-      if (b2.y<=1)
-        b2.y=scr.y-2;
-      if (b2.y>=scr.y-1)
-        b2.y=2;
+      ball.x = ball.movhor ? ball.x+1 : ball.x-1;
+      ball.y = ball.movver ? ball.y+1 : ball.y-1;
+
+      if (paddle_right.y <= 1)        paddle_right.y=scr.y-2;
+      if (paddle_right.y >= scr.y-1)  paddle_right.y=2;
+      if (paddle_left.y  <= 1)        paddle_left.y=scr.y-2;
+      if (paddle_left.y  >= scr.y-1)  paddle_left.y=2;
+
     }
+
+    /* user input */
     switch (getch()) {
-      case KEY_DOWN: b1.y++; break;
-      case KEY_UP:   b1.y--; break;
-      case 'q':      b2.y--; break;
-      case 'a':      b2.y++; break;
+      case KEY_DOWN: paddle_right.y++; break;
+      case KEY_UP:   paddle_right.y--; break;
+      case 'q':      paddle_left.y--; break;
+      case 'a':      paddle_left.y++; break;
       case 'p':      getchar(); break;
-      case 0x1B:    endwin(); end++; break;
+      case 0x1B:    endwin(); end++; break; // ESC key
     }
+
     erase();
-    mvprintw(2,scr.x/2-2,"%i | %i",b1.c,b2.c);
+
+    mvprintw(2,scr.x/2-2,"%i | %i",paddle_right.c,paddle_left.c);
     mvvline(0,scr.x/2,ACS_VLINE,scr.y);
+    
     attron(COLOR_PAIR(1));
-    mvprintw(b.y,b.x,"o");
-    for(i=-1;i<2;i++){
-      mvprintw(b1.y+i,b1.x,"|");
-      mvprintw(b2.y+i,b2.x,"|");}
+    mvprintw(ball.y,ball.x,"o");
     attroff(COLOR_PAIR(1));
+
+    attron(COLOR_PAIR(2));
+    for(i=-1;i<2;i++){
+        mvprintw(paddle_right.y+i,paddle_right.x,"|");
+        mvprintw(paddle_left.y+i,paddle_left.x,"|");
+    }
+    attroff(COLOR_PAIR(2));
   }
 }
 
