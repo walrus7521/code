af9e698d43bc024b04cca274aa390f24b444fda4
Bart Bartel <bartb7@gmail.com>
2019-01-01 16:50:52 -0800

    pu

--- toys/misc/go/scan2.go
+++ toys/misc/go/scan2.go
@@ -9,10 +9,11 @@ import (
 // demo reading multiple files
 // and readling line by line
 func main() {
+    data := make([]string, 0)
     counts := make(map[string]int)
     files := os.Args[1:] // list of files
     if len(files) == 0 {
-        countLines(os.Stdin, counts)
+        countLines(os.Stdin, counts, &data)
     } else {
         for _, arg := range files {
             f, err := os.Open(arg)
@@ -20,22 +21,29 @@ func main() {
                 fmt.Fprintf(os.Stderr, "dup2: %v\n", err);
                 continue
             }
-            countLines(f, counts)
+            countLines(f, counts, &data)
             f.Close()
         }
     }
 
+    fmt.Printf("hash: %d\n", len(counts))
     for line, n := range counts {
         if n > 0 {
             fmt.Printf("%d\t%s\n", n, line)
         }
     }
+
+    fmt.Printf("array: %d\n", len(data))
+    for i := 0; i < len(data); i++ {
+        fmt.Printf("[%d] => %s\n", i, data[i]);
+    }
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, data *[]string) {
     input := bufio.NewScanner(f)
     for input.Scan() {
         counts[input.Text()]++
+        *data = append(*data, input.Text())
     }
 }
 
