53e2f31fb485b4ac66a02e64475e4e720ae574b9
Bart Bartel <bartbartel@Barts-MBP.hsd1.wa.comcast.net>
2019-01-09 09:02:28 -0800

    auto

--- toys/misc/obj-c/hello.m
+++ toys/misc/obj-c/hello.m
@@ -9,7 +9,28 @@
 
 #import <Foundation/Foundation.h>
 
+struct node {
+    int id;
+    char *key;
+    int value;
+    struct node *left;
+    struct node *right;
+} node;
+
+void traverse(struct node *tree)
+{
+    if (tree == NULL) return;
+    if (tree->left) traverse(tree->left);
+    printf("%s %d\n", tree->key, tree->value);
+    if (tree->right) traverse(tree->right);
+}
+
 int main(int argc, char** argv)
 {
   NSLog(@"Hello, world");
+  struct node *root = (struct node *) malloc(sizeof(struct node));
+  root->key = strdup("dude");
+  root->value = 42;
+  root->left = root->right = NULL;
+  traverse(root);
 }
