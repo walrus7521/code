7c081deb1c5e91820957687b1497e9b9a90dcacc
Bart Bartel <bartb7@gmail.com>
2019-01-22 08:17:02 -0800

    pu

--- toys/misc/js/cheat.js
+++ toys/misc/js/cheat.js
@@ -46,7 +46,9 @@ function parse_pkt(pkt) {
     var str = printf("pkt: %s", pkt);
     console.log(str);
     console.log(colors.green(str));
+    console.log(colors.yellow(str));
     console.log(colors.red(str));
+    console.log(colors.blue(str));
 }
 
 csv
