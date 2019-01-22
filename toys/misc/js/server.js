33fd3a1ab47bbc2c7df35381045697ebd70729d1
Bart Bartel <bartb7@gmail.com>
2019-01-22 08:24:15 -0800

    pu

---
+++ toys/misc/js/server.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+// connect in browser at: http://localhost:3000/
+//server.js
+const http = require('http'),
+      server = http.createServer();
+
+server.on('request',(request,response)=>{
+   response.writeHead(200,{'Content-Type':'text/plain'});
+   response.write('wusup dawg');
+   response.end();
+});
+
+server.listen(3000,()=>{
+  console.log('Node server created at port 3000');
+});
+
