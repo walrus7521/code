0bd22175e10295de66bcfdd34cfc875722d445aa
Bart Bartel <BartB@Corp.helitrak.com>
2019-01-23 15:26:57 -0800

    st

--- toys/cbox/compiler/perlish/test.pl
+++ toys/cbox/compiler/perlish/test.pl
@@ -2,8 +2,8 @@
 
 $path = "..\\..\\..";
 
-$cmd = "c:\\llvm\\bin\\clang -v -S -I$path\\MCP\\Header -I$path\\Common\\Header -I$path\\Model\\Common\\Header -I$path\\Model\\Inner_Loop\\Header -I$path\\Model\\OuterLoop\\Header -I$path\\Lib\\Stellaris_CM3\\Include\\Stellarisware\\inc -I$path\\Tools\\COTS_Libraries\\Stellarisware -Xclang -fdump-record-layouts main_MCP.c > mcp.ast";
-system($cmd);
+#$cmd = "c:\\llvm\\bin\\clang -v -S -I$path\\MCP\\Header -I$path\\Common\\Header -I$path\\Model\\Common\\Header -I$path\\Model\\Inner_Loop\\Header -I$path\\Model\\OuterLoop\\Header -I$path\\Lib\\Stellaris_CM3\\Include\\Stellarisware\\inc -I$path\\Tools\\COTS_Libraries\\Stellarisware -Xclang -fdump-record-layouts main_MCP.c > mcp.ast";
+#system($cmd);
 
 $cmd = ".\\isolate_records.pl < .\\mcp.ast > .\\mcp.recs";
 system($cmd);
