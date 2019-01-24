159fd24297ad07c84ca6c203bab8bf90c39a4c23
Bart Bartel <bartbartel@Barts-MBP.hsd1.wa.comcast.net>
2019-01-23 20:03:35 -0800

    auto

--- toys/cbox/compiler/perlish/recdec.pl
+++ toys/cbox/compiler/perlish/recdec.pl
@@ -12,24 +12,24 @@ $::RD_WARN   = 1; # Enable warnings. This will warn on unused rules &c.
 $::RD_HINT   = 1; # Give out hints to help fix problems.
 
 my $grammar = <<'_EOGRAMMAR_';
-   # Terminals (macros that can't expand further)
+    # Terminals (macros that can't expand further)
 
-   OP       : m([-+*/%])      # Mathematical operators
-   INTEGER  : /[-+]?\d+/      # Signed integers
-   VARIABLE : /\w[a-z0-9_]*/i # Variable
+    OP       : m([-+*/%])      # Mathematical operators
+    INTEGER  : /[-+]?\d+/      # Signed integers
+    VARIABLE : /\w[a-z0-9_]*/i # Variable
 
-   typeDecl : (INTEGER) '|' (VARIABLE) (VARIABLE)
+    typeDecl : (INTEGER) '|' (VARIABLE) (VARIABLE)
               { return main::typeDecl(@item) }
 
-   startrule: typeDecl(s)
+    startrule: typeDecl(s)
 
 _EOGRAMMAR_
 
 sub typeDecl {
-   shift;
-   #print "@_\n";
-   my ($offset,$pipe,$type,$id) = @_;
-   #print "$offset - $type -> $id\n";
+    shift;
+    #print "@_\n";
+    my ($offset,$pipe,$type,$id) = @_;
+    print "$offset - $type -> $id\n";
 }
 
 my $parser = Parse::RecDescent->new($grammar);
@@ -39,6 +39,4 @@ while (<>) {
     $parser->startrule($_);
 }
 
-#print "        24 |   VECTOR3 mags\n"; $parser->startrule("        24 |   VECTOR3 mags");
-#print "        24 |     F32 X\n";      $parser->startrule("        24 |     F32 X");
 
