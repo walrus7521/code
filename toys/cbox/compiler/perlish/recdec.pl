dcba3293cd564e1e2edea9f582f4749eba1babac
Bart Bartel <BartB@Corp.helitrak.com>
2019-01-24 10:09:04 -0800

    st

--- toys/cbox/compiler/perlish/recdec.pl
+++ toys/cbox/compiler/perlish/recdec.pl
@@ -20,22 +20,55 @@ my $grammar = <<'_EOGRAMMAR_';
 
     space    : <skip:''>
     indent   : space(s?)
-    typeDecl : (NUM) '|' (PRIM) (ID)
-              { print "1 - "; return main::typeDecl(@item,$prevcolumn,$thiscolumn) }
-             | (NUM) '|' (ID) (ID)
-              { print "2 - "; return main::typeDecl(@item,$prevcolumn,$thiscolumn) }
-             | (NUM) '|' (ID)
-              { print "3 - "; return main::typeDecl(@item,$prevcolumn,$thiscolumn) }
+    startcol : '' { $thiscolumn }    # NEED THE '' TO STEP PAST TOKEN SEP
+    endcol   :  { $prevcolumn }              
+    typeDecl : (NUM) ('|') (PRIM) (ID)
+              { my $indent = $itempos[3]{offset}{from}; 
+                print "1 - ($indent): $item[1], $item[2], $item[3], $item[4]\n"; 
+                #print "$itempos[2]{offset}{from}\n";
+                #print "$itempos[3]{offset}{from}\n";
+                #print "$itempos[$item[2]]{column}{to}\n";
+                #return main::term(@item,$prevcolumn,$thiscolumn) 
+              }
+             | (NUM) ('|') (ID) (ID)
+              { my $indent = $itempos[3]{offset}{from};
+                print "2 - ($indent): $item[1], $item[2], $item[3], $item[4]\n"; 
+                #return main::non_term(@item,$prevcolumn,$thiscolumn) 
+              }
+             | (NUM) ('|') (ID)
+              { my $indent = $itempos[1]{offset}{from};
+                print "3 - ($indent): $item[1], $item[2], $item[3]\n"; 
+                #return main::non_term(@item,$prevcolumn,$thiscolumn) 
+              }
 
     startrule: typeDecl(s)
 
 _EOGRAMMAR_
 
-sub typeDecl {
+my @stack = (); # path accumulation stack (struct names by nesting)
+sub get_path {
+    my @tmp1 = @stack;
+    my @tmp2;
+    my $path = "";
+    if (@stack == 0) { return ""; }
+    while (@tmp1) { my $tmp = pop(@tmp1); push(@tmp2, $tmp); } # reverse @stack
+    while (@tmp2) { my $val = pop @tmp2; $path = $path . $val . "."; } # build path
+    return $path;
+}
+
+sub term {
+    shift;
+    #print "@_\n";
+    my $path = get_path();
+    my ($offset,$pipe,$type,$id,$col1,$col2) = @_;
+    print "TERM: $offset : $type : $id : $path\n";
+}
+sub non_term {
     shift;
     #print "@_\n";
     my ($offset,$pipe,$type,$id,$col1,$col2) = @_;
-    print "$offset : $type : $id\n";
+    push(@stack, $id);
+    print "PROD: $offset : $type : $id\n";
 }
 
 my $parser = Parse::RecDescent->new($grammar);
