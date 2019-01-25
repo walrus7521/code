a37fae316965e18fe8291db39934d09cc744d088
Bart Bartel <bartb7@gmail.com>
2019-01-25 07:43:39 -0800

    pu

--- toys/cbox/compiler/coverage/main.c
+++ toys/cbox/compiler/coverage/main.c
@@ -1,11 +1,19 @@
 
 int fun1();
 int fun2();
-int fun3();
 int fun4();
 
-int fun1() { return fun2(); }
-int fun2() { return fun3(); }
+int fun1() { 
+    int c = 3, x;
+    if (c < 2) {
+        x = c;
+    } else {
+        x = 0;
+    }
+    return fun2(); 
+}
+
+int fun2() { return 0;      }
 int fun3() { return 42;     }
 
 
