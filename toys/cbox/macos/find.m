e13f0a1d96429c9cd1d4575bbaf52b68eac90fa6
Bart Bartel <bartb7@gmail.com>
2019-01-29 11:04:17 -0800

    pu

---
+++ toys/cbox/macos/find.m
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) 
+{
+
+    @autoreleasepool
+    {
+        NSFileManager *manager;
+        manager = [NSFileManager defaultManager];
+
+        NSString *home;
+        home = [@"~" stringByExpandingTildeInPath];
+
+        NSDirectoryEnumerator *direnum;
+        direnum = [manager enumeratorAtPath:home];
+        printf("here\n");
+
+        NSMutableArray *files;
+        files = [NSMutableArray arrayWithCapacity:42];
+
+        NSString *filename;
+        while (filename = [direnum nextObject]) {
+            if ([[filename pathExtension] isEqualTo: @"pl"]) {
+                NSLog(@"%@", filename);
+                [files addObject: filename];
+            }
+        }
+
+        NSEnumerator *fileenum;
+        fileenum = [files objectEnumerator];
+
+        while (filename = [fileenum nextObject]) {
+            NSLog(@"%@", filename);
+        }       
+    }
+
+    return 0;
+}
+
+
