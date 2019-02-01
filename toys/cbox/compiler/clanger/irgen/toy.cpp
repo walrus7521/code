87fecc3b3dec767808ac403132fa800ef887c0ab
Bart Bartel <bartb7@gmail.com>
2019-02-01 08:55:09 -0800

    pu

--- toys/cbox/compiler/clanger/irgen/toy.cpp
+++ toys/cbox/compiler/clanger/irgen/toy.cpp
@@ -1,15 +1,28 @@
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+
+#include <string>
+#include <vector>
 
 using namespace llvm;
 
-//static LLVMContext &Context = getGlobalContext();
 static LLVMContext Context;
 static Module *module = new Module("my compiler", Context);
 
+Function *createFunc(IRBuilder<> &Builder, std::string Name) {
+    FunctionType *funcType = llvm::FunctionType::get(Builder.getInt32Ty(), false);
+    Function *fooFunc = llvm::Function::Create(funcType, 
+            llvm::Function::ExternalLinkage, Name, module);
+    return fooFunc;
+}
+
 int main(int argc, char const *argv[])
 {
-    //module->dump(); // broken!!
+    static IRBuilder<> Builder(Context);
+    Function *fooFunc = createFunc(Builder, "foo");
+    verifyFunction(*fooFunc);
     module->print(llvm::errs(), nullptr);
     return 0;
 }
