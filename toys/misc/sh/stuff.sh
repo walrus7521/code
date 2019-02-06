1e180946e150d10fb6e68d2647d99d801773ffee
Bart Bartel <bartb7@gmail.com>
2019-02-05 16:47:21 -0800

    pu

--- toys/misc/sh/stuff.sh
+++ toys/misc/sh/stuff.sh
@@ -1,5 +1,24 @@
 #!/bin/bash 
 
+
+# ╔═════════════════╦════════════════════════════════════════╗
+# ║ Syntax          ║ Result                                 ║
+# ╠═════════════════╬════════════════════════════════════════╣
+# ║ arr=()          ║ Create empty array                     ║
+# ║ arr=(1 2 3)     ║ Initialize array                       ║
+# ║ ${arr[2]}       ║ Retrieve third element                 ║
+# ║ ${arr[@]}       ║ Retrieve all elements                  ║
+# ║ ${!arr[@]}      ║ Retrieve array indices                 ║
+# ║ ${#arr[@]}      ║ Calculate array size                   ║
+# ║ arr[0]=3        ║ Overwrite 1st element                  ║
+# ║ arr+=(4)        ║ Append value(s)                        ║
+# ║ str=$(ls)       ║ Save ls output as string               ║
+# ║ arr=( $(ls) )   ║ Save ls output as array of files       ║
+# ║ ${arr[@]:s:n}   ║ Elements at indices n to s+n           ║
+# ║ ${str//ab/c}    ║ For a given string, replace ab with c  ║
+# ║ ${arr[@]//ab/c} ║ For each array item, replace ab with c ║
+# ╚═════════════════╩════════════════════════════════════════╝
+
 counter=0
 while [  $counter -lt 10 ]; do
     echo The counter is $counter
@@ -15,5 +34,14 @@ for ((i=1; i<=n; i++)) {
 for i in $( ls ); do
     echo item: $i
 done
+
+allThreads=(1 2 4 8 16 32 64 128)
+myArray=(1 2 "three" 4 "five")
+
+for t in ${allThreads[@]}; do
+    printf 'dude: %d => "%d"\n' $t $t
+done
+
+
 
 
