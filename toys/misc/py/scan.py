e4959e6a5a5c7d20d9fd17499b79e57959b5c089
Bart Bartel <bartb7@gmail.com>
2019-02-11 13:40:45 -0800

    pu

--- toys/misc/py/scan.py
+++ toys/misc/py/scan.py
@@ -7,14 +7,18 @@ from colorama import Fore
 
 regexp = "(import)"
 
-for line in sys.stdin:
-    if line:
-        line = line.rstrip()
-        cap = re.search(regexp, line)
-        if (cap):
-            n = len(cap.groups())
-            print(Fore.RED, n)
-#           str = "c:/www/" + cap.group(1)
-#           print("from was: " + str + ":> " + line)
-
-print(Fore.RESET)
+try:
+    for line in sys.stdin:
+        if line:
+            line = line.rstrip()
+            cap = re.search(regexp, line)
+            if (cap):
+                n = len(cap.groups())
+                print(Fore.BLUE, n)
+    #           str = "c:/www/" + cap.group(1)
+    #           print("from was: " + str + ":> " + line)
+    raise Exception
+except Exception:
+    print(Fore.RED, "you're fucked")
+finally:
+    print(Fore.RESET)
