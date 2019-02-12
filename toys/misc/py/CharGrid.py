7a7efa74fee3c53d7e6fedfbdf7f3483cc9404d5
Bart Bartel <bartb7@gmail.com>
2019-02-12 10:01:05 -0800

    pu

---
+++ toys/misc/py/CharGrid.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 Qtrac Ltd. All rights reserved
+
+"""
+
+This module provides grid control functions.
+
+>>> resize(14, 50)
+>>> add_rectangle(0, 0, *get_size())
+>>> add_vertical_line(5, 10, 13)
+>>> add_vertical_line(2, 9, 12, "!")
+>>> add_horizontal_line(3, 10, 20, "+")
+>>> add_rectangle(0, 0, 5, 5, "%")
+>>> add_rectangle(5, 7, 12, 40, "#", True)
+>>> add_rectangle(7, 9, 10, 38, " ")
+>>> add_text(8, 10, "This is the CharGrid module")
+>>> add_text(1, 32, "Pleasantville", "@")
+>>> add_rectangle(6, 42, 11, 46, fill=True)
+>>> render(False)
+
+"""
+
+import sys
+import subprocess
+
+### custom exceptions
+class RangeError(Exception): pass
+class RowRangeError(RangeError): pass
+class ColumnRangeError(RangeError): pass
+
+### private data
+_CHAR_ASSERT_TEMPLATE = ("char must be a single character: '{0}' "
+                         "is too long")
+_max_rows = 25
+_max_columns = 80
+_grid = []
+_background_char = " "
+
+if sys.platform.startswith("win"):
+    def clear_screen():
+        subprocess.call(["cmd.exe", "/C", "cls"])
+else:
+    def clear_screen():
+        subprocess.call(["clear"])
+clear_screen.__doc__ = """Clears the screen using the underlying \
+window system's clear screen command"""
+
+
