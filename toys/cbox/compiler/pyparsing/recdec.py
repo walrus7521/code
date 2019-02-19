13a0453d4a728d3d85a45b7fe180bcf6f8f08ebb
Bart Bartel <BartB@Corp.helitrak.com>
2019-02-19 13:53:54 -0800

    st

--- toys/cbox/compiler/pyparsing/recdec.py
+++ toys/cbox/compiler/pyparsing/recdec.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python3
 
-# todo: bit transitions are broken
-#       fix dotted path
-#       add .flag_bf. to bit paths
-#       convert path stack to queue
-#       test on full mcp.rec
-#       document
+# @run: python3 recdec.py > out
+# @todo: add support for arrays
 
 from pyparsing import (
-    alphanums, col, alphas, nums, CharsNotIn, Forward, restOfLine,
-    Group, hexnums, OneOrMore, Optional, ParseException, White,
-    ParseSyntaxException, Suppress, Word, ZeroOrMore, CaselessLiteral,
-    CaselessKeyword, Literal, Regex, LineEnd)
-import pyparsing as pp
+    alphanums, col, alphas, nums, CharsNotIn, Forward, restOfLine, Group, hexnums, OneOrMore,
+    Optional, ParseException, White, ParseSyntaxException, Suppress, Word, ZeroOrMore,
+    CaselessLiteral, CaselessKeyword, Literal, Regex, LineEnd, Keyword, MatchFirst)
 import collections
 
 Struct = collections.namedtuple("Struct", "offset level type name path bit")
 
 structs      = []
-stack        = [] # path accumulation stack (struct names by nesting)
+path_queue   = [] # path accumulation names by nesting)
 delta_levels = 0
 this_level   = 0
 last_level   = 0
@@ -26,70 +20,35 @@ bit_count    = 0
 top          = ""
 
 def get_path():
-    global stack
-    tmp1 = stack.copy()
-    tmp2 = []
+    global path_queue
     path = ""
-    if (len(stack) == 0): return path
-    while (len(tmp1)):
-        tmp = tmp1.pop()
-        tmp2.append(tmp) # reverse @stack
-    while (len(tmp2)): # build path
-        val = tmp2.pop()
-        path += val
-        path += "."
+    tmp1 = path_queue.copy()
+    while (len(tmp1)): # build path
+        path += (tmp1.pop() + ".")
+    path = path[:-1] # delete extra dot in path
     return path;
 
 def dump_structs():
     for s in structs:
-        if (s.bit >= 0):
-            print("  {0}.{1} {2} {3}".format(s.path, s.name, s.type, s.bit))
-        elif (len(s.path)):
-            print("  {0}.{1} {2}".format(s.path, s.name, s.type))
-        else:
-            print("  {0} {1}".format(s.name, s.type))
-
-def dump_structs2():
-    for s in structs:
-        print("  {0} '|' {1} {2}".format(s.offset, s.name, s.type))
-
-def do_skip1(tokens):
-    #print("skip1")
-    pass
-    
-def do_skip2(tokens):
-    #print("skip2")
-    pass
-    
-def do_skip3(tokens):
-    #print("skip3")
-    pass
-
-def do_skip(tokens):
-    global bit_count
-    #bit_count = 0
-    #print("skip")
-    pass
-    #for t in tokens:
-    #    print(t)
+        if (s.bit >= 0):    print("  {0}.{1} {2} {3}".format(s.path, s.name, s.type, s.bit))
+        elif (len(s.path)): print("  {0}.{1} {2}".format(s.path, s.name, s.type))
+        else:               print("  {0} {1}".format(s.name, s.type))
 
 def do_top(tokens):
-    global structs, stack, top
+    global structs, path_queue, top
     if (len(structs)):
         print("STRUCT: {0}".format(top))
-        dump_structs2()
-    else:
-        top = tokens.id
-    structs      = []
-    stack        = []
-    pass
+        dump_structs()
+    else: top = tokens.id
+    structs    = [] # clear out the structs and path queue
+    path_queue = []
 
 def do_bits(tokens):
     global bit_count, delta_levels, this_level, last_level
     this_level = len(tokens.white)
     delta_levels = this_level - last_level
-    #print("bits: delta {0} {1}".format(delta_levels, tokens.id))
-    structs.append(Struct(tokens.num, delta_levels, tokens.type, tokens.id, get_path(), bit_count))
+    path = (get_path() + ".flags_bf")
+    structs.append(Struct(tokens.num, delta_levels, tokens.type, tokens.id, path, bit_count))
     bit_count += 1
     last_level = this_level
 
@@ -97,12 +56,11 @@ def do_term(tokens):
     global delta_levels, this_level, last_level, bit_count
     this_level = len(tokens.white)
     delta_levels = this_level - last_level
-    #print("delta: {0} {1}".format(delta_levels, tokens.id))
     if (bit_count > 0):
         bit_count = 0
     if "flags" in tokens.id: return # ignore names with 'flag' in terminals
     if (delta_levels < 0):
-        stack.pop()
+        path_queue.pop()
     structs.append(Struct(tokens.num, delta_levels, tokens.type, tokens.id, get_path(), -1))
     last_level = this_level
 
@@ -111,20 +69,18 @@ def do_struct(tokens):
     this_level = len(tokens.white)
     delta_levels = this_level - last_level
     if (bit_count > 0):
-        #print("oldbits: delta {0} {1}".format(delta_levels, tokens.id))
-        #bit_count = 0
-        stack.pop()        
+        path_queue.pop()        
     if (delta_levels < 0):
         dx = abs(delta_levels)
-        while (dx and len(stack)):
-            stack.pop()
+        while (dx and len(path_queue)):
+            path_queue.pop()
             dx -= 1
-    #print("post: delta {0} {1}".format(delta_levels, tokens.id))
-    stack.append(tokens.id) # push new id on stack
+    path_queue.append(tokens.id) # push new id on path_queue
     last_level = this_level
 
-Types = (F32, U32, S32, U8, BOOLEAN) = map(pp.Keyword, """F32 U32 S32 U8 BOOLEAN""".split())
-Types = pp.MatchFirst(list(Types))
+### Tokens ###
+Types = (F32, U32, S32, U8, BOOLEAN) = map(Keyword, """F32 U32 S32 U8 BOOLEAN""".split())
+Types = MatchFirst(list(Types))
 
 TYPE    = Group(F32 | U32 | U8 | BOOLEAN)("type")
 INDENT  = White()("white")
@@ -132,37 +88,29 @@ NUM     = Word(nums)("num")
 ID      = Word(alphas+'_', alphanums+'_')("id")
 EOL     = LineEnd().suppress()
 
-skip1     = (NUM + "|" + INDENT + Word("struct") + Regex(r".*"))
-skip2     = (INDENT + "|" + "[sizeof" + Regex(r".*"))
-skip3     = (EOL)
-skip      = (skip1 | skip2 | skip3)
+### Grammar ###
+skip     = ((NUM + "|" + INDENT + Word("struct") + Regex(r".*"))
+         |  (INDENT + "|" + "[sizeof" + Regex(r".*"))
+         |  (EOL))
 terminal  = (NUM + "|" + INDENT + TYPE + ID)
 structure = (NUM + "|" + INDENT + ID + ID)
 bitflags  = (NUM + ":" + NUM + "-" + NUM + "|" + INDENT + TYPE + ID)
 toplevel  = (NUM + "|" + ID) # top level structure name
 
-decl = ( skip | terminal | structure | bitflags | toplevel )
-
-skip1.setParseAction(do_skip1)
-skip2.setParseAction(do_skip2)
-skip3.setParseAction(do_skip3)
-skip.setParseAction(do_skip)
 terminal.setParseAction(do_term)
 structure.setParseAction(do_struct)
 toplevel.setParseAction(do_top)
 bitflags.setParseAction(do_bits)
+
+decl = ( skip | terminal | structure | bitflags | toplevel )
 parser = OneOrMore(decl)
 
 def do_parse(file_name):
-    global delta_levels, this_level, last_level
-    delta_levels = this_level = last_level = 0
     file = open(file_name, "r")
-    try:
-        parser.parseFile(file)
+    try: parser.parseFile(file)
     except ParseException as err:
         print("parse error: {0}".format(err))
 
 filename = "mcp.recs" #"telem.rec"
 do_parse(filename)
-#exit()
 
