91eb8405e48e011dee9e555058359dbf2aee733b
Bart Bartel <BartB@Corp.helitrak.com>
2019-03-06 13:29:12 -0800

    st

--- toys/cbox/algo/comp/play.cc
+++ toys/cbox/algo/comp/play.cc
@@ -22,8 +22,25 @@ void pi_decimal_places()
     printf("\n");
 }
 
+// wrong!!
+float newPrecision(float n, float i) 
+{ 
+    return floor(pow(10,i)*n)/pow(10,i); 
+}
+
+void pi_decimal_places2(int n)
+{
+   float num = 3.14159265358979323846;
+  
+   // 4 digits after the decimal point 
+   num = floor(10000*num)/10000; 
+  
+   printf("%f", num); 
+}
+
 int main()
 {
-    pi_decimal_places();
+    //pi_decimal_places2(7);
+    printf("%f\n", newPrecision(M_PI, 9.0));
 }
 
