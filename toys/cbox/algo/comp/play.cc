4f4385d97919fd43707f0625c4699550c6f887d1
Bart Bartel <bartb7@gmail.com>
2019-03-07 08:48:07 -0800

    pu

--- toys/cbox/algo/comp/play.cc
+++ toys/cbox/algo/comp/play.cc
@@ -1,5 +1,12 @@
 #include <cstdio>
+#include <iostream>
+#include <cstdlib>
 #include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
 
 void pi_decimal_places(int n)
 {
@@ -27,9 +34,25 @@ void pi_n_decimal_places(int n)
     printf("%.*lf\n", n, pi); // ****
 }
 
+
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(x) (x).resize(unique(ALL(x)) - (x).begin())
+void n_rand_ints_sorted(int n)
+{
+    int arr[] = {1,2,2,2,3,3,2,2,1};
+    n = sizeof(arr)/sizeof(arr[0]);
+    vector<int> v(arr, arr+n);
+    sort(ALL(v));
+    UNIQUE(v);
+    for (auto x : v) {
+        cout << x << endl;
+    }
+}
+
 int main()
 {
-    pi_decimal_places(9);
-    pi_n_decimal_places(11);
+    //pi_decimal_places(9);
+    //pi_n_decimal_places(11);
+    n_rand_ints_sorted(32);
 }
 
