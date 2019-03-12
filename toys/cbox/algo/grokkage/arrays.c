eb302e3a4859f93c496baf23d33b13128d493255
Bart Bartel <BartB@Corp.helitrak.com>
2019-03-12 12:37:59 -0700

    st

--- toys/cbox/algo/grokkage/arrays.c
+++ toys/cbox/algo/grokkage/arrays.c
@@ -348,34 +348,26 @@ void ss_merge(int arr[], int l, int m, int r)
     int L[n1], R[n2];
  
     /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
+    for (i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (j = 0; j < n2; j++) R[j] = arr[m + 1+ j];
  
     /* Merge the temp arrays back into arr[l..r]*/
     i = 0; // Initial index of first subarray
     j = 0; // Initial index of second subarray
     k = l; // Initial index of merged subarray
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
             arr[k] = L[i];
             i++;
-        }
-        else
-        {
+        } else {
             arr[k] = R[j];
             j++;
         }
         k++;
     }
- 
     /* Copy the remaining elements of L[], if there
        are any */
-    while (i < n1)
-    {
+    while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
@@ -383,8 +375,7 @@ void ss_merge(int arr[], int l, int m, int r)
  
     /* Copy the remaining elements of R[], if there
        are any */
-    while (j < n2)
-    {
+    while (j < n2) {
         arr[k] = R[j];
         j++;
         k++;
@@ -393,8 +384,7 @@ void ss_merge(int arr[], int l, int m, int r)
 
 void merge_sort(int arr[], int l, int r)
 {
-    if (l < r)
-    {
+    if (l < r) {
         // Same as (l+r)/2, but avoids overflow for
         // large l and h
         int m = l+(r-l)/2; // partition
