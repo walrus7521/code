a9fb2a95192552a3f5ebd0d598e5014405c56504
Bart Bartel <BartB@Corp.helitrak.com>
2019-04-17 11:09:21 -0700

    st

--- toys/cbox/algo/rubik/grok.c
+++ toys/cbox/algo/rubik/grok.c
@@ -30,6 +30,20 @@ void insertion(int a[], int n)
 // merge sort
 void merge(int a[], int l, int m, int r)
 {
+    int i, j, k, n1, n2;
+    n1 = m - l + 1;
+    n2 = r - m;
+    int L[n1], R[n2];
+    for (i = 0; i < n1; i++) L[i]=a[l+i];
+    for (j = 0; j < n2; j++) R[j]=a[m+1+j];
+    i = j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) { a[k++] = L[i++]; } 
+        else              { a[k++] = R[j++]; }
+    }
+    while (i < n1) { a[k++] = L[i++]; }
+    while (j < n2) { a[k++] = R[j++]; }
 }
 
 void merge_sort(int a[], int l, int r)
