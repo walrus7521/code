e11215474655c1b360d4a95162193104f2963ead
Bart Bartel <BartB@Corp.helitrak.com>
2019-05-14 09:32:16 -0700

    st

---
+++ toys/cbox/algo/rubik/types.h
@@ -0,0 +1,19 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
+typedef struct list {
+    struct list *next;
+    int val;
+} list;
+
+typedef struct tree {
+    struct tree *left, *right;
+    int val;
+} tree;
+
+// array abstractions
+// queue, stack
+
+#endif //_TYPES_H_
