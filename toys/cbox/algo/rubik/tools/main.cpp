002f954cb91873894a236db42f85ad2b9260d44f
Bart Bartel <BartB@Corp.helitrak.com>
2019-06-07 09:47:22 -0700

    st

--- toys/cbox/algo/rubik/tools/main.cpp
+++ toys/cbox/algo/rubik/tools/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
-#include <cstdio>
-#include "time24.hpp"
+#include "utils.h"
 
 int main()
 {
-    time24 t;
-
+    ll test_l;      // long long
+    vi test_vi;     // vector<int>
+    ii test_ii;     // pair<int, int>
+    vii test_vii;   // vector<pair<int, int>>
+    si test_si;     // set<int>
+    msi test_msi;   // map<string, int>
 }
