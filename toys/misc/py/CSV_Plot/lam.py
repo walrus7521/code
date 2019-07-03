e9748b5e83cd4b4451c84b35475c735c6e302bd1
Bart Bartel <BartB@Corp.helitrak.com>
2019-07-03 11:08:20 -0700

    st

--- toys/misc/py/CSV_Plot/lam.py
+++ toys/misc/py/CSV_Plot/lam.py
@@ -20,7 +20,10 @@ else:
 my_list = [1, 5, 4, 6, 8, 11, 3, 12]
 
 new_list = list(filter(lambda x: (x%2 == 0) , my_list))
-
 # Output: [4, 6, 8, 12]
 print(new_list)
+
+new_list2 = list(map(lambda x: (x%2 == 0) , my_list))
+# Output: [False, False, True, True, True, False, False, True]
+print(new_list2)
 
