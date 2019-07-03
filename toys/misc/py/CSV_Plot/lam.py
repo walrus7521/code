4d71feb28f90ed8073f4fa4fc44b840a3868e55e
Bart Bartel <BartB@Corp.helitrak.com>
2019-07-03 10:59:38 -0700

    st

---
+++ toys/misc/py/CSV_Plot/lam.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+triggers = ['x', 'y', 'z']
+print(triggers)
+
+trigger = {'x': 3, 'y': 4, 'z': 5}
+row     = {'x': 3, 'y': 4, 'z': 5}
+
+is_triggered = list(filter(lambda k : (float(row[k]) >= trigger[k]), triggers))
+
+print(len(is_triggered))
+
+if (len(is_triggered)):
+    print("triggered")
+else:
+    print("no trigger")
+
+
+
+my_list = [1, 5, 4, 6, 8, 11, 3, 12]
+
+new_list = list(filter(lambda x: (x%2 == 0) , my_list))
+
+# Output: [4, 6, 8, 12]
+print(new_list)
+
