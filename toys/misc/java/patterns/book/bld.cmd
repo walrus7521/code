c91da434c850e7774734f0aebbd03d5fa9acacca
Bart Bartel <BartB@Corp.helitrak.com>
2019-09-11 10:24:11 -0700

    st

---
+++ toys/misc/java/patterns/book/bld.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+javac FlyBehavior.java 
+javac QuackBehavior.java 
+javac FlyWithWings.java 
+javac FlyNoWay.java 
+javac QuackBehavior.java 
+javac Quack.java 
+javac Squeak.java 
+javac MuteQuack.java 
+javac Duck.java 
+javac MiniDuckSimulator.java 
+java MiniDuckSimulator
+
