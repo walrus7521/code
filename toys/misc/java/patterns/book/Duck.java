b87bcc179c26316a2beb2e4d5806412c0e461fa4
Bart Bartel <bartb7@gmail.com>
2019-09-11 08:18:08 -0700

    pu

--- toys/misc/java/patterns/book/Duck.java
+++ toys/misc/java/patterns/book/Duck.java
@@ -1,11 +1,4 @@
 
-public interface QuackBehavior {
-    public void quack();
-}
-public interface FlyBehavior {
-    public void fly();
-}
-
 public abstract class Duck {
 
     FlyBehavior   flyBehavior;
@@ -24,23 +17,5 @@ public abstract class Duck {
     public void swim() {
         System.out.println("All ducks float, even decoys");
     }
-    public void display() {
-    }
-}
-
-public class MallardDuck extends Duck {
-    public MallardDuck() {
-        quackBehavior = new Quack();
-        flyBehavior = new FlyWithWings();
-    }
-    public void display() {
-        System.out.println("I'm a real Mallard duck");
-    }
-}
-
-class Main {
-    public static void main(String args[]) {
-    }
-
 }
 
