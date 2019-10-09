acfeb823f4d0aada826994f6f0c9c558d2649d13
Bart Bartel <bartbartel@Barts-MBP-4.hsd1.wa.comcast.net>
2019-10-09 08:46:50 -0700

    auto

--- toys/misc/go/list.go
+++ toys/misc/go/list.go
@@ -13,10 +13,69 @@ type ListNode struct {
     next *ListNode
 }
 
+func (list *List) Size() int {
+    return list.count
+}
+
+func (list *List) IsEmpty() bool {
+    return (list.count == 0)
+}
 
 func (list *List) AddHead(value int) {
     list.head = &ListNode{value, list.head}
     list.count++
+}
+
+func (list *List) AddTail(value int) {
+    curr := list.head
+    newNode := &ListNode{value, nil}
+    list.count++
+
+    if curr == nil {
+        list.head = newNode
+        return
+    }
+
+    for curr.next != nil {
+        curr = curr.next
+    }
+
+    curr.next = newNode
+}
+
+func (list *List) DeleteNode(delValue int) bool {
+	temp := list.head
+	if list.IsEmpty() {
+		fmt.Println("EmptyListException")
+		return false
+	}
+
+	if delValue == list.head.value {
+		list.head = list.head.next
+		list.count--
+		return true
+	}
+
+	for temp.next != nil {
+		if temp.next.value == delValue {
+			temp.next = temp.next.next
+			list.count--
+			return true
+		}
+		temp = temp.next
+	}
+	return false
+}
+
+func (list *List) Find(data int) bool {
+	temp := list.head
+	for temp != nil {
+		if temp.value == data {
+			return true
+		}
+		temp = temp.next
+	}
+	return false
 }
 
 func (list *List) Print() {
@@ -30,10 +89,25 @@ func (list *List) Print() {
 
 func main() {
     mylist := List{}
-    mylist.AddHead(42)
-    mylist.AddHead(43)
-    mylist.AddHead(44)
+    values := []int{42, 43, 44, 17}
+    for _,v := range values {
+        mylist.AddHead(v)
+    }
+    fmt.Printf("%d items in list: ", mylist.Size())
     mylist.Print()
-    fmt.Printf("%d\n", mylist.count)
+    var index = 0
+    for mylist.IsEmpty() != true {
+        var v = values[index]
+        index++
+        fmt.Printf("dude: %d\n", v)
+        mylist.DeleteNode(v)
+    }
+    fmt.Printf("%d items in list: ", mylist.Size())
+    mylist.Print()
+    if mylist.Find(4) == true {
+        fmt.Printf("found\n")
+    } else {
+        fmt.Printf("not found\n")
+    }
 }
 
