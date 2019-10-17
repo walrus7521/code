1ea2265d845329d63856b2c3b3723db85dbf565a
Bart Bartel <BartB@Corp.helitrak.com>
2019-10-17 15:50:14 -0700

    st

--- toys/misc/go/websrv.go
+++ toys/misc/go/websrv.go
@@ -13,7 +13,8 @@ var count int
 func main() {
     http.HandleFunc("/", handler)
     http.HandleFunc("/count", counter)
-    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+    //log.Fatal(http.ListenAndServe("localhost:8000", nil))
+    http.ListenAndServe(":80", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
