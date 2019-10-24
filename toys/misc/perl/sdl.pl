07993d9b34b2245d03adeaa4180331f7870e84b4
Bart Bartel <BartB@Corp.helitrak.com>
2019-10-23 17:39:50 -0700

    st

--- toys/misc/perl/sdl.pl
+++ toys/misc/perl/sdl.pl
@@ -5,8 +5,21 @@ use warnings;
 
 use SDL;
 use SDLx::App;
+use SDL::Video;
+use SDL::Surface;
+use SDL::Rect;
+use SDL::Image;
+    
+# http://sdl.perl.org/SDL-Tutorial-LunarLander.html
+#
+#my $app = SDLx::App->new( w => 800, h => 600, t => 'Space Invaders - A clone' );
 
-my $app = SDLx::App->new( w => 800, h => 600, t => 'Space Invaders - A clone' );
+my $app = SDLx::App->new(
+        title  => "Lunar Lander",
+        width  => 800,
+        height => 600,
+        depth  => 32,
+    );
 
 $app->draw_rect([ $app->width / 4, $app->height / 4,
 $app->width / 2, $app->height / 2, ],
