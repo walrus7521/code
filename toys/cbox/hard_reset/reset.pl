4b774d5fdd45e0fb49584508c9165b55d8dccea6
Bart <Bart@Bartels-iMac.hsd1.wa.comcast.net>
2019-10-26 08:19:47 -0700

    update

--- toys/cbox/hard_reset/reset.pl
+++ toys/cbox/hard_reset/reset.pl
@@ -1,11 +1,31 @@
 #!/usr/bin/env perl
 
-sub reset() {
-    print("hard reset ..\n");
+# https://perldoc.perl.org/perlreftut.html
+#
+sub hard_reset {
+    my @input = @_;
+    print(scalar @input, "> @input\n");
+    my @rocks = ();
+    $rocks[0] = "soft";
+    $rocks[1] = "sand";
+    $rocks[2] = "hard";
+    ($rocks[1], $rocks[2]) = ($rocks[2], $rocks[1]);
+    print("hard reset ..: @rocks\n");
+}
+
+sub get_resetter {
+    return &hard_reset;
 }
 
 sub main() {
-    &reset();
+    print("dude: "); <> or die "you suck";
+    #$this_reset = hard_reset;
+    #$this_reset;
+    #&hard_reset("wusup");
+    $the_resetter = &get_resetter;
+    #&the_resetter("susup");
+
+    print(<STDERR>, "you suck\n");
 }
 
 &main();
