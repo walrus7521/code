e8ed803d05a3caeb46135b7bb8dbdcf44e7b1e25
Bart <Bart@Bartels-iMac.hsd1.wa.comcast.net>
2019-11-15 15:02:22 -0800

    update

--- toys/rubik/fifi.c
+++ toys/rubik/fifi.c
@@ -9,31 +9,24 @@ uint32_t idx=0,size=0;
 
 int data_avail();
 
-void print_dots()
-{
+void print_dots() {
     int i;
     int sz = data_avail();
-    //printf("sz: %d\n", sz);
     printf("                   \r");
     for (i = 0; i < sz; i++) { printf("."); }
     printf("\r");
     fflush(stdout);
 }
 
-int my_log2(uint32_t index)
-{
+int my_log2(uint32_t index) {
     int targetlevel = 0;
     while (index >>= 1) ++targetlevel;
     return targetlevel;
 }
 
-void init()
-{
-    size = 0;
-}
+void init() { size = 0; }
 
-int space_avail()
-{
+int space_avail() {
     if (size == 0) return FIFO_SZ;
     int v = ((int)my_log2(size)) & FIFO_SZ_MASK;
     //printf("size: %04x, v: %x\n", size, v);
@@ -41,30 +34,23 @@ int space_avail()
     return (v ? spc : FIFO_SZ-1);
 }
 
-int data_avail()
-{
+int data_avail() {
     int spc = space_avail();
     int daval = FIFO_SZ - spc;
     //printf("spc: %d, daval: %x\n", spc, daval);
     return daval;
-    return (spc ? daval : 0);
 }
 
 
-int peek()
-{
-    return buf[idx-1];
-}
+int peek() { return buf[idx-1]; }
 
-char get()
-{
+char get() {
     size &= ~(1 << --idx);
     printf("get() idx: %02d dat: %c sz: %08x\n", idx, buf[idx], size);
     return (buf[idx]);
 }
 
-void put(char data)
-{
+void put(char data) {
     buf[idx] = data;
     size |= (1 << idx++);
     printf("put() idx: %02d dat: %c sz: %08x\n", idx-1, data, size);
@@ -73,51 +59,15 @@ void put(char data)
 void test()
 {
     int i, curs=0;
-    char data[] = {'a','b','c','d','e','f','g','h',
-                   'i','j','k','l','m','n','o','p',
-                   'q','r','s','t','u','v','w','x',
-                   'y','z','0','1','2','3','4','5'};
+    char data[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+                   'q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5'};
 
-    while (space_avail()) {
-        put(data[curs++]);
-    }
-
-    while (data_avail()) {
-        char c = get();
-        //printf("got: %c\n", c);
-    }
-
-}
-
-void dev()
-{
-    int i, spc, dat;
-    size = 0x00;
-    spc = space_avail();
-    dat = data_avail();
-    printf("sz: %02x, spc: %02d, dat: %02d\n", size, spc, dat);
-    for (i = 0; i < FIFO_SZ; i++) {
-        size |= (1<< i);
-        spc = space_avail();
-        dat = data_avail();
-        printf("sz: %02x, spc: %02d, dat: %02d\n", size, spc, dat);
-        //print_dots();
-        sleep(1);
-    }
-    for (i = FIFO_SZ-1; i >= 0; i--) {
-        size &= ~(1<< i);
-        spc = space_avail();
-        dat = data_avail();
-        printf("sz: %02x, spc: %02d, dat: %02d\n", size, spc, dat);
-        //print_dots();
-        sleep(1);
-    }
-
+    while (space_avail()) { put(data[curs++]); }
+    while (data_avail()) { char c = get(); /*printf("got: %c\n", c);*/ }
 }
 
 int main()
 {
     test();
-    return 0;
 }
 
