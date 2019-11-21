a7cc239d906d755de65ed136c772981e1ce883a1
Bart Bartel <BartB@Corp.helitrak.com>
2019-11-21 15:34:27 -0800

    st

--- toys/tasks/doit.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import daemon
-
-from spam import do_main_program
-
-with daemon.DaemonContext():
-    do_main_program()
--- toys/tasks/spam.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-def do_main_program():
-    print("dude")
-
-
