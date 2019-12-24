067e4bfe530101d3aa79e1ecea1425bacd964e2e
Bart <Bart@Bartels-iMac.hsd1.wa.comcast.net>
2019-12-24 09:35:43 -0800

    update

---
+++ toys/rubik/fin.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#from googlefinance import getQuotes
+#import json
+#print(json.dump(getQuotes('AAPL'), indent=2))
+
+from fxcmpy import fxcmpy_tick_data_reader as tdr
+import datetime as dt
+
+print(tdr.get_available_symbols())
+
+start = dt.datetime(2018, 6, 25)
+stop  = dt.datetime(2018, 6, 30)
+
+td = tdr('EURUSD', start, stop)
+
+
