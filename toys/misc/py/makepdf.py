de475279c870b4c7128681615c74874a872457f9
Bart <Bart@Bartels-iMac.hsd1.wa.comcast.net>
2020-02-16 06:07:19 -0800

    update

--- toys/misc/py/makepdf.py
+++ toys/misc/py/makepdf.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 
-from fpdf import FPDF
-
-pdf = FPDF()
-
+from fpdf import FPDF 
+   
+# save FPDF() class into  
+# a variable pdf 
+pdf = FPDF()    
+   
 # Add a page 
 pdf.add_page() 
-  
+   
 # set style and size of font  
 # that you want in the pdf 
 pdf.set_font("Arial", size = 15) 
   
-# create a cell 
-pdf.cell(200, 10, txt = "GeeksforGeeks",  ln = 1, align = 'C') 
+# open the text file in read mode 
+f = open("segtree.cpp", "r") 
   
-# add another cell 
-pdf.cell(200, 10, txt = "A Computer Science portal for geeks.", ln = 2, align = 'C') 
-  
+# insert the texts in pdf 
+for x in f: 
+    pdf.cell(200, 10, txt = x, ln = 1, align = 'C') 
+   
 # save the pdf with name .pdf 
-pdf.output("GFG.pdf")    
-
+pdf.output("mygfg.pdf")    
