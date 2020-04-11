66159bd06b8c0109859585f7eabaf044547d85d3
Bart Bartel <bartbartel@Barts-MBP.hsd1.wa.comcast.net>
2020-04-11 16:14:20 -0700

    auto

--- toys/misc/py/finance/fft.py
+++ toys/misc/py/finance/fft.py
@@ -8,6 +8,7 @@ import scipy.fftpack
 
 # Number of samplepoints
 N = 600
+N2 = 600.0
 # sample spacing
 T = 1.0 / 800.0
 x = np.linspace(0.0, N*T, N)
@@ -17,4 +18,5 @@ xf = np.linspace(0.0, 1.0/(2.0*T), int(N/2))
 
 fig, ax = plt.subplots()
 ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
+#ax.plot(yf)
 plt.show()
