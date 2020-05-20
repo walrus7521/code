c9120ce6b7d8fb1e0b45602899b6fd0a290a0015
Bart <v-babart@microsoft.com>
2020-05-20 08:20:10 -0700

    Add commments to ardfirm.py pyFirmata usage.

--- toys/misc/py/ardfirm.py
+++ toys/misc/py/ardfirm.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+# https://realpython.com/arduino-python/
+# https://github.com/tino/pyFirmata
+# http://firmata.org/wiki/Main_Page
+# https://github.com/firmata/protocol
+# https://pyfirmata.readthedocs.io/en/latest/
+
+"""
+mega = {
+...         'digital' : tuple(x for x in range(54)),
+...         'analog' : tuple(x for x in range(16)),
+...         'pwm' : tuple(x for x in range(2,14)),
+...         'use_ports' : True,
+...         'disabled' : (0, 1, 14, 15) # Rx, Tx, Crystal
+...         }
+"""
+
 import pyfirmata
 import time
 
