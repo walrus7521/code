6e130abf14f5d3b1162d2e373ad675d14c39708a
Bart <v-babart@microsoft.com>
2020-05-29 15:49:14 -0700

    Update web app.

--- toys/webapp/dude/js/main.js
+++ toys/webapp/dude/js/main.js
@@ -1,9 +1,9 @@
 window.onload = () => {
-    'use strict';
+  'use strict';
 
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('./sw.js');
-    }
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
+  }
 }
 
