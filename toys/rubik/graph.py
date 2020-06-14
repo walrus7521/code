ffae02c4dea980205ef2ccc9492c287bf8b9c065
Bart Bartel <bartbartel@Barts-MBP.hsd1.wa.comcast.net>
2020-06-14 13:57:17 -0700

    auto

--- toys/rubik/graph.py
+++ toys/rubik/graph.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict 
 import networkx
  
 class Graph:
-    def __init__(self):
-        self.graph = defaultdict(list)
+    def __init__(self, V):
+        self.V = V  # no of vertices
+        self.adj = [[] for i in range(V)]
         self.gx    = networkx.Graph()
 
     def addEdge(self, u, v):
-        self.graph[u].append(v)
+        self.adj[u].append(v)
         self.gx.add_edge(u, v)
 
     def draw(self):
@@ -18,58 +18,66 @@ class Graph:
         #networkx.draw_networkx(self.gx)
 
     def BFS(self, s):
-        visited  = [False] * (len(self.graph))
+        visited  = [False] * (len(self.adj))
         queue = []
         queue.append(s)
         visited[s] = True
         while queue:
             s = queue.pop(0)
             print(s, end = " ")
-            for i in self.graph[s]:
+            for i in self.adj[s]:
                 if visited[i] == False:
                     queue.append(i)
                     visited[i] = True
         
-    def DFSUtil(self, v, visited): 
-        # Mark the current node as visited  
-        # and print it 
-        visited[v] = True
-        print(v, end = ' ') 
-        # Recur for all the vertices  
-        # adjacent to this vertex 
-        for i in self.graph[v]: 
-            if visited[i] == False: 
-                self.DFSUtil(i, visited) 
-  
-    # The function to do DFS traversal. It uses 
-    # recursive DFSUtil() 
-    def DFS(self, v): 
-        # Mark all the vertices as not visited 
-        visited = [False] * (max(self.graph)+1) 
-        # Call the recursive helper function  
-        # to print DFS traversal 
-        self.DFSUtil(v, visited) 
+    def DFS(self, v):
+        stack = []
+        visited = [False] * (len(self.adj))
+        stack.append(v)
+        while stack:
+            s = stack[-1] # last item on list
+            stack.pop()
+            if (not visited[s]):
+                print(s, end = " ")
+                visited[s] = True
+            for i in self.adj[s]:
+                if visited[i] == False:
+                    stack.append(i)
+                    
 
-
-def get_char():
+        
+def getchar():
     a = input('').split(" ")[0]
-   
-if __name__ == "__main__":
-    g = Graph()
 
+def init(g):
     g.addEdge(0, 1) 
     g.addEdge(0, 2) 
     g.addEdge(1, 2) 
     g.addEdge(2, 0) 
     g.addEdge(2, 3) 
-    g.addEdge(3, 3) 
-  
+#   g.addEdge(3, 3) 
+ 
+def init2(g):
+    g.addEdge(1, 0) 
+    g.addEdge(0, 2) 
+    g.addEdge(2, 1) 
+    g.addEdge(0, 3) 
+    g.addEdge(1, 4) 
+ 
+if __name__ == "__main__":
+    g = Graph(5)
+    init2(g)
+ 
     v = 2;
     print ("BFS from vertex {0})", v) 
     g.BFS(v) 
     print();
+#exit(0)
     print ("DFS from vertex {0})", v) 
-    g.DFS(v) 
+    g.DFS(0) 
     print();
+
+#   g.draw()
+#   getchar()
     
 
