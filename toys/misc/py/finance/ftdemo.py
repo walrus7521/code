ee27b25bba3078793f43d5a0ab675dea76e7b926
Bart Bartel <bartbartel@Barts-MBP.hsd1.wa.comcast.net>
2020-06-28 08:04:09 -0700

    auto

--- toys/misc/py/finance/ftdemo.py
+++ toys/misc/py/finance/ftdemo.py
@@ -14,40 +14,45 @@ phase = float(phase)
 T = 1 / freq
 
 t = np.linspace(0.0, N*1.0, N)
+print(t)
 
 y = np.sin(2*np.pi*t*freq + phase)
     
 f = np.arange(N)/(N*tau) # f = [0, 1.(N*tau), 2/(N*tau), ...]
+print(f)
 
 ### compute the transform
 yt = np.zeros(N, dtype=complex)
-Method = input('Compute transform by: 1) Direct summation; 2) FFT :')
 
 import time
 startTime = time.time()
-if Method == '1':
-    twoPiN = -2.0 * np.pi * (1j)/N # (1j = sqrt(-1)
-    for k in range(N):
-        for j in range(N):
-            expTerm = np.exp( twoPiN*j*k )
-            yt[k] += y[j] * expTerm
-
-else: # FFT method
-    yt = np.fft.fft(y)
-
+yt = np.fft.fft(y)
 stopTime = time.time()
 
 print('Elapsed time = ', stopTime - startTime, ' seconds')
 
+for i in range(len(yt)):
+#for ys in yt:
+    if yt[i].imag > 10.0:
+        print(i, yt[i].imag)
+
 #Graph the time series and its transform
 plt.subplot(1, 2, 1) # Left plot
+ax = plt.gca()
+ax.set_xticks(np.arange(0, N, 4))
+ax.set_yticks(np.arange(0, N, 4))
+plt.grid()
 plt.plot(t, y)
 plt.title('Original time series')
 plt.xlabel('Time')
 
 plt.subplot(1, 2, 2) # Right plot
+ax = plt.gca()
+ax.set_xticks(np.arange(0, 1.0, 0.05))
+#ax.set_yticks(np.arange(0, N, 4))
 plt.plot(f, np.real(yt), '-', f, np.imag(yt), '--')
 plt.legend(['Real', 'Imaginery '])
+plt.grid()
 plt.title('Fourier tranform')
 plt.xlabel('Frequency')
 
@@ -59,6 +64,7 @@ for i in range(N):
     powspec[i] = abs(yt[i])**2
 plt.semilogy(f, powspec, '-')
 plt.title('Power spectrum (unnormalized)')
+plt.grid()
 plt.xlabel('Frequency')
 plt.ylabel('Power')
 
