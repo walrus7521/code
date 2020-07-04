e86ffd2881ba45c0a29b55c91bb280838b2a0ead
Bart Bartel <bartbartel@Barts-MBP.hsd1.wa.comcast.net>
2020-07-04 08:24:23 -0700

    auto

--- toys/rubik/knap.py
+++ toys/rubik/knap.py
@@ -6,7 +6,7 @@ items = [
     ('guitar', 1,  1500),
     ('stereo', 4,  3000),
     ('laptop', 3,  2000),
-#   ('iphone', 1,  2000),
+    ('iphone', 1,  2000),
     ]
 # capacity columns independent of column index
 col_wts = [1, 2, 3, 4]
@@ -19,49 +19,43 @@ def show_grid():
             print(grid[row][col], end=' ')
         print('')
 
-def curr_max(row, col):
+def old_max(row, col):
     if row >= 1:
         print("cmax: ", row, col, grid[row-1][col])
         return grid[row-1][col]
     return 0
 
-def prev_max(row, col):
-    if row >= 0:
-        print("pmax: ", row, col, grid[row][col])
-        return grid[row][col]
+def lighter_max(row, col):
+    if row >= 1:
+        print("lmax: ", row, col, grid[row-1][col])
+        return grid[row-1][col]
     return 0
 
 for row in range(len(items)):
     name_item = items[row][0]
     wt_item   = items[row][1]
     val_item  = items[row][2]
-#   print(item, wt_item, val_item)
+
     for col in range(capacity):
         col_wt = col_wts[col]
-        curr_max_value = curr_max(row, col)
-        max_val = max(curr_max_value, val_item)
-        pmax_value = grid[row][col] #prev_max(row, col)
-        new_max_value = 0
-        print("===========================")
+
+        old_max_value = old_max(row, col)
+        max_val = max(old_max_value, val_item)
+
         print(row, col, val_item)
         show_grid()
         if wt_item < col_wt:
-            rem_wt = col_wt - wt_item
-            rem_value = grid[row][rem_wt] #prev_max(row, rem_wt)
-#           new_max_value = val_item + grid[row][rem_wt] #prev_max(row, rem_wt)
-            new_max_value = val_item + grid[row][rem_wt] #prev_max(row, rem_wt)
-            if new_max_value > curr_max_value:
+            residual_wt = col_wt - wt_item
+            residual_wt_col = residual_wt - 1
+            new_max_value = val_item + lighter_max(row, residual_wt_col)
+            if new_max_value > old_max_value:
                 grid[row][col] = new_max_value
-                print("here1: ", grid[row][col])
             else:
-                grid[row][col] = curr_max_value
-                print("here2: ", grid[row][col])
+                grid[row][col] = old_max_value
         elif wt_item == col_wt:
             grid[row][col] = max_val
-            print("here3: ", grid[row][col])
         else: # wt_item > col_wt
-            grid[row][col] = curr_max_value #val_item
-            print("here4: ", grid[row][col])
+            grid[row][col] = old_max_value
 
 print("++++++++++++++++++++++++")
 show_grid()
