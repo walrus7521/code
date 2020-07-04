7b5762f446e0089be34cb8a233931ab338d59656
Bart Bartel <bartbartel@Barts-MBP.hsd1.wa.comcast.net>
2020-07-04 11:55:25 -0700

    auto

---
+++ toys/rubik/camping.py
@@ -0,0 +1,14 @@
+__float__ = False
+capacity = 6 # lbs
+items = [
+     # name,  wt, value
+    ('water',  3,  10),
+    ('book',   1,   3),
+    ('food',   2,   9),
+    ('jacket', 2,   5),
+    ('camera', 1,   6),
+    ]
+# capacity columns independent of column index
+col_wts = [1, 2, 3, 4, 5, 6]
+
+
