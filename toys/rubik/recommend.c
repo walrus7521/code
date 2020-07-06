ac7d3ef9daf144bd2656f178a8c356d5080b49bc
Bart <v-babart@microsoft.com>
2020-07-06 13:22:33 -0700

    Recommender

---
+++ toys/rubik/recommend.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef enum { COMEDY, ACTION, DRAMA, HORROR, ROMANCE, N_CATS } CATS;
+
+typedef struct {
+    char name[8];
+    int cats[N_CATS];
+} user_t;
+#define N_USERS (3)
+user_t users[] = 
+{
+    {.name = "Priyanka", .cats = {3, 4, 4, 1, 4}},
+    {.name = "Justin",   .cats = {4, 3, 5, 1, 5}},
+    {.name = "Morpheus", .cats = {2, 5, 1, 3, 1}},
+};
+
+float dxes[N_USERS+1];
+
+float calc_dx(int i, int j)
+{
+    float dx = 0.0f;
+    printf("%s -> %s\n", users[i].name, users[j].name);
+    for (int n = 0; n < N_CATS; n++) {
+        int diff = users[i].cats[n] - users[j].cats[n];
+        float power = pow(diff, 2.0f);
+        printf("[%d]: (%d - %d) = %d %f\n", n, users[i].cats[n], users[j].cats[n], diff, power);
+        dx += power;
+    }
+    dx = sqrt(dx);
+    printf("dx(%d,%d): %f\n", i, j, dx);
+    return dx;
+}
+
+int main()
+{
+    memset(dxes, 0, sizeof(dxes));
+    // permute users
+    for (int i = 0; i < N_USERS; i++) {
+        for (int j = i+1; j < N_USERS; j++) {
+            dxes[i+j] = calc_dx(i, j);
+        }
+    }
+
+    for (int i = 1; i < N_USERS+1; i++) {
+        printf("dx[%d] = %f\n", i, dxes[i]);
+    }
+}
+
