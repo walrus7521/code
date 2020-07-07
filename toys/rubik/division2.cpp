1192a7594546f18edbf37544f76833d08d262fd7
Bart <v-babart@microsoft.com>
2020-07-07 10:48:19 -0700

    finished UVa 725 - Divide

--- toys/rubik/division2.cpp
+++ toys/rubik/division2.cpp
@@ -3,66 +3,77 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-void divide()
+bool all_diff(int m, int n)
+{
+    int numerator = m;
+    int denominator = n;
+    set<int> num;
+    set<int> den;
+    vector<int> intersection;
+
+    for (int i = 0; i < 5; i++) {
+        num.insert(m % 10);            
+        m /= 10;
+        den.insert(n % 10);            
+        n /= 10;
+    }
+    
+    // if both sets have 5 items
+    if (num.size() < 5 || den.size() < 5) {
+        return false;
+    }
+
+    set_intersection(num.begin(), num.end(), den.begin(), den.end(), 
+            std::inserter(intersection, intersection.begin()));
+    // if sets are unique return true
+    if (!intersection.size()) {
+        return true;
+    }
+    return false;    
+}
+
+int main()
 {
     int N;
-    int abcde, fghij;
+    int numerator, denominator, fghij, x;
+    bool found;
 
     set<int> num;
     set<int> den;
     vector<pair<int, int>> answer;
     vector<int> intersection;
 
-    int x, save_x;
     for (N = 2; N <= 79; N++) {
+        found = false;
+        for (int denominator = 98765; denominator >= 1234; denominator--) {
 
-        for (int start = 98765; start >= 1234; start--) {
-
-            fghij = start;
+            fghij = denominator;
             x = fghij * N;
-            save_x = x;
+            numerator = x;
 
             if (x > 98765 || x < 1234) {
                 continue;
             }
 
-            intersection.clear();
-            num.clear();
-            den.clear();
-        
-            //printf("x: %d = %d x %d\n", x, fghij, N);
-            for (int i = 0; i < 5; i++) {
-                den.insert(fghij % 10);            
-                fghij /= 10;
-                num.insert(x % 10);            
-                x /= 10;
+            // if sets are unique, add to answer
+            if (all_diff(numerator, denominator)) {
+                printf("%05d / %05d = %d\n", numerator, denominator, N);
+                answer.push_back(make_pair(numerator, denominator));
+                found = true;
             }
-            // if both sets have 5 items
-            if (num.size() < 5 || den.size() < 5) {
-                continue;
-            }
-            // if sets are unique
-            set_intersection(num.begin(), num.end(), den.begin(), den.end(), std::inserter(intersection, intersection.begin()));
-            // 
-            // create pair save_x and start and add to answer vector
-            if (!intersection.size()) {
-                answer.push_back(make_pair(save_x, start));
-            }
+        }
+        if (!found) {
+            printf("no solutions for %d\n", N);
         }
 
     }
 
-    for (auto it = answer.begin() ; it != answer.end(); ++it) {
-        printf("%05d, %05d\n", it->first, it->second);
-    }
-
-}
-
-int main()
-{
-    divide();
+//    for (auto it = answer.begin() ; it != answer.end(); ++it) {
+//        printf("%05d, %05d\n", it->first, it->second);
+//    }
 }
 
