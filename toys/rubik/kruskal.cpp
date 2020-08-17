ff1433d9ff10615a90ae5480e9f03c81b8d52577
Bart Bartel <bartbartel@Barts-MBP.hsd1.wa.comcast.net>
2020-08-17 07:27:16 -0700

    auto

--- toys/rubik/kruskal.cpp
+++ toys/rubik/kruskal.cpp
@@ -1,67 +1,43 @@
-#include "graph9.hpp"
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>
+#include <set>
+using namespace std;
 
-// union-find API
-class subset {
-public:
-    int parent, rank;
-};
+typedef pair<int, int> ii;
 
-// find set of an element i
-// uses path compression.
-int Find(subset subsets[], int i)
-{
-    // find root and make root as parent of i
-    // path compression
-    if (subsets[i].parent != i)
-        subsets[i].parent = Find(subsets, subsets[i].parent);
-    return subsets[i].parent;
-}
-
-void Union(subset subsets[], int x, int y)
-{
-    int xroot = Find(subsets, x);
-    int yroot = Find(subsets, y);
-    // attach smaller rank tree under root of high
-    // rank tree (union by rank)
-    if (subsets[xroot].rank < subsets[yroot].rank)
-        subsets[xroot].parent = yroot;
-    else if (subsets[xroot].rank > subsets[yroot].rank)
-        subsets[yroot].parent = xroot;
-    // if ranks are the same, make one as root and
-    // increment its rank by one
-    else {
-        subsets[yroot].parent = xroot;
-        subsets[xroot].rank++;
-    }
-}
-
-int myComp(const void *a, const void *b)
-{
-    Edge* a1 = (Edge*) a;
-    Edge* b1 = (Edge*) b;
-    return (a1->key > b1->key);
-}
-
-void kruskal(Graph& g)
-{
-    int V = g.v; // number of vertices
-    Edge result[V];
-    int e = 0;
-    int i = 0;
-
-    qsort(g.arr_E, g.e, sizeof(g.arr_E[0]), myComp);
-}
+// Sets
+vector<int> pset(1000);
+void initSet(int _size) {pset.resize(_size); for(int i=0;i<=_size;i++) pset[i] = i;}
+int findSet(int i) {return (pset[i] == i) ? i : (pset[i] = findSet(pset[i]));}
+void unionSet(int i, int j) {pset[findSet(i)] = findSet(j);}
+bool isSameSet(int i, int j) {return findSet(i) == findSet(j);}
 
 int main()
 {
-    Graph g(4,5);
+    priority_queue<pair<int, ii>> EdgeList;
+    int E, V, wt, u, v;
+    scanf("%d %d", &V, &E);
+    printf("[%d %d]\n", V, E);
+    for (int i = 0; i < E; i++) {
+        scanf("%d %d %d", &u, &v, &wt);
+        EdgeList.push(make_pair(-wt, make_pair(u, v)));// negate wts for min heap
+        printf("%d %d %d\n", u, v, wt);
+    }
 
-    addEdge(g, 0, 1, 10);
-    addEdge(g, 0, 2, 6);
-    addEdge(g, 0, 3, 5);
-    addEdge(g, 1, 3, 15);
-    addEdge(g, 2, 3, 4);
-    
-    kruskal(g);
+    int mst_cost = 0;
+    initSet(V);
+    while (!EdgeList.empty()) {
+        pair<int, ii> front = EdgeList.top(); EdgeList.pop();
+        if (!isSameSet(front.second.first, front.second.second)) {
+            // no cycle
+            mst_cost += (-front.first); // add negated -weight of e
+            unionSet(front.second.first, front.second.second); // link vertices
+            printf("add: %d -- %d == %d\n", front.second.first, 
+                    front.second.second, -front.first);
+        }
+    }
+    printf("mst cost: %d\n", mst_cost);
 }
 
