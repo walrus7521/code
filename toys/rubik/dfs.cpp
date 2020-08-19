df4f98f6c261be0a394b543e7d02cbd0754e04cb
Bart <v-babart@microsoft.com>
2020-08-19 10:46:41 -0700

    add DFS

--- toys/rubik/dfs.cpp
+++ toys/rubik/dfs.cpp
@@ -1,7 +1,16 @@
 #include "util.h"
 
+// applications:
+//     connected components undirected graph
+//     flood fill - labeling connected components
+//     property check
+//     articulation points and bridges
+//     strongly connected components in directed graph
+//     topological sort on DAG
+
 int dfs_num[MAX_NUM];
 int dfs_parent[MAX_NUM];
+int dfs_low[MAX_NUM];
 
 void graphCheck(int u)
 {
@@ -28,7 +37,7 @@ void dfs(int u)
 {
     dfs_num[u] = DFS_BLACK;
     printf(" %d", u);
-    TRvii(AdjList[u], v) // traversing vector of ii
+    TRvii (AdjList[u], v) // traversing vector of ii
         if (dfs_num[v->first] == DFS_WHITE) // avoid cycle
             dfs(v->first);
 }
@@ -49,7 +58,7 @@ void find_connected()
 void floodfill(int u, int color)
 {
     dfs_num[u] = color;
-    TRvii(AdjList[u], v) // traversing vector of ii
+    TRvii (AdjList[u], v) // traversing vector of ii
         if (dfs_num[v->first] == DFS_WHITE) // avoid cycle
             floodfill(v->first, color);
 }
@@ -66,14 +75,59 @@ void flood_fill_util()
         printf("Vertex %d has color %d\n", i, dfs_num[i]);
 }
 
+int dfsNumberCounter = 0;
+int dfsRoot = 0;
+int rootChildren = 0;
+bool articulation_vertex[MAX_NUM];
+void articulationPointAndBridge(int u)
+{
+    dfs_low[u] = dfs_num[u] = dfsNumberCounter++; // dfs_low[u] <= dfs_num[u]
+    TRvii (AdjList[u], v)
+        if (dfs_num[v->first] == DFS_WHITE) { // tree edge
+            dfs_parent[v->first] = u;
+            if (u == dfsRoot)
+                rootChildren++;
+            articulationPointAndBridge(v->first);
+            if (dfs_low[v->first] >= dfs_num[u]) // for articulation point
+                articulation_vertex[u] = true;
+            if (dfs_low[v->first] > dfs_num[u]) // for bridge
+                printf(" Edge (%d, %d) is a bridge\n", u, v->first);
+            dfs_low[u] = min(dfs_low[u], dfs_low[v->first]); // update dfs_low[u]
+        }
+        else if (v->first != dfs_parent[u]) // back edge and not direct cycle
+            dfs_low[u] = min(dfs_low[u], dfs_num[v->first]); // update dfs_low[u]
+}
+
+void articulationPointAndBridge_util()
+{
+    dfsNumberCounter = 0;
+    printf("Bridges:\n");
+    REP (i, 0, V-1)
+        if (dfs_num[i] == DFS_WHITE) {
+            dfsRoot = i; rootChildren = 0;
+            articulationPointAndBridge(i);
+            articulation_vertex[dfsRoot] = (rootChildren > 1); // special case
+        }
+    printf("Articulation Points:\n");
+    REP (i, 0, V-1)
+        if (articulation_vertex[i])
+            printf(" Vertex %d\n", i);
+}
+
 int main()
 {
     read_graph();
+
     memset(dfs_num, DFS_WHITE, sizeof(dfs_num));
-    memset(dfs_parent, DFS_WHITE, sizeof(dfs_num));
+    memset(dfs_parent, DFS_WHITE, sizeof(dfs_parent));
+    memset(dfs_low, DFS_WHITE, sizeof(dfs_low));
+    memset(articulation_vertex, false, sizeof(articulation_vertex));
+
     // dfs(0);
-    find_connected();
+    //find_connected();
     //flood_fill_util();
+    articulationPointAndBridge_util();
+
     printf("\n");
 }
 
